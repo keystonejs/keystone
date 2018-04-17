24d6a228e2518567b0e0941feadae3aa76258dd5
Joss Mackison <joss.mackison@gmail.com>
2018-04-18 09:37:32 +1000

    UI shuffle (#6)
    - introduce flow to ui package
    - move and rename buttons: ButtonGroup --> layout: FluidGroup
    - fix bolt dependency react-router-dom
    - prettier font implementation

--- packages/admin-ui/client/pages/StyleGuide.js
+++ packages/admin-ui/client/pages/StyleGuide.js
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from 'react';
 
 import Nav from '../components/Nav';
-import { Container } from '@keystonejs/ui/src/primitives/layout';
+import { Container, FluidGroup } from '@keystonejs/ui/src/primitives/layout';
 import { Title } from '@keystonejs/ui/src/primitives/typography';
-import { Button, ButtonGroup } from '@keystonejs/ui/src/primitives/buttons';
+import { Button } from '@keystonejs/ui/src/primitives/buttons';
 import { Input } from '@keystonejs/ui/src/primitives/forms';
 import {
   FieldContainer,
@@ -21,6 +21,7 @@ export default class StyleGuide extends Component {
           <div style={{ marginBottom: 200 }}>
             <ButtonGuide />
             <FieldGuide />
+            <LayoutGuide />
           </div>
         </Container>
       </Fragment>
@@ -32,15 +33,15 @@ const ButtonGuide = () => (
   <Fragment>
     <h2>Buttons</h2>
     <h4>Variant: Solid</h4>
-    <ButtonGroup>
+    <FluidGroup isInline>
       <Button>Default</Button>
       <Button appearance="primary">Primary</Button>
       <Button appearance="create">Create</Button>
       <Button appearance="warning">Warning</Button>
       <Button appearance="danger">Danger</Button>
-    </ButtonGroup>
+    </FluidGroup>
     <h4>Variant: Link</h4>
-    <ButtonGroup>
+    <FluidGroup isInline>
       <Button variant="link">Default</Button>
       <Button variant="link" appearance="text">
         Text
@@ -54,7 +55,7 @@ const ButtonGuide = () => (
       <Button variant="link" appearance="delete">
         Delete
       </Button>
-    </ButtonGroup>
+    </FluidGroup>
   </Fragment>
 );
 
@@ -67,14 +68,20 @@ const FieldGuide = () => (
     <FieldContainer>
       <FieldLabel>Label</FieldLabel>
       <FieldInput>
-        <Input placeholder="All the space!" />
+        <Input placeholder="Max width 500px" />
       </FieldInput>
     </FieldContainer>
-    <h4>Composition</h4>
-    <ButtonGroup growIndices={[1]}>
+  </Fragment>
+);
+
+const LayoutGuide = () => (
+  <Fragment>
+    <h2>Layout</h2>
+    <h4>Fluid Group</h4>
+    <FluidGroup growIndexes={[1]}>
       <Button>Alpha</Button>
       <Input placeholder="All the space!" />
       <Button appearance="primary">Omega</Button>
-    </ButtonGroup>
+    </FluidGroup>
   </Fragment>
 );
