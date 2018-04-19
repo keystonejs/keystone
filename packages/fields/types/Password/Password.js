7c1cce84abfc26129079d62499223054dd597710
Jed Watson <jed.watson@me.com>
2018-04-20 02:35:10 +1000

    Implementing password field filtering

--- packages/fields/types/Password/Password.js
+++ packages/fields/types/Password/Password.js
@@ -11,4 +11,17 @@ module.exports = class Password extends Field {
       [this.path]: { type: String, ...mongooseOptions },
     });
   }
+  getGraphqlQueryArgs() {
+    return `
+      ${this.path}_is_set: Boolean
+    `;
+  }
+  getQueryConditions(args) {
+    const conditions = [];
+    const is_set = `${this.path}_is_set`;
+    if (is_set in args) {
+      conditions.push(args[is_set] ? { $ne: null } : null);
+    }
+    return conditions;
+  }
 };
