207b651f1e736eb58b2387c3e6dead5e7b707f31
Jed Watson <jed.watson@me.com>
2018-04-29 01:19:47 +1000

    Adding /reset-db endpoint

--- packages/test-project/index.js
+++ packages/test-project/index.js
@@ -23,7 +23,7 @@ keystone.createList('User', {
       ],
     },
     password: {
-      type: SecurePassword
+      type: SecurePassword,
     },
   },
 });
@@ -39,14 +39,23 @@ const admin = new AdminUI(keystone);
 const server = new WebServer(keystone, {
   'cookie secret': 'qwerty',
   'admin ui': admin,
-  'adminPath': '/admin'
+  adminPath: '/admin',
+});
+
+server.app.get('/reset-db', (req, res) => {
+  const reset = async () => {
+    await keystone.mongoose.connection.dropDatabase();
+    await keystone.createItems(initialData);
+    res.redirect('/admin');
+  };
+  reset();
 });
 
 async function start() {
   keystone.connect();
   server.start();
   const users = await keystone.lists.User.model.find();
-  if (!users.length || process.env.RESET_DB) {
+  if (!users.length) {
     await keystone.mongoose.connection.dropDatabase();
     await keystone.createItems(initialData);
   }
