10b3ce7b7bd7e0d694bf0d8262f2619c77cdcf34
Jed Watson <jed.watson@me.com>
2018-04-30 01:03:06 +1000

    Adding icons package and octicons build

---
+++ packages/icons/build/template.js
@@ -0,0 +1,35 @@
+const template = ({
+  componentName,
+  height,
+  width,
+  viewBox,
+  ariaHidden,
+  svgContents,
+}) => `
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ${componentName} extends Component {
+  render() {
+    var defaults = {
+      'aria-hidden': ${ariaHidden},
+      height: ${height},
+      width: ${width},
+      viewBox: '${viewBox}',
+      style: {
+        display: 'inline-block',
+        verticalAlign: 'text-top',
+        fill: 'currentColor',
+      },
+    };
+    return (
+      <svg {...defaults} {...this.props}>
+        ${svgContents}
+      </svg>
+    );
+  }
+}
+export default ${componentName};
+`;
+
+module.exports = template;
