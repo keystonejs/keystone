d487f091d207d1cf9b4a9706f0389b674e7de80c
Jess Telford <jess+github@jes.st>
2018-05-17 08:50:17 +1000

    Better null state handling for template rendering

--- packages/ui/src/template.js
+++ packages/ui/src/template.js
@@ -1,4 +1,9 @@
 export default ({ template, data }) => {
+  if (!template || !data || typeof data !== 'object') {
+    // TODO: Be better
+    console.warn(`Unable to render template (${template}) with data:`, data);
+    return '';
+  }
   const keys = Object.keys(data);
   // Only replace exact syntax with enumerable keys
   // Also allows spaces around key name
