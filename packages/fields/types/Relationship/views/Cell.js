1a463bd4e0c06d3da2b3d11341545ec89e3f4fe5
Jess Telford <jess+github@jes.st>
2018-05-17 08:50:30 +1000

    Correctly render many relationships in cells

--- packages/fields/types/Relationship/views/Cell.js
+++ packages/fields/types/Relationship/views/Cell.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import renderTemplate from '@keystonejs/ui/src/template';
 
 export default ({ data, field, Link }) => {
+  if (!data) {
+    return null;
+  }
   const refList = field.adminMeta.getListByKey(field.config.ref);
   return (
-    <Link path={refList.path} id={data.id}>
-      {renderTemplate({ template: refList.displayTemplate, data })}
-    </Link>
+    <Fragment>
+      {(Array.isArray(data) ? data : [data]).filter(Boolean).map((item, index) =>
+        <Fragment key={item.id}>
+          {!!index ? ', ' : ''}
+          <Link path={refList.path} id={item.id}>
+            {renderTemplate({ template: refList.displayTemplate, data: item })}
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
