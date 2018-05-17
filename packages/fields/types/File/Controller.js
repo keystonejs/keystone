d210357dc267f4461b30d8c369c295bd3c9295ca
Jess Telford <jess+github@jes.st>
2018-05-17 13:34:07 +1000

    File field provides a query fragment for admin UI

--- packages/fields/types/File/Controller.js
+++ packages/fields/types/File/Controller.js
@@ -1,3 +1,13 @@
 import FieldController from '../../Controller';
 
-export default class FileController extends FieldController {}
+export default class FileController extends FieldController {
+  getQueryFragment = () => `
+    ${this.path} {
+       id
+       path
+       filename
+       mimetype
+       encoding
+    }
+  `
+}
