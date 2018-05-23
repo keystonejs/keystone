27ac02b581595307b5558d8d4740334a722446c3
Joss Mackison <joss.mackison@gmail.com>
2018-05-23 23:39:20 +1000

    add cell views for file & cloudinary fields

---
+++ packages/fields/types/CloudinaryImage/views/Cell.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default ({ data }) => {
+  if (!data) return null;
+
+  return (
+    <div
+      css={{
+        alignItems: 'center',
+        display: 'flex',
+        height: 24,
+        lineHeight: 0,
+        width: 24,
+      }}
+    >
+      <img
+        alt={data.filename}
+        css={{ maxHeight: '100%', maxWidth: '100%' }}
+        src={data.publicUrlTransformed}
+      />
+    </div>
+  );
+};
