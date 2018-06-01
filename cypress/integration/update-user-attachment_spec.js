2b7cfc88074fc06a193bd09b5691ab1fccd919aa
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-06-01 12:02:01 +1000

    added cloudinary env to circleci, started to fix #95, fetchPolicy pending by Jess
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- cypress/integration/update-user-attachment_spec.js
+++ cypress/integration/update-user-attachment_spec.js
@@ -3,33 +3,33 @@ describe('Adding a file', function() {
     return cy
       .graphql_query(
         `
-      query {
-        allUsers(first: 1) {
-          id
-          name
-        }
-      }
-    `
+          query {
+            allUsers(first: 1) {
+              id
+              name
+            }
+          }
+        `
       )
       .then(({ allUsers: [user] }) => {
         const fileContent = `Some important content ${Math.random()}`;
         cy.visit(`http://localhost:3000/admin/users/${user.id}`);
-        cy.writeFile('cypress/fixtures/upload.txt', fileContent);
-        cy.upload_file('input[name=attachment][type=file]', 'upload.txt');
+        cy.writeFile('cypress/mock/upload.txt', fileContent);
+        cy.upload_file('input[name=attachment][type=file]', '../mock/upload.txt');
         cy.get('button[type="submit"]').click();
         cy.contains('-upload.txt');
         return cy
           .graphql_query(
             `
-        query {
-          User(where: { id: "${user.id}" }) {
-            attachment {
-              id
-              publicUrl
-            }
-          }
-        }
-      `
+              query {
+                User(where: { id: "${user.id}" }) {
+                  attachment {
+                    id
+                    publicUrl
+                  }
+                }
+              }
+            `
           )
           .then(({ User: { attachment } }) => {
             // Assert the URL is visible in the admin UI
