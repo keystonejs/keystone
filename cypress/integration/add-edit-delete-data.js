b638f7b38e6a99555295c0b293e926f0260bfa0b
Joss Mackison <joss.mackison@gmail.com>
2018-06-15 13:52:20 +1000

    resolve cypress tests;
    NOTE: using `{ force: true }` on some cypress commands, woud like to revisit in the future

--- cypress/integration/add-edit-delete-data.js
+++ cypress/integration/add-edit-delete-data.js
@@ -33,7 +33,7 @@ describe('Adding data', () => {
         cy.get(`#${item}`).type(data[item]);
       });
 
-      cy.get('div[class*="Dialog"] button[appearance="create"]').click();
+      cy.get('div[role="dialog"] button[appearance="create"]').click();
 
       Object.keys(data).forEach(item => {
         cy.get(`#${item}`).should('have.value', data[item]);
