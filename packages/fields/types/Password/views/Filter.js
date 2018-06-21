8e10741ade0cadaece5d746d2fc110e00b92ec66
Jed Watson <jed.watson@me.com>
2018-06-21 23:29:40 +1000

    Disabling refs in Password Filter view
    @jossmac we need support for refs (and focusing? not sure if that works as expected or not) in react-radios
    
    I'm commenting this out with a TODO until that's updated because this is throwing console errors - stateless (function) components don't support refs

--- packages/fields/types/Password/views/Filter.js
+++ packages/fields/types/Password/views/Filter.js
@@ -11,11 +11,18 @@ export default class PasswordFilterView extends Component<Props> {
     this.props.onChange(boolValue);
   };
   render() {
-    const { innerRef, value } = this.props;
+    // TODO: RadioGroup needs support for ref forwarding, or innerRef, and
+    // focusing to work as expected here
+
+    const { /* innerRef, */ value } = this.props;
     const textValue = value ? 'is_set' : 'is_not_set';
 
     return (
-      <RadioGroup onChange={this.handleChange} ref={innerRef} value={textValue}>
+      <RadioGroup
+        onChange={this.handleChange}
+        // ref={innerRef}
+        value={textValue}
+      >
         <Radio value="is_set">Is Set</Radio>
         <Radio value="is_not_set">Is NOT Set</Radio>
       </RadioGroup>
