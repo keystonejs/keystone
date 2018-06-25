4258773652b2ee86ec088e4e3d2253734b38d3a2
Jess Telford <jess+github@jes.st>
2018-06-25 16:17:07 +1000

    Add a login test

---
+++ cypress/projects/login/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  User: [
+    {
+      name: 'Boris Bozic',
+      email: 'boris@keystonejs.com',
+      company: 'thinkmill',
+      password: 'correctbattery',
+    },
+    {
+      name: 'Jed Watson',
+      email: 'jed@keystonejs.com',
+      company: 'thinkmill',
+      password: 'horsestaple',
+    },
+  ],
+};
