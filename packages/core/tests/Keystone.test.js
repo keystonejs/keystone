571e91fb0fd99d2e142e448de418776352c2dfc1
Jess Telford <jess+github@jes.st>
2018-06-28 12:05:35 +1000

    Fix error when creating item

--- packages/core/tests/Keystone.test.js
+++ packages/core/tests/Keystone.test.js
@@ -1,5 +1,6 @@
 const Keystone = require('../Keystone');
 const List = require('../List');
+const { Text, Relationship } = require('@keystonejs/fields');
 
 class MockType {}
 
@@ -63,4 +64,78 @@ test('Keystone.createList()', () => {
   expect(keystone.listsArray[0]).toBeInstanceOf(List);
 
   expect(keystone.listsArray[0]).toBe(keystone.lists['User']);
+});
+
+describe('Keystone.createItems()', () => {
+
+  const lists = {
+    User: {
+      key: 'User',
+      config: {
+        fields: {
+          name: { type: Text },
+          posts: { type: Relationship, many: true, ref: 'Post' },
+        },
+      },
+      adapter: {
+        create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+        itemsQuery: jest.fn(),
+      },
+    },
+    Post: {
+      key: 'Post',
+      config: {
+        fields: {
+          title: { type: Text },
+          author: { type: Relationship, ref: 'User' },
+        },
+      },
+      adapter: {
+        create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+        itemsQuery: jest.fn(),
+      },
+    },
+  };
+
+  beforeEach(() => {
+    // Reset call counts, etc, back to normal
+    lists.User.adapter.itemsQuery.mockReset();
+    lists.User.adapter.create.mockReset();
+    lists.User.adapter.update.mockReset();
+    lists.User.adapter.delete.mockReset();
+    lists.Post.adapter.itemsQuery.mockReset();
+    lists.Post.adapter.create.mockReset();
+    lists.Post.adapter.update.mockReset();
+    lists.Post.adapter.delete.mockReset();
+  });
+
+  test('creates items', async () => {
+    const keystone = new Keystone({
+      adapter: new MockAdapter(),
+      name: 'Jest Test',
+    });
+
+    // mock the lists
+    keystone.lists = lists;
+
+    await keystone.createItems({
+      User: [
+        { name: 'Jess' },
+        { name: 'Lauren' },
+      ],
+      Post: [
+        { title: 'Hello world' },
+        { title: 'Goodbye' },
+      ],
+    });
+
+    expect(keystone.lists.User.adapter.create).toHaveBeenCalledWith({ name: 'Jess' });
+    expect(keystone.lists.User.adapter.create).toHaveBeenCalledWith({ name: 'Lauren' });
+    expect(keystone.lists.Post.adapter.create).toHaveBeenCalledWith({ title: 'Hello world' });
+    expect(keystone.lists.Post.adapter.create).toHaveBeenCalledWith({ title: 'Goodbye' });
+  });
 });
