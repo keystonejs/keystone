60c57529c8cd0a30852be4ffcea51c6fbb833dc8
Jess Telford <jess+github@jes.st>
2018-07-03 10:19:07 +1000

    Correct data inserts

--- projects/login/data.js
+++ projects/login/data.js
@@ -3,13 +3,11 @@ module.exports = {
     {
       name: 'Boris Bozic',
       email: 'boris@keystonejs.com',
-      company: 'thinkmill',
       password: 'correctbattery',
     },
     {
       name: 'Jed Watson',
       email: 'jed@keystonejs.com',
-      company: 'thinkmill',
       password: 'horsestaple',
     },
   ],
