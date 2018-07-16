4c9400ef2c82db63a6d2ba2d77213d457d0751a2
Tim Leslie <timl@thinkmill.com.au>
2018-07-16 12:38:03 +1000

    Prettier

--- packages/server/WebServer/graphql.js
+++ packages/server/WebServer/graphql.js
@@ -3,21 +3,20 @@ const bodyParser = require('body-parser');
 const { apolloUploadExpress } = require('apollo-upload-server');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
+export function createGraphQLMiddleware(keystone, { apiPath, graphiqlPath }) {
+  const app = express();
 
-export function createGraphQLMiddleware(keystone, { apiPath, graphiqlPath, }) {
-    const app = express();
-
-    // add the Admin GraphQL API
-    const schema = keystone.getAdminSchema();
-    app.use(
-      apiPath,
-      bodyParser.json(),
-      // TODO: Make configurable
-      apolloUploadExpress({ maxFileSize: 200 * 1024 * 1024, maxFiles: 5 }),
-      graphqlExpress({ schema })
-    );
-    if (graphiqlExpress) {
-        app.use(graphiqlPath, graphiqlExpress({ endpointURL: apiPath }));
-    }
-    return app;
+  // add the Admin GraphQL API
+  const schema = keystone.getAdminSchema();
+  app.use(
+    apiPath,
+    bodyParser.json(),
+    // TODO: Make configurable
+    apolloUploadExpress({ maxFileSize: 200 * 1024 * 1024, maxFiles: 5 }),
+    graphqlExpress({ schema })
+  );
+  if (graphiqlExpress) {
+    app.use(graphiqlPath, graphiqlExpress({ endpointURL: apiPath }));
   }
+  return app;
+}
