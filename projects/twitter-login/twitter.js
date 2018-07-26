f8c9cbaf21f5e2ac6ac1337702cee24354ab8316
Tom Walker <bladey@me.com>
2018-07-26 14:23:29 +1000

    WIP commit for Twitter Login example improvements.

--- projects/twitter-login/twitter.js
+++ projects/twitter-login/twitter.js
@@ -23,7 +23,6 @@ exports.configureTwitterAuth = function(keystone, server) {
       // If not set, will just call `next()`
       sessionExists: (itemId, req, res) => {
         console.log(`Already logged in as ${itemId} 🎉`);
-        // logged in already? Send 'em home!
         return res.redirect('/api/session');
       },
     })
@@ -34,23 +33,44 @@ exports.configureTwitterAuth = function(keystone, server) {
     '/auth/twitter/callback',
     twitterAuth.authenticateMiddleware({
       async verified(item, info, req, res) {
+
+        // You could try and find user by email address here to match users
+        // if you get the email data back from Twitter, then refer to
+        // connectItem, for example:
+        // await keystone.auth.User.twitter.connectItem(req, { item });
+        // ...
+
+        // If we don't have a matching user in our system, force redirect to
+        // the create step
         if (!item) {
-          return res.redirect('/auth/twitter/details');
+          return res.redirect('/auth/twitter/create');
         }
 
+        // Otheriwse create a session based on the user we have already
+        const list = keystone.getListByKey('User');
+        await keystone.session.create(req, { item, list });
+
+        // Redirect on sign in
         res.redirect('/api/session');
       },
       failedVerification(error, req, res) {
-        console.log('🤔 Failed to verify Twitter login creds');
+        console.log('Failed to verify Twitter login creds');
         res.redirect('/');
       },
     })
   );
 
-  // Sample page to collect a name, submits to the completion step
-  server.app.get('/auth/twitter/details', (req, res) => {
+  // Sample page to collect a name, submits to the completion step which will
+  // create a user
+  server.app.get('/auth/twitter/create', (req, res) => {
+    // Redirect if we're already signed in
     if (req.user) {
       return res.redirect('/api/session');
+    }
+    // If we don't have a keystoneTwitterSessionId at this point, the form
+    // submission will fail, so fail out to the first step
+    if (!req.session.keystoneTwitterSessionId) {
+      return res.redirect('/auth/twitter');
     }
 
     res.send(`
@@ -66,19 +86,22 @@ exports.configureTwitterAuth = function(keystone, server) {
     '/auth/twitter/complete',
     server.express.urlencoded({ extended: true }),
     async (req, res, next) => {
+
+      // Redirect if we're already signed in
       if (req.user) {
         return res.redirect('/api/session');
       }
 
+      // Create a new User
       try {
         const list = keystone.getListByKey('User');
-
         const item = await keystone.createItem(list.key, {
           name: req.body.name,
         });
 
         await keystone.auth.User.twitter.connectItem(req, { item });
         await keystone.session.create(req, { item, list });
+
         res.redirect('/api/session');
       } catch (createError) {
         next(createError);
