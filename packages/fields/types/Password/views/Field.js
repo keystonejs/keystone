b75bd6bdeee621690952a5df61fa1046cc58c2bb
Joss Mackison <joss.mackison@gmail.com>
2018-08-17 16:29:36 +1000

    input/confirm interface for password field

--- packages/fields/types/Password/views/Field.js
+++ packages/fields/types/Password/views/Field.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 import { FieldContainer, FieldLabel, FieldInput } from '@keystonejs/ui/src/primitives/fields';
 import { Input } from '@keystonejs/ui/src/primitives/forms';
+import { FlexGroup } from '@keystonejs/ui/src/primitives/layout';
+import { Button } from '@keystonejs/ui/src/primitives/buttons';
+import { EyeIcon, LockIcon } from '@keystonejs/icons';
+import { A11yText } from '@keystonejs/ui/src/primitives/typography';
 
 export default class PasswordField extends Component {
-  onChange = event => {
+  focusTarget = createRef();
+  state = {
+    isEditing: false,
+    showInputValue: false,
+    inputPassword: '',
+    inputConfirm: '',
+  };
+  onChange = ({ target }) => {
     const { field, onChange } = this.props;
-    onChange(field, event.target.value);
+    const { name, value } = target;
+
+    this.setState({ [name]: value }, () => {
+      if (name === 'inputPassword') onChange(field, value);
+    });
+  };
+  toggleInterface = () => {
+    function maybeFocus() {
+      if (this.state.isEditing && this.focusTarget.current) {
+        this.focusTarget.current.focus();
+      }
+    }
+
+    this.setState(state => ({ isEditing: !state.isEditing }), maybeFocus);
+  };
+  toggleMode = () => {
+    this.setState(state => ({ showInputValue: !state.showInputValue }));
   };
   render() {
+    const { isEditing, inputPassword, inputConfirm, showInputValue } = this.state;
     const { autoFocus, field, item } = this.props;
     const value = item[field.path] || '';
     const htmlID = `ks-input-${field.path}`;
@@ -17,14 +45,43 @@ export default class PasswordField extends Component {
       <FieldContainer>
         <FieldLabel htmlFor={htmlID}>{field.label}</FieldLabel>
         <FieldInput>
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="password"
-            value={value}
-            onChange={this.onChange}
-            id={htmlID}
-          />
+          {isEditing ? (
+            <FlexGroup growIndexes={[0, 1]}>
+              <Input
+                autoComplete="off"
+                autoFocus={autoFocus}
+                id={htmlID}
+                innerRef={this.focusTarget}
+                name="inputPassword"
+                onChange={this.onChange}
+                placeholder="New Password"
+                type={showInputValue ? 'text' : 'password'}
+                value={inputPassword}
+              />
+              <Input
+                autoComplete="off"
+                autoFocus={autoFocus}
+                name="inputConfirm"
+                onChange={this.onChange}
+                placeholder="Confirm Password"
+                type={showInputValue ? 'text' : 'password'}
+                value={inputConfirm}
+              />
+              <Button
+                isActive={showInputValue}
+                onClick={this.toggleMode}
+                title={showInputValue ? 'Hide Text' : 'Show Text'}
+                variant="ghost"
+              >
+                <A11yText>{showInputValue ? 'Hide Text' : 'Show Text'}</A11yText>
+                <div css={{ width: 20 }}>{showInputValue ? <LockIcon /> : <EyeIcon />}</div>
+              </Button>
+            </FlexGroup>
+          ) : (
+            <Button onClick={this.toggleInterface} variant="ghost">
+              {value ? 'Update Password' : 'Set Password'}
+            </Button>
+          )}
         </FieldInput>
       </FieldContainer>
     );
