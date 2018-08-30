0f8d3c7ca0570abf1b887ee62794199548b6be07
Tim Leslie <timl@thinkmill.com.au>
2018-08-30 14:57:09 +1000

    Rename birthday to lastOnline in filter tests

--- packages/fields/types/DateTime/filterTests.js
+++ packages/fields/types/DateTime/filterTests.js
@@ -7,34 +7,34 @@ export const name = 'DateTime';
 export const getTestFields = () => {
   return {
     name: { type: Text },
-    birthday: { type: DateTime },
+    lastOnline: { type: DateTime },
   };
 };
 
 export const initItems = () => {
   return [
-    { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-    { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-    { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-    { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-    { name: 'person5', birthday: null },
+    { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+    { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+    { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+    { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+    { name: 'person5', lastOnline: null },
   ];
 };
 
 export const filterTests = app => {
   const match = (filter, targets, done) => {
-    matchFilter(app, filter, '{ name, birthday }', targets, done, 'name');
+    matchFilter(app, filter, '{ name, lastOnline }', targets, done, 'name');
   };
 
   test('No filter', done => {
     match(
       undefined,
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-        { name: 'person5', birthday: null },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person5', lastOnline: null },
       ],
       done
     );
@@ -44,145 +44,145 @@ export const filterTests = app => {
     match(
       'where: { }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-        { name: 'person5', birthday: null },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person5', lastOnline: null },
       ],
       done
     );
   });
 
-  test('Filter: birthday', done => {
+  test('Filter: lastOnline', done => {
     match(
-      'where: { birthday: "2000-01-20T00:08:00.000+10:00" }',
-      [{ name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' }],
+      'where: { lastOnline: "2000-01-20T00:08:00.000+10:00" }',
+      [{ name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' }],
       done
     );
   });
 
-  test('Filter: birthday_not', done => {
+  test('Filter: lastOnline_not', done => {
     match(
-      'where: { birthday_not: "2000-01-20T00:08:00.000+10:00" }',
+      'where: { lastOnline_not: "2000-01-20T00:08:00.000+10:00" }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-        { name: 'person5', birthday: null },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person5', lastOnline: null },
       ],
       done
     );
   });
 
-  test('Filter: birthday_not null', done => {
+  test('Filter: lastOnline_not null', done => {
     match(
-      'where: { birthday_not: null }',
+      'where: { lastOnline_not: null }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
       ],
       done
     );
   });
 
-  test('Filter: birthday_lt', done => {
+  test('Filter: lastOnline_lt', done => {
     match(
-      'where: { birthday_lt: "1950-10-01T23:59:59.999-10:00" }',
-      [{ name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' }],
+      'where: { lastOnline_lt: "1950-10-01T23:59:59.999-10:00" }',
+      [{ name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' }],
       done
     );
   });
 
-  test('Filter: birthday_lte', done => {
+  test('Filter: lastOnline_lte', done => {
     match(
-      'where: { birthday_lte: "1950-10-01T23:59:59.999-10:00" }',
+      'where: { lastOnline_lte: "1950-10-01T23:59:59.999-10:00" }',
       [
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
       ],
       done
     );
   });
 
-  test('Filter: birthday_gt', done => {
+  test('Filter: lastOnline_gt', done => {
     match(
-      'where: { birthday_gt: "1950-10-01T23:59:59.999-10:00" }',
+      'where: { lastOnline_gt: "1950-10-01T23:59:59.999-10:00" }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
       ],
       done
     );
   });
 
-  test('Filter: birthday_gte', done => {
+  test('Filter: lastOnline_gte', done => {
     match(
-      'where: { birthday_gte: "1950-10-01T23:59:59.999-10:00" }',
+      'where: { lastOnline_gte: "1950-10-01T23:59:59.999-10:00" }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
       ],
       done
     );
   });
 
-  test('Filter: birthday_in (empty list)', done => {
-    match('where: { birthday_in: [] }', [], done);
+  test('Filter: lastOnline_in (empty list)', done => {
+    match('where: { lastOnline_in: [] }', [], done);
   });
 
-  test('Filter: birthday_not_in (empty list)', done => {
+  test('Filter: lastOnline_not_in (empty list)', done => {
     match(
-      'where: { birthday_not_in: [] }',
+      'where: { lastOnline_not_in: [] }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-        { name: 'person5', birthday: null },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person5', lastOnline: null },
       ],
       done
     );
   });
 
-  test('Filter: birthday_in', done => {
+  test('Filter: lastOnline_in', done => {
     match(
-      'where: { birthday_in: ["1990-12-31T12:34:56.789+01:23", "2000-01-20T00:08:00.000+10:00", "1950-10-01T23:59:59.999-10:00"] }',
+      'where: { lastOnline_in: ["1990-12-31T12:34:56.789+01:23", "2000-01-20T00:08:00.000+10:00", "1950-10-01T23:59:59.999-10:00"] }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
       ],
       done
     );
   });
 
-  test('Filter: birthday_not_in', done => {
+  test('Filter: lastOnline_not_in', done => {
     match(
-      'where: { birthday_not_in: ["1990-12-31T12:34:56.789+01:23", "2000-01-20T00:08:00.000+10:00", "1950-10-01T23:59:59.999-10:00"] }',
+      'where: { lastOnline_not_in: ["1990-12-31T12:34:56.789+01:23", "2000-01-20T00:08:00.000+10:00", "1950-10-01T23:59:59.999-10:00"] }',
       [
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
-        { name: 'person5', birthday: null },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person5', lastOnline: null },
       ],
       done
     );
   });
 
-  test('Filter: birthday_in null', done => {
-    match('where: { birthday_in: [null] }', [{ name: 'person5', birthday: null }], done);
+  test('Filter: lastOnline_in null', done => {
+    match('where: { lastOnline_in: [null] }', [{ name: 'person5', lastOnline: null }], done);
   });
 
-  test('Filter: birthday_not_in null', done => {
+  test('Filter: lastOnline_not_in null', done => {
     match(
-      'where: { birthday_not_in: [null] }',
+      'where: { lastOnline_not_in: [null] }',
       [
-        { name: 'person1', birthday: '1990-12-31T12:34:56.789+01:23' },
-        { name: 'person2', birthday: '2000-01-20T00:08:00.000+10:00' },
-        { name: 'person3', birthday: '1950-10-01T23:59:59.999-10:00' },
-        { name: 'person4', birthday: '1666-04-12T00:08:00.000+10:00' },
+        { name: 'person1', lastOnline: '1990-12-31T12:34:56.789+01:23' },
+        { name: 'person2', lastOnline: '2000-01-20T00:08:00.000+10:00' },
+        { name: 'person3', lastOnline: '1950-10-01T23:59:59.999-10:00' },
+        { name: 'person4', lastOnline: '1666-04-12T00:08:00.000+10:00' },
       ],
       done
     );
