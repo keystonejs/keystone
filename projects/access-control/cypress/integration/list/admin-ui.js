bf35359b9a412e7c68432584e5bcdcedaa6606aa
jossmac <>
2018-11-19 15:54:58 +1100

    fix tests

--- projects/access-control/cypress/integration/list/admin-ui.js
+++ projects/access-control/cypress/integration/list/admin-ui.js
@@ -363,12 +363,11 @@ describe('Access Control Lists > Admin UI', () => {
 
             cy.visit(`admin/${slug}`);
 
-            // JM 180823: We have no idea why we need { force: true } here
-            // Without it the test fails, but only on..
-            //   {"create":true,"read":true,"update":true,"delete":true}
-            cy.get('#ks-list-table input[type="checkbox"]')
+            // The first label inside thead wraps a visibly-hidden checkbox which
+            // cypress can't find
+            cy.get('#ks-list-table > thead label')
               .first()
-              .click({ force: true });
+              .click();
             cy.get('button[data-test-name="update"]').should('exist');
           });
 
@@ -397,7 +396,9 @@ describe('Access Control Lists > Admin UI', () => {
 
             cy.visit(`admin/${slug}`);
 
-            cy.get('#ks-list-table input[type="checkbox"]')
+            // The first label inside thead wraps a visibly-hidden checkbox which
+            // cypress can't find
+            cy.get('#ks-list-table > thead label')
               .first()
               .click();
             cy.get('button[data-test-name="update"]').should('not.exist');
