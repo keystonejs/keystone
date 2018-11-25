7cb6ced3de9d3f8aff3e1268d68605e64067bfab
mitchellhamilton <mitchell@hamil.town>
2018-11-25 17:33:58 +1000

    Improve accessibility

--- packages/admin-ui/client/components/Nav/index.js
+++ packages/admin-ui/client/components/Nav/index.js
@@ -6,6 +6,7 @@ import { withRouter } from 'react-router';
 import PropToggle from 'react-prop-toggle';
 import styled from '@emotion/styled';
 import { jsx } from '@emotion/core';
+import { SkipNavLink, SkipNavContent } from '@reach/skip-nav';
 
 import {
   TerminalIcon,
@@ -220,6 +221,7 @@ class Nav extends Component {
                 isActive={isDragging}
                 styles={{ cursor: 'col-resize', 'user-select': 'none' }}
               />
+              <SkipNavLink />
               <PrimaryNav
                 onMouseEnter={this.handleMouseEnter}
                 onMouseLeave={this.handleMouseLeave}
@@ -318,6 +320,7 @@ class Nav extends Component {
                   )}
                 </Tooltip>
               </PrimaryNav>
+              <SkipNavContent />
               <Page style={makeResizeStyles('marginLeft')}>{children}</Page>
             </PageWrapper>
           );
