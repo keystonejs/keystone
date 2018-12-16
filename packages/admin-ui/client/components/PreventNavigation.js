9269fe459af0362a8e9eb129983d0faf4fd42bbf
mitchellhamilton <mitchell@hamil.town>
2018-12-16 15:37:23 +1000

    Add some comments

--- packages/admin-ui/client/components/PreventNavigation.js
+++ packages/admin-ui/client/components/PreventNavigation.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Prompt } from 'react-router';
 
 export default function PreventNavigation() {
+  // to handle when the user closes the tab, does an actual browser navigation away or etc.
   useEffect(() => {
     const handler = event => {
       event.preventDefault();
@@ -9,5 +10,6 @@ export default function PreventNavigation() {
     window.addEventListener('beforeunload', handler);
     return () => window.removeEventListener('beforeunload', handler);
   }, []);
+  // to handle the user clicking a react-router Link
   return <Prompt when message="This page has unsaved data. Are you sure you want to leave?" />;
 }
