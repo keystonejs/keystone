2e2ab654039c0f3544e94a0045986e45cab2d833
Jess Telford <jess+github@jes.st>
2018-12-18 17:15:41 +1100

    Add basic docs search

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -6,18 +6,27 @@ async function getPackagePlugins() {
 
   const workspaces = await bolt.getWorkspaces({ cwd: rootDir });
 
-  return workspaces
-    .map(({ dir, config }) => ({ dir, name: config.name }))
-    .filter(({ dir }) => fs.existsSync(dir))
-    .map(({ name, dir }) => ({
+  return [
+    ...workspaces
+      .map(({ dir, config }) => ({ dir, name: config.name }))
+      .filter(({ dir }) => fs.existsSync(dir))
+      .map(({ name, dir }) => ({
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          // This `name` will show up as `sourceInstanceName` on a node's "parent"
+          // See `gatsby-node.js` for where it's used.
+          name,
+          path: `${dir}/`,
+        },
+      })),
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
-        // This `name` will show up as `sourceInstanceName` on a node's "parent"
-        // See `gatsby-node.js` for where it's used.
-        name,
-        path: `${dir}/`,
+        name: 'docs',
+        path: `${rootDir}/docs`,
       },
-    }));
+    },
+  ];
 }
 
 async function getGatsbyConfig() {
@@ -35,6 +44,33 @@ async function getGatsbyConfig() {
       },
       `gatsby-transformer-remark`,
       // TODO: The remark plugin can be made a lot smarter with its own plugins. I've left them off.
+      // TODO: prepend relative markdown URLs with the workspaceSlug field
+      {
+        resolve: 'gatsby-plugin-lunr',
+        options: {
+          languages: [{ name: 'en' }],
+          // Fields to index. If store === true value will be stored in index file.
+          // Attributes for custom indexing logic. See https://lunrjs.com/docs/lunr.Builder.html for details
+          fields: [
+            { name: 'content' },
+            { name: 'slug', store: true },
+            { name: 'workspace', store: true },
+            { name: 'heading', store: true, attributes: { boost: 20 } },
+          ],
+          // How to resolve each field's value for a supported node type
+          resolvers: {
+            // For any node of type MarkdownRemark, list how to resolve the fields' values
+            MarkdownRemark: {
+              content: node => node.rawMarkdownBody,
+              slug: node => node.fields.slug,
+              workspace: node => node.fields.workspace,
+              heading: node => node.fields.heading,
+            },
+          },
+          //custom index file name, default is search_index.json
+          filename: 'search_index.json',
+        },
+      },
     ],
   };
 }
