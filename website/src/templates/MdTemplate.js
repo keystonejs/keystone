69b3875f95b3e7db57d2aa5b4e58f866cc705562
Ben Conolly <ben@thinkmill.com.au>
2019-01-07 15:53:42 +1100

    added in syntax highlighting with mdx

--- website/src/templates/MdTemplate.js
+++ website/src/templates/MdTemplate.js
@@ -5,6 +5,7 @@ import { MDXProvider } from '@mdx-js/tag';
 
 import Search from '../components/search';
 import Heading from '../components/markdown/Heading';
+import Code from '../components/markdown/Code';
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -30,9 +31,10 @@ export default function Template({
               h4: props => <Heading {...props} tag="h4" />,
               h5: props => <Heading {...props} tag="h5" />,
               h6: props => <Heading {...props} tag="h6" />,
+              code: Code,
             }}
           >
-          <MDXRenderer>{code.body}</MDXRenderer>
+            <MDXRenderer>{code.body}</MDXRenderer>
           </MDXProvider>
         </div>
       </div>
