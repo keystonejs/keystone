0607eef38a441f2dea5bdd894c7a3628c6982d28
Ben Conolly <ben@thinkmill.com.au>
2019-01-08 16:25:44 +1100

    Small(ish) website changes (#597)
    * Add website readme
    
    * mdx now renders as intended
    
    * properly canonize mutation diagram
    
    * Add ids to headings so they are linkeable
    
    * added in syntax highlighting with mdx
    
    * Add plugin to resolve relative links

---
+++ website/src/components/markdown/Code.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import reactAddonsTextContent from 'react-addons-text-content';
+import theme from '../../prism-themes/duotoneLight';
+
+/*
+This solution should be seen as extremely temporary and not a platonic ideal of code highlighting.
+
+At the time this was written gatsby-remark-prism was not working with gatsby-mdx (leading to explosions).
+This implementation is a best-guess of using prism-react-renderer with mdx. If better patterns are found
+I highly encourage changing this.
+
+Leaving styling the inline component until we have emotion
+*/
+
+export default props =>
+  props.className ? (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={reactAddonsTextContent(props.children)}
+      language={props.className.replace('language-', '')}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  ) : (
+    <code {...props} />
+  );
