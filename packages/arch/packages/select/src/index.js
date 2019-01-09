e474efb2202738bde6ac7cb4de85fc71fc9d5901
mitchellhamilton <mitchell@hamil.town>
2019-01-09 10:56:54 +1000

    Update select

--- packages/arch/packages/select/src/index.js
+++ packages/arch/packages/select/src/index.js
@@ -1,53 +1,81 @@
 // @flow
 
-import React from 'react';
+import * as React from 'react';
 import ReactSelect from 'react-select';
+import { colors } from '@arch-ui/theme';
 
-// Re-Style React Select
-// ------------------------------
-const colors = '#2684FF';
-const borderRadius = 15;
+// ==============================
+// Styled Select
+// ==============================
 
-const selectStyles = {
-  control: (base: *, { isFocused }: { isFocused: Boolean }) => ({
-    ...base,
-    backgroundColor: 'white',
-    color: 'inherit',
-    borderRadius: borderRadius,
-    borderColor: isFocused ? colors.primary : colors.N20,
-    boxShadow: isFocused
-      ? `inset 0 1px 1px rgba(0, 0, 0, 0.075),
-        0 0 0 3px ${colors.primary}`
-      : 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
-    fontFamily: 'inherit',
-    fontSize: 14,
-    transition: 'border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s',
-    '&:hover': {
-      borderColor: isFocused ? colors.primary : colors.N30,
-    },
-  }),
-  option: (base: *) => ({
-    ...base,
-    fontSize: 14,
-  }),
-  singleValue: (base: *) => ({
-    ...base,
-    color: 'inherit',
-    fontSize: 14,
-  }),
-  multiValueLabel: (base: *) => ({
-    ...base,
-    color: 'inherit',
-    fontSize: 14,
-  }),
-  multiValueRemove: (base: *) => ({
-    ...base,
-    svg: {
-      height: '100%',
-    },
-  }),
+const indicatorStyles = (provided, { isDisabled, isFocused }) => {
+  let styles = {
+    color: colors.N20,
+    ':hover': !isDisabled && !isFocused ? { color: colors.N40 } : null,
+  };
+  if (isDisabled) styles = { color: colors.N10 };
+  if (isFocused) {
+    styles = { color: colors.N60, ':hover': { color: colors.N80 } };
+  }
+  return {
+    ...provided,
+    ...styles,
+  };
 };
+const selectStyles = {
+  control: (provided, { isFocused }) => {
+    const focusStyles = isFocused
+      ? {
+          borderColor: colors.primary,
+          boxShadow: `inset 0 1px 1px rgba(0, 0, 0, 0.075),
+      0 0 0 3px ${colors.B.A20}`,
+          outline: 0,
+        }
+      : null;
+    return {
+      ...provided,
+      backgroundColor: 'white',
+      borderColor: colors.N20,
+      fontSize: '0.9em',
+      minHeight: 35,
+      minWidth: '200px',
+      ':hover': { borderColor: colors.N30 },
+      ...focusStyles,
+    };
+  },
+  clearIndicator: indicatorStyles,
+  dropdownIndicator: indicatorStyles,
+  menu: p => ({ ...p, fontSize: '0.9em' }),
+  option: (p, { isDisabled, isFocused, isSelected }) => {
+    let bg = 'inherit';
+    if (isFocused) bg = colors.B.L90;
+    if (isSelected) bg = colors.primary;
 
-const Select = (props: *) => <ReactSelect styles={selectStyles} isClearable {...props} />;
+    let txt = 'inherit';
+    if (isFocused) txt = colors.primary;
+    if (isSelected) txt = 'white';
+    if (isDisabled) txt = colors.N40;
+
+    const cssPseudoActive =
+      !isSelected && !isDisabled
+        ? {
+            backgroundColor: colors.B.L80,
+            color: colors.B.D20,
+          }
+        : {};
+
+    return {
+      ...p,
+      backgroundColor: bg,
+      color: txt,
+
+      ':active': cssPseudoActive,
+    };
+  },
+  menuPortal: p => ({ ...p, zIndex: 3 }),
+};
+export const Select = ({ innerRef, ...props }: { innerRef: React.Ref<*> }) => (
+  <ReactSelect ref={innerRef} styles={selectStyles} {...props} />
+);
 
 export default Select;
