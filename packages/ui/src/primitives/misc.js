ff0e9c33778f448feac48c06eaf8f115f2146198
mitchellhamilton <mitchell@hamil.town>
2019-01-10 11:22:49 +1000

    Delete @voussoir/ui/src

--- packages/ui/src/primitives/misc.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This function wraps a component and adds a `data-selector` attribue
-// which can be used by test system to select DOM elements.
-export const withSelector = (id, WrappedComponent) =>
-  process.env.NODE_ENV === 'production'
-    ? WrappedComponent
-    : props => <WrappedComponent data-selector={id} {...props} />;
