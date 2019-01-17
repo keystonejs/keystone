45bc90d5f090ec855e8328d5dfd8dcd16aba7879
Nathan Simpson <hello@nathansimpson.design>
2019-01-18 10:34:20 +1100

    Adding in all files from other branch

---
+++ demo-projects/todo/index.js
@@ -0,0 +1,43 @@
+// Keystone imports
+const { Keystone } = require('@voussoir/core');
+const { AdminUI } = require('@voussoir/admin-ui');
+const { Text } = require('@voussoir/fields');
+const { WebServer } = require('@voussoir/server');
+const { MongooseAdapter } = require('@voussoir/adapter-mongoose');
+
+// Imports for the app
+const Bundler = require('parcel');
+const path = require('path');
+const staticPath = path.join(process.cwd(), 'public');
+const bundler = new Bundler(path.join(staticPath, 'index.html'));
+
+const keystone = new Keystone({
+  name: 'Keystone To-Do List',
+  adapter: new MongooseAdapter(),
+});
+
+keystone.createList('Todo', {
+  fields: {
+    name: { type: Text },
+  },
+});
+
+const admin = new AdminUI(keystone, { adminPath: '/admin' });
+
+const server = new WebServer(keystone, {
+  'cookie secret': 'qwerty',
+  'admin ui': admin,
+  port: 3000,
+});
+
+server.app.use(bundler.middleware());
+
+async function start() {
+  await keystone.connect();
+  server.start();
+}
+
+start().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ demo-projects/todo/public/styles.js
@@ -0,0 +1,48 @@
+const tint = (opacity, darkness) =>
+  `hsla(261, 84%, ${darkness == 'dark' ? '14%' : darkness == 'light' ? '95%' : '60%'}, ${opacity ||
+    1})`;
+
+const styles = {
+  mainHeading: {
+    textTransform: 'uppercase',
+    fontWeight: 900,
+    marginTop: 0,
+  },
+
+  app: {
+    padding: 50,
+    maxWidth: 450,
+    color: tint(1, 'dark'),
+    fontFamily: 'system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,sans-serif',
+  },
+  formInput: {
+    color: tint(1, 'dark'),
+    padding: '12px 16px',
+    fontSize: '1.25em',
+    width: '100%',
+    borderRadius: 6,
+    border: 0,
+    background: tint(0.3),
+  },
+
+  listItem: {
+    padding: '32px 16px',
+    fontSize: '1.25em',
+    fontWeight: 600,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    borderTop: `1px solid ${tint(0.2)}`,
+  },
+
+  deleteIcon: { width: 20, height: 20, fill: tint() },
+
+  deleteButton: {
+    background: 0,
+    border: 0,
+    padding: 0,
+    cursor: 'pointer',
+  },
+};
+
+export default styles;
