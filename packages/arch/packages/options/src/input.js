97c2139ce0bb0f94a90bda65c61608822a150c21
mitchellhamilton <mitchell@hamil.town>
2019-01-24 10:39:38 +1000

    Remove flow stuff

--- packages/arch/packages/options/src/input.js
+++ packages/arch/packages/options/src/input.js
@@ -1,21 +1,7 @@
-// @flow
 // a fork of https://github.com/JedWatson/react-select/blob/ba76246a92fe9371b5d4f8795d30119b045dcaba/src/components/Input.js
 // without react-input-autosize because it's causing expensive layout recalcs
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { type ElementRef } from 'react';
-import type { PropsWithStyles, ClassNamesState } from '../types';
-
-export type InputProps = PropsWithStyles & {
-  cx: (?string | null, ?ClassNamesState, ?string) => string | void,
-  /** Reference to the internal element */
-  innerRef: (ElementRef<*>) => void,
-  /** Set whether the input should be visible. Does not affect input size. */
-  isHidden: boolean,
-  /** Whether the input is disabled */
-  isDisabled?: boolean,
-  className?: string,
-};
 
 const inputStyle = isHidden => ({
   background: 0,
@@ -37,7 +23,7 @@ const Input = ({
   theme,
   selectProps,
   ...props
-}: InputProps) => (
+}) => (
   <div css={getStyles('input', { theme, ...props })}>
     <input
       className={cx(null, { input: true }, className)}
