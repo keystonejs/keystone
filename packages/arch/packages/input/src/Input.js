cfea697bb500bfaddd8f72663d62fff862c1b82f
Jess Telford <jess+github@jes.st>
2019-01-29 23:14:49 +1100

    Clean up content editor UI

--- packages/arch/packages/input/src/Input.js
+++ packages/arch/packages/input/src/Input.js
@@ -10,39 +10,42 @@ import { alpha } from '@arch-ui/color-utils';
 // Basic Input
 // ------------------------------
 
+export const inputStyles = (props = {}) => ({
+  ...buttonAndInputBase,
+  backgroundColor: props.disabled ? colors.N10 : 'white',
+  borderColor: colors.N20,
+  boxShadow: 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
+  color: 'inherit',
+  width: '100%',
+
+  ':hover': {
+    borderColor: colors.N30,
+    outline: 0,
+  },
+  ':focus': {
+    borderColor: colors.primary,
+    boxShadow: `inset 0 1px 1px rgba(0, 0, 0, 0.075),
+        0 0 0 3px ${alpha(colors.primary, 0.2)}`,
+    outline: 0,
+  },
+  '&[disabled]': {
+    borderColor: colors.N15,
+    boxShadow: 'none',
+    backgroundColor: colors.N05,
+  },
+  ...(props.isMultiline
+    ? {
+        lineHeight: 'inherit',
+        height: 'auto',
+      }
+    : undefined),
+});
+
 type InputProps = { isMultiline?: boolean, disabled?: boolean };
 export const Input: AbstractComponent<
   InputProps,
   HTMLInputElement | HTMLTextAreaElement
-> = forwardRef(({ isMultiline, ...props }: InputProps, ref) => {
-  const css = {
-    ...buttonAndInputBase,
-    backgroundColor: props.disabled ? colors.N10 : 'white',
-    borderColor: colors.N20,
-    boxShadow: 'inset 0 1px 1px rgba(0, 0, 0, 0.075)',
-    color: 'inherit',
-    width: '100%',
-
-    ':hover': {
-      borderColor: colors.N30,
-      outline: 0,
-    },
-    ':focus': {
-      borderColor: colors.primary,
-      boxShadow: `inset 0 1px 1px rgba(0, 0, 0, 0.075),
-        0 0 0 3px ${alpha(colors.primary, 0.2)}`,
-      outline: 0,
-    },
-    '&[disabled]': {
-      borderColor: colors.N15,
-      boxShadow: 'none',
-      backgroundColor: colors.N05,
-    },
-  };
-  return isMultiline ? (
-    <textarea ref={ref} css={{ ...css, lineHeight: 'inherit', height: 'auto' }} {...props} />
-  ) : (
-    // $FlowFixMe
-    <input ref={ref} css={css} {...props} />
-  );
+> = forwardRef((props: InputProps, ref) => {
+  const Component = props.isMultiline ? 'textarea' : 'input';
+  return <Component ref={ref} css={inputStyles(props)} {...props} />;
 });
