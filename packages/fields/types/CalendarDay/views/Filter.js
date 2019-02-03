bd3e0bcf9ff0678f134587af373ffdd9e4c14509
mitchellhamilton <mitchell@hamil.town>
2019-02-03 17:13:55 +1000

    Make CalendarDay Filter use TextDayPicker

--- packages/fields/types/CalendarDay/views/Filter.js
+++ packages/fields/types/CalendarDay/views/Filter.js
@@ -2,7 +2,7 @@
 
 import React, { Component, type Ref } from 'react';
 import { parse, format } from 'date-fns';
-import { DayPicker } from '@arch-ui/day-picker';
+import { TextDayPicker } from '@arch-ui/day-picker';
 
 const FORMAT = 'YYYY-MM-DD';
 
@@ -23,27 +23,21 @@ export default class CalendarDayFilterView extends Component<Props, State> {
     this.state = { value: format(new Date(), FORMAT) };
   }
 
-  handleSelectedChange = (day: Date) => {
+  handleSelectedChange = (day: Date | null) => {
     const { onChange } = this.props;
+    if (day === null) {
+      day = new Date();
+    }
     const value = format(day, FORMAT);
     onChange(value);
     this.setState({ value });
   };
 
   render() {
-    const { filter, field } = this.props;
+    const { filter } = this.props;
 
     if (!filter) return null;
 
-    return (
-      <DayPicker
-        startCurrentDateAt={parse(this.state.value)}
-        selectedDate={parse(this.state.value)}
-        onSelectedChange={this.handleSelectedChange}
-        yearRangeFrom={field.config.yearRangeFrom}
-        yearRangeTo={field.config.yearRangeTo}
-        yearPickerType={field.config.yearPickerType}
-      />
-    );
+    return <TextDayPicker date={parse(this.state.value)} onChange={this.handleSelectedChange} />;
   }
 }
