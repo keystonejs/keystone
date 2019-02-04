1a986e9caa039c7a2113b8e7c65cdf78899e4cde
Jess Telford <jess+github@jes.st>
2019-02-04 12:42:09 +1100

    Debounce searches and show results on first page load

--- website/src/utils/search.js
+++ website/src/utils/search.js
@@ -12,19 +12,48 @@ function prettyTitle(node) {
   return pretty === '' ? 'index' : pretty;
 }
 
-export function getResults(query) {
-  if (!query || !window.__LUNR__) return [];
-  const lunrIndex = window.__LUNR__['en'];
-  const results = lunrIndex.index.search(query); // you can  customize your search , see https://lunrjs.com/guides/searching.html
-  return (
-    results
-      .map(({ ref }) => {
-        let node = lunrIndex.store[ref];
+let lunrLoaded;
 
-        return { ...node, title: prettyTitle(node) };
-      })
-      // Make sure `tutorials` are always first
-      .sort((a, b) => (a.workspace !== b.workspace && a.workspace === 'tutorials' ? -1 : 0))
-  );
-  return results;
+export function getResults(query, options = { limit: Infinity }) {
+  if (!query) {
+    return Promise.resolve({ results: [], length: 0 });
+  }
+
+  // Wait until lunr is loaded.
+  // See https://github.com/humanseelabs/gatsby-plugin-lunr/issues/11
+  if (!lunrLoaded) {
+    lunrLoaded = new Promise((resolve, reject) => {
+      if (window.__LUNR__) {
+        return resolve(window.__LUNR__);
+      }
+      const interval = window.setInterval(() => {
+        if (window.__LUNR__) {
+          window.clearInterval(interval);
+          window.clearTimeout(timeout);
+          return resolve(window.__LUNR__);
+        }
+      }, 200);
+
+      const timeout = window.setTimeout(() => {
+        window.clearInterval(interval);
+        reject('Unable to load search index');
+      }, 5000);
+    });
+  }
+
+  return lunrLoaded.then(({ en: lunrIndex }) => {
+    const results = lunrIndex.index.search(query); // you can  customize your search , see https://lunrjs.com/guides/searching.html
+    return {
+      total: results.length,
+      results: results
+        .map(({ ref }) => {
+          let node = lunrIndex.store[ref];
+
+          return { ...node, title: prettyTitle(node) };
+        })
+        // Make sure `tutorials` are always first
+        .sort((a, b) => (a.workspace !== b.workspace && a.workspace === 'tutorials' ? -1 : 0))
+        .slice(0, options.limit),
+    };
+  });
 }
