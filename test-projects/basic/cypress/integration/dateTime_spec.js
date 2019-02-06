507b60759e3f297b7af37e2af1691c4bbe90d2f6
mitchellhamilton <mitchell@hamil.town>
2019-02-06 18:58:33 +1000

    Fix calendar day tests

--- test-projects/basic/cypress/integration/dateTime_spec.js
+++ test-projects/basic/cypress/integration/dateTime_spec.js
@@ -10,17 +10,15 @@ import {
   addMinutes,
   subMinutes,
   getYear,
+  addDays,
 } from 'date-fns';
 
 const today = setMinutes(new Date(), 0);
 const path = '/admin/users?fields=_label_%2Cdob%2ClastOnline';
 const lastOnline = '2018-08-16T11:08:18.886+10:00';
 
-const getDateButtonSetTo = date => `button:contains("${format(date, 'Do MMMM YYYY')}")`;
-const getDateTimeButtonSetTo = date => `button:contains("${format(date, 'MM/DD/YYYY h:mm A')}")`;
-const getDaySelector = date =>
-  `#ks-day-${date.getDate()}-${date.getMonth()}-${date.getFullYear()}:not([disabled])`;
-const getMonthSelector = date => `#ks-month-${date.getMonth()}-${date.getFullYear()}`;
+const calendarDayInputSelector = `#ks-daypicker-dob`;
+const dateTimeInputSelector = `#ks-daypicker-last-online`;
 
 const getCellFromSecondRow = index =>
   `#ks-list-table tbody > tr:nth-child(2) > td:nth-child(${index})`;
@@ -36,78 +34,29 @@ describe('CalendarDay Component - Formatting', () => {
 
   it('should format date correctly on the details page', () => {
     cy.get(getCellFromSecondRow(2)).click();
-    cy.get('#ks-input-dob').contains('1st January 1990');
+    cy.get(calendarDayInputSelector).should('have.value', '1st January 1990');
   });
 });
 
 describe('CalendarDay Component - Functionality', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit(path);
     cy.get(`#ks-list-table tbody > tr:nth-child(1) > td:nth-child(2)`).click();
   });
 
-  it(`should appear and dissapear`, () => {
-    cy.get('button:contains("Set Date")')
-      .first()
-      .click();
-    cy.get('#ks-daypicker-dob').should('exist');
+  it(`can accept natural language like tomorrow`, () => {
+    cy.get(calendarDayInputSelector).type('tomorrow');
     cy.get('label:contains("Name")').click();
-    cy.get('#ks-daypicker-dob').should('not.exist');
+    cy.get(calendarDayInputSelector).should(
+      'have.value',
+      format(addDays(new Date(), 1), 'Do MMMM YYYY')
+    );
   });
 
-  it(`can select a day in this month`, () => {
-    cy.get('button:contains("Set Date")')
-      .first()
-      .click();
-    cy.get(getDaySelector(today)).scrollIntoView();
-    cy.get(getDaySelector(today)).click();
+  it(`can accept a date`, () => {
+    cy.get(calendarDayInputSelector).type('20 september 2015');
     cy.get('label:contains("Name")').click();
-    cy.get(getDateButtonSetTo(today)).should('exist');
-  });
-
-  it(`can use arrows to set month`, () => {
-    cy.get(getDateButtonSetTo(today)).click();
-    cy.get(getMonthSelector(today)).scrollIntoView();
-    cy.get(`button:contains("Previous Month")`).click();
-    cy.get(`#ks-select-month`).should('have.value', `${subMonths(today, 1).getMonth()}`);
-  });
-
-  it.skip(`can use 'Select' to set month`, () => {
-    cy.get(getDateButtonSetTo(today)).click();
-    cy.get(`#ks-select-month`).select('Jun');
-    cy.wait(500);
-    cy.get(getDaySelector(setMonth(subMonths(today, 2), 5))).click();
-    cy.get('label:contains("Name")').click();
-    cy.get(getDateButtonSetTo(setMonth(subMonths(today, 2), 5)))
-      .should('exist')
-      .click();
-
-    // success, resetting to previous state
-    cy.log('success, resetting to previous state');
-    cy.get(`#ks-select-month`).select(format(today, 'MMM'));
-    cy.get(`#ks-input-year`).then(input => {
-      if (input.value != getYear(today)) {
-        cy.get(`#ks-input-year`).type(`{backspace}{backspace}${format(today, 'YY')}`);
-      }
-    });
-    cy.get(getDaySelector(today)).click();
-    cy.get('label:contains("Name")').click();
-  });
-
-  it(`can use input to set year`, () => {
-    cy.get(getDateButtonSetTo(today)).click();
-    cy.get(`#ks-input-year`).type('{backspace}{backspace}15');
-    cy.get(getDaySelector(setYear(today, 2015))).click();
-    cy.get('label:contains("Name")').click();
-    cy.get(getDateButtonSetTo(setYear(today, 2015)))
-      .should('exist')
-      .click();
-
-    // success, resetting to previous state
-    cy.log('success, resetting to previous state');
-    cy.get(`#ks-input-year`).type(`{backspace}{backspace}${format(today, 'YY')}`);
-    cy.get(getDaySelector(today)).click();
-    cy.get('label:contains("Name")').click();
+    cy.get(calendarDayInputSelector).should('have.value', '20th September 2015');
   });
 });
 
@@ -133,15 +82,6 @@ describe('DateTime Component - Functionality', () => {
   before(() => {
     cy.visit(path);
     cy.get(`#ks-list-table tbody > tr:nth-child(1) > td:nth-child(2)`).click();
-  });
-
-  it(`should appear and dissapear`, () => {
-    cy.get('button:contains("Set Date & Time")')
-      .first()
-      .click();
-    cy.get('#ks-input-lastOnline-picker').should('exist');
-    cy.get('label:contains("Name")').click();
-    cy.get('#ks-input-lastOnline-picker').should('not.exist');
   });
 
   it(`can select a day in this month`, () => {
