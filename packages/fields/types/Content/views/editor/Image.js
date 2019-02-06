d47c6260467b9a349eefb70af9738bd74ca4b4de
Jess Telford <jess+github@jes.st>
2019-02-07 08:24:55 +1100

    Hooks lint rules

--- packages/fields/types/Content/views/editor/Image.js
+++ packages/fields/types/Content/views/editor/Image.js
@@ -1,10 +1,43 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, forwardRef } from 'react';
 import { Popper } from 'react-popper';
 import { useStateWithEqualityCheck } from './hooks';
 
-let Render = ({ children }) => children();
+let PopperRender = forwardRef(
+  ({ scheduleUpdate, alignment, isFocused, style, onAlignmentChange }, ref) => {
+    useLayoutEffect(scheduleUpdate, [alignment]);
+    return (
+      <div
+        ref={ref}
+        css={{
+          display: isFocused ? 'block' : 'none',
+          backgroundColor: 'black',
+          padding: 8,
+        }}
+        style={style}
+      >
+        {['left', 'center', 'right'].map(align => {
+          return (
+            <button
+              type="button"
+              key={align}
+              onMouseDown={event => {
+                // so that the image block doesn't get deselected
+                event.preventDefault();
+              }}
+              onClick={() => {
+                onAlignmentChange(align);
+              }}
+            >
+              {align}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
 
 let popperModifiers = {
   flip: { enabled: false },
@@ -28,44 +61,11 @@ let Image = ({ alignment, attributes, isFocused, src, onAlignmentChange, ...prop
         }}
       />
       <Popper modifiers={popperModifiers} placement="top" referenceElement={referenceElement}>
-        {({ style, ref, scheduleUpdate }) => {
-          return (
-            <Render>
-              {() => {
-                useLayoutEffect(scheduleUpdate, [alignment]);
-                return (
-                  <div
-                    ref={ref}
-                    css={{
-                      display: isFocused ? 'block' : 'none',
-                      backgroundColor: 'black',
-                      padding: 8,
-                    }}
-                    style={style}
-                  >
-                    {['left', 'center', 'right'].map(align => {
-                      return (
-                        <button
-                          type="button"
-                          key={align}
-                          onMouseDown={event => {
-                            // so that the image block doesn't get deselected
-                            event.preventDefault();
-                          }}
-                          onClick={() => {
-                            onAlignmentChange(align);
-                          }}
-                        >
-                          {align}
-                        </button>
-                      );
-                    })}
-                  </div>
-                );
-              }}
-            </Render>
-          );
-        }}
+        {({ style, ref, scheduleUpdate }) => (
+          <PopperRender
+            {...{ scheduleUpdate, alignment, ref, isFocused, style, onAlignmentChange }}
+          />
+        )}
       </Popper>
     </div>
   );
