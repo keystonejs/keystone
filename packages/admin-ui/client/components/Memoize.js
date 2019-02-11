57c8f4ee0b778f3b3de9aff45b5e0cdfa5b4e579
mitchellhamilton <mitchell@hamil.town>
2019-02-11 22:10:40 +1000

    Remove Memoize component because I didn't actually end up using it

--- packages/admin-ui/client/components/Memoize.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import { useMemo, type Node } from 'react';
-
-export function Memoize({ children, deps }: { children: () => Node, deps: Array<any> }) {
-  return useMemo(children, deps);
-}
