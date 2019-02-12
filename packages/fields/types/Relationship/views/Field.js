9d092ae19b97228630399424344ea2886796f954
Mitchell Hamilton <mitchell@hamil.town>
2019-02-13 06:57:45 +1000

    "Set as <logged in user>" for relationships (#700)
    * "Set as <logged in user>" for relationships
    
    Closes #676
    
    * Fix a comment
    
    * Update packages/fields/types/Relationship/views/Field.js
    
    Co-Authored-By: mitchellhamilton <mitchell@hamil.town>
    
    * Use authenticated+listKey
    
    * Remove unused imports

--- packages/fields/types/Relationship/views/Field.js
+++ packages/fields/types/Relationship/views/Field.js
@@ -1,12 +1,55 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import { FieldContainer, FieldLabel, FieldInput } from '@arch-ui/fields';
 import { ShieldIcon } from '@arch-ui/icons';
-import { colors } from '@arch-ui/theme';
+import { colors, gridSize } from '@arch-ui/theme';
+import { Button } from '@arch-ui/button';
 
 import RelationshipSelect from './RelationshipSelect';
+
+function SetAsCurrentUser({ listKey, value, onAddUser, many }) {
+  let path = 'authenticated' + listKey;
+  return (
+    <Query
+      query={gql`
+        query User {
+          ${path} {
+            _label_
+            id
+          }
+        }
+      `}
+    >
+      {({ data }) => {
+        if (data && data[path]) {
+          let userId = data[path].id;
+          if (
+            value !== null &&
+            (many ? value.some(item => item.id === userId) : value.id === userId)
+          ) {
+            return null;
+          }
+          return (
+            <Button
+              css={{ marginLeft: gridSize }}
+              variant="ghost"
+              onClick={() => {
+                onAddUser(data[path]);
+              }}
+            >
+              {many ? 'Add' : 'Set as'} {data[path]._label_}
+            </Button>
+          );
+        }
+        return null;
+      }}
+    </Query>
+  );
+}
 
 export default class RelationshipField extends Component {
   onChange = option => {
@@ -19,11 +62,11 @@ export default class RelationshipField extends Component {
     }
   };
   render() {
-    const { autoFocus, field, value, renderContext, error } = this.props;
-    const { many } = field.config;
+    const { autoFocus, field, value, renderContext, error, onChange } = this.props;
+    const { many, ref } = field.config;
+    const { authList, withAuth } = field.adminMeta;
     const htmlID = `ks-input-${field.path}`;
     const canRead = !(error instanceof Error && error.name === 'AccessDeniedError');
-
     return (
       <FieldContainer>
         <FieldLabel
@@ -50,6 +93,16 @@ export default class RelationshipField extends Component {
             htmlID={htmlID}
             onChange={this.onChange}
           />
+          {withAuth && ref === authList && (
+            <SetAsCurrentUser
+              many={many}
+              onAddUser={user => {
+                onChange(many ? (value || []).concat(user) : user);
+              }}
+              value={value}
+              listKey={authList}
+            />
+          )}
         </FieldInput>
       </FieldContainer>
     );
