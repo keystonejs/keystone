302c6f1bb0cbf2887dbf2840f5bef62e11c781b2
Tim Leslie <timl@thinkmill.com.au>
2019-02-13 14:33:53 +1100

    Add support for DateTime field to knex adapter

--- packages/fields/types/DateTime/Implementation.js
+++ packages/fields/types/DateTime/Implementation.js
@@ -62,7 +62,80 @@ class _DateTime extends Implementation {
 
 const toDate = s => s && DateTime.fromISO(s, { zone: 'utc' }).toJSDate();
 
-class MongoDateTimeInterface extends MongooseFieldAdapter {
+const CommonDateTimeInterface = superclass =>
+  class extends superclass {
+    setupHooks({ addPreSaveHook, addPostReadHook }) {
+      const field_path = this.path;
+      const utc_field = `${field_path}_utc`;
+      const offset_field = `${field_path}_offset`;
+
+      // Updates the relevant value in the item provided (by referrence)
+      addPreSaveHook(item => {
+        // Only run the hook if the item actually contains the datetime field
+        // NOTE: Can't use hasOwnProperty here, as the mongoose data object
+        // returned isn't a POJO
+        if (!(field_path in item)) {
+          return item;
+        }
+
+        const datetimeString = item[field_path];
+
+        // NOTE: Even though `0` is a valid timestamp (the unix epoch), it's not a valid ISO string,
+        // so it's ok to check for falseyness here.
+        if (!datetimeString) {
+          item[utc_field] = null;
+          item[offset_field] = null;
+          delete item[field_path]; // Never store this field
+          return item;
+        }
+
+        if (!DateTime.fromISO(datetimeString, { zone: 'utc' }).isValid) {
+          throw new Error(
+            'Validation failed: DateTime must be either `null` or a valid ISO 8601 string'
+          );
+        }
+
+        item[utc_field] = toDate(datetimeString);
+        item[offset_field] = DateTime.fromISO(datetimeString, { setZone: true }).toFormat('ZZ');
+        delete item[field_path]; // Never store this field
+
+        return item;
+      });
+
+      addPostReadHook(item => {
+        // If there's no fields stored in the DB (can happen with MongoDB), then
+        // don't bother trying to process anything
+        // NOTE: Can't use hasOwnProperty here, as the mongoose data object
+        // returned isn't a POJO
+        if (!(utc_field in item) && !(offset_field in item)) {
+          return item;
+        }
+
+        if (!item[utc_field] || !item[offset_field]) {
+          item[field_path] = null;
+          return item;
+        }
+
+        const datetimeString = DateTime.fromJSDate(item[utc_field], { zone: 'utc' })
+          .setZone(
+            new FixedOffsetZone(
+              DateTime.fromISO(`1234-01-01T00:00:00${item[offset_field]}`, {
+                setZone: true,
+              }).offset
+            )
+          )
+          .toISO();
+
+        item[field_path] = datetimeString;
+        item[utc_field] = undefined;
+        item[offset_field] = undefined;
+
+        return item;
+      });
+    }
+  };
+
+class MongoDateTimeInterface extends CommonDateTimeInterface(MongooseFieldAdapter) {
   addToMongooseSchema(schema) {
     const { mongooseOptions } = this.config;
     const field_path = this.path;
@@ -75,76 +148,6 @@ class MongoDateTimeInterface extends MongooseFieldAdapter {
       // These are the actual fields we care about storing in the database.
       [utc_field]: { type: Date, ...mongooseOptions },
       [offset_field]: { type: String, ...mongooseOptions },
-    });
-  }
-
-  setupHooks({ addPreSaveHook, addPostReadHook }) {
-    const field_path = this.path;
-    const utc_field = `${field_path}_utc`;
-    const offset_field = `${field_path}_offset`;
-
-    // Updates the relevant value in the item provided (by referrence)
-    addPreSaveHook(item => {
-      // Only run the hook if the item actually contains the datetime field
-      // NOTE: Can't use hasOwnProperty here, as the mongoose data object
-      // returned isn't a POJO
-      if (!(field_path in item)) {
-        return item;
-      }
-
-      const datetimeString = item[field_path];
-
-      // NOTE: Even though `0` is a valid timestamp (the unix epoch), it's not a valid ISO string,
-      // so it's ok to check for falseyness here.
-      if (!datetimeString) {
-        item[utc_field] = null;
-        item[offset_field] = null;
-        item[field_path] = undefined; // Never store this field
-        return item;
-      }
-
-      if (!DateTime.fromISO(datetimeString, { zone: 'utc' }).isValid) {
-        throw new Error(
-          'Validation failed: DateTime must be either `null` or a valid ISO 8601 string'
-        );
-      }
-
-      item[utc_field] = toDate(datetimeString);
-      item[offset_field] = DateTime.fromISO(datetimeString, { setZone: true }).toFormat('ZZ');
-      item[field_path] = undefined; // Never store this field
-
-      return item;
-    });
-
-    addPostReadHook(item => {
-      // If there's no fields stored in the DB (can happen with MongoDB), then
-      // don't bother trying to process anything
-      // NOTE: Can't use hasOwnProperty here, as the mongoose data object
-      // returned isn't a POJO
-      if (!(utc_field in item) && !(offset_field in item)) {
-        return item;
-      }
-
-      if (!item[utc_field] || !item[offset_field]) {
-        item[field_path] = null;
-        return item;
-      }
-
-      const datetimeString = DateTime.fromJSDate(item[utc_field], { zone: 'utc' })
-        .setZone(
-          new FixedOffsetZone(
-            DateTime.fromISO(`1234-01-01T00:00:00${item[offset_field]}`, {
-              setZone: true,
-            }).offset
-          )
-        )
-        .toISO();
-
-      item[field_path] = datetimeString;
-      item[utc_field] = undefined;
-      item[offset_field] = undefined;
-
-      return item;
     });
   }
 
@@ -161,15 +164,30 @@ class MongoDateTimeInterface extends MongooseFieldAdapter {
   }
 }
 
-class KnexDateTimeInterface extends KnexFieldAdapter {
-  createColumn(table) {
-    table.text(this.path);
+class KnexDateTimeInterface extends CommonDateTimeInterface(KnexFieldAdapter) {
+  constructor() {
+    super(...arguments);
+    const field_path = this.path;
+    const utc_field = `${field_path}_utc`;
+    const offset_field = `${field_path}_offset`;
+    this.realKeys = [utc_field, offset_field];
+    this.sortKey = utc_field;
   }
+
+  createColumn(table) {
+    const field_path = this.path;
+    const utc_field = `${field_path}_utc`;
+    const offset_field = `${field_path}_offset`;
+
+    table.timestamp(utc_field, { useTz: false });
+    table.text(offset_field);
+  }
+
   getQueryConditions(f, g) {
     return {
-      ...this.equalityConditions(f, g),
-      ...this.orderingConditions(f, g),
-      ...this.inConditions(f, g),
+      ...this.equalityConditions(v => f(toDate(v)), p => g(`${p}_utc`)),
+      ...this.orderingConditions(v => f(toDate(v)), p => g(`${p}_utc`)),
+      ...this.inConditions(v => f(toDate(v)), p => g(`${p}_utc`)),
     };
   }
 }
