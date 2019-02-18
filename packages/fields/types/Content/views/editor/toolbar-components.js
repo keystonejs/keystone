b9271fb68769e88da5f923e792b48a5d9994d109
mitchellhamilton <mitchell@hamil.town>
2019-02-18 17:35:12 +1000

    Change a thing

--- packages/fields/types/Content/views/editor/toolbar-components.js
+++ packages/fields/types/Content/views/editor/toolbar-components.js
@@ -1,46 +1,40 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { forwardRef } from 'react';
 import { colors, gridSize } from '@arch-ui/theme';
 import { lighten, darken } from '@arch-ui/color-utils';
 import Tooltip from '@arch-ui/tooltip';
 import { A11yText } from '@arch-ui/typography';
 
-export let BasicToolbarButton = forwardRef(({ isActive, ...props }, ref) => {
-  return (
-    <button
-      type="button"
-      ref={ref}
-      css={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        border: 0,
-        cursor: 'pointer',
-        color: isActive ? colors.primary : 'white',
-        ':hover,:focus': {
-          color: isActive ? darken(colors.primary, 10) : lighten(colors.primary, 40),
-        },
-        ':active': {
-          color: isActive ? darken(colors.primary, 25) : lighten(colors.primary, 10),
-        },
-        fontSize: 16,
-        outline: 'none',
-      }}
-      {...props}
-    />
-  );
-});
-
 export let ToolbarButton = ({ isActive, label, icon, ...props }) => {
   return (
     <Tooltip placement="top" css={{ marginBottom: gridSize / 2 }} content={label}>
       {ref => (
-        <BasicToolbarButton ref={ref} isActive={isActive} {...props}>
+        <button
+          type="button"
+          css={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'transparent',
+            border: 0,
+            cursor: 'pointer',
+            color: isActive ? colors.primary : 'white',
+            ':hover,:focus': {
+              color: isActive ? darken(colors.primary, 10) : lighten(colors.primary, 40),
+            },
+            ':active': {
+              color: isActive ? darken(colors.primary, 25) : lighten(colors.primary, 10),
+            },
+            fontSize: 16,
+            outline: 'none',
+          }}
+          ref={ref}
+          isActive={isActive}
+          {...props}
+        >
           {icon}
           <A11yText>{label}</A11yText>
-        </BasicToolbarButton>
+        </button>
       )}
     </Tooltip>
   );
