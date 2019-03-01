cf318fc6710c6b1882956d5c7c2f3cdb4e59363b
mitchellhamilton <mitchell@hamil.town>
2019-03-02 08:46:49 +1000

    Fix heroku deployment

--- test-projects/basic/index.js
+++ test-projects/basic/index.js
@@ -181,7 +181,7 @@ server.app.get('/reset-db', (req, res) => {
 server.app.use(staticRoute, server.express.static(staticPath));
 
 async function start() {
-  await keystone.connect();
+  await keystone.connect(process.env.MONGODB_URI);
   server.start();
   const users = await keystone.lists.User.adapter.findAll();
   if (!users.length) {
