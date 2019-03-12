9be302df4c2deea0b538661d21a2755ff3cf25ea
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-03-12 14:46:33 +1100

    working on #813
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- packages/create-keystone-app/bin/cli.js
+++ packages/create-keystone-app/bin/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const arg = require('arg');
-
+const chalk = require('chalk');
 const generator = require('./generator');
 
 // Setup all the args
@@ -9,32 +10,53 @@ const argSpec = {
   '--help': Boolean,
   '-h': '--help',
   '--version': Boolean,
+  '-V': '--version',
 };
 
-// Parse the command line args coming in
-const args = arg(argSpec, {
-  // Capture the project name only
-  permissive: false,
-});
+let args;
+try {
+  // Parse the command line args coming in
+  args = arg(argSpec, {
+    // Capture the project name only
+    permissive: false,
+  });
+} catch (error) {
+  if (error.code === 'ARG_UNKNOWN_OPTION') {
+    console.error(chalk.red(`\nError: ${error.message}`));
+    console.info(generator.help());
+    process.exit(0);
+  }
+}
 
 if (args['--version']) {
-  console.log(generator.version());
+  console.info(generator.version());
   process.exit(0);
 }
 
 if (args['--help']) {
-  console.log(generator.help());
+  console.info(generator.help());
   process.exit(0);
 }
 
 // if project name is missing print help
 if (args._.length === 0) {
-  console.log(generator.help());
+  console.info(generator.help());
+  process.exit(0);
+}
+
+// check if folder exists and is not empty
+const name = generator.createAppName(args._.join(' '));
+try {
+	generator.checkEmptyDir(name);
+}
+catch (error) {
+	console.error(chalk.red(`\n${error}`));
+  console.info(generator.help());
   process.exit(0);
 }
 
 // Everything else is assumed to be a command we want to execute - more options added
-generator.exec(args).catch(error => {
+generator.exec(name).catch(error => {
   console.error(error);
   process.exit(1);
 });
