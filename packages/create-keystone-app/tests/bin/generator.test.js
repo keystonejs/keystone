3e26171ee34d09657a7319970aa8132a28d3b4b0
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2019-03-12 17:46:39 +1100

    fixed tests
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- packages/create-keystone-app/tests/bin/generator.test.js
+++ packages/create-keystone-app/tests/bin/generator.test.js
@@ -1,12 +1,40 @@
-const generator = require('../../bin/generator');
 const version = require('../../package.json').version;
+const child_process = require('child_process');
+const path = require('path');
 
 describe('create-keystone-app generator', () => {
   test('prints version', () => {
-    expect(generator.version()).toBe(version);
+    const cli = child_process.spawnSync('node', [
+      path.normalize(`${__dirname}/../../bin/cli.js`),
+      '--version',
+    ]);
+    expect(cli.stdout.toString().trim()).toBe(version);
+    expect(cli.stderr.toString().trim()).toEqual('');
   });
 
   test('prints help', () => {
-    expect(generator.help({})).toEqual(expect.stringContaining('Usage'));
+    const cli = child_process.spawnSync('node', [
+      path.normalize(`${__dirname}/../../bin/cli.js`),
+      '--help',
+    ]);
+    expect(cli.stdout.toString().trim()).toEqual(expect.stringContaining('Usage'));
+    expect(cli.stderr.toString().trim()).toEqual('');
+  });
+
+  test('prints error and help when used wrong', () => {
+    const cli = child_process.spawnSync('node', [
+      path.normalize(`${__dirname}/../../bin/cli.js`),
+      '-x',
+    ]);
+    expect(cli.stderr.toString().trim()).toEqual(
+      expect.stringContaining('Unknown or unexpected option')
+    );
+    expect(cli.stdout.toString().trim()).toEqual(expect.stringContaining('Usage'));
+  });
+
+  test('prints help when no arguments given', () => {
+    const cli = child_process.spawnSync('node', [path.normalize(`${__dirname}/../../bin/cli.js`)]);
+    expect(cli.stdout.toString().trim()).toEqual(expect.stringContaining('Usage'));
+    expect(cli.stderr.toString().trim()).toEqual('');
   });
 });
