74b12826a22f17d424e14c0f36b6353b50742683
Nathan Simpson <hello@nathansimpson.design>
2019-03-15 10:21:35 +1100

    Some polish

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,7 +9,7 @@ import Illustration from '../images/illustration.svg';
 export default () => (
   <>
     <Global styles={globalStyles} />
-    <Header key="global-header" style={{ position: 'fixed', left: 0, right: 0, top: 0 }} />
+    <Header key="global-header" />
     <Hero />
 
     {/* <Footer textCenter /> */}
@@ -20,7 +20,6 @@ const Hero = () => (
   <div
     css={{
       display: 'flex',
-      fontSize: 18,
       minHeight: '100vh',
     }}
   >
@@ -29,7 +28,8 @@ const Hero = () => (
         display: 'flex',
         '@media (max-width: 920px)': {
           flexDirection: 'column',
-          marginTop: 70,
+          marginTop: 50,
+          textAlign: 'center',
         },
 
         '@media (min-width: 920px)': {
@@ -45,6 +45,10 @@ const Hero = () => (
             fontWeight: 800,
             lineHeight: 1,
             margin: 0,
+
+            '@media (max-width: 920px)': {
+              fontSize: '1.5em',
+            },
           }}
         >
           Build amazing node.js applications, faster.
@@ -52,7 +56,7 @@ const Hero = () => (
           {/* Build applications faster with Keystone 5, a node.js CMS and API provider */}
         </h1>
 
-        <div css={{ color: colors.N80, margin: '3em 0', lineHeight: 1.6 }}>
+        <div css={{ color: colors.N80, fontSize: 18, margin: '3em 0', lineHeight: 1.6 }}>
           <p>
             Keystone 5 includes first-class GraphQL support, a modular architecture and an improved
             Admin UI.
@@ -61,32 +65,25 @@ const Hero = () => (
             We're focusing less on serving the front-end, and more on providing a scalable platform
             for content management and node.js applications.
           </p>
-          {/* <p>
-            We've been building the next generation of KeystoneJS at Thinkmill and are excited to
-            start sharing it with the community.
-          </p> */}
         </div>
         <div
           css={{
             display: 'inline-flex',
             alignItems: 'center',
+            '@media (max-width: 920px)': {
+              flexDirection: 'column',
+            },
           }}
         >
-          <Button
-            appearance="primary"
-            variant="solid"
-            to="/quick-start"
-            style={{ margin: 0, marginRight: 8 }}
-          >
+          <Button appearance="primary" variant="solid" to="/quick-start" style={{ marginRight: 8 }}>
             Get Started
           </Button>
-          <Button
-            appearance="primary"
+          <a
             href="https://github.com/keystonejs/keystone-5"
-            style={{ margin: 0 }}
+            style={{ margin: '0', color: 'black', textDecoration: 'underline' }}
           >
             View on GitHub
-          </Button>
+          </a>
         </div>
         <p css={{ color: '#8E8E93', lineHeight: 1.6 }}>
           Keystone 5 is currently in alpha release and under intensive development by Thinkmill and
