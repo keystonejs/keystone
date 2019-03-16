668b6cb83759f21fea77cac25325e83f56854179
mitchellhamilton <mitchell@hamil.town>
2019-03-17 09:55:48 +1000

    Put field type views onto field controllers

--- packages/fields/Controller.js
+++ packages/fields/Controller.js
@@ -1,11 +1,12 @@
 export default class FieldController {
-  constructor(config, list, adminMeta) {
+  constructor(config, list, adminMeta, views) {
     this.config = config;
     this.label = config.label;
     this.path = config.path;
     this.type = config.type;
     this.list = list;
     this.adminMeta = adminMeta;
+    this.views = views;
   }
 
   // TODO: This is a bad default; we should (somehow?) inspect the fields provided
