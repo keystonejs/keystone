e02fba43df52284aafb026978166c9ddbb33ae26
mitchellhamilton <mitchell@hamil.town>
2019-03-17 10:39:04 +1000

    Build Content blocks

--- packages/fields/types/Content/index.js
+++ packages/fields/types/Content/index.js
@@ -14,13 +14,13 @@ module.exports = {
     knex: KnexContentInterface,
   },
   blocks: {
-    blockquote: { viewPath: path.join(__dirname, './views/editor/blocks/blockquote') },
-    embed: { viewPath: path.join(__dirname, './views/editor/blocks/embed') },
-    heading: { viewPath: path.join(__dirname, './views/editor/blocks/heading') },
-    image: { viewPath: path.join(__dirname, './views/editor/blocks/image-container') },
-    link: { viewPath: path.join(__dirname, './views/editor/blocks/link') },
-    orderedList: { viewPath: path.join(__dirname, './views/editor/blocks/ordered-list') },
-    unorderedList: { viewPath: path.join(__dirname, './views/editor/blocks/unordered-list') },
+    blockquote: { viewPath: path.join(__dirname, './views/blocks/blockquote') },
+    embed: { viewPath: path.join(__dirname, './views/blocks/embed') },
+    heading: { viewPath: path.join(__dirname, './views/blocks/heading') },
+    image: { viewPath: path.join(__dirname, './views/blocks/image-container') },
+    link: { viewPath: path.join(__dirname, './views/blocks/link') },
+    orderedList: { viewPath: path.join(__dirname, './views/blocks/ordered-list') },
+    unorderedList: { viewPath: path.join(__dirname, './views/blocks/unordered-list') },
     // not exposing list-item since it's only used internally by the other blocks
     // not exposing paragraph since it's included by default
   },
