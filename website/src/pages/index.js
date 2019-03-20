b72efdd4144e88b0c8e288262b12c16841633e37
Joss Mackison <jmackison@atlassian.com>
2019-03-21 09:50:16 +1100

    add text to homepage icons

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -93,9 +93,13 @@ const Hero = () => (
           contributors around the world.
         </p>
         <div css={mq({ display: 'flex', margin: [`2em auto`, `2em auto`, `2em 0`] })}>
-          <IconTwitter href="https://twitter.com/keystonejs" target="_blank" />
-          <IconGithub href="https://github.com/keystonejs/keystone-5" target="_blank" />
-          <IconSlack href="https://launchpass.com/keystonejs" target="_blank" />
+          <IconTwitter href="https://twitter.com/keystonejs" target="_blank" title="Twitter" />
+          <IconGithub
+            href="https://github.com/keystonejs/keystone-5"
+            target="_blank"
+            title="Github"
+          />
+          <IconSlack href="https://launchpass.com/keystonejs" target="_blank" title="Slack" />
         </div>
       </InnerLayout>
       <LgDeviceColPlaceholder />
@@ -230,6 +234,7 @@ const IconGithub = props => (
         d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
       />
     </svg>
+    <A11yText>KeystoneJS repository on GitHub</A11yText>
   </Icon>
 );
 const IconTwitter = props => (
@@ -241,6 +246,7 @@ const IconTwitter = props => (
         fillRule="nonzero"
       />
     </svg>
+    <A11yText>Hear about KeystoneJS on Twitter</A11yText>
   </Icon>
 );
 const IconSlack = props => (
@@ -252,5 +258,24 @@ const IconSlack = props => (
         <path d="M15.135 18.89a2.503 2.503 0 0 1 2.497 2.497 2.503 2.503 0 0 1-2.497 2.497 2.503 2.503 0 0 1-2.496-2.497V18.89h2.496zM15.135 17.632a2.503 2.503 0 0 1-2.496-2.497 2.503 2.503 0 0 1 2.496-2.496h6.252a2.503 2.503 0 0 1 2.497 2.496 2.503 2.503 0 0 1-2.497 2.497h-6.252z" />
       </g>
     </svg>
+    <A11yText>Discuss KeystoneJS on Slack</A11yText>
   </Icon>
 );
+const A11yText = ({ tag: Tag, ...props }: A11yTextProps) => (
+  <Tag
+    css={{
+      border: 0,
+      clip: 'rect(1px, 1px, 1px, 1px)',
+      height: 1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      whiteSpace: 'nowrap',
+      width: 1,
+    }}
+    {...props}
+  />
+);
+A11yText.defaultProps = {
+  tag: 'span',
+};
