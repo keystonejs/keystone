513b20b40e5f87e5c6da2ab3622e8daf16d683c2
Jed Watson <jed.watson@me.com>
2019-03-27 01:20:15 +1100

    Rewriting Editor with hooks

--- packages/fields-wysiwyg-tinymce/views/Editor.js
+++ packages/fields-wysiwyg-tinymce/views/Editor.js
@@ -1,30 +1,75 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import tinymce from 'tinymce/tinymce';
 import 'tinymce/themes/silver';
 
-import eventProps from './eventProps';
+const Editor = props => {
+  const { value, name, isDisabled } = props;
 
-const isValidEvent = name => !!eventProps[name];
+  const elementRef = useRef(null);
+  const editorRef = useRef(null);
+  const contentRef = useRef(value);
 
-const isFunction = fn => typeof fn === 'function';
+  const editorIsReady = () => editorRef.current && editorRef.current.initialized;
 
-const bindEventProps = (props, editor, initEvent) => {
-  Object.keys(props)
-    .filter(isValidEvent)
-    .forEach(prop => {
-      const handler = props[prop];
-      if (isFunction(handler)) {
-        if (prop === 'onInit') {
-          handler(initEvent, editor);
-        } else {
-          editor.on(prop.substring(2), e => handler(e, editor));
-        }
-      }
-    });
+  const handleChange = () => {
+    const { onChange } = props;
+    const content = editorRef.current.getContent();
+    if (contentRef.current === content) return;
+    contentRef.current = content;
+    if (typeof onChange === 'function') {
+      onChange(content);
+    }
+  };
+
+  // Set up the Editor
+  useEffect(() => {
+    const { autoFocus, plugins, toolbar } = props;
+    // TODO: allow additional options to be mixed in
+    const options = {
+      auto_focus: autoFocus,
+      branding: false,
+      content_css: '/tinymce/skins/content/default/content.css',
+      menubar: false,
+      plugins: plugins,
+      readonly: isDisabled,
+      skin_url: '/tinymce/skins/ui/oxide',
+      target: elementRef.current,
+      toolbar: toolbar,
+      toolbar_drawer: 'floating',
+      setup: editor => {
+        editorRef.current = editor;
+        editor.on('init', () => {
+          if (typeof value === 'string') {
+            editor.setContent(value);
+          }
+          editor.on('change keyup setcontent', handleChange);
+        });
+      },
+    };
+    tinymce.init(options);
+
+    return () => {
+      tinymce.remove(editorRef.current);
+    };
+  }, []);
+
+  // Update the Editor content when the value prop changes
+  useEffect(() => {
+    if (!editorIsReady() || typeof value !== 'string' || value === contentRef.current) return;
+    editorRef.current.setContent(value);
+  }, [value]);
+
+  // Update the Editor mode when the isDisabled prop changes
+  useEffect(() => {
+    if (!editorIsReady()) return;
+    editorRef.current.setMode(isDisabled ? 'readonly' : 'design');
+  }, [isDisabled]);
+
+  return <textarea ref={elementRef} style={{ visibility: 'hidden' }} name={name} />;
 };
 
-export const editorPropTypes = {
+Editor.propTypes = {
   autoFocus: PropTypes.bool,
   isDisabled: PropTypes.bool,
   name: PropTypes.string,
@@ -32,68 +77,6 @@ export const editorPropTypes = {
   plugins: PropTypes.array,
   toolbar: PropTypes.array,
   value: PropTypes.string,
-  ...eventProps,
 };
 
-export default class Editor extends Component {
-  static propTypes = editorPropTypes;
-  element = null;
-  componentDidMount() {
-    const { autoFocus, plugins, toolbar, isDisabled, value } = this.props;
-    // TODO: allow additional options to be mixed in
-    const options = {
-      auto_focus: autoFocus,
-      branding: false,
-      content_css: '/tinymce/skins/content/default/content.css',
-      plugins: plugins,
-      readonly: isDisabled,
-      skin_url: '/tinymce/skins/ui/oxide',
-      target: this.element,
-      toolbar: toolbar,
-      setup: editor => {
-        this.editor = editor;
-        editor.on('init', e => {
-          editor.setContent(value || '');
-          editor.on('change keyup setcontent', this.handleChange);
-          bindEventProps(this.props, editor, e);
-        });
-      },
-    };
-    tinymce.init(options);
-  }
-  componentWillReceiveProps(nextProps) {
-    if (!this.editor || !this.editor.initialized) return;
-    this.currentContent = this.currentContent || this.editor.getContent();
-    if (
-      typeof nextProps.value === 'string' &&
-      nextProps.value !== this.props.value &&
-      nextProps.value !== this.currentContent
-    ) {
-      this.editor.setContent(nextProps.value);
-    }
-    if (
-      typeof nextProps.isDisabled === 'boolean' &&
-      nextProps.isDisabled !== this.props.isDisabled
-    ) {
-      this.editor.setMode(nextProps.isDisabled ? 'readonly' : 'design');
-    }
-  }
-  componentWillUnmount() {
-    tinymce.remove(this.editor);
-  }
-  handleChange = () => {
-    const content = this.editor.getContent();
-    if (this.currentContent === content) return;
-    this.currentContent = content;
-    const { onChange } = this.props;
-    if (isFunction(onChange)) {
-      onChange(content);
-    }
-  };
-  render() {
-    const { name } = this.props;
-    return (
-      <textarea ref={el => (this.element = el)} style={{ visibility: 'hidden' }} name={name} />
-    );
-  }
-}
+export default Editor;
