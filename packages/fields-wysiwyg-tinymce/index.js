a2d05aabe79c9836f5e4e5696751e74a33585142
Jed Watson <jed.watson@me.com>
2019-03-27 00:39:16 +1100

    Adding static middleware to serve TinyMCE assets

--- packages/fields-wysiwyg-tinymce/index.js
+++ packages/fields-wysiwyg-tinymce/index.js
@@ -1,13 +1,20 @@
 const { Text } = require('@keystone-alpha/fields');
 const path = require('path');
 
+function bindStaticMiddleware(server) {
+  const tinymce = require.resolve('tinymce');
+  const tinymcePath = tinymce.substr(0, tinymce.lastIndexOf('/'));
+  server.app.use('/tinymce', server.express.static(tinymcePath));
+}
+
 module.exports = {
   type: 'Wysiwyg',
   implementation: Text.implementation,
   views: {
     Controller: '@keystone-alpha/fields/types/Text/views/Controller',
-    Field: path.join(__dirname, './views/Field'),
+    Field: path.join(__dirname, './views/Field/Field'),
     Filter: '@keystone-alpha/fields/types/Text/views/Filter',
   },
   adapters: Text.adapters,
+  bindStaticMiddleware,
 };
