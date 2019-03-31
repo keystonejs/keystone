f4be08f270fe0c5400f48e87e5bde4d53be73a82
Tim Leslie <616382+timleslie@users.noreply.github.com>
2019-03-31 13:15:15 +1100

    Update file-adapter

--- packages/file-adapters/index.js
+++ packages/file-adapters/index.js
@@ -1,3 +1,6 @@
+const LocalFileAdapter = require('./src/local-file');
+const CloudinaryAdapter = require('./src/cloudinary');
+
 module.exports = {
   LocalFileAdapter: require('./local-file'),
   CloudinaryAdapter: require('./cloudinary'),
