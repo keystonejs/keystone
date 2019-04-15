199a281992743481338fb451020cb95870f80da3
mitchellhamilton <mitchell@hamil.town>
2019-04-15 19:32:21 +1000

    WIP more tests

---
+++ packages/build-field-types/__fixtures__/valid-monorepo-that-logs-stuff/packages/package-one/src/index.js
@@ -0,0 +1,4 @@
+import { message } from "@my-cool-scope/package-two";
+
+console.log("message from package one");
+console.log(message);
---
+++ packages/build-field-types/__fixtures__/valid-monorepo-that-logs-stuff/packages/package-two/src/index.js
@@ -0,0 +1,3 @@
+console.log("message from package two");
+
+export let message = "message from package two but logged by package one";
