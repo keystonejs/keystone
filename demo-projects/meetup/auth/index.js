1e43664fb5bcbb305b710dca9311ecf48a4470ef
ticipp@gmail.com <ticipp@gmail.com>
2019-05-05 16:36:36 +1000

    Format code

--- demo-projects/meetup/auth/index.js
+++ demo-projects/meetup/auth/index.js
@@ -4,28 +4,28 @@ const authRouter = express.Router();
 
 const createEmailAuthRoutes = require('./emailAuth');
 
-module.exports = function createAuthRoutes (keystone) {
-	authRouter.get('/session', (req, res) => {
-	  const data = {
-	    signedIn: !!req.session.keystoneItemId,
-	    userId: req.session.keystoneItemId,
-	  };
-	  res.json(data);
-	});
+module.exports = function createAuthRoutes(keystone) {
+  authRouter.get('/session', (req, res) => {
+    const data = {
+      signedIn: !!req.session.keystoneItemId,
+      userId: req.session.keystoneItemId,
+    };
+    res.json(data);
+  });
 
-	authRouter.get('/signout', async (req, res, next) => {
-		try {
-			await endAuthedSession(req);
-			res.json({
-				success: true,
-			});
-		} catch (e) {
-			next(e);
-		}
-	});
+  authRouter.get('/signout', async (req, res, next) => {
+    try {
+      await endAuthedSession(req);
+      res.json({
+        success: true,
+      });
+    } catch (e) {
+      next(e);
+    }
+  });
 
-	// Email/Password auth
-	authRouter.use('/email', createEmailAuthRoutes(keystone));
+  // Email/Password auth
+  authRouter.use('/email', createEmailAuthRoutes(keystone));
 
-	return authRouter;
+  return authRouter;
 };
--- demo-projects/meetup/site/pages/signin.js
+++ demo-projects/meetup/site/pages/signin.js
@@ -3,58 +3,58 @@ import Router from 'next/router';
 import { useAuth } from '../lib/authetication';
 
 export default () => {
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
-	const { isAuthenticated, isLoading, signin } = useAuth();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const { isAuthenticated, isLoading, signin } = useAuth();
 
-	const handleSubmit = () => {
-		event.preventDefault();
-		signin({ email, password });
-	};
+  const handleSubmit = () => {
+    event.preventDefault();
+    signin({ email, password });
+  };
 
-	// if login success - redirect to homepage
-	useEffect(() => {
-		if (isAuthenticated) {
-			Router.push('/');
-		}
-	});
+  // if login success - redirect to homepage
+  useEffect(() => {
+    if (isAuthenticated) {
+      Router.push('/');
+    }
+  });
 
-	return (
-		<>
-			<p>Sign in to continue to create, collaborate, and discover.</p>
+  return (
+    <>
+      <p>Sign in to continue to create, collaborate, and discover.</p>
 
-			<form noValidate onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="email">Email</label>
-					<input
-						required
-						type="text"
-						autoComplete="email"
-						placeholder="you@awesome.com"
-						disabled={isLoading}
-						onChange={e => setEmail(e.target.value)}
-					/>
-				</div>
-				<div>
-					<label htmlFor="password">Passwor</label>
-					<input
-						required
-						type="password"
-						id="password"
-						minLength="8"
-						placeholder="supersecret"
-						autoComplete="new-password"
-						disabled={isLoading}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
-				{/* <a href="/forgot">Forgot password?</a> */}
-				{isLoading ? (
-					<button disabled>Signing in...</button>
-				) : (
-					<button type="submit">Sign in</button>
-				)}
-			</form>
-		</>
-	);
+      <form noValidate onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            required
+            type="text"
+            autoComplete="email"
+            placeholder="you@awesome.com"
+            disabled={isLoading}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Passwor</label>
+          <input
+            required
+            type="password"
+            id="password"
+            minLength="8"
+            placeholder="supersecret"
+            autoComplete="new-password"
+            disabled={isLoading}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        {/* <a href="/forgot">Forgot password?</a> */}
+        {isLoading ? (
+          <button disabled>Signing in...</button>
+        ) : (
+          <button type="submit">Sign in</button>
+        )}
+      </form>
+    </>
+  );
 };
