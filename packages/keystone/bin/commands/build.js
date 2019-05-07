9247ae066cdb155c735bdf0ab15d81e44f746629
mitchellhamilton <mitchell@hamil.town>
2019-05-07 14:56:33 +1000

    Deduplicate a util

--- packages/keystone/bin/commands/build.js
+++ packages/keystone/bin/commands/build.js
@@ -1,21 +1,7 @@
 const path = require('path');
-const endent = require('endent');
 const fs = require('fs-extra');
 const keystone = require('@keystone-alpha/core');
-
-function getEntryFileFullPath(args, { exeName, _cwd }) {
-  const entryFile = args['--entry'] ? args['--entry'] : keystone.DEFAULT_ENTRY;
-  try {
-    return Promise.resolve(require.resolve(path.resolve(_cwd, entryFile)));
-  } catch (error) {
-    return Promise.reject(
-      new Error(endent`
-        --entry=${entryFile} was passed to ${exeName}, but '${entryFile}' couldn't be found in ${process.cwd()}.
-        Ensure you're running ${exeName} from within the root directory of the project.
-      `)
-    );
-  }
-}
+const { getEntryFileFullPath } = require('../utils');
 
 module.exports = {
   // prettier-ignore
