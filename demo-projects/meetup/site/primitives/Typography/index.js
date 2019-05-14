bcaa6ad4d3905b36da70f6334f3d5da4d20fee67
Jed Watson <jed.watson@me.com>
2019-05-14 17:17:40 +1000

    Cleaning up typography

--- demo-projects/meetup/site/primitives/Typography/index.js
+++ demo-projects/meetup/site/primitives/Typography/index.js
@@ -3,57 +3,36 @@
 import { jsx } from '@emotion/core';
 import { fontSizes } from '../../theme';
 
-export function H1({ children, ...props }) {
-  return (
-    <h1 css={{ fontSize: fontSizes.xxxl, fontWeight: 600, lineHeight: 1, margin: 0 }} {...props}>
-      {children}
-    </h1>
-  );
-}
-
-export function H2({ children, ...props }) {
-  return (
-    <h2 css={{ fontSize: fontSizes.xxl, fontWeight: 600, lineHeight: 1.15, margin: 0 }} {...props}>
-      {children}
-    </h2>
-  );
-}
-
-export function H3({ children, ...props }) {
-  return (
-    <h3 css={{ fontSize: fontSizes.xl, fontWeight: 600, lineHeight: 1.15, margin: 0 }} {...props}>
-      {children}
-    </h3>
-  );
-}
-
-export function Headline({ children, as = 'h2', size = 2, ...props }) {
+export const Headline = ({ as = 'h2', size, ...props }) => {
   const TagName = as;
+  const asSize = size ? `h${size}` : as;
   const baseStyles = { margin: 0, fontWeight: 600 };
   const fontStyles = {
-    '1': {
+    h1: {
       fontSize: fontSizes.xxxl,
       lineHeight: 1,
     },
-    '2': {
+    h2: {
       fontSize: fontSizes.xxl,
       lineHeight: 1.15,
     },
-    '3': {
+    h3: {
       fontSize: fontSizes.xl,
       lineHeight: 1.25,
     },
-    '4': {
+    h4: {
       fontSize: fontSizes.lg,
     },
-    '5': {
+    h5: {
       fontSize: fontSizes.md,
     },
   };
 
-  return (
-    <TagName css={[baseStyles, fontStyles[size]]} {...props}>
-      {children}
-    </TagName>
-  );
-}
+  return <TagName css={[baseStyles, fontStyles[asSize]]} {...props} />;
+};
+
+export const H1 = props => <Headline {...props} as="h1" />;
+export const H2 = props => <Headline {...props} as="h2" />;
+export const H3 = props => <Headline {...props} as="h3" />;
+export const H4 = props => <Headline {...props} as="h4" />;
+export const H5 = props => <Headline {...props} as="h5" />;
--- demo-projects/meetup/site/primitives/index.js
+++ demo-projects/meetup/site/primitives/index.js
@@ -2,4 +2,4 @@ export { default as Section } from './Section';
 export { default as Container } from './Container';
 export { default as Separator } from './Separator';
 export { default as Button } from './Button';
-export { default as Typography } from './Typography';
+export * from './Typography';
