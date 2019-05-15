27364ec97df147e30dc74ded52f076f27243166d
Joss Mackison <jmackison@atlassian.com>
2019-05-15 18:12:26 +1000

    fix format

--- demo-projects/meetup/site/components/GoogleAnalytics.js
+++ demo-projects/meetup/site/components/GoogleAnalytics.js
@@ -4,12 +4,12 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 const GoogleAnalytics = () => {
-	const { GA_DOMAIN = 'auto', GA_PROPERTY } = publicRuntimeConfig;
+  const { GA_DOMAIN = 'auto', GA_PROPERTY } = publicRuntimeConfig;
 
-	return GA_PROPERTY ? (
-		<>
-			<script async src="//www.google-analytics.com/analytics.js" />
-			<script>{`
+  return GA_PROPERTY ? (
+    <>
+      <script async src="//www.google-analytics.com/analytics.js" />
+      <script>{`
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -18,8 +18,8 @@ const GoogleAnalytics = () => {
 			ga('create', '${GA_PROPERTY}', '${GA_DOMAIN}');
 			ga('send', 'pageview');
 			`}</script>
-		</>
-	) : null;
+    </>
+  ) : null;
 };
 
 export default GoogleAnalytics;
