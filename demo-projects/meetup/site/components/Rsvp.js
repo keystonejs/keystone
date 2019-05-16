f42f2d14d48835d4089a00b4ed63814d7c9b1735
Sean Doyle <sean@thinkmill.com.au>
2019-05-16 16:29:28 +1000

    updated rsvp componentto not handle UI

--- demo-projects/meetup/site/components/Rsvp.js
+++ demo-projects/meetup/site/components/Rsvp.js
@@ -2,8 +2,6 @@
 import { Mutation, Query } from 'react-apollo';
 import { jsx } from '@emotion/core';
 
-import { Button, Loading, Error } from '../primitives';
-import { colors } from '../theme';
 import { useAuth } from '../lib/authetication';
 import { GET_RSVPS, UPDATE_RSVP, ADD_RSVP } from '../graphql/rsvps';
 
@@ -21,7 +19,7 @@ function validateRsvp({ userRsvps, eventRsvps, event }) {
   }
 }
 
-const Rsvp = ({ eventId }) => {
+const Rsvp = ({ eventId, children }) => {
   const { isAuthenticated, user } = useAuth();
 
   if (!isAuthenticated) {
@@ -31,68 +29,52 @@ const Rsvp = ({ eventId }) => {
   return (
     <Query query={GET_RSVPS} variables={{ event: eventId, user: user.id }}>
       {({ data, loading, error }) => {
-        if (loading && !data) return <Loading />;
-        if (error) return <Error error={error} />;
+        if (loading && !data) return children({ loading });
+        if (error) return children({ error });
 
         const { userRsvps, eventRsvps, event } = data;
 
         const errorMessage = validateRsvp({ userRsvps, eventRsvps, event });
 
         if (errorMessage) {
-          return <p css={{ color: colors.greyMedium, margin: 0 }}>{errorMessage}</p>;
+          return children({ error: errorMessage });
         }
 
         return (
           <Mutation
             mutation={userRsvps[0] ? UPDATE_RSVP : ADD_RSVP}
-            refetchQueries={({ data }) =>
-              data.updateRsvp
-                ? []
-                : [
-                    {
-                      query: GET_RSVPS,
-                      variables: { event: eventId, user: user.id },
-                    },
-                  ]
-            }
+            refetchQueries={({ data }) => {
+              if (!data.updateRsvp) {
+                return [
+                  {
+                    query: GET_RSVPS,
+                    variables: { event: eventId, user: user.id },
+                  },
+                ];
+              } else {
+                return [];
+              }
+            }}
           >
             {(updateRsvp, { error: mutationError }) => {
               if (mutationError) {
-                return <Error error={mutationError} />;
+                return children({ error: mutationError });
               }
 
-              const variables = {
-                rsvp: userRsvps[0] ? userRsvps[0].id : null,
-                event: eventId,
-                user: user.id,
-              };
+              const rsvpToEvent = status =>
+                updateRsvp({
+                  variables: {
+                    rsvp: userRsvps[0] ? userRsvps[0].id : null,
+                    event: eventId,
+                    user: user.id,
+                    status,
+                  },
+                });
 
-              const status = userRsvps[0] ? userRsvps[0].status : null;
+              const isGoing = userRsvps[0] ? userRsvps[0].status === 'yes' : false;
+              const canRsvp = eventRsvps.length < event.maxRsvps;
 
-              const isGoing = status === 'yes' ? true : false;
-              return (
-                <div>
-                  <span css={{ padding: '0 1rem' }}>Will you be attending?</span>
-                  <Button
-                    disabled={isGoing || eventRsvps.length >= event.maxRsvps}
-                    background={isGoing ? colors.purple : colors.greyLight}
-                    foreground={isGoing ? 'white' : colors.greyDark}
-                    css={{ marginLeft: '.5rem' }}
-                    onClick={() => updateRsvp({ variables: { ...variables, status: 'yes' } })}
-                  >
-                    yes
-                  </Button>
-                  <Button
-                    disabled={!isGoing}
-                    background={!isGoing ? colors.purple : colors.greyLight}
-                    foreground={!isGoing ? 'white' : colors.greyDark}
-                    css={{ marginLeft: '.5rem' }}
-                    onClick={() => updateRsvp({ variables: { ...variables, status: 'no' } })}
-                  >
-                    no
-                  </Button>
-                </div>
-              );
+              return children({ isGoing, canRsvp, rsvpToEvent });
             }}
           </Mutation>
         );
