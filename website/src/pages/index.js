cbd312d4f613a40042f1ef3de41a5385f748b537
Simon Vrachliotis <simon@simonswiss.com>
2019-05-23 17:01:17 +1000

    WIP adding egghead video on homepage

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React, { useState } from 'react'; // eslint-disable-line no-unused-vars
 import { jsx, Global } from '@emotion/core';
 import { colors, globalStyles } from '@arch-ui/theme';
 import { SkipNavContent } from '@reach/skip-nav';
@@ -11,6 +11,7 @@ import { CONTAINER_GUTTERS } from '../components/Container';
 import { HEADER_HEIGHT } from '../components/Header';
 import { media, mediaOnly, mediaMax, mq } from '../utils/media';
 import illustrationPNG from '../assets/illustration.png';
+import videoThumbnailPNG from '../assets/video-thumbnail.png';
 
 export default () => (
   <Layout>
@@ -37,6 +38,106 @@ const CustomContainer = props => (
   />
 );
 
+const VideoWrapper = props => {
+  const [showVideo, setShowVideo] = useState(false);
+  return (
+    <div
+      css={{
+        backgroundColor: '#f2f2f2',
+        backgroundImage: `url(${videoThumbnailPNG})`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        position: 'relative',
+        borderRadius: 24,
+        boxShadow: '0 8px 32px rgba(0,0,0,0.2)',
+        '&:before': {
+          content: '""',
+          display: 'block',
+          width: '100%',
+          paddingTop: '56.25%',
+        },
+      }}
+      {...props}
+    >
+      <div
+        css={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <VideoEmbed showVideo={showVideo} />
+        {!showVideo && (
+          <button
+            css={{
+              position: 'relative',
+              height: '64px',
+              width: '64px',
+              display: 'block',
+              content: '" "',
+              boxShadow: '0 24px 48px 0 rgba(0,0,0,0.32)',
+              borderRadius: 32,
+              border: 'none',
+              backgroundColor: '#fff',
+              transition: 'all 0.2s',
+              ':hover': {
+                backgroundColor: '#e7e7e7',
+              },
+              cursor: 'pointer',
+            }}
+            onClick={() => setShowVideo(true)}
+          >
+            <svg
+              width="26"
+              height="32"
+              xmlns="http://www.w3.org/2000/svg"
+              css={{ marginLeft: 10, marginTop: 6 }}
+            >
+              <path
+                css={{}}
+                d="M1.524.938l23.092 14.21a1 1 0 0 1 0 1.704L1.524 31.062A1 1 0 0 1 0 30.21V1.79A1 1 0 0 1 1.524.938z"
+                fill="#3c90ff"
+                fillRule="evenodd"
+              />
+            </svg>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const VideoEmbed = ({ showVideo }) => (
+  <div
+    css={{
+      position: 'absolute',
+      height: '100%',
+      width: '100%',
+      transition: 'opacity 0.2s',
+      opacity: showVideo ? 1 : 0,
+      iframe: {
+        width: '100%',
+        height: '100%',
+        outline: 'none',
+        border: 0,
+      },
+    }}
+  >
+    <iframe
+      css={{ display: 'block' }}
+      src="https://egghead.io/lessons/graphql-create-a-hello-world-todo-app-with-keystone-5/embed"
+      frameBorder="0"
+      allowFullScreen
+    />
+  </div>
+);
+
 const Hero = () => (
   <div css={{ overflow: 'hidden' }}>
     <SkipNavContent />
@@ -48,10 +149,10 @@ const Hero = () => (
         lineHeight: 1.6,
         minHeight: `calc(100vh - ${HEADER_HEIGHT}px)`,
 
-        [media.sm]: {
-          background: `url(${illustrationPNG}) no-repeat center right`,
-          backgroundSize: '761px 410px',
-        },
+        // [media.sm]: {
+        //   background: `url(${illustrationPNG}) no-repeat center right`,
+        //   backgroundSize: '761px 410px',
+        // },
       })}
     >
       <InnerLayout>
@@ -120,7 +221,28 @@ const Hero = () => (
           <IconSlack href="https://launchpass.com/keystonejs" target="_blank" title="Slack" />
         </div>
       </InnerLayout>
-      <LgDeviceColPlaceholder />
+      <LgDeviceColPlaceholder>
+        <div>
+          <VideoWrapper />
+          <div css={{ position: 'relative', transform: 'translateX(40px)' }}>
+            <svg
+              css={{ position: 'absolute', top: 0, left: 0 }}
+              width="35"
+              height="36"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="#000" fillRule="nonzero">
+                <path d="M34.376 36a.657.657 0 0 1-.19-.029c-.281-.088-28.038-9.063-30.184-35.31a.615.615 0 0 1 .574-.658.618.618 0 0 1 .671.562c2.079 25.42 29.046 34.157 29.318 34.244a.61.61 0 0 1 .405.77.625.625 0 0 1-.593.421z" />
+                <path d="M.623 9a.65.65 0 0 1-.31-.079.57.57 0 0 1-.229-.805L5.01 0l7.695 4.505c.292.17.383.535.202.812a.643.643 0 0 1-.859.192L5.452 1.65 1.168 8.707A.646.646 0 0 1 .623 9z" />
+              </g>
+            </svg>
+            <p css={{ paddingLeft: 54, paddingTop: 24 }}>
+              Click to see how to get a "Hello World" app up and running under 3 minutes with the
+              Keystone 5 CLI.
+            </p>
+          </div>
+        </div>
+      </LgDeviceColPlaceholder>
     </CustomContainer>
   </div>
 );
@@ -183,8 +305,10 @@ const LgDeviceColPlaceholder = props => (
       display: 'none',
 
       [media.sm]: {
-        display: 'block',
         flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       },
     }}
     {...props}
