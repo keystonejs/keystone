1372892abac7bdb1ad60723603c5844b28b863b8
mitchellhamilton <mitchell@hamil.town>
2019-05-24 21:03:43 +1000

    Add @babel/plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
     '@babel/preset-flow',
   ],
   plugins: [
+    '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
     '@babel/proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
