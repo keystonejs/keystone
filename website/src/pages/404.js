4f16d4ac9f9370ef043d8a33db9e956e33daa1f8
Jordan Overbye <jordanoverbye@gmail.com>
2019-05-24 19:37:25 +1000

    Create 404 page

---
+++ website/src/pages/404.js
@@ -0,0 +1,46 @@
+/** @jsx jsx */
+
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { jsx, Global } from '@emotion/core';
+import { globalStyles } from '@arch-ui/theme';
+import { SkipNavContent } from '@reach/skip-nav';
+
+import Layout from '../templates/layout';
+import { Container } from '../components/Container';
+import { gridSize } from '@arch-ui/theme';
+
+export default () => (
+  <Layout>
+    {() => (
+      <>
+        <Global styles={globalStyles} />
+        <NotFound />
+      </>
+    )}
+  </Layout>
+);
+
+const layoutGutter = gridSize * 4;
+
+const NotFound = () => (
+  <div>
+    <SkipNavContent />
+    <Container
+      css={{
+        paddingTop: layoutGutter,
+        paddingBottom: layoutGutter,
+      }}
+    >
+      <h1
+        css={{
+          marginTop: 0,
+          fontSize: ['2.4rem', '3.2rem'],
+          lineHeight: 1,
+        }}
+      >
+        Page Not Found
+      </h1>
+      <p>We couldn't find what you were looking for.</p>
+    </Container>
+  </div>
+);
