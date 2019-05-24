37e86fc7f3f5d9513357e9bddfe5427c031e2785
Nathan Simpson <hello@nathansimpson.design>
2019-05-24 11:46:19 +1000

    passing absolute path

--- packages/app-next/index.js
+++ packages/app-next/index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const next = require('next');
 const nextBuild = require('next/dist/build').default;
 
 class NextApp {
   constructor({ dir, nextRoutes }) {
-    this._dir = dir;
+    this._dir = path.resolve(dir);
     this._nextRoutes = nextRoutes;
   }
 
