d96fa4589eb453bb77a724411d80c5ea6f897637
Joss Mackison <jmackison@atlassian.com>
2019-05-29 18:01:16 +1000

    ensure unique meta tags per page

--- demo-projects/meetup/site/components/Meta.js
+++ demo-projects/meetup/site/components/Meta.js
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 
+import { Children } from 'react';
+import uniqBy from 'lodash.uniqby';
 import getConfig from 'next/config';
 import Head from 'next/head';
 import { jsx } from '@emotion/core';
@@ -14,26 +16,38 @@ export const makeMetaUrl = path => {
   return `${url}${path}`;
 };
 
+const logoPath = makeMetaUrl(meetup.logo.src);
+
+const rootTags = [
+  <meta dismiss property="og:url" content={meetup.siteUrl} />,
+  <meta dismiss property="og:type" content="website" />,
+  <meta dismiss property="og:locale" content="en" />,
+  <meta dismiss property="og:site_name" content={meetup.name} />,
+  <meta dismiss property="og:image" content={logoPath} />,
+  <meta dismiss property="og:image:width" content={meetup.logo.width} />,
+  <meta dismiss property="og:image:height" content={meetup.logo.height} />,
+  <meta dismiss name="twitter:site" content={meetup.twitterHandle} />,
+  <meta dismiss name="twitter:card" content="summary" />,
+  <meta dismiss name="twitter:image" content={logoPath} />,
+];
+
+function getUniqueTags(children) {
+  // NOTE: the concatenation order is important for the unique filter;
+  // we want to give `children` precedence over root tags.
+  const tags = Children.toArray(children.concat(rootTags));
+  const uniqueTags = uniqBy(tags, t => t.props.name || t.props.property);
+  return uniqueTags.map((tag, idx) => <tag.type key={tag.key || idx} {...tag.props} />);
+}
+
 export default function PageMeta({ children, description, title, titleExclusive }) {
   const titleContent = titleExclusive ? titleExclusive : title ? `${title} | ${meetup.name}` : null;
-  const logoPath = makeMetaUrl(meetup.logo.src);
+  const uniqueTags = children ? getUniqueTags(children) : rootTags;
 
   return (
     <Head>
       {titleContent && <title>{titleContent}</title>}
       {description && <meta name="description" content={description} />}
-      {children}
-
-      <meta property="og:url" content={meetup.siteUrl} />
-      <meta property="og:type" content="website" />
-      <meta property="og:locale" content="en" />
-      <meta property="og:site_name" content={meetup.name} />
-      <meta property="og:image" content={logoPath} />
-      <meta property="og:image:width" content={meetup.logo.width} />
-      <meta property="og:image:height" content={meetup.logo.height} />
-      <meta name="twitter:site" content={meetup.twitterHandle} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image" content={logoPath} />
+      {uniqueTags}
     </Head>
   );
 }
