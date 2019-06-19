3677ce836898c39fd8af78a18bf6c311d8630da8
Jess Telford <jess+github@jes.st>
2019-06-19 18:35:02 +1000

    Implement OR where filtering for json adapter

--- packages/adapter-json/index.js
+++ packages/adapter-json/index.js
@@ -3,6 +3,7 @@ const FileAsync = require('lowdb/adapters/FileAsync');
 const lowdb = require('lowdb');
 const lodashId = require('lodash-id');
 const pSettle = require('p-settle');
+const memoizeOne = require('memoize-one');
 
 const {
   BaseKeystoneAdapter,
@@ -65,6 +66,54 @@ class JSONAdapter extends BaseKeystoneAdapter {
   }
 }
 
+/**
+ * Testing with:
+ *
+ *
+mutation add {
+  first: createTodo(data: { name: "Do something" }) {
+    id
+    name
+  }
+  second: createTodo(data: { name: "Foo a thing" }) {
+    id
+    name
+  }
+}
+
+query allTodos {
+  allTodos {
+    id
+    name
+  }
+}
+
+query whereID {
+  allTodos(where: { id_not_in: ["50c571fe-c3dd-4f39-be94-5e56ecae43ac"] }) {
+    id
+    name
+  }
+}
+
+query whereName {
+  allTodos(
+    where: {
+      name_contains: "some"
+      OR: [{ name_contains: "Dont" }, { name_contains_i: "THING" }]
+    }
+  ) {
+    id
+    name
+  }
+}
+
+query onlyOR {
+  allTodos(where: { OR: [{ name_contains_i: "do" }, { name_contains_i: "foo" }] }) {
+    id
+    name
+  }
+}
+ */
 class JSONListAdapter extends BaseListAdapter {
   constructor(key, parentAdapter) {
     super(...arguments);
@@ -72,6 +121,37 @@ class JSONListAdapter extends BaseListAdapter {
     this._parentAdapter = parentAdapter;
     this.getDBCollection = parentAdapter.getLowDBInstanceForList.bind(parentAdapter, key);
     this.getListAdapterByKey = parentAdapter.getListAdapterByKey.bind(parentAdapter);
+
+    this._allQueryConditions = memoizeOne(() => {
+      const idMatcher = value => item => (value || []).includes(item.id);
+
+      const buildChainableClauses = this._buildChainableClauses.bind(this);
+
+      // prettier-ignore
+      return {
+        id:        value =>  _ =>          _.matchesProperty('id', value),
+        id_not:    value =>  _ => _.negate(_.matchesProperty('id', value)),
+
+        id_in:     value => () =>          idMatcher(value),
+        id_not_in: value =>  _ => _.negate(idMatcher(value)),
+
+        AND: values => _ => _.overEvery(
+          flatten(values.map(buildChainableClauses))
+            .map(clause => clause(_))
+        ),
+
+        OR: values => _ => _.overSome(
+          flatten(values.map(buildChainableClauses))
+            .map(clause => clause(_))
+        ),
+
+        ...objMerge(
+          this.fieldAdapters.map(fieldAdapter =>
+            fieldAdapter.getQueryConditions(fieldAdapter.dbPath)
+          )
+        ),
+      };
+    });
   }
 
   async _create(input) {
@@ -126,55 +206,56 @@ class JSONListAdapter extends BaseListAdapter {
       .value();
   }
 
-  _allQueryConditions() {
-    const idMatcher = value => item => (value || []).includes(item.id);
-
-    return {
-      id: value => (chain, _) => chain.filter(_.matchesProperty('id', value)),
-      id_not: value => (chain, _) => chain.reject(_.matchesProperty('id', value)),
-      id_in: value => chain => chain.filter(idMatcher(value)),
-      id_not_in: value => chain => chain.reject(idMatcher(value)),
-      ...objMerge(
-        this.fieldAdapters.map(fieldAdapter =>
-          fieldAdapter.getQueryConditions(fieldAdapter.dbPath)
-        )
-      ),
-    };
-  }
-
-  _buildChainableClauses(where, conditions) {
+  _buildChainableClauses(where) {
+    const conditions = this._allQueryConditions();
     // Build up a list of functions we want to chain together as AND filters
-    return flatten(Object.entries(where).map(
-      ([condition, value]) => {
-
-        if (condition === 'AND') {
-          return flatten(value.map(ANDWhere => this._buildChainableClauses(ANDWhere, conditions)));
-        }
-
-        return [conditions[condition](value)];
-      }
-    ));
+    return Object.entries(where).map(([condition, value]) => {
+      // TODO: if (isRelationship(condition)) { return buildRelationshipClause(condition, value) }
+      return conditions[condition](value);
+    });
   }
 
   _constructWhereChain(chain, where) {
     const lodash = this._parentAdapter.getLodash();
-    const conditions = this._allQueryConditions();
-    const clauses = this._buildChainableClauses(where, conditions);
-
-    return clauses.reduce(
-      (whereChain, clause) => clause(whereChain, lodash),
+    return this._buildChainableClauses(where).reduce(
+      (whereChain, clause) => whereChain.filter(clause(lodash)),
       chain
     );
   }
 
+  /**
+   * Given a where clause such as:
+   *
+   * where: {
+   *   name_contains: 'foo',
+   *   AND: [
+   *     { done: false },
+   *     { deleted: false },
+   *   ],
+   *   OR: [
+   *     { name_contains: 'bar' },
+   *     { name_contains: 'zip' },
+   *   ],
+   * }
+   *
+   * Turn this into a filter on the collection which ultimately ends up as:
+   *
+   * collection
+   *   .filter(item => new RegExp(f('foo')).test(item.name))
+   *   .filter(_.overEvery([
+   *     .filter(item => item.done === false)
+   *     .filter(item => item.deleted === false)
+   *   ]))
+   *   .filter(_.overSome([
+   *     item => new RegExp(f('bar')).test(item.name),
+   *     item => new RegExp(f('zip')).test(item.name),
+   *   ]))
+   */
   async _itemsQuery({ where, first, skip, orderBy, search }, { meta = false } = {}) {
     let chain = this.getDBCollection();
 
     if (where) {
       chain = this._constructWhereChain(chain, where);
-      // TODO
-      //debugger;
-      //throw new Error('JSONAdapter#_itemsQuery() `where` param not implemented');
     }
 
     if (search) {
@@ -221,72 +302,67 @@ class JSONFieldAdapter extends BaseFieldAdapter {
   //   `f`: (non-string methods only) A value transformation function which converts from a string type
   //        provided by graphQL into a native adapter type.
   equalityConditions(dbPath, f = identity) {
+    // prettier-ignore
     return {
-      [this.path]: value => (chain, _) => chain.filter(_.matchesProperty(dbPath, f(value))),
-      [`${this.path}_not`]: value => (chain, _) =>
-        chain.reject(_.matchesProperty(dbPath, f(value))),
+      [this.path]:          value => _ =>          _.matchesProperty(dbPath, f(value)),
+      [`${this.path}_not`]: value => _ => _.negate(_.matchesProperty(dbPath, f(value))),
     };
   }
 
   equalityConditionsInsensitive(dbPath) {
     const f = escapeRegExp;
     const matcher = value => item => new RegExp(`^${f(value)}$`, 'i').test(item[dbPath]);
+    // prettier-ignore
     return {
-      [`${this.path}_i`]: value => chain => chain.filter(matcher(value)),
-      [`${this.path}_not_i`]: value => chain => chain.reject(matcher(value)),
+      [`${this.path}_i`]:     value => () =>          matcher(value),
+      [`${this.path}_not_i`]: value =>  _ => _.negate(matcher(value)),
     };
   }
 
   inConditions(dbPath, f = identity) {
     const matcher = value => item => (value || []).map(f).includes(item[dbPath]);
+    // prettier-ignore
     return {
-      [`${this.path}_in`]: value => chain => chain.filter(matcher(value)),
-      [`${this.path}_not_in`]: value => chain => chain.reject(matcher(value)),
+      [`${this.path}_in`]:     value => () =>          matcher(value),
+      [`${this.path}_not_in`]: value =>  _ => _.negate(matcher(value)),
     };
   }
 
   orderingConditions(dbPath, f = identity) {
+    // prettier-ignore
     return {
-      [`${this.path}_lt`]: value => chain => chain.filter(item => item[dbPath] < f(value)),
-      [`${this.path}_lte`]: value => chain => chain.filter(item => item[dbPath] <= f(value)),
-      [`${this.path}_gt`]: value => chain => chain.filter(item => item[dbPath] > f(value)),
-      [`${this.path}_gte`]: value => chain => chain.filter(item => item[dbPath] >= f(value)),
+      [`${this.path}_lt`]:  value => () => item => item[dbPath] <  f(value),
+      [`${this.path}_lte`]: value => () => item => item[dbPath] <= f(value),
+      [`${this.path}_gt`]:  value => () => item => item[dbPath] >  f(value),
+      [`${this.path}_gte`]: value => () => item => item[dbPath] >= f(value),
     };
   }
 
   stringConditions(dbPath) {
     const f = escapeRegExp;
+    const regexTest = (regex, item) => new RegExp(regex).test(item[dbPath]);
+    // prettier-ignore
     return {
-      [`${this.path}_contains`]: value => chain =>
-        chain.filter(item => new RegExp(f(value)).test(item[dbPath])),
-      [`${this.path}_not_contains`]: value => chain =>
-        chain.filter(item => !new RegExp(f(value)).test(item[dbPath])),
-      [`${this.path}_starts_with`]: value => chain =>
-        chain.filter(item => new RegExp(`^${f(value)}`).test(item[dbPath])),
-      [`${this.path}_not_starts_with`]: value => chain =>
-        chain.filter(item => !new RegExp(`^${f(value)}`).test(item[dbPath])),
-      [`${this.path}_ends_with`]: value => chain =>
-        chain.filter(item => new RegExp(`${f(value)}$`).test(item[dbPath])),
-      [`${this.path}_not_ends_with`]: value => chain =>
-        chain.filter(item => !new RegExp(`${f(value)}$`).test(item[dbPath])),
+      [`${this.path}_contains`]:        value => () => item =>  regexTest(f(value), item),
+      [`${this.path}_not_contains`]:    value => () => item => !regexTest(f(value), item),
+      [`${this.path}_starts_with`]:     value => () => item =>  regexTest(`^${f(value)}`, item),
+      [`${this.path}_not_starts_with`]: value => () => item => !regexTest(`^${f(value)}`, item),
+      [`${this.path}_ends_with`]:       value => () => item =>  regexTest(`${f(value)}$`, item),
+      [`${this.path}_not_ends_with`]:   value => () => item => !regexTest(`${f(value)}$`, item),
     };
   }
 
   stringConditionsInsensitive(dbPath) {
     const f = escapeRegExp;
+    const regexTest = (regex, item) => new RegExp(regex, 'i').test(item[dbPath]);
+    // prettier-ignore
     return {
-      [`${this.path}_contains_i`]: value => chain =>
-        chain.filter(item => new RegExp(f(value), 'i').test(item[dbPath])),
-      [`${this.path}_not_contains_i`]: value => chain =>
-        chain.filter(item => !new RegExp(f(value), 'i').test(item[dbPath])),
-      [`${this.path}_starts_with_i`]: value => chain =>
-        chain.filter(item => new RegExp(`^${f(value)}`, 'i').test(item[dbPath])),
-      [`${this.path}_not_starts_with_i`]: value => chain =>
-        chain.filter(item => !new RegExp(`^${f(value)}`, 'i').test(item[dbPath])),
-      [`${this.path}_ends_with_i`]: value => chain =>
-        chain.filter(item => new RegExp(`${f(value)}$`, 'i').test(item[dbPath])),
-      [`${this.path}_not_ends_with_i`]: value => chain =>
-        chain.filter(item => !new RegExp(`${f(value)}$`, 'i').test(item[dbPath])),
+      [`${this.path}_contains_i`]:        value => () => item =>  regexTest(f(value), item),
+      [`${this.path}_not_contains_i`]:    value => () => item => !regexTest(f(value), item),
+      [`${this.path}_starts_with_i`]:     value => () => item =>  regexTest(`^${f(value)}`, item),
+      [`${this.path}_not_starts_with_i`]: value => () => item => !regexTest(`^${f(value)}`, item),
+      [`${this.path}_ends_with_i`]:       value => () => item =>  regexTest(`${f(value)}$`, item),
+      [`${this.path}_not_ends_with_i`]:   value => () => item => !regexTest(`${f(value)}$`, item),
     };
   }
 }
