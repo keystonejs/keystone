d66c262b2ec0d2d4106f5b951daeb6d9c63afd6a
Jess Telford <jess+github@jes.st>
2019-06-25 18:21:13 +1000

    Initial Unsplash search UI copied from Tici's work on the old site

--- packages/fields/src/types/Unsplash/views/blocks/unsplash-image.js
+++ packages/fields/src/types/Unsplash/views/blocks/unsplash-image.js
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { Fragment, useState, createContext, useContext } from 'react';
-import { Button } from '@arch-ui/button';
+import { Fragment, useState, useCallback, createContext, useContext } from 'react';
 import pluralize from 'pluralize';
 
 export let type = 'unsplashImage';
@@ -14,36 +13,359 @@ let Context = createContext(null);
 
 export let Provider = Context.Provider;
 
+function attributeUrl(url, { source, medium }) {
+  const attributedUrl = new URL(url);
+
+  // Adding source attribution to the URL for Unsplash to track usage
+  // https://help.unsplash.com/articles/2511315-guideline-attribution
+  if (source) {
+    attributedUrl.searchParams.set('utm_source', source);
+    if (medium) {
+      attributedUrl.searchParams.set('utm_medium', medium);
+    }
+  }
+
+  return attributedUrl;
+}
+
+const UnsplashImage = ({
+  width,
+  height,
+  unsplashId,
+  publicUrl,
+  alt,
+  user,
+  onClick,
+  unsplashUrl,
+}) => {
+  const options = useContext(Context);
+
+  const imgUrl = attributeUrl(publicUrl, options.attribution);
+  const userUrl = attributeUrl(user.url, options.attribution);
+
+  const linkStyles = {
+    color: 'white',
+    textDecoration: 'underline',
+  };
+
+  return (
+    <figure
+      css={[
+        {
+          flex: 'auto',
+          height: '250px',
+          minWidth: '150px',
+          margin: '0 10px 10px 0',
+          position: 'relative',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          overflow: 'hidden',
+        },
+        width <= height ? { width: '300px' } : { width: '100px' },
+      ]}
+      id={unsplashId}
+      onClick={onClick}
+    >
+      <img
+        src={imgUrl}
+        alt={alt}
+        css={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          verticalAlign: 'middle',
+        }}
+      />
+      <div
+        css={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          backgroundImage:
+            'linear-gradient(180deg,rgba(0,0,0,.2) 0,transparent 40%,transparent 60%,rgba(0,0,0,.3))',
+          zIndex: 1,
+          opacity: 0,
+          border: '4px solid transparent',
+          transition: 'opacity 0.3s ease-in-out',
+          '&:hover': {
+            opacity: 1,
+            border: '4px solid black',
+          },
+        }}
+      >
+        <p
+          css={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            margin: 10,
+            fontSize: 14,
+            fonWeight: 'bold',
+            color: 'white',
+          }}
+        >
+          Photo by{' '}
+          <a css={linkStyles} href={userUrl} target="_blank" rel="noopener noreferrer">
+            {user.name}
+          </a>{' '}
+          on{' '}
+          <a css={linkStyles} href={unsplashUrl} target="_blank" rel="noopener noreferrer">
+            Unsplash
+          </a>
+        </p>
+      </div>
+    </figure>
+  );
+};
+
+const Search = () => {
+  const options = useContext(Context);
+  const [loading, setLoading] = useState(false);
+  const [searchPage, setSearchPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState({
+    total: 954,
+    totalPages: 96,
+    results: [
+      {
+        id: 'abc',
+        unsplashId: 'def',
+        publicUrl: 'https://via.placeholder.com/400',
+        width: 1000 + Math.random() * 100,
+        height: 1000 + Math.random() * 100,
+        alt: 'an image',
+        user: {
+          name: 'Jess',
+          url: 'http://example.com',
+        },
+      },
+      {
+        id: 'abc',
+        unsplashId: 'def',
+        publicUrl: 'https://via.placeholder.com/400',
+        width: 1000 + Math.random() * 100,
+        height: 1000 + Math.random() * 100,
+        alt: 'an image',
+        user: {
+          name: 'Jess',
+          url: 'http://example.com',
+        },
+      },
+      {
+        id: 'abc',
+        unsplashId: 'def',
+        publicUrl: 'https://via.placeholder.com/400',
+        width: 1000 + Math.random() * 100,
+        height: 1000 + Math.random() * 100,
+        alt: 'an image',
+        user: {
+          name: 'Jess',
+          url: 'http://example.com',
+        },
+      },
+      {
+        id: 'abc',
+        unsplashId: 'def',
+        publicUrl: 'https://via.placeholder.com/400',
+        width: 1000 + Math.random() * 100,
+        height: 1000 + Math.random() * 100,
+        alt: 'an image',
+        user: {
+          name: 'Jess',
+          url: 'http://example.com',
+        },
+      },
+      {
+        id: 'abc',
+        unsplashId: 'def',
+        publicUrl: 'https://via.placeholder.com/400',
+        width: 1000 + Math.random() * 100,
+        height: 1000 + Math.random() * 100,
+        alt: 'an image',
+        user: {
+          name: 'Jess',
+          url: 'http://example.com',
+        },
+      },
+    ],
+  });
+
+  const showPrevious = useCallback(() => setSearchPage(Math.max(1, searchPage - 1)), [
+    searchPage,
+    setSearchPage,
+  ]);
+
+  const showNext = useCallback(
+    () =>
+      setSearchPage(
+        Math.min((searchResults && searchResults.totalPages) || Infinity, searchPage + 1)
+      ),
+    [searchPage, setSearchPage]
+  );
+
+  const onChange = useCallback(
+    event => {
+      event.preventDefault();
+      event.stopPropagation();
+      setSearchTerm(event.target.value);
+      if (event.target.value.length > 3) {
+        console.log('call API: ', event.target.value);
+        /*
+         *
+query searchImages {
+  searchUnsplash(query: "computer") {
+    total
+    totalPages
+    results {
+      id
+      unsplashId
+      publicUrl: publicUrlTransformed(transformation: { w: "400"})
+      width
+      height
+      alt
+      user {
+        name
+        url
+      }
+    }
+  }
+}
+        */
+      }
+    },
+    [searchTerm, setSearchTerm]
+  );
+
+  const unsplashUrl = attributeUrl('https://unsplash.com', options.attribution);
+
+  return (
+    <Fragment>
+      <label htmlFor="unsplash-block-search-input">
+        Type a keyword to search images on Unsplash
+      </label>
+      <input
+        type="text"
+        id="unsplash-block-search-input"
+        value={searchTerm}
+        onChange={onChange}
+        onClick={e => {
+          e.stopPropagation();
+        }}
+        css={{
+          flex: 10,
+          display: 'inline',
+          border: 'none',
+          backgroundColor: '#f6f6f6',
+          padding: 15,
+          fontSize: 18,
+          outline: 'none',
+          border: '3px solid #f6f6f6',
+          '&:focus': {
+            borderColor: '#bfbfbf',
+          },
+          borderRadius: '1000px',
+        }}
+      />
+      {!searchResults && !loading && (
+        <div
+          css={{
+            display: 'flex',
+            flexFlow: 'column wrap',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            padding: '40px 0',
+            color: 'darkgray',
+            border: '2px dashed #E4E4E4',
+          }}
+        >
+          <p>Images will appear here when you hit Enter</p>
+        </div>
+      )}
+      <div css={{ position: 'relative' }}>
+        {searchResults && searchResults.results.length ? (
+          <Fragment>
+            <div
+              css={{
+                display: 'flex',
+                flexFlow: 'colunm nowrap',
+                justifyContent: 'space-between',
+                padding: '20px 0',
+                width: '100%',
+                fontSize: 'smaller',
+                color: 'darkgray',
+              }}
+            >
+              <span>{searchPage > 1 && <a onClick={showPrevious}>Previous</a>}</span>
+              <span css={{ width: '100%', textAlign: 'center' }}>
+                {searchResults.total} results
+              </span>
+              <span>{searchPage < searchResults.totalPages && <a onClick={showNext}>Next</a>}</span>
+            </div>
+            <div
+              css={{
+                margin: 0,
+                padding: 0,
+                display: 'flex',
+                flexFlow: 'row wrap',
+                justifyContent: 'space-evenly',
+                width: 'calc(100% + 10px)',
+              }}
+            >
+              {searchResults.results.map(image => (
+                <UnsplashImage
+                  key={image.unsplashId}
+                  {...image}
+                  unsplashUrl={unsplashUrl}
+                  onClick={() => console.log('image clicked:', image.unsplashId)}
+                />
+              ))}
+            </div>
+          </Fragment>
+        ) : null}
+        {loading && (
+          <div
+            css={
+              searchResults
+                ? {
+                    position: 'absolute',
+                    height: 50,
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    backgroundColor: 'white',
+                  }
+                : {
+                    position: 'relative',
+                    height: 300,
+                  }
+            }
+          >
+            Loading...
+          </div>
+        )}
+      </div>
+    </Fragment>
+  );
+};
+
 let Block = ({ unsplashId, unsplashData, onChange, onRemove }) => {
-  let [currentValue, setCurrentValue] = useState(unsplashId);
   let options = useContext(Context);
 
   let unsplash = null;
 
   if (unsplashData) {
-    const { source, medium } = options.attribution;
-    const { publicUrl, description, user } = unsplashData;
+    const { publicUrl, alt, user } = unsplashData;
 
-    const imgUrl = new URL(publicUrl);
-    const userUrl = new URL(user.url);
-    const unsplashUrl = new URL('https://unsplash.com');
-
-    // Adding source attribution to the URL for Unsplash to track usage
-    // https://help.unsplash.com/articles/2511315-guideline-attribution
-    if (source) {
-      imgUrl.searchParams.set('utm_source', source);
-      userUrl.searchParams.set('utm_source', source);
-      unsplashUrl.searchParams.set('utm_source', source);
-      if (medium) {
-        imgUrl.searchParams.set('utm_medium', medium);
-        userUrl.searchParams.set('utm_medium', medium);
-        unsplashUrl.searchParams.set('utm_medium', medium);
-      }
-    }
+    const imgUrl = attributeUrl(publicUrl, options.attribution);
+    const userUrl = attributeUrl(user.url, options.attribution);
+    const unsplashUrl = attributeUrl('https://unsplash.com', options.attribution);
 
     unsplash = (
       <div>
-        <img css={{ maxWidth: '100%' }} src={imgUrl} alt={description} />
+        <img css={{ maxWidth: '100%' }} src={imgUrl} alt={alt} />
         <em>
           Photo by <a href={userUrl}>{user.name}</a> on <a href={unsplashUrl}>Unsplash</a>
         </em>
@@ -53,47 +375,7 @@ let Block = ({ unsplashId, unsplashData, onChange, onRemove }) => {
 
   return (
     <Fragment>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          onChange(currentValue);
-        }}
-      >
-        <div css={{ width: '100%', display: 'flex' }}>
-          <input
-            type="text"
-            placeholder="Enter an Unsplash Image ID and press enter"
-            css={{
-              flex: 10,
-              display: 'inline',
-              border: 'none',
-              backgroundColor: 'transparent',
-              outline: 'none',
-              paddingTop: 8,
-              paddingBottom: 8,
-              fontSize: 18,
-            }}
-            onClick={e => {
-              e.stopPropagation();
-            }}
-            value={currentValue}
-            onChange={e => {
-              setCurrentValue(e.target.value);
-            }}
-          />
-          <div
-            css={{
-              display: 'inline-flex',
-              alignItems: 'center',
-              padding: 8,
-            }}
-          >
-            <Button appearance="danger" onClick={onRemove} type="button">
-              Remove
-            </Button>
-          </div>
-        </div>
-      </form>
+      <Search />
       {unsplash}
     </Fragment>
   );
@@ -111,6 +393,7 @@ export function Sidebar({ editor }) {
     </button>
   );
 }
+
 export function Node({ node, editor }) {
   return (
     <Block
