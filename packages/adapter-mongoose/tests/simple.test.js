1d7f550253fe7dc39dc345fa2aca0619d5a0a663
John Molomby <john@thinkmill.com.au>
2019-07-12 17:50:18 +1000

    Updating the Mongoose list adapter so it doesn't add an id field itself but instead refer to the MongoId type. Removing exceptional/hardcoded `id` related query conditions from the tokenizer and related tests. Adding default primary key config to the MongoID type and hacking it up to implicitly support the `_id` path when operating as a primary key and to cast back and forth between native ObjectIds.

--- packages/adapter-mongoose/tests/simple.test.js
+++ packages/adapter-mongoose/tests/simple.test.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const tokenizerFactory = require('../lib/tokenizers/simple');
 
 describe('Simple tokenizer', () => {
@@ -64,29 +63,5 @@ describe('Simple tokenizer', () => {
     expect(getQueryConditions).toHaveBeenCalledTimes(1);
     expect(nameConditions).toHaveBeenCalledTimes(1);
     expect(nameConditions).toHaveBeenCalledWith('hi', { name: 'hi' });
-  });
-
-  test('Correctly handles id query keys', () => {
-    const nameConditions = jest.fn(() => ({ foo: 'bar' }));
-    const simpleConditions = { name: nameConditions };
-    const getQueryConditions = jest.fn(() => simpleConditions);
-    const getRelatedListAdapterFromQueryPath = jest.fn(() => ({
-      fieldAdapters: [{ getQueryConditions }],
-    }));
-
-    const simple = tokenizerFactory({ getRelatedListAdapterFromQueryPath });
-
-    expect(simple({ id: '123412341234' }, 'id', ['name'])).toMatchObject({
-      matchTerm: { _id: { $eq: mongoose.Types.ObjectId('123412341234') } },
-    });
-    expect(simple({ id_not: '123412341234' }, 'id_not', ['name'])).toMatchObject({
-      matchTerm: { _id: { $ne: mongoose.Types.ObjectId('123412341234') } },
-    });
-    expect(simple({ id_in: ['123412341234'] }, 'id_in', ['name'])).toMatchObject({
-      matchTerm: { _id: { $in: [mongoose.Types.ObjectId('123412341234')] } },
-    });
-    expect(simple({ id_not_in: ['123412341234'] }, 'id_not_in', ['name'])).toMatchObject({
-      matchTerm: { _id: { $not: { $in: [mongoose.Types.ObjectId('123412341234')] } } },
-    });
   });
 });
