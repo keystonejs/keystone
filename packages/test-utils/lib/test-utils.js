EvilCat108/2ACCELERATE-keystone-5

JSON & Memory Database Adapters

packages/test-utils/lib/test-utils.js
@@ -123,10 +123,31 @@ function keystoneKnexTest(setupKeystoneFn, testFn) {
   };
 }
 
+function keystoneJSONTest(setupKeystoneFn, testFn) {
+  return async function() {
+    const setup = await setupKeystoneFn('json');
+    const { keystone } = setup;
+
+    await keystone.connect();
+
+    return pFinally(
+      testFn({
+        ...setup,
+        create: getCreate(keystone),
+        findById: getFindById(keystone),
+        findOne: getFindOne(keystone),
+        update: getUpdate(keystone),
+      }),
+      () => keystone.disconnect()
+    );
+  };
+}
+
 function multiAdapterRunners() {
   return [
     { runner: keystoneMongoTest, adapterName: 'mongoose' },
     { runner: keystoneKnexTest, adapterName: 'knex' },
+    { runner: keystoneJSONTest, adapterName: 'json' },
   ];
 }
 

keystonejs/keystone

packages/test-utils/lib/test-utils.js
@@ -130,10 +130,32 @@ function keystoneKnexRunner(setupKeystoneFn, testFn) {
   };
 }
 
+function keystoneJSONRunner(setupKeystoneFn, testFn) {
+  return async function() {
+    const setup = await setupKeystoneFn('json');
+    const { keystone } = setup;
+
+    await keystone.connect();
+
+    return pFinally(
+      testFn({
+        ...setup,
+        create: getCreate(keystone),
+        findById: getFindById(keystone),
+        findOne: getFindOne(keystone),
+        update: getUpdate(keystone),
+      }),
+      () => keystone.disconnect()
+    );
+  };
+}
+
+
 function multiAdapterRunners(only) {
   return [
     { runner: keystoneMongoRunner, adapterName: 'mongoose' },
     { runner: keystoneKnexRunner, adapterName: 'knex' },
+    { runner: keystoneJSONRunner, adapterName: 'json' },
   ].filter(a => typeof only === 'undefined' || a.adapterName === only);
 }
 