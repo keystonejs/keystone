9da0c85f9179f794ab7ed9d0f0d71ec06919cdfe
Gautam Singh <gautamsi@users.noreply.github.com>
2019-08-28 19:26:56 +0530

    us apollo hooks  in blog demo

--- demo-projects/blog/app/pages/post/[slug].js
+++ demo-projects/blog/app/pages/post/[slug].js
@@ -2,8 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 import gql from 'graphql-tag';
-import React from 'react';
-import { Mutation, Query } from 'react-apollo';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useState } from 'react';
 
 import { jsx } from '@emotion/core';
@@ -119,150 +118,139 @@ const Comments = ({ data }) => (
 );
 
 const AddComments = ({ users, post }) => {
+  const [createComment] = useMutation(ADD_COMMENT, {
+    update: (cache, { data: data }) => {
+      const { allComments, allUsers, allPosts } = cache.readQuery({
+        query: ALL_QUERIES,
+        variables: { slug: post.slug },
+      });
+
+      cache.writeQuery({
+        query: ALL_QUERIES,
+        variables: { slug: post.slug },
+        data: {
+          allPosts,
+          allUsers,
+          allComments: allComments.concat([data.createComment]),
+        },
+      });
+    },
+  });
   let user = users.filter(u => u.email == 'user@keystonejs.com')[0];
   let [comment, setComment] = useState('');
 
   return (
     <div>
       <h2>Add new Comment</h2>
-      <Mutation
-        mutation={ADD_COMMENT}
-        update={(cache, { data: data }) => {
-          const { allComments, allUsers, allPosts } = cache.readQuery({
-            query: ALL_QUERIES,
-            variables: { slug: post.slug },
-          });
+      <form
+        onSubmit={e => {
+          e.preventDefault();
 
-          cache.writeQuery({
-            query: ALL_QUERIES,
-            variables: { slug: post.slug },
-            data: {
-              allPosts,
-              allUsers,
-              allComments: allComments.concat([data.createComment]),
+          createComment({
+            variables: {
+              body: comment,
+              author: user.id,
+              postId: post.id,
+              posted: new Date(),
             },
           });
+
+          setComment('');
         }}
       >
-        {createComment => (
-          <form
-            onSubmit={e => {
-              e.preventDefault();
+        <textarea
+          type="text"
+          placeholder="Write a comment"
+          name="comment"
+          css={{
+            padding: 12,
+            fontSize: 16,
+            width: '100%',
+            height: 60,
+            border: 0,
+            borderRadius: 6,
+            resize: 'none',
+          }}
+          value={comment}
+          onChange={event => {
+            setComment(event.target.value);
+          }}
+        />
 
-              createComment({
-                variables: {
-                  body: comment,
-                  author: user.id,
-                  postId: post.id,
-                  posted: new Date(),
-                },
-              });
-
-              setComment('');
-            }}
-          >
-            <textarea
-              type="text"
-              placeholder="Write a comment"
-              name="comment"
-              css={{
-                padding: 12,
-                fontSize: 16,
-                width: '100%',
-                height: 60,
-                border: 0,
-                borderRadius: 6,
-                resize: 'none',
-              }}
-              value={comment}
-              onChange={event => {
-                setComment(event.target.value);
-              }}
-            />
-
-            <input
-              type="submit"
-              value="Submit"
-              css={{
-                padding: '6px 12px',
-                borderRadius: 6,
-                background: 'hsl(200, 20%, 50%)',
-                fontSize: '1em',
-                color: 'white',
-                border: 0,
-                marginTop: 6,
-              }}
-            />
-          </form>
-        )}
-      </Mutation>
+        <input
+          type="submit"
+          value="Submit"
+          css={{
+            padding: '6px 12px',
+            borderRadius: 6,
+            background: 'hsl(200, 20%, 50%)',
+            fontSize: '1em',
+            color: 'white',
+            border: 0,
+            marginTop: 6,
+          }}
+        />
+      </form>
     </div>
   );
 };
 
-class PostPage extends React.Component {
-  static getInitialProps({ query: { slug } }) {
-    return { slug };
-  }
-  render() {
-    const { slug } = this.props;
-    return (
-      <Layout>
-        <Header />
-        <div css={{ margin: '48px 0' }}>
-          <Link href="/" passHref>
-            <a css={{ color: 'hsl(200,20%,50%)', cursor: 'pointer' }}>{'< Go Back'}</a>
-          </Link>
-          <Query query={ALL_QUERIES} variables={{ slug }}>
-            {({ data, loading, error }) => {
-              if (loading) return <p>loading...</p>;
-              if (error) return <p>Error!</p>;
+const PostPage = ({ slug }) => {
+  const { data, loading, error } = useQuery(ALL_QUERIES, { variables: { slug } });
+  const post = data.allPosts && data.allPosts[0];
+  return (
+    <Layout>
+      <Header />
+      <div css={{ margin: '48px 0' }}>
+        <Link href="/" passHref>
+          <a css={{ color: 'hsl(200,20%,50%)', cursor: 'pointer' }}>{'< Go Back'}</a>
+        </Link>
+        {loading ? (
+          <p>loading...</p>
+        ) : error ? (
+          <p>Error!</p>
+        ) : post ? (
+          <>
+            <div
+              css={{
+                background: 'white',
+                margin: '24px 0',
+                boxShadow: '0px 10px 20px hsla(200, 20%, 20%, 0.20)',
+                marginBottom: 32,
+                borderRadius: 6,
+                overflow: 'hidden',
+              }}
+            >
+              <Head>
+                <title>{post.title}</title>
+              </Head>
+              {post.image ? <img src={post.image.publicUrl} css={{ width: '100%' }} /> : null}
+              <article css={{ padding: '1em' }}>
+                <h1 css={{ marginTop: 0 }}>{post.title}</h1>
+                <section dangerouslySetInnerHTML={{ __html: post.body }} />
+                <div css={{ marginTop: '1em', borderTop: '1px solid hsl(200, 20%, 80%)' }}>
+                  <p css={{ fontSize: '0.8em', marginBottom: 0, color: 'hsl(200, 20%, 50%)' }}>
+                    Posted by {post.author ? post.author.name : 'someone'} on{' '}
+                    {format(post.posted, 'DD/MM/YYYY')}
+                  </p>
+                </div>
+              </article>
+            </div>
 
-              const post = data.allPosts && data.allPosts[0];
+            <Comments data={data} />
 
-              if (!post) return <p>404: Post not found</p>;
+            <AddComments post={post} users={data.allUsers} />
+          </>
+        ) : (
+          <p>404: Post not found</p>
+        )}
+      </div>
+    </Layout>
+  );
+};
 
-              return (
-                <>
-                  <div
-                    css={{
-                      background: 'white',
-                      margin: '24px 0',
-                      boxShadow: '0px 10px 20px hsla(200, 20%, 20%, 0.20)',
-                      marginBottom: 32,
-                      borderRadius: 6,
-                      overflow: 'hidden',
-                    }}
-                  >
-                    <Head>
-                      <title>{post.title}</title>
-                    </Head>
-                    {post.image ? <img src={post.image.publicUrl} css={{ width: '100%' }} /> : null}
-                    <article css={{ padding: '1em' }}>
-                      <h1 css={{ marginTop: 0 }}>{post.title}</h1>
-                      <section dangerouslySetInnerHTML={{ __html: post.body }} />
-                      <div css={{ marginTop: '1em', borderTop: '1px solid hsl(200, 20%, 80%)' }}>
-                        <p
-                          css={{ fontSize: '0.8em', marginBottom: 0, color: 'hsl(200, 20%, 50%)' }}
-                        >
-                          Posted by {post.author ? post.author.name : 'someone'} on{' '}
-                          {format(post.posted, 'DD/MM/YYYY')}
-                        </p>
-                      </div>
-                    </article>
-                  </div>
-
-                  <Comments data={data} />
-
-                  <AddComments post={post} users={data.allUsers} />
-                </>
-              );
-            }}
-          </Query>
-        </div>
-      </Layout>
-    );
-  }
-}
+PostPage.getInitialProps = ({ query: { slug } }) => {
+  return { slug };
+};
 
 export default PostPage;
--- demo-projects/blog/app/pages/post/new.js
+++ demo-projects/blog/app/pages/post/new.js
@@ -3,7 +3,7 @@ import { jsx } from '@emotion/core';
 import Link from 'next/link';
 
 import gql from 'graphql-tag';
-import { Mutation, Query } from 'react-apollo';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useState } from 'react';
 
 import styled from '@emotion/styled';
@@ -52,6 +52,16 @@ const ADD_POST = gql`
   }
 `;
 
+const ADMIN_USERS = gql`
+  {
+    allUsers(where: { isAdmin: true }) {
+      name
+      email
+      id
+    }
+  }
+`;
+
 export default () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
@@ -59,7 +69,13 @@ export default () => {
   const [authorId, setAuthorId] = useState('');
   const [showBanner, setShowBanner] = useState(false);
   const [slug, setSlug] = useState('');
-
+  const { data, loading, error } = useQuery(ADMIN_USERS);
+  const [createPost] = useMutation(ADD_POST, {
+    update: (cache, { data: { createPost } }) => {
+      setSlug(createPost.slug);
+      setShowBanner(true);
+    },
+  });
   return (
     <Layout>
       <div css={{ margin: '48px 0' }}>
@@ -94,121 +110,93 @@ export default () => {
           </div>
         )}
 
-        <Query
-          query={gql`
-            {
-              allUsers(where: { isAdmin: true }) {
-                name
-                email
-                id
-              }
-            }
-          `}
-        >
-          {({ data, loading, error }) => {
-            if (loading) return <p>loading...</p>;
-            if (error) return <p>Error!</p>;
+        {loading ? (
+          <p>loading...</p>
+        ) : error ? (
+          <p>Error!</p>
+        ) : (
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              createPost({
+                variables: {
+                  title,
+                  body,
+                  image,
+                  authorId: authorId || data.allUsers[0].id,
+                  posted: new Date(),
+                },
+              });
 
-            return (
-              <Mutation
-                mutation={ADD_POST}
-                update={(cache, { data: { createPost } }) => {
-                  setSlug(createPost.slug);
-                  setShowBanner(true);
+              setTitle('');
+              setBody('');
+            }}
+          >
+            <FormGroup>
+              <Label htmlFor="title">Title:</Label>
+              <Input
+                type="text"
+                name="title"
+                value={title}
+                onChange={event => {
+                  setTitle(event.target.value);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="body">Body:</Label>
+              <textarea
+                css={{
+                  width: '100%',
+                  padding: 8,
+                  fontSize: '1em',
+                  borderRadius: 4,
+                  border: '1px solid hsl(200,20%,70%)',
+                  height: 200,
+                  resize: 'none',
+                }}
+                name="body"
+                value={body}
+                onChange={event => {
+                  setBody(event.target.value);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="image">Image URL:</Label>
+              <Input
+                type="file"
+                name="image"
+                // value={image}
+                onChange={event => {
+                  setImage(event.target.files[0]);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="admin">Post as:</Label>
+              <select
+                name="admin"
+                css={{
+                  width: '100%',
+                  height: 32,
+                  fontSize: '1em',
+                  borderRadius: 4,
+                  border: '1px solid hsl(200,20%,70%)',
+                }}
+                value={authorId}
+                onSelect={event => {
+                  setAuthorId(event.target.value);
                 }}
               >
-                {createPost => {
-                  return (
-                    <form
-                      onSubmit={e => {
-                        e.preventDefault();
-                        createPost({
-                          variables: {
-                            title,
-                            body,
-                            image,
-                            authorId: authorId || data.allUsers[0].id,
-                            posted: new Date(),
-                          },
-                        });
-
-                        setTitle('');
-                        setBody('');
-                      }}
-                    >
-                      <FormGroup>
-                        <Label htmlFor="title">Title:</Label>
-                        <Input
-                          type="text"
-                          name="title"
-                          value={title}
-                          onChange={event => {
-                            setTitle(event.target.value);
-                          }}
-                        />
-                      </FormGroup>
-                      <FormGroup>
-                        <Label htmlFor="body">Body:</Label>
-                        <textarea
-                          css={{
-                            width: '100%',
-                            padding: 8,
-                            fontSize: '1em',
-                            borderRadius: 4,
-                            border: '1px solid hsl(200,20%,70%)',
-                            height: 200,
-                            resize: 'none',
-                          }}
-                          name="body"
-                          value={body}
-                          onChange={event => {
-                            setBody(event.target.value);
-                          }}
-                        />
-                      </FormGroup>
-                      <FormGroup>
-                        <Label htmlFor="image">Image URL:</Label>
-                        <Input
-                          type="file"
-                          name="image"
-                          // value={image}
-                          onChange={event => {
-                            setImage(event.target.files[0]);
-                          }}
-                        />
-                      </FormGroup>
-                      <FormGroup>
-                        <Label htmlFor="admin">Post as:</Label>
-                        <select
-                          name="admin"
-                          css={{
-                            width: '100%',
-                            height: 32,
-                            fontSize: '1em',
-                            borderRadius: 4,
-                            border: '1px solid hsl(200,20%,70%)',
-                          }}
-                          value={authorId}
-                          onSelect={event => {
-                            setAuthorId(event.target.value);
-                          }}
-                        >
-                          {data.allUsers.map(user => (
-                            <option
-                              value={user.id}
-                              key={user.id}
-                            >{`${user.name} <${user.email}>`}</option>
-                          ))}
-                        </select>
-                      </FormGroup>
-                      <input type="submit" value="submit" />
-                    </form>
-                  );
-                }}
-              </Mutation>
-            );
-          }}
-        </Query>
+                {data.allUsers.map(user => (
+                  <option value={user.id} key={user.id}>{`${user.name} <${user.email}>`}</option>
+                ))}
+              </select>
+            </FormGroup>
+            <input type="submit" value="submit" />
+          </form>
+        )}
       </div>
     </Layout>
   );
