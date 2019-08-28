d80c641d3bf5285a36f08406fd5db6480cfa21e2
Gautam Singh <gautamsi@users.noreply.github.com>
2019-08-28 14:43:58 +0530

    formatting

--- packages/app-admin-ui/client/pages/Item/index.js
+++ packages/app-admin-ui/client/pages/Item/index.js
@@ -414,6 +414,8 @@ const ItemPage = ({ list, itemId, adminPath, getListByKey }) => {
     }
   );
 
+  // If the views load before the API request comes back, keep showing
+  // the loading component
   if (loading) {
     return <PageLoading />;
   }
@@ -448,9 +450,7 @@ const ItemPage = ({ list, itemId, adminPath, getListByKey }) => {
           .filter(({ maybeAccess }) => !!maybeAccess.update)
           .map(field => () => field.initFieldView())
       )}
-      {// If the views load before the API request comes back, keep showing
-      // the loading component
-      item ? (
+      {item ? (
         <main>
           <DocTitle>
             {item._label_} - {list.singular}
