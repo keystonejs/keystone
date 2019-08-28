cba6f61e9a7648e72b521a2ec3528fc43329b7ef
Gautam Singh <gautamsi@users.noreply.github.com>
2019-08-29 02:25:59 +0530

    replace remaining Mutation with useMutation hook

--- packages/app-admin-ui/client/components/DeleteItemModal.js
+++ packages/app-admin-ui/client/components/DeleteItemModal.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Mutation } from 'react-apollo';
+import { useMutation } from '@apollo/react-hooks';
 import { Button } from '@arch-ui/button';
 import Confirm from '@arch-ui/confirm';
 
@@ -14,45 +14,40 @@ type Props = {
 };
 
 export default function DeleteItemModal({ isOpen, item, list, onClose, onDelete }: Props) {
+  const [deleteItem, { loading }] = useMutation(list.deleteMutation);
   return (
-    <Mutation mutation={list.deleteMutation}>
-      {(deleteItem, { loading }) => {
-        return (
-          <Confirm
-            isOpen={isOpen}
-            onKeyDown={e => {
-              if (e.key === 'Escape' && !loading) {
-                onClose();
-              }
-            }}
-          >
-            <p style={{ marginTop: 0 }}>
-              Are you sure you want to delete <strong>{item._label_}</strong>?
-            </p>
-            <footer>
-              <Button
-                appearance="danger"
-                variant="ghost"
-                onClick={() => {
-                  if (loading) return;
-                  onDelete(deleteItem({ variables: { id: item.id } }));
-                }}
-              >
-                Delete
-              </Button>
-              <Button
-                variant="subtle"
-                onClick={() => {
-                  if (loading) return;
-                  onClose();
-                }}
-              >
-                Cancel
-              </Button>
-            </footer>
-          </Confirm>
-        );
+    <Confirm
+      isOpen={isOpen}
+      onKeyDown={e => {
+        if (e.key === 'Escape' && !loading) {
+          onClose();
+        }
       }}
-    </Mutation>
+    >
+      <p style={{ marginTop: 0 }}>
+        Are you sure you want to delete <strong>{item._label_}</strong>?
+      </p>
+      <footer>
+        <Button
+          appearance="danger"
+          variant="ghost"
+          onClick={() => {
+            if (loading) return;
+            onDelete(deleteItem({ variables: { id: item.id } }));
+          }}
+        >
+          Delete
+        </Button>
+        <Button
+          variant="subtle"
+          onClick={() => {
+            if (loading) return;
+            onClose();
+          }}
+        >
+          Cancel
+        </Button>
+      </footer>
+    </Confirm>
   );
 }
