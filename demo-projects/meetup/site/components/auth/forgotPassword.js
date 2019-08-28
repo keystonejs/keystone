5386ed649c1219e3c333bae0ff8059c5e6462d31
Gautam Singh <gautamsi@users.noreply.github.com>
2019-08-29 04:38:03 +0530

    complete demo projects with hooks instead of queries update apollo-boost for latest apollo hooks.

--- demo-projects/meetup/site/components/auth/forgotPassword.js
+++ demo-projects/meetup/site/components/auth/forgotPassword.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { useState, useEffect } from 'react';
-import { Mutation } from 'react-apollo';
+import { useMutation } from '@apollo/react-hooks';
 import Router from 'next/router';
 import { jsx } from '@emotion/core';
 import gql from 'graphql-tag';
@@ -10,7 +10,7 @@ import { useAuth } from '../../lib/authetication';
 import { Button, Field, Group, Label, Link, Input } from '../../primitives/forms';
 import { gridSize, colors } from '../../theme';
 
-export const CREATE_FOGOT_PASSWORD_TOKEN = gql`
+export const CREATE_FORGOT_PASSWORD_TOKEN = gql`
   mutation startPasswordRecovery($email: String!) {
     startPasswordRecovery(email: $email) {
       id
@@ -34,62 +34,58 @@ export default ({ onSuccess, onClickSignin }) => {
       Router.push('/');
     }
   }, [isAuthenticated]);
-
-  return (
-    <Mutation
-      mutation={CREATE_FOGOT_PASSWORD_TOKEN}
-      onCompleted={() => {
+  const [startPasswordRecovery, { error: mutationError, loading }] = useMutation(
+    CREATE_FORGOT_PASSWORD_TOKEN,
+    {
+      onCompleted: () => {
         setEmailSent(true);
 
         if (onSuccess && typeof onSuccess === 'function') {
           onSuccess();
         }
-      }}
-    >
-      {(startPasswordRecovery, { error: mutationError, loading }) => {
-        return (
-          <>
-            {mutationError && (
-              <p css={{ color: colors.red }}>There is no account with the email "{email}"</p>
-            )}
+      },
+    }
+  );
+  return (
+    <>
+      {mutationError && (
+        <p css={{ color: colors.red }}>There is no account with the email "{email}"</p>
+      )}
 
-            <form
-              css={{ marginTop: gridSize * 3 }}
-              noValidate
-              onSubmit={handleSubmit(startPasswordRecovery)}
-            >
-              <Field>
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  required
-                  type="text"
-                  autoFocus
-                  autoComplete="email"
-                  placeholder="you@awesome.com"
-                  disabled={isAuthenticated}
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </Field>
+      <form
+        css={{ marginTop: gridSize * 3 }}
+        noValidate
+        onSubmit={handleSubmit(startPasswordRecovery)}
+      >
+        <Field>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            required
+            type="text"
+            autoFocus
+            autoComplete="email"
+            placeholder="you@awesome.com"
+            disabled={isAuthenticated}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Field>
 
-              <Group>
-                {loading ? (
-                  <Button disabled>Sending email...</Button>
-                ) : emailSent ? (
-                  <Button disabled css={{ background: colors.greyLight, color: colors.greyMedium }}>
-                    Email sent
-                  </Button>
-                ) : (
-                  <Button type="submit">Send</Button>
-                )}
-                <Link href="/signin" onClick={onClickSignin}>
-                  Sign in
-                </Link>
-              </Group>
-            </form>
-          </>
-        );
-      }}
-    </Mutation>
+        <Group>
+          {loading ? (
+            <Button disabled>Sending email...</Button>
+          ) : emailSent ? (
+            <Button disabled css={{ background: colors.greyLight, color: colors.greyMedium }}>
+              Email sent
+            </Button>
+          ) : (
+            <Button type="submit">Send</Button>
+          )}
+          <Link href="/signin" onClick={onClickSignin}>
+            Sign in
+          </Link>
+        </Group>
+      </form>
+    </>
   );
 };
