keystonejs/keystone

commit 30f6b7ebc4f733d8ca3188db93f1395bcce628a0

upgrade react-toast-notifications version (with hooks) (#1544)
* upgrade react-toast-notifications
use useToasts hook when possible.

* add changeset

packages/app-admin-ui/client/pages/Item/index.js
@@ -4,7 +4,7 @@ import { Component, Fragment, Suspense, useMemo, useCallback } from 'react';
 import styled from '@emotion/styled';
 import { Mutation, Query } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
-import { withToastManager } from 'react-toast-notifications';
+import { useToasts } from 'react-toast-notifications';
 import memoizeOne from 'memoize-one';
 
 import { Container } from '@arch-ui/layout';
@@ -149,7 +149,12 @@ const ItemDetails = withRouter(
         return;
       }
 
-      const { onUpdate, toastManager, updateItem, item: initialData } = this.props;
+      const {
+        onUpdate,
+        toastManager: { addToast },
+        updateItem,
+        item: initialData,
+      } = this.props;
 
       const fieldsObject = this.getFieldsObject();
 
@@ -191,7 +196,7 @@ const ItemDetails = withRouter(
             messages.push(`${totalWarnings} warning${totalWarnings > 1 ? 's' : ''}`);
           }
 
-          toastManager.add(`Validation failed: ${messages.join(' and ')}.`, {
+          addToast(`Validation failed: ${messages.join(' and ')}.`, {
             autoDismiss: true,
             appearance: errors.length ? 'error' : 'warning',
           });
@@ -214,7 +219,7 @@ const ItemDetails = withRouter(
             </div>
           );
 
-          toastManager.add(toastContent, {
+          addToast(toastContent, {
             autoDismiss: true,
             appearance: 'success',
           });
@@ -373,8 +378,9 @@ const ItemNotFound = ({ adminPath, errorMessage, list }) => (
   </PageError>
 );
 
-const ItemPage = ({ list, itemId, adminPath, getListByKey, toastManager }) => {
+const ItemPage = ({ list, itemId, adminPath, getListByKey }) => {
   const itemQuery = list.getItemQuery(itemId);
+  const { addToast } = useToasts();
   return (
     <Suspense fallback={<PageLoading />}>
       {/* network-only because the data we mutate with is important for display
@@ -433,7 +439,7 @@ const ItemPage = ({ list, itemId, adminPath, getListByKey, toastManager }) => {
                       updateError.message
                     );
 
-                    toastManager.add(toastContent, {
+                    addToast(toastContent, {
                       appearance: 'error',
                     });
                   }}
@@ -450,7 +456,7 @@ const ItemPage = ({ list, itemId, adminPath, getListByKey, toastManager }) => {
                         onUpdate={() =>
                           refetch().then(refetchedData => deserializeItem(list, refetchedData.data))
                         }
-                        toastManager={toastManager}
+                        toastManager={{ addToast }}
                         updateInProgress={updateInProgress}
                         updateErrorMessage={updateError && updateError.message}
                         updateItem={updateItem}
@@ -469,4 +475,4 @@ const ItemPage = ({ list, itemId, adminPath, getListByKey, toastManager }) => {
   );
 };
 
-export default withToastManager(ItemPage);
+export default ItemPage;

demo-projects/meetup/site/components/AvatarUpload.js
@@ -4,14 +4,16 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from 'react-apollo-hooks';
+import { useToasts } from 'react-toast-notifications';
 import { Avatar } from './Avatar';
 
 const validImageTypes = 'image/gif, image/jpeg, image/jpg, image/png';
 
-export const AvatarUpload = ({ userId, size, toastManager }) => {
+export const AvatarUpload = ({ userId, size }) => {
   const [loadingImage, setLoadingImage] = useState(false);
   const updateAvatar = useMutation(UPDATE_AVATAR);
   const { data, loading } = useQuery(USER, { variables: { userId } });
+  const { addToast } = useToasts();
   if (loading) return null;
 
   const user = data.User;
@@ -23,7 +25,7 @@ export const AvatarUpload = ({ userId, size, toastManager }) => {
 
     // Validate file type
     if (!file.type || validImageTypes.indexOf(file.type) === -1) {
-      toastManager.add('Please provide a valid image type: GIF, JPG, or PNG.', {
+      addToast('Please provide a valid image type: GIF, JPG, or PNG.', {
         appearance: 'error',
         autoDismiss: true,
       });
@@ -32,7 +34,7 @@ export const AvatarUpload = ({ userId, size, toastManager }) => {
 
     // Validate file size
     if (file.size > 5 * 1024 * 1024) {
-      toastManager.add('Maximum image size is 5MB.', {
+      addToast('Maximum image size is 5MB.', {
         appearance: 'error',
         autoDismiss: true,
       });
@@ -48,7 +50,7 @@ export const AvatarUpload = ({ userId, size, toastManager }) => {
     })
       .then(() => {
         setLoadingImage(false);
-        toastManager.add('New avatar image saved successfully.', {
+        addToast('New avatar image saved successfully.', {
           appearance: 'success',
           autoDismiss: true,
         });
@@ -109,7 +111,6 @@ export const AvatarUpload = ({ userId, size, toastManager }) => {
 
 AvatarUpload.propTypes = {
   size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge']).isRequired,
-  toastManager: PropTypes.object,
   userId: PropTypes.string.isRequired,
 };
 

demo-projects/meetup/site/pages/profile.js
@@ -1,7 +1,7 @@
 import React, { Component, useState, useCallback } from 'react';
 import gql from 'graphql-tag';
 import { useFormState } from 'react-use-form-state';
-import { withToastManager } from 'react-toast-notifications';
+import { useToasts } from 'react-toast-notifications';
 import { useMutation } from 'react-apollo-hooks';
 
 import { AvatarUpload } from '../components/AvatarUpload';
@@ -39,14 +39,14 @@ export default class ProfilePage extends Component {
   }
 }
 
-const Profile = withToastManager(props => {
-  const { user, toastManager } = props;
+const Profile = ({ user }) => {
   const [formState, { text, email, password }] = useFormState({
     name: user.name,
     email: user.email,
     twitterHandle: user.twitterHandle || '',
     image: user.image,
   });
+  const { addToast } = useToasts();
   const [validationErrors, setValidationErrors] = useState({});
   const [updatingUser, setUpdatingUser] = useState(false);
 
@@ -73,7 +73,7 @@ const Profile = withToastManager(props => {
             ...formState,
           },
         });
-        toastManager.add('Changes saved successfully.', {
+        addToast('Changes saved successfully.', {
           appearance: 'success',
           autoDismiss: true,
         });
@@ -84,7 +84,7 @@ const Profile = withToastManager(props => {
         );
         setValidationErrors({ password: `${errorMessage}` });
 
-        toastManager.add('Please try again.', {
+        addToast('Please try again.', {
           appearance: 'error',
           autoDismiss: true,
         });
@@ -97,7 +97,7 @@ const Profile = withToastManager(props => {
 
   return (
     <div>
-      <AvatarUpload userId={user.id} size="xlarge" toastManager={toastManager} />
+      <AvatarUpload userId={user.id} size="xlarge" />
       <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="name">
           Name
@@ -150,7 +150,7 @@ const Profile = withToastManager(props => {
       </form>
     </div>
   );
-});
+};
 
 const USER = gql`
   query user {

commit 91bb312c315851d0f7dadf55ddfa502e451f1bca

fix issues introduced by #1544

packages/app-admin-ui/client/pages/Item/index.js
@@ -97,7 +97,14 @@ const ItemDetails = withRouter(
       }
     };
     onDelete = deletePromise => {
-      const { adminPath, history, list, item } = this.props;
+      const {
+        adminPath,
+        history,
+        list,
+        item,
+        toastManager: { addToast },
+      } = this.props;
+
       deletePromise
         .then(() => {
           if (this.mounted) {
@@ -105,10 +112,10 @@ const ItemDetails = withRouter(
           }
           history.push(`${adminPath}/${list.path}`);
 
-          toastItemSuccess(this.props.toast, item, 'Deleted successfully');
+          toastItemSuccess({ addToast }, item, 'Deleted successfully');
         })
         .catch(error => {
-          toastError(this.props.toast, error);
+          toastError({ addToast }, error);
         });
     };
 

packages/app-admin-ui/client/util.js
@@ -28,7 +28,7 @@ export const deconstructErrorsToDataShape = error => {
 // Toast Formatters
 // ==============================
 
-export function toastItemSuccess(toast, item, message = 'Success') {
+export function toastItemSuccess({ addToast }, item, message = 'Success') {
   const toastContent = (
     <div>
       {item && item._label_ ? <strong>{item._label_}</strong> : null}
@@ -36,13 +36,13 @@ export function toastItemSuccess(toast, item, message = 'Success') {
     </div>
   );
 
-  toast.addToast(toastContent, {
+  addToast(toastContent, {
     autoDismiss: true,
     appearance: 'success',
-  })();
+  });
 }
 
-export function toastError(toast, error) {
+export function toastError({ addToast }, error) {
   const [title, ...rest] = error.message.split(/\:/);
   const toastContent = rest.length ? (
     <div>
@@ -53,9 +53,9 @@ export function toastError(toast, error) {
     error.message
   );
 
-  toast.addToast(toastContent, {
+  addToast(toastContent, {
     appearance: 'error',
-  })();
+  });
 }
 
 // ==============================