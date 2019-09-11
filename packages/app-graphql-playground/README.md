c99c7cd22905e06deb83c17b109ebfccddca4f11
Tim Leslie <timl@thinkmill.com.au>
2019-09-12 09:49:06 +1000

    Add a new GraphQLPlaygroundApp to decouple from the GraphQLApp (#1619)

---
+++ packages/app-graphql-playground/README.md
@@ -0,0 +1,38 @@
+<!--[meta]
+section: api
+subSection: apps
+title: GraphQL Playground App
+draft: true
+[meta]-->
+
+# GraphQL Playground App
+
+A Keystone App that creates an Apollo GraphQL playground.
+
+## Usage
+
+```javascript
+const { Keystone } = require('@keystone-alpha/keystone');
+const { GraphQLApp } = require('@keystone-alpha/app-graphql');
+const { GraphQLAppPlayground } = require('@keystone-alpha/app-graphql-playground');
+const { AdminUIApp } = require('@keystone-alpha/app-admin-ui');
+
+// Ensure that the GraphQLApp and GraphQLAppPlayground are referring to the same endpoint
+const apiPath = '/admin/api';
+module.exports = {
+  new Keystone(),
+  apps: [
+    // Disable the default playground on this app
+    new GraphQLApp({ apiPath, graphiqlPath: undefined }),
+    new GraphQLAppPlayground({ apiPath })
+    new AdminUIApp()
+  ],
+};
+```
+
+## Config
+
+| Option         | Type     | Default           | Description                               |
+| -------------- | -------- | ----------------- | ----------------------------------------- |
+| `apiPath`      | `String` | `/admin/api`      | Change the API path                       |
+| `graphiqlPath` | `String` | `/admin/graphiql` | Change the Apollo GraphQL playground path |
--- packages/app-graphql/index.js
+++ packages/app-graphql/index.js
@@ -1,5 +1,7 @@
 const assert = require('nanoassert');
-const createGraphQLMiddleware = require('./lib/graphql');
+const express = require('express');
+const { restrictAudienceMiddleware } = require('@keystone-alpha/session');
+const { GraphQLPlaygroundApp } = require('@keystone-alpha/app-graphql-playground');
 const { createApolloServer } = require('./lib/apolloServer');
 
 class GraphQLApp {
@@ -42,22 +44,30 @@ class GraphQLApp {
    * @return Array<middlewares>
    */
   prepareMiddleware({ keystone, dev }) {
-    const middlewares = [];
-
     const server = createApolloServer(keystone, this._apollo, this._schemaName, dev);
     // GraphQL API always exists independent of any adminUI or Session
     // settings We currently make the admin UI public. In the future we want
     // to be able to restrict this to a limited audience, while setting up a
     // separate public API with much stricter access control.
-    middlewares.push(
-      createGraphQLMiddleware(
-        server,
-        { apiPath: this._apiPath, graphiqlPath: this._graphiqlPath },
-        { isPublic: true }
-      )
-    );
+    const apiPath = this._apiPath;
+    const graphiqlPath = this._graphiqlPath;
+    const app = express();
 
-    return middlewares;
+    if (graphiqlPath) {
+      // This is a convenience to make the out of the box experience slightly simpler.
+      // We should reconsider support for this at some point in the future. -TL
+      app.use(
+        new GraphQLPlaygroundApp({ apiPath, graphiqlPath }).prepareMiddleware({ keystone, dev })
+      );
+    }
+
+    // { cors: false } - prevent ApolloServer from overriding Keystone's CORS configuration.
+    // https://www.apollographql.com/docs/apollo-server/api/apollo-server.html#ApolloServer-applyMiddleware
+    // This probably isn't the right place to put this restriction middleware. -TL
+    const restrict = restrictAudienceMiddleware({ isPublic: true });
+    app.use(apiPath, restrict);
+    app.use(server.getMiddleware({ path: apiPath, cors: false }));
+    return app;
   }
 
   /**
