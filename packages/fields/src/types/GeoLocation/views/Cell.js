4fc640a41391da7a7faafe92c3fbbe25c6049edb
Jerry <hmlamad@gmail.com>
2019-09-25 01:16:00 +0800

    Refactor Cell component

--- packages/fields/src/types/GeoLocation/views/Cell.js
+++ packages/fields/src/types/GeoLocation/views/Cell.js
@@ -5,20 +5,18 @@ import type { CellProps } from '../../../types';
 
 type Props = CellProps<boolean>;
 
-const roundToDecimalPlace = (value, decimalPlace) =>
-  Math.round(value * 10 ** decimalPlace) / 10 ** decimalPlace;
-
 export default class CheckboxCellView extends Component<Props> {
+  roundToDecimalPlace = (value, decimalPlace) =>
+    Math.round(value * 10 ** decimalPlace) / 10 ** decimalPlace;
   render() {
     const { data } = this.props;
     if (!data || !data.lat || !data.lng) {
       return null;
     }
-    return (
-      <span>{`Lng: ${roundToDecimalPlace(data.lng, 5)}, Lat: ${roundToDecimalPlace(
-        data.lat,
-        5
-      )}`}</span>
-    );
+    const locations = [
+      `Lng: ${this.roundToDecimalPlace(data.lng, 5)}`,
+      `Lat: ${this.roundToDecimalPlace(data.lat, 5)}`,
+    ];
+    return <span>{locations.join(', ')}</span>;
   }
 }
