15075d552072a42e70649e423536968192865551
Jordan Overbye <jordanoverbye@gmail.com>
2019-10-04 21:01:56 +1000

    Simplify cell

--- packages/fields/src/types/Location/views/Cell.js
+++ packages/fields/src/types/Location/views/Cell.js
@@ -1,43 +1,11 @@
 // @flow
-/** @jsx jsx */
-
-import { jsx } from '@emotion/core';
-import { Fragment } from 'react';
 import type { CellProps } from '../../../types';
 
 type Props = CellProps<string>;
 
 const Cell = (props: Props) => {
-  if (!props.data) {
-    return null;
-  }
-
-  return (
-    <Fragment>
-      <div
-        style={{
-          // using inline styles instead of emotion for setting the color
-          // since emotion doesn't escape styles so it could be used for CSS injection
-          backgroundColor: props.data,
-        }}
-        css={{
-          borderRadius: 3,
-          display: 'inline-block',
-          height: 18,
-          width: 18,
-          marginRight: 10,
-          verticalAlign: 'middle',
-        }}
-      />
-      <span
-        css={{
-          verticalAlign: 'middle',
-        }}
-      >
-        {JSON.stringify(props.data)}
-      </span>
-    </Fragment>
-  );
+  if (!props.data) return null;
+  return props.data.formattedAddress;
 };
 
 export default Cell;
