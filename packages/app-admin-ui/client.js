22c72586f4b6254803fbb32c54e42beda4f52717
Gautam Singh <gautamsi@users.noreply.github.com>
2019-10-31 16:15:17 +0530

    update export  format

--- packages/app-admin-ui/client.js
+++ packages/app-admin-ui/client.js
@@ -1,19 +1,9 @@
-import ListTable from './client/components/ListTable';
-import CreateItemModal from './client/components/CreateItemModal';
-import DeleteItemModal from './client/components/DeleteItemModal';
-import DeleteManyItemsModal from './client/components/DeleteManyItemsModal';
-import PageLoading from './client/components/PageLoading';
-import ToastContainer from './client/components/ToastContainer';
-import UpdateManyItemsModal from './client/components/UpdateManyItemsModal';
-
+export { default as ListTable } from './client/components/ListTable';
+export { default as CreateItemModal } from './client/components/CreateItemModal';
+export { default as DeleteItemModal } from './client/components/DeleteItemModal';
+export { default as DeleteManyItemsModal } from './client/components/DeleteManyItemsModal';
+export { default as PageLoading } from './client/components/PageLoading';
+export { default as ToastContainer } from './client/components/ToastContainer';
+export { default as UpdateManyItemsModal } from './client/components/UpdateManyItemsModal';
 export { Popout } from './client/components/Popout';
 export { useAdminMeta } from './client/providers/AdminMeta';
-export {
-  CreateItemModal,
-  DeleteItemModal,
-  DeleteManyItemsModal,
-  ListTable,
-  PageLoading,
-  ToastContainer,
-  UpdateManyItemsModal,
-};
