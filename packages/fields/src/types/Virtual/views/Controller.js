df9f1476b1ff243f3e3116a54043042b3aa8ef3c
Mike <mike@madebymike.com.au>
2019-11-28 10:35:21 +1100

    Remove filter methods from controller

--- packages/fields/src/types/Virtual/views/Controller.js
+++ packages/fields/src/types/Virtual/views/Controller.js
@@ -4,18 +4,5 @@ export default class VirtualController extends FieldController {
     return `${this.path}${this.config.graphQLSelection}`;
   };
 
-  getFilterGraphQL = ({ type, value }) => {
-    const key = type === 'is' ? `${this.path}` : `${this.path}_${type}`;
-    return `${key}: "${value}"`;
-  };
-
-  getFilterLabel = ({ label }) => {
-    return `${this.label} ${label.toLowerCase()}`;
-  };
-
-  formatFilter = ({ label, value }) => {
-    return `${this.getFilterLabel({ label })}: "${value}"`;
-  };
-
   getFilterTypes = () => [];
 }
