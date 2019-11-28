4161724f068664ef1ffd75398b0f71c6adb2cf1b
Charles Dang <exodia339@gmail.com>
2019-11-28 16:40:20 +1100

    Moved date range validation to validateInput
    This ensures an error actually shows when saving. Previously the value was just not saved.
    Also removed the year length validation. It didn't seem very useful.

--- .changeset/spotty-flowers-breathe.md
+++ .changeset/spotty-flowers-breathe.md
@@ -7,3 +7,4 @@ Fixed several issues with the CalendarDay field:
 - Ensured format, yearRangeFrom, and yearRangeTo defaults are set correctly
 - Removed non-functional yearPickerType
 - Made displayed date format consistent between list and detailed views
+- Moved date range validation to validateInput
--- packages/fields/src/types/CalendarDay/views/Field.js
+++ packages/fields/src/types/CalendarDay/views/Field.js
@@ -1,31 +1,15 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { getYear } from 'date-fns';
 import { Component } from 'react';
 
 import { FieldContainer, FieldLabel, FieldInput } from '@arch-ui/fields';
 import { TextDayPicker } from '@arch-ui/day-picker';
+import { Alert } from '@arch-ui/alert';
 
 export default class CalendarDayField extends Component {
-  handleSelectedChange = value => {
-    const {
-      field: {
-        config: { yearRangeTo, yearRangeFrom },
-      },
-      onChange,
-    } = this.props;
-    const inputYear = getYear(value);
-    if (
-      value === null ||
-      (inputYear.toString().length <= 4 && inputYear <= yearRangeTo && inputYear >= yearRangeFrom)
-    ) {
-      onChange(value);
-    }
-  };
-
   render() {
-    const { autoFocus, field, value, errors } = this.props;
+    const { autoFocus, field, value, errors, onChange } = this.props;
     const htmlID = `ks-input-${field.path}`;
 
     return (
@@ -37,9 +21,16 @@ export default class CalendarDayField extends Component {
             autoFocus={autoFocus}
             date={value}
             format={field.config.format}
-            onChange={this.handleSelectedChange}
+            onChange={onChange}
           />
         </FieldInput>
+
+        {errors.map(({ message, data }) => (
+          <Alert appearance="danger" key={message}>
+            {message}
+            {data ? ` - ${JSON.stringify(data)}` : null}
+          </Alert>
+        ))}
       </FieldContainer>
     );
   }
