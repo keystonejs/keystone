367b22f64be65ee97ea9e53552f20a0ffdf905ac
Mike <mike@madebymike.com.au>
2019-12-03 12:38:30 +1100

    Mitchells magic

--- .circleci/run-or-bail.js
+++ .circleci/run-or-bail.js
@@ -4,7 +4,7 @@ let shouldRunCI = false;
 
 // process.env.CIRCLE_JOB;
 
-let stdout = execSync('git', ['diff', '--name-only', 'master']);
+let stdout = execSync('git', ['diff', '--name-only', 'master'], { stdio: 'inherit' });
 let changedFiles = stdout.toString().split('\n');
 
 if (changedFiles.every(filename => filename.endsWith('.md'))) {
