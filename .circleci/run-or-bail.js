6774ace2908c687e3bfab46c9ef542307265b8a3
Mike <mike@madebymike.com.au>
2019-12-03 13:02:28 +1100

    This will work!

--- .circleci/run-or-bail.js
+++ .circleci/run-or-bail.js
@@ -1,12 +1,11 @@
-const { execSync } = require('child_process');
+const { spawnSync } = require('child_process');
 
 let shouldRunCI = false;
 
 // process.env.CIRCLE_JOB;
 
-let stdout = execSync('git', ['diff', '--name-only', 'master'], {
+let { stdout } = spawnSync('/usr/bin/git', ['diff', '--name-only', 'master'], {
   stdio: 'inherit',
-  shell: '/bin/bash',
 });
 let changedFiles = stdout.toString().split('\n');
 
@@ -17,5 +16,5 @@ if (changedFiles.every(filename => filename.endsWith('.md'))) {
 
 if (shouldRunCI === false) {
   console.log('Test skipped');
-  execSync('circleci-agent', ['step', 'halt'], { stdio: 'inherit', shell: '/bin/bash' });
+  spawnSync('circleci-agent', ['step', 'halt'], { stdio: 'inherit' });
 }
