2ebb444a330550c7fcc1f2104c080d8662fa0002
Mike <mike@madebymike.com.au>
2019-12-03 12:19:04 +1100

    Conditional CI

---
+++ .circleci/run-or-bail.js
@@ -0,0 +1,18 @@
+const { execSync } = require('child_process');
+
+let shouldRunCI = true;
+
+// process.env.CIRCLE_JOB;
+
+let stdout = execSync('git', ['diff', '--name-only', 'master']);
+let changedFiles = stdout.toString().split('\n');
+
+if (changedFiles.every(filename => filename.endsWith('.md'))) {
+  console.log('No code changes skipping CI');
+  shouldRunCI = false;
+}
+
+if (shouldRunCI === false) {
+  console.log('Test skipped');
+  execSync('circleci-agent', ['step', 'halt'], { stdio: 'inherit' });
+}
