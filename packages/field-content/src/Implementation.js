c7217c44b4205772a63d7eff6740bfad467453d9
Charles Dang <exodia339@gmail.com>
2019-12-06 12:02:16 +1100

    Cleaned up duplicate code in aux list creation

--- packages/field-content/src/Implementation.js
+++ packages/field-content/src/Implementation.js
@@ -167,16 +167,6 @@ export class Content extends Relationship.implementation {
             };
           },
         },
-        access: Object.entries(listConfig.listAccess).reduce(
-          (acc, [schemaName, access]) => ({
-            ...acc,
-            [schemaName]: Object.entries(access).reduce(
-              (acc, [op, rule]) => ({ ...acc, [op]: !!rule }), // Reduce the entries to truthy values
-              {}
-            ),
-          }),
-          {}
-        ),
       });
     }
 
