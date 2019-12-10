dbe5b69f1d7bdeb1007ad322d5da953b2c4a9f4e
Charles Dang <exodia339@gmail.com>
2019-12-10 17:35:20 +1100

    Admin UI: convert remaining uses of Apollo components to hooks (#2064)
    * Convert remaining uses of Apollo components to hooks
    
    * Admin UI: updated dependencies

--- packages/app-admin-ui/client/pages/Home/index.js
+++ packages/app-admin-ui/client/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { Query } from '@apollo/react-components';
+import { useQuery } from '@apollo/react-hooks';
 
 import { Container, Grid, Cell } from '@arch-ui/layout';
 import { PageTitle } from '@arch-ui/typography';
@@ -83,6 +83,9 @@ const ListProvider = ({ getListByKey, listKeys, ...props }) => {
   // TODO: A permission query to limit which lists are visible
   const lists = listKeys.map(key => getListByKey(key));
 
+  const query = gqlCountQueries(lists);
+  const { data, error } = useQuery(query, { fetchPolicy: 'cache-and-network', errorPolicy: 'all' });
+
   if (lists.length === 0) {
     return (
       <Fragment>
@@ -99,50 +102,46 @@ const ListProvider = ({ getListByKey, listKeys, ...props }) => {
     );
   }
 
-  const query = gqlCountQueries(lists);
+  let allowedLists = lists;
+
+  if (error) {
+    if (!error.graphQLErrors || !error.graphQLErrors.length) {
+      return (
+        <PageError>
+          <p>{error.message}</p>
+        </PageError>
+      );
+    }
+
+    const deniedQueries = error.graphQLErrors
+      .filter(({ name }) => name === 'AccessDeniedError')
+      .map(({ path }) => path && path[0]);
+
+    if (deniedQueries.length !== error.graphQLErrors.length) {
+      // There were more than Access Denied Errors, so throw a normal
+      // error message
+      return (
+        <PageError>
+          <p>{error.message}</p>
+        </PageError>
+      );
+    }
+
+    allowedLists = allowedLists.filter(
+      list => deniedQueries.indexOf(list.gqlNames.listQueryMetaName) === -1
+    );
+  }
+
   return (
     <Fragment>
       <DocTitle>Home</DocTitle>
-      <Query query={query} fetchPolicy="cache-and-network" errorPolicy="all">
-        {({ data, error }) => {
-          let allowedLists = lists;
-
-          if (error) {
-            if (!error.graphQLErrors || !error.graphQLErrors.length) {
-              return (
-                <PageError>
-                  <p>{error.message}</p>
-                </PageError>
-              );
-            }
-
-            const deniedQueries = error.graphQLErrors
-              .filter(({ name }) => name === 'AccessDeniedError')
-              .map(({ path }) => path && path[0]);
-
-            if (deniedQueries.length !== error.graphQLErrors.length) {
-              // There were more than Access Denied Errors, so throw a normal
-              // error message
-              return (
-                <PageError>
-                  <p>{error.message}</p>
-                </PageError>
-              );
-            }
-
-            allowedLists = allowedLists.filter(
-              list => deniedQueries.indexOf(list.gqlNames.listQueryMetaName) === -1
-            );
-          }
-
-          // NOTE: `loading` is intentionally omitted here
-          // the display of a loading indicator for counts is defered to the
-          // list component so we don't block rendering the lists immediately
-          // to the user.
-
-          return <HomePage lists={allowedLists} data={data} {...props} />;
-        }}
-      </Query>
+      {
+        // NOTE: `loading` is intentionally omitted here
+        // the display of a loading indicator for counts is deferred to the
+        // list component so we don't block rendering the lists immediately
+        // to the user.
+      }
+      <HomePage lists={allowedLists} data={data} {...props} />;
     </Fragment>
   );
 };
