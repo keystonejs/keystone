0fae73d1e43970095c491397f716437aeaf250be
Liam Clarke <liam.att.clarke@gmail.com>
2019-12-11 15:43:43 -0500

    Added S3 file adapter (#1986)

--- packages/file-adapters/index.js
+++ packages/file-adapters/index.js
@@ -1,4 +1,5 @@
 const LocalFileAdapter = require('./lib/local-file');
 const CloudinaryAdapter = require('./lib/cloudinary');
+const S3Adapter = require('./lib/s3');
 
-module.exports = { LocalFileAdapter, CloudinaryAdapter };
+module.exports = { LocalFileAdapter, CloudinaryAdapter, S3Adapter };
