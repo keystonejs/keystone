c8e5eccd219683b9ca2567eb0246d3c032445174
Gautam Singh <gautamsi@users.noreply.github.com>
2019-12-22 11:25:52 +0530

    lint fix

--- demo-projects/meetup/site/pages/_app.js
+++ demo-projects/meetup/site/pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import gql from 'graphql-tag';
 import { ApolloProvider } from 'react-apollo';
