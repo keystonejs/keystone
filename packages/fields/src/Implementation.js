34ab962d2603636bd6e3114c78d1e9fb5e6aff43
Gautam Singh <gautamsi@users.noreply.github.com>
2020-01-31 13:41:38 +0530

    pass field config and list admin config into adminMeta

--- packages/fields/src/Implementation.js
+++ packages/fields/src/Implementation.js
@@ -171,6 +171,7 @@ class Field {
       // functions
       defaultValue: typeof this.defaultValue !== 'function' ? this.defaultValue : undefined,
       isPrimaryKey: this.isPrimaryKey,
+      ...this.config,
       // NOTE: This data is serialised, so we're unable to pass through any
       // access control _functions_. But we can still check for the boolean case
       // and pass that through (we assume that if there is a function, it's a
