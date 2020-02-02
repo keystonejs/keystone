7ff9d610794ab08c4dac2591a8d663230a71f342
Richard van der Dys <richard.vanderdys@gmail.com>
2020-02-02 11:34:08 +0200

    Added way to use keystone from Next App

--- packages/app-next/index.js
+++ packages/app-next/index.js
@@ -3,17 +3,22 @@ const next = require('next');
 const nextBuild = require('next/dist/build').default;
 
 class NextApp {
-  constructor({ dir } = {}) {
+  constructor({ dir, keystone } = {}) {
     if (!dir || typeof dir !== 'string') {
       throw new Error('NextApp requires a "dir" option, which must be a string.');
     }
     this._dir = path.resolve(dir);
+    this._keystone = keystone;
   }
 
   async prepareMiddleware({ dev, distDir }) {
     const nextApp = next({ distDir, dir: this._dir, dev });
+    const handler = nextApp.getRequestHandler();
     await nextApp.prepare();
-    return nextApp.getRequestHandler();
+    return (req, res) => {
+      req.keystone = this._keystone;
+      return handler(req, res);
+    };
   }
 
   async build() {
