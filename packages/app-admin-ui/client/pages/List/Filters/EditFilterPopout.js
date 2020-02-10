d864e9b1cfec9144593634c32f0c90746d939615
Charles Dang <exodia339@gmail.com>
2020-02-10 16:16:02 +1100

    naaaahhhhhh

--- packages/app-admin-ui/client/pages/List/Filters/EditFilterPopout.js
+++ packages/app-admin-ui/client/pages/List/Filters/EditFilterPopout.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState /*, useRef*/ } from 'react';
 
 import { POPOUT_GUTTER } from '../../../components/Popout';
 import PopoutForm from './PopoutForm';
