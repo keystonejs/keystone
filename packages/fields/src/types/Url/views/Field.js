142488a7ca9b17618745c3fa56b773a909c6dc8b
Charles Dang <exodia339@gmail.com>
2020-02-19 15:40:47 +1100

    Converted most field React components to functional ones

--- packages/fields/src/types/Url/views/Field.js
+++ packages/fields/src/types/Url/views/Field.js
@@ -1,42 +1,39 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component } from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import { Input } from '@arch-ui/input';
 
-export default class UrlField extends Component {
-  onChange = event => {
-    this.props.onChange(event.target.value);
+const UrlField = ({ onChange, autoFocus, field, value: serverValue, errors }) => {
+  const handleChange = event => {
+    onChange(event.target.value);
   };
-  render() {
-    const { autoFocus, field, value: serverValue, errors } = this.props;
-    const value = serverValue || '';
-    const htmlID = `ks-input-${field.path}`;
-    const canRead = errors.every(
-      error => !(error instanceof Error && error.name === 'AccessDeniedError')
-    );
-    const error = errors.find(
-      error => error instanceof Error && error.name === 'AccessDeniedError'
-    );
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="url"
-            value={canRead ? value : undefined}
-            placeholder={canRead ? undefined : error.message}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  const value = serverValue || '';
+  const htmlID = `ks-input-${field.path}`;
+  const canRead = errors.every(
+    error => !(error instanceof Error && error.name === 'AccessDeniedError')
+  );
+  const error = errors.find(error => error instanceof Error && error.name === 'AccessDeniedError');
+
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <Input
+          autoComplete="off"
+          autoFocus={autoFocus}
+          type="url"
+          value={canRead ? value : undefined}
+          placeholder={canRead ? undefined : error.message}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default UrlField;
