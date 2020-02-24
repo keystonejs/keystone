keystonejs/keystone

commit c059b63c6ebdbb60ac4095d1efd791d598b2756c

Converted most field React components to functional ones (#2397)

packages/fields/src/types/Password/views/Field.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component, createRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import { Alert } from '@arch-ui/alert';
@@ -11,107 +11,102 @@ import { Button } from '@arch-ui/button';
 import { EyeIcon, LockIcon } from '@arch-ui/icons';
 import { A11yText } from '@arch-ui/typography';
 
-export default class PasswordField extends Component {
-  focusTarget = createRef();
-  state = {
-    isEditing: false,
-    showInputValue: false,
-    inputPassword: '',
-    inputConfirm: '',
-  };
-  onChange = ({ target }) => {
-    const { name, value } = target;
+const PasswordField = ({ onChange, autoFocus, field, value: serverValue, errors, warnings }) => {
+  const focusTarget = useRef();
 
-    this.setState(({ inputPassword, inputConfirm }) => {
-      this.props.onChange({
-        inputPassword,
-        inputConfirm,
-        [name]: value,
-      });
-      return { [name]: value };
+  const [isEditing, setIsEditing] = useState(false);
+  const [showInputValue, setShowInputValue] = useState(false);
+  const [inputPassword, setInputPassword] = useState('');
+  const [inputConfirm, setInputConfirm] = useState('');
+
+  useEffect(() => {
+    onChange({
+      inputPassword,
+      inputConfirm,
     });
-  };
-  toggleInterface = () => {
-    function maybeFocus() {
-      if (this.state.isEditing && this.focusTarget.current) {
-        this.focusTarget.current.focus();
-      }
+  }, [inputPassword, inputConfirm]);
+
+  useEffect(() => {
+    if (isEditing && focusTarget.current) {
+      focusTarget.current.focus();
     }
+  }, [isEditing]);
 
-    this.setState(state => ({ isEditing: !state.isEditing }), maybeFocus);
+  const toggleInterface = () => {
+    setIsEditing(!isEditing);
   };
-  toggleMode = () => {
-    this.setState(state => ({ showInputValue: !state.showInputValue }));
-  };
-  render() {
-    const { isEditing, inputPassword, inputConfirm, showInputValue } = this.state;
-    const { autoFocus, field, value: serverValue, errors, warnings } = this.props;
-    const value = serverValue || '';
-    const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          {isEditing ? (
-            <FlexGroup growIndexes={[0, 1]}>
-              <Input
-                autoComplete="off"
-                autoFocus={autoFocus}
-                id={htmlID}
-                ref={this.focusTarget}
-                name="inputPassword"
-                onChange={this.onChange}
-                placeholder="New Password"
-                type={showInputValue ? 'text' : 'password'}
-                value={inputPassword}
-              />
-              <Input
-                autoComplete="off"
-                autoFocus={autoFocus}
-                id={`${htmlID}-confirm`}
-                name="inputConfirm"
-                onChange={this.onChange}
-                placeholder="Confirm Password"
-                type={showInputValue ? 'text' : 'password'}
-                value={inputConfirm}
-              />
-              <Button
-                isActive={showInputValue}
-                onClick={this.toggleMode}
-                title={showInputValue ? 'Hide Text' : 'Show Text'}
-                variant="ghost"
-              >
-                <A11yText>{showInputValue ? 'Hide Text' : 'Show Text'}</A11yText>
-                <div css={{ width: 20 }}>{showInputValue ? <LockIcon /> : <EyeIcon />}</div>
-              </Button>
-            </FlexGroup>
-          ) : (
-            <Button id={`${htmlID}-button`} onClick={this.toggleInterface} variant="ghost">
-              {value ? 'Update Password' : 'Set Password'}
+  const toggleMode = () => {
+    setShowInputValue(!showInputValue);
+  };
+
+  const value = serverValue || '';
+  const htmlID = `ks-input-${field.path}`;
+
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        {isEditing ? (
+          <FlexGroup growIndexes={[0, 1]}>
+            <Input
+              autoComplete="off"
+              autoFocus={autoFocus}
+              id={htmlID}
+              ref={focusTarget}
+              name="inputPassword"
+              onChange={e => setInputPassword(e.target.value)}
+              placeholder="New Password"
+              type={showInputValue ? 'text' : 'password'}
+              value={inputPassword}
+            />
+            <Input
+              autoComplete="off"
+              autoFocus={autoFocus}
+              id={`${htmlID}-confirm`}
+              name="inputConfirm"
+              onChange={e => setInputConfirm(e.target.value)}
+              placeholder="Confirm Password"
+              type={showInputValue ? 'text' : 'password'}
+              value={inputConfirm}
+            />
+            <Button
+              isActive={showInputValue}
+              onClick={toggleMode}
+              title={showInputValue ? 'Hide Text' : 'Show Text'}
+              variant="ghost"
+            >
+              <A11yText>{showInputValue ? 'Hide Text' : 'Show Text'}</A11yText>
+              <div css={{ width: 20 }}>{showInputValue ? <LockIcon /> : <EyeIcon />}</div>
             </Button>
-          )}
-        </FieldInput>
+          </FlexGroup>
+        ) : (
+          <Button id={`${htmlID}-button`} onClick={toggleInterface} variant="ghost">
+            {value ? 'Update Password' : 'Set Password'}
+          </Button>
+        )}
+      </FieldInput>
 
-        {errors.length
-          ? errors.map(({ message, data }) => (
-              <Alert appearance="danger" key={message}>
-                {message}
-                {data ? ` - ${JSON.stringify(data)}` : null}
-              </Alert>
-            ))
-          : null}
+      {errors.length
+        ? errors.map(({ message, data }) => (
+            <Alert appearance="danger" key={message}>
+              {message}
+              {data ? ` - ${JSON.stringify(data)}` : null}
+            </Alert>
+          ))
+        : null}
 
-        {warnings.length
-          ? warnings.map(({ message, data }) => (
-              <Alert appearance="warning" key={message}>
-                {message}
-                {data ? ` - ${JSON.stringify(data)}` : null}
-              </Alert>
-            ))
-          : null}
-      </FieldContainer>
-    );
-  }
-}
+      {warnings.length
+        ? warnings.map(({ message, data }) => (
+            <Alert appearance="warning" key={message}>
+              {message}
+              {data ? ` - ${JSON.stringify(data)}` : null}
+            </Alert>
+          ))
+        : null}
+    </FieldContainer>
+  );
+};
+
+export default PasswordField;

packages/fields/src/types/Checkbox/views/Field.js
@@ -1,34 +1,33 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component } from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 
 import { CheckboxPrimitive } from '@arch-ui/controls';
 
-export default class TextField extends Component {
-  onChange = event => {
-    this.props.onChange(event.target.checked);
+const TextField = ({ onChange, autoFocus, field, value, errors }) => {
+  const handleChange = event => {
+    onChange(event.target.checked);
   };
-  render() {
-    const { autoFocus, field, value, errors } = this.props;
-    const checked = value || false;
-    const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput css={{ height: 35 }}>
-          <CheckboxPrimitive
-            autoFocus={autoFocus}
-            checked={checked}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  const checked = value || false;
+  const htmlID = `ks-input-${field.path}`;
+
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput css={{ height: 35 }}>
+        <CheckboxPrimitive
+          autoFocus={autoFocus}
+          checked={checked}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default TextField;

packages/fields/src/types/Decimal/views/Field.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import {
   FieldContainer,
@@ -9,13 +9,13 @@ import {
 } from '@arch-ui/fields';
 import { Input } from '@arch-ui/input';
 
-export default class TextField extends Component {
-  onChange = event => {
+const TextField = ({ onChange, autoFocus, field, value, errors }) => {
+  const handleChange = event => {
     const value = event.target.value;
-    this.props.onChange(value.replace(/[^0-9.,]+/g, ''));
+    onChange(value.replace(/[^0-9.,]+/g, ''));
   };
 
-  valueToString = value => {
+  const valueToString = value => {
     // Make the value a string to prevent loss of accuracy and precision.
     if (typeof value === 'string') {
       return value;
@@ -27,31 +27,30 @@ export default class TextField extends Component {
     }
   };
 
-  render() {
-    const { autoFocus, field, value, errors } = this.props;
-    const {
-      // currency,
-      // digits,
-      symbol,
-    } = field.config;
-    const htmlID = `ks-input-${field.path}`;
+  const {
+    // currency,
+    // digits,
+    symbol,
+  } = field.config;
+  const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          {symbol && <Currency>{symbol}</Currency>}
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="text"
-            value={this.valueToString(value)}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        {symbol && <Currency>{symbol}</Currency>}
+        <Input
+          autoComplete="off"
+          autoFocus={autoFocus}
+          type="text"
+          value={valueToString(value)}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default TextField;

packages/fields/src/types/Float/views/Field.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import { Input } from '@arch-ui/input';
 
-export default class TextField extends Component {
-  onChange = event => {
+const TextField = ({ onChange, autoFocus, field, value, errors }) => {
+  const handleChange = event => {
     const value = event.target.value;
     // Similar implementation as per old Keystone version
     if (/^-?\d*\.?\d*$/.test(value)) {
-      this.props.onChange(value);
+      onChange(value);
     }
   };
 
-  render() {
-    const { autoFocus, field, value, errors } = this.props;
-    const htmlID = `ks-input-${field.path}`;
+  const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="text"
-            value={value}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <Input
+          autoComplete="off"
+          autoFocus={autoFocus}
+          type="text"
+          value={value}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default TextField;

packages/fields/src/types/Integer/views/Field.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import { Input } from '@arch-ui/input';
 
-export default class TextField extends Component {
-  onChange = event => {
+const TextField = ({ onChange, autoFocus, field, value, errors }) => {
+  const handleChange = event => {
     const value = event.target.value;
-    this.props.onChange(value.replace(/\D/g, ''));
+    onChange(value.replace(/\D/g, ''));
   };
 
-  valueToString = value => {
+  const valueToString = value => {
     // Make the value a string to keep react happy.
     if (typeof value === 'string') {
       return value;
@@ -21,25 +21,24 @@ export default class TextField extends Component {
     }
   };
 
-  render() {
-    const { autoFocus, field, value, errors } = this.props;
-    const htmlID = `ks-input-${field.path}`;
+  const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="text"
-            value={this.valueToString(value)}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <Input
+          autoComplete="off"
+          autoFocus={autoFocus}
+          type="text"
+          value={valueToString(value)}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default TextField;

packages/fields/src/types/OEmbed/views/Field.js
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component } from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import { Input } from '@arch-ui/input';
@@ -41,50 +40,47 @@ const PlaceholderPreview = ({ originalUrl, fieldPath }) => (
   />
 );
 
-export default class OEmbedField extends Component {
-  onChange = event => {
-    this.props.onChange({
+const OEmbedField = ({ onChange, autoFocus, field, value = null, savedValue = null, errors }) => {
+  const handleChange = event => {
+    onChange({
       originalUrl: event.target.value,
     });
   };
 
-  render() {
-    const { autoFocus, field, value = null, savedValue = null, errors } = this.props;
-    const htmlID = `ks-oembed-${field.path}`;
-    const canRead = errors.every(
-      error => !(error instanceof Error && error.name === 'AccessDeniedError')
-    );
-    const error = errors.find(
-      error => error instanceof Error && error.name === 'AccessDeniedError'
-    );
-    const hasChanged = field.hasChanged({ [field.path]: savedValue }, { [field.path]: value });
+  const htmlID = `ks-oembed-${field.path}`;
+  const canRead = errors.every(
+    error => !(error instanceof Error && error.name === 'AccessDeniedError')
+  );
+  const error = errors.find(error => error instanceof Error && error.name === 'AccessDeniedError');
+  const hasChanged = field.hasChanged({ [field.path]: savedValue }, { [field.path]: value });
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <Input
-            autoComplete="off"
-            autoFocus={autoFocus}
-            type="url"
-            value={(canRead && value && value.originalUrl) || ''}
-            placeholder={canRead ? undefined : error.message}
-            onChange={this.onChange}
-            id={htmlID}
-          />
-        </FieldInput>
-        {value && value.originalUrl && hasChanged && (
-          <PlaceholderPreview originalUrl={value.originalUrl} fieldPath={field.path} />
-        )}
-        {value && value.originalUrl && !hasChanged && (
-          <StyledPreview
-            preview={value.preview}
-            originalUrl={value.originalUrl}
-            fieldPath={field.path}
-          />
-        )}
-      </FieldContainer>
-    );
-  }
-}
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <Input
+          autoComplete="off"
+          autoFocus={autoFocus}
+          type="url"
+          value={(canRead && value && value.originalUrl) || ''}
+          placeholder={canRead ? undefined : error.message}
+          onChange={handleChange}
+          id={htmlID}
+        />
+      </FieldInput>
+      {value && value.originalUrl && hasChanged && (
+        <PlaceholderPreview originalUrl={value.originalUrl} fieldPath={field.path} />
+      )}
+      {value && value.originalUrl && !hasChanged && (
+        <StyledPreview
+          preview={value.preview}
+          originalUrl={value.originalUrl}
+          fieldPath={field.path}
+        />
+      )}
+    </FieldContainer>
+  );
+};
+
+export default OEmbedField;

packages/fields/src/types/Relationship/views/Field.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component, Fragment } from 'react';
+import { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
@@ -158,9 +158,18 @@ function CreateAndAddItem({ field, item, onCreate, CreateItemModal }) {
   );
 }
 
-export default class RelationshipField extends Component {
-  onChange = option => {
-    const { field, onChange } = this.props;
+const RelationshipField = ({
+  autoFocus,
+  field,
+  value,
+  renderContext,
+  errors,
+  onChange,
+  item,
+  list,
+  CreateItemModal,
+}) => {
+  const handleChange = option => {
     const { many } = field.config;
     if (many) {
       onChange(option ? option.map(i => i.value) : []);
@@ -168,65 +177,55 @@ export default class RelationshipField extends Component {
       onChange(option ? option.value : null);
     }
   };
-  render() {
-    const {
-      autoFocus,
-      field,
-      value,
-      renderContext,
-      errors,
-      onChange,
-      item,
-      list,
-      CreateItemModal,
-    } = this.props;
-    const { many, ref } = field.config;
-    const { authStrategy } = field.adminMeta;
-    const htmlID = `ks-input-${field.path}`;
 
-    const relatedList = field.adminMeta.getListByKey(field.config.ref);
+  const { many, ref } = field.config;
+  const { authStrategy } = field.adminMeta;
+  const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <div css={{ flex: 1 }}>
-            <RelationshipSelect
-              autoFocus={autoFocus}
-              isMulti={many}
-              field={field}
-              value={value}
-              errors={errors}
-              renderContext={renderContext}
-              htmlID={htmlID}
-              onChange={this.onChange}
-            />
-          </div>
-          <ListProvider list={relatedList}>
-            <CreateAndAddItem
-              onCreate={item => {
-                onChange(many ? (value || []).concat(item) : item);
-              }}
-              field={field}
-              item={item}
-              list={list}
-              CreateItemModal={CreateItemModal}
-            />
-          </ListProvider>
-          {authStrategy && ref === authStrategy.listKey && (
-            <SetAsCurrentUser
-              many={many}
-              onAddUser={user => {
-                onChange(many ? (value || []).concat(user) : user);
-              }}
-              value={value}
-              listKey={authStrategy.listKey}
-            />
-          )}
-          <LinkToRelatedItems field={field} value={value} />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  const relatedList = field.adminMeta.getListByKey(field.config.ref);
+
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <div css={{ flex: 1 }}>
+          <RelationshipSelect
+            autoFocus={autoFocus}
+            isMulti={many}
+            field={field}
+            value={value}
+            errors={errors}
+            renderContext={renderContext}
+            htmlID={htmlID}
+            onChange={handleChange}
+          />
+        </div>
+        <ListProvider list={relatedList}>
+          <CreateAndAddItem
+            onCreate={item => {
+              onChange(many ? (value || []).concat(item) : item);
+            }}
+            field={field}
+            item={item}
+            list={list}
+            CreateItemModal={CreateItemModal}
+          />
+        </ListProvider>
+        {authStrategy && ref === authStrategy.listKey && (
+          <SetAsCurrentUser
+            many={many}
+            onAddUser={user => {
+              onChange(many ? (value || []).concat(user) : user);
+            }}
+            value={value}
+            listKey={authStrategy.listKey}
+          />
+        )}
+        <LinkToRelatedItems field={field} value={value} />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default RelationshipField;

packages/fields/src/types/Select/views/Field.js
@@ -1,55 +1,52 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
-import { Component } from 'react';
 
 import { FieldContainer, FieldLabel, FieldDescription, FieldInput } from '@arch-ui/fields';
 import Select from '@arch-ui/select';
 
-export default class SelectField extends Component {
-  onChange = option => {
-    this.props.onChange(option ? option.value : null);
+const SelectField = ({ onChange, autoFocus, field, value: serverValue, renderContext, errors }) => {
+  const handleChange = option => {
+    onChange(option ? option.value : null);
   };
-  render() {
-    const { autoFocus, field, value: serverValue, renderContext, errors } = this.props;
-    const value = field.options.find(i => i.value === serverValue);
-    const htmlID = `ks-input-${field.path}`;
-    const canRead = errors.every(
-      error => !(error instanceof Error && error.name === 'AccessDeniedError')
-    );
-    const error = errors.find(
-      error => error instanceof Error && error.name === 'AccessDeniedError'
-    );
 
-    const selectProps =
-      renderContext === 'dialog'
-        ? {
-            menuPortalTarget: document.body,
-            menuShouldBlockScroll: true,
-          }
-        : null;
+  const value = field.options.find(i => i.value === serverValue);
+  const htmlID = `ks-input-${field.path}`;
+  const canRead = errors.every(
+    error => !(error instanceof Error && error.name === 'AccessDeniedError')
+  );
+  const error = errors.find(error => error instanceof Error && error.name === 'AccessDeniedError');
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
-        <FieldInput>
-          <div css={{ flex: 1 }}>
-            <Select
-              autoFocus={autoFocus}
-              value={canRead ? value : undefined}
-              placeholder={canRead ? undefined : error.message}
-              options={field.options}
-              onChange={this.onChange}
-              isClearable
-              id={`react-select-${htmlID}`}
-              inputId={htmlID}
-              instanceId={htmlID}
-              {...selectProps}
-            />
-          </div>
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  const selectProps =
+    renderContext === 'dialog'
+      ? {
+          menuPortalTarget: document.body,
+          menuShouldBlockScroll: true,
+        }
+      : null;
+
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      {field.config.adminDoc && <FieldDescription>{field.config.adminDoc}</FieldDescription>}
+      <FieldInput>
+        <div css={{ flex: 1 }}>
+          <Select
+            autoFocus={autoFocus}
+            value={canRead ? value : undefined}
+            placeholder={canRead ? undefined : error.message}
+            options={field.options}
+            onChange={handleChange}
+            isClearable
+            id={`react-select-${htmlID}`}
+            inputId={htmlID}
+            instanceId={htmlID}
+            {...selectProps}
+          />
+        </div>
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default SelectField;

commit 77dedd47f075966e77fe07475dfa95cdffea8683

Fixed regression in Password field from c059b63
The password was being reset after changing another list item.

packages/fields/src/types/Password/views/Field.js
@@ -20,10 +20,12 @@ const PasswordField = ({ onChange, autoFocus, field, value: serverValue, errors,
   const [inputConfirm, setInputConfirm] = useState('');
 
   useEffect(() => {
-    onChange({
-      inputPassword,
-      inputConfirm,
-    });
+    if (isEditing) {
+      onChange({
+        inputPassword,
+        inputConfirm,
+      });
+    }
   }, [inputPassword, inputConfirm]);
 
   useEffect(() => {