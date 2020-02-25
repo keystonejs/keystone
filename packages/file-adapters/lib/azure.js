a8637e7b7a66dfc7d9a11842fcef0ee24c63a668
allan-ferencz <allan.ferencz@slalom.com>
2020-02-24 20:21:49 -0500

    Yarn Format

--- packages/file-adapters/lib/azure.js
+++ packages/file-adapters/lib/azure.js
@@ -2,28 +2,32 @@ const urlJoin = require('url-join');
 const { BlobServiceClient } = require('@azure/storage-blob');
 
 module.exports = class AzureAdapter {
-  constructor({ accountName , accountKey, containerName }) {
+  constructor({ accountName, accountKey, containerName }) {
     this.accountName = accountName;
     this.containerName = containerName;
-    const blobServiceClient = BlobServiceClient.fromConnectionString(`DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net`);
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      `DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net`
+    );
     this.containerClient = blobServiceClient.getContainerClient(this.containerName);
   }
 
   save({ stream, filename: originalFilename, id, mimetype, encoding }) {
     const filename = this.getFilename({ id, originalFilename });
     if (!filename) {
-      throw new Error(
-        'Custom function AzureAdapter.getFilename() returned no or invalid value.'
-      );
+      throw new Error('Custom function AzureAdapter.getFilename() returned no or invalid value.');
     }
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const blockBlobClient = this.containerClient.getBlockBlobClient(filename);
       try {
-        const uploadBlobResponse = await blockBlobClient.uploadStream(stream, undefined, undefined, { blobHTTPHeaders: { blobContentType: mimetype, blobContentEncoding: encoding } });
-        if(uploadBlobResponse.requestId){
+        const uploadBlobResponse = await blockBlobClient.uploadStream(
+          stream,
+          undefined,
+          undefined,
+          { blobHTTPHeaders: { blobContentType: mimetype, blobContentEncoding: encoding } }
+        );
+        if (uploadBlobResponse.requestId) {
           resolve({ ...uploadBlobResponse, filename });
-        }
-        else{
+        } else {
           reject();
         }
       } catch (error) {
@@ -39,21 +43,19 @@ module.exports = class AzureAdapter {
 
   delete(file) {
     if (!file) throw new Error("Missing required argument 'file'.");
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const blockBlobClient = this.containerClient.getBlockBlobClient(file.filename);
       try {
         const uploadBlobResponse = await blockBlobClient.delete();
-        if(uploadBlobResponse.requestId){
+        if (uploadBlobResponse.requestId) {
           resolve({ ...uploadBlobResponse });
-        }
-        else{
+        } else {
           reject();
         }
       } catch (error) {
         reject();
       }
     });
-
   }
 
   getFilename({ id, originalFilename }) {
@@ -61,6 +63,10 @@ module.exports = class AzureAdapter {
   }
 
   publicUrl({ filename }) {
-    return urlJoin(`https://${this.accountName}.blob.core.windows.net`, this.containerName, filename);
+    return urlJoin(
+      `https://${this.accountName}.blob.core.windows.net`,
+      this.containerName,
+      filename
+    );
   }
 };
