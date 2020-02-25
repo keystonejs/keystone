1020921c2608344c7a61aca0b66d652e6a70313a
allan-ferencz <allan.ferencz@slalom.com>
2020-02-24 20:15:33 -0500

    Added Azure File Adapter

--- packages/file-adapters/index.js
+++ packages/file-adapters/index.js
@@ -1,5 +1,6 @@
 const LocalFileAdapter = require('./lib/local-file');
 const CloudinaryAdapter = require('./lib/cloudinary');
 const S3Adapter = require('./lib/s3');
+const AzureAdapter = require('./lib/azure');
 
-module.exports = { LocalFileAdapter, CloudinaryAdapter, S3Adapter };
+module.exports = { LocalFileAdapter, CloudinaryAdapter, S3Adapter, AzureAdapter };
---
+++ packages/file-adapters/lib/azure.js
@@ -0,0 +1,66 @@
+const urlJoin = require('url-join');
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+module.exports = class AzureAdapter {
+  constructor({ accountName , accountKey, containerName }) {
+    this.accountName = accountName;
+    this.containerName = containerName;
+    const blobServiceClient = BlobServiceClient.fromConnectionString(`DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net`);
+    this.containerClient = blobServiceClient.getContainerClient(this.containerName);
+  }
+
+  save({ stream, filename: originalFilename, id, mimetype, encoding }) {
+    const filename = this.getFilename({ id, originalFilename });
+    if (!filename) {
+      throw new Error(
+        'Custom function AzureAdapter.getFilename() returned no or invalid value.'
+      );
+    }
+    return new Promise(async(resolve, reject) => {
+      const blockBlobClient = this.containerClient.getBlockBlobClient(filename);
+      try {
+        const uploadBlobResponse = await blockBlobClient.uploadStream(stream, undefined, undefined, { blobHTTPHeaders: { blobContentType: mimetype, blobContentEncoding: encoding } });
+        if(uploadBlobResponse.requestId){
+          resolve({ ...uploadBlobResponse, filename });
+        }
+        else{
+          reject();
+        }
+      } catch (error) {
+        reject();
+      }
+    });
+  }
+
+  /**
+   * Deletes the given file from Azure Blob Storage
+   * @param file file field data
+   */
+
+  delete(file) {
+    if (!file) throw new Error("Missing required argument 'file'.");
+    return new Promise(async(resolve, reject) => {
+      const blockBlobClient = this.containerClient.getBlockBlobClient(file.filename);
+      try {
+        const uploadBlobResponse = await blockBlobClient.delete();
+        if(uploadBlobResponse.requestId){
+          resolve({ ...uploadBlobResponse });
+        }
+        else{
+          reject();
+        }
+      } catch (error) {
+        reject();
+      }
+    });
+
+  }
+
+  getFilename({ id, originalFilename }) {
+    return `${id}-${originalFilename}`;
+  }
+
+  publicUrl({ filename }) {
+    return urlJoin(`https://${this.accountName}.blob.core.windows.net`, this.containerName, filename);
+  }
+};
