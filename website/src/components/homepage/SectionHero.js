bc34a1f4b8eb7dcf92e5b5fa1bbeb828bfb41f60
Jordan Overbye <jordanoverbye@gmail.com>
2020-03-04 19:39:07 +1000

    Update homepage

--- website/src/components/homepage/SectionHero.js
+++ website/src/components/homepage/SectionHero.js
@@ -6,48 +6,51 @@ import { colors } from '@arch-ui/theme';
 import { media, mediaMax, mq } from '../../utils/media';
 
 import { HomepageSection } from './HomepageSection';
+import { VideoIntro } from './VideoIntro';
 
 const SectionHero = () => (
   <HomepageSection>
-    <div css={{ textAlign: 'center', maxWidth: 800, margin: '0 auto' }}>
-      <Heading>An open-source, scalable platform and CMS to build NodeJS applications</Heading>
-    </div>
-    <div css={{ textAlign: 'center', maxWidth: 640, margin: '0 auto' }}>
+    <Grid>
       <div>
+        <Heading>An open-source, scalable platform and CMS to build NodeJS applications</Heading>
         <p css={{ fontSize: '1.25rem', lineHeight: 1.3, color: colors.N60 }}>
           KeystoneJS comes with first-class GraphQL support, a highly extensible architecture, and a
           wonderful Admin UI.
         </p>
+        <ButtonWrapper>
+          <Button
+            appearance="primary"
+            variant="solid"
+            to="/quick-start/"
+            css={{
+              [media.sm]: { marginRight: 4 },
+            }}
+          >
+            Get Started
+          </Button>
+          <Button
+            variant="link"
+            href="https://github.com/keystonejs/keystone-5"
+            css={{
+              [media.sm]: { marginLeft: 4 },
+            }}
+            target="_blank"
+          >
+            View on GitHub
+          </Button>
+        </ButtonWrapper>
       </div>
-      <ButtonWrapper>
-        <Button
-          appearance="primary"
-          variant="solid"
-          to="/quick-start/"
-          css={{
-            [media.sm]: { marginRight: 4 },
-          }}
-        >
-          Get Started
-        </Button>
-        <Button
-          variant="link"
-          href="https://github.com/keystonejs/keystone-5"
-          css={{
-            [media.sm]: { marginLeft: 4 },
-          }}
-          target="_blank"
-        >
-          View on GitHub
-        </Button>
-      </ButtonWrapper>
-    </div>
+      <VideoIntro />
+    </Grid>
   </HomepageSection>
 );
 
-// ==============================
-// Misc
-// ==============================
+const Grid = props => (
+  <div
+    css={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '1.5rem' }}
+    {...props}
+  />
+);
 
 const Heading = props => (
   <h1
@@ -66,7 +69,6 @@ const ButtonWrapper = props => (
     css={mq({
       alignItems: 'center',
       display: 'flex',
-      justifyContent: 'center',
       margin: ['1em 0', '1em 0', '1em 0', '2em 0'],
 
       [mediaMax.xs]: {
