16d9c73445e7770d152199f16035a240f6d8377f
Jordan Overbye <jordanoverbye@gmail.com>
2020-03-10 08:34:28 +1000

    website: Redesign of homepage (#2475)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,58 +6,33 @@ import { globalStyles } from '@arch-ui/theme';
 import { SkipNavContent } from '@reach/skip-nav';
 
 import Layout from '../templates/layout';
-import { HomepageContent } from '../components/homepage/HomepageContent';
-import { VideoIntro } from '../components/homepage/VideoIntro';
-import { CONTAINER_GUTTERS, CONTAINER_WIDTH } from '../components/Container';
-import { HEADER_HEIGHT } from '../components/Header';
 import { Container, Sidebar } from '../components';
-import { mq } from '../utils/media';
 
-export default () => (
-  <Layout>
-    {({ sidebarIsVisible, sidebarOffset }) => (
-      <>
-        <Global styles={globalStyles} />
-        <Container>
-          <Sidebar isVisible={sidebarIsVisible} offsetTop={sidebarOffset} mobileOnly />
-        </Container>
-        <Hero />
-      </>
-    )}
-  </Layout>
+import { SectionHero } from '../components/homepage/SectionHero';
+import { SectionCode } from '../components/homepage/SectionCode';
+import { SectionFeatures } from '../components/homepage/SectionFeatures';
+import { SectionTechnology } from '../components/homepage/SectionTechnology';
+import { HomepageFooter } from '../components/homepage/HomepageFooter';
+
+const Homepage = () => (
+  <>
+    <Layout>
+      {({ sidebarIsVisible, sidebarOffset }) => (
+        <>
+          <Global styles={globalStyles} />
+          <Container>
+            <Sidebar isVisible={sidebarIsVisible} offsetTop={sidebarOffset} mobileOnly />
+          </Container>
+          <SkipNavContent />
+          <SectionHero />
+          <SectionCode />
+          <SectionFeatures />
+          <SectionTechnology />
+          <HomepageFooter />
+        </>
+      )}
+    </Layout>
+  </>
 );
 
-const CustomContainer = props => (
-  <div
-    css={mq({
-      boxSizing: 'border-box',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      paddingLeft: CONTAINER_GUTTERS,
-      paddingRight: CONTAINER_GUTTERS,
-      width: [null, null, 992, CONTAINER_WIDTH],
-    })}
-    {...props}
-  />
-);
-
-const Hero = () => (
-  <div css={{ overflow: 'hidden' }}>
-    <SkipNavContent />
-    <CustomContainer
-      css={mq({
-        maxWidth: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: ['flex-start', 'flex-start', 'flex-start', 'center'],
-        flexDirection: ['column', 'column', 'column', 'row'],
-        fontSize: [14, 18],
-        lineHeight: 1.6,
-        minHeight: `calc(100vh - ${HEADER_HEIGHT}px)`,
-      })}
-    >
-      <HomepageContent />
-      <VideoIntro />
-    </CustomContainer>
-  </div>
-);
+export default Homepage;
