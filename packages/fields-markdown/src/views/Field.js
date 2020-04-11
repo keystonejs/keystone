k5ui/k5js

consume isReadOnly in all the other non core fields

packages/fields-markdown/src/views/Field.js
@@ -60,7 +60,7 @@ let IconToolbarButton = ({ isActive, label, icon, tooltipPlacement = 'top', ...p
   );
 };
 
-export default function MarkdownField({ field, errors, value, onChange }) {
+export default function MarkdownField({ field, errors, value, onChange, isReadOnly }) {
   const htmlID = `ks-input-${field.path}`;
   const accessError = errors.find(
     error => error instanceof Error && error.name === 'AccessDeniedError'
@@ -81,6 +81,7 @@ export default function MarkdownField({ field, errors, value, onChange }) {
               icon={<tool.icon />}
               onClick={onClick}
               label={tool.label}
+              isDisabled={isReadOnly}
             />
           );
         })}
@@ -122,6 +123,7 @@ export default function MarkdownField({ field, errors, value, onChange }) {
             tabSize: '2',
             lineWrapping: true,
             addModeClass: true,
+            readOnly: isReadOnly,
           }}
           editorDidMount={editor => {
             setTools(getTools(editor));

keystonejs/keystone

packages/fields-markdown/src/views/Field.js
@@ -60,7 +60,7 @@ let IconToolbarButton = ({ isActive, label, icon, tooltipPlacement = 'top', ...p
   );
 };
 
-export default function MarkdownField({ field, errors, value, onChange }) {
+export default function MarkdownField({ field, errors, value, onChange, isReadOnly }) {
   const htmlID = `ks-input-${field.path}`;
   const accessError = errors.find(
     error => error instanceof Error && error.name === 'AccessDeniedError'
@@ -81,6 +81,7 @@ export default function MarkdownField({ field, errors, value, onChange }) {
               icon={<tool.icon />}
               onClick={onClick}
               label={tool.label}
+              disabled={isReadOnly}
             />
           );
         })}
@@ -122,6 +123,7 @@ export default function MarkdownField({ field, errors, value, onChange }) {
             tabSize: '2',
             lineWrapping: true,
             addModeClass: true,
+            readOnly: isReadOnly,
           }}
           editorDidMount={editor => {
             setTools(getTools(editor));