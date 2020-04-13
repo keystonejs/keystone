0ebaf2b61870c2adf749537f82de2680094c91b7
Charles Dang <exodia339@gmail.com>
2020-04-13 19:02:00 +1100

    Can't have a ref

--- packages/fields/src/types/Checkbox/views/Filter.js
+++ packages/fields/src/types/Checkbox/views/Filter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { RadioGroup, Radio } from '@arch-ui/filters';
 
-const CheckboxFilterView = ({ onChange, filter, innerRef, value }) => {
+const CheckboxFilterView = ({ onChange, filter, value }) => {
   if (!filter) return null;
 
   return (
-    <RadioGroup onChange={onChange} ref={innerRef} value={value}>
+    <RadioGroup onChange={onChange} value={value}>
       <Radio value="true">Checked</Radio>
       <Radio value="false">Unchecked</Radio>
       <Radio value="null">Not set</Radio>
