d9ef5495cd74501c247566c067102460e55a692d
Charles Dang <exodia339@gmail.com>
2020-04-20 11:03:03 +1100

    Remove all other css properties

--- packages/arch/packages/input/src/HiddenInput.js
+++ packages/arch/packages/input/src/HiddenInput.js
@@ -6,21 +6,5 @@ import { forwardRef } from 'react';
 // ------------------------------
 
 export const HiddenInput = forwardRef((props, ref) => (
-  <input
-    ref={ref}
-    tabIndex="-1"
-    css={{
-      display: 'none',
-      border: 0,
-      height: 1,
-      margin: 0,
-      opacity: 0,
-      overflow: 'hidden',
-      padding: 0,
-      position: 'absolute',
-      whiteSpace: 'nowrap',
-      width: 1,
-    }}
-    {...props}
-  />
+  <input ref={ref} css={{ display: 'none' }} {...props} />
 ));
