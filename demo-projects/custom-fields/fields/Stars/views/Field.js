53a8c102128b86fdfb0714eb36ec0267c92b9c18
Mike <mike@madebymike.com.au>
2020-04-22 16:29:48 +1000

    Update docs

--- demo-projects/custom-fields/fields/Stars/views/Field.js
+++ demo-projects/custom-fields/fields/Stars/views/Field.js
@@ -4,21 +4,12 @@ import { jsx } from '@emotion/core';
 import { FieldContainer, FieldLabel, FieldInput } from '@arch-ui/fields';
 import Stars from './Stars';
 
-const StarsField = ({ field, value, errors, onChange }) => {
-  const handleChange = newValue => {
-    onChange(newValue);
-  };
-
-  const { starCount } = field.config;
-  const htmlID = `ks-input-${field.path}`;
-
-  return (
-    <FieldContainer>
-      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-      <FieldInput>
-        <Stars count={starCount} value={value} onChange={handleChange} />
-      </FieldInput>
-    </FieldContainer>
-  );
-};
+const StarsField = ({ field, value, errors, onChange }) => (
+  <FieldContainer>
+    <FieldLabel htmlFor={`ks-input-${field.path}`} field={field} errors={errors} />
+    <FieldInput>
+      <Stars count={field.config.starCount} value={value} onChange={onChange} />
+    </FieldInput>
+  </FieldContainer>
+);
 export default StarsField;
