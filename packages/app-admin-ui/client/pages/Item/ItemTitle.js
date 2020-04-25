8a135a88ae6f3a4434db0ba7033cad2e5f18651e
Charles Dang <exodia339@gmail.com>
2020-04-26 00:21:20 +1100

    Fixed list-level `adminDoc` not actually doing anything (#2808)
    * Fixed list-level `adminDoc` not actually doing anything
    
    * Cleaned up ItemTitle
    
    Co-authored-by: Jed Watson <jed.watson@me.com>

--- packages/app-admin-ui/client/pages/Item/ItemTitle.js
+++ packages/app-admin-ui/client/pages/Item/ItemTitle.js
@@ -5,6 +5,7 @@ import { memo } from 'react';
 import { ChevronLeftIcon } from '@arch-ui/icons';
 import { FlexGroup } from '@arch-ui/layout';
 import { IconButton } from '@arch-ui/button';
+import { gridSize } from '@arch-ui/theme';
 import { PageTitle } from '@arch-ui/typography';
 
 import { Search } from './Search';
@@ -12,15 +13,22 @@ import { useUIHooks } from '../../providers/Hooks';
 import { useList } from '../../providers/List';
 import AddNewItem from './AddNewItem';
 import ItemId from './ItemId';
-import { HeaderInset } from '../Home/components';
+
+import ListDescription from '../../components/ListDescription';
+
+const Container = ({ children }) => {
+  const padding = gridSize * 2;
+  return <div css={{ paddingLeft: padding, paddingRight: padding }}>{children}</div>;
+};
 
 export const ItemTitle = memo(function ItemTitle({ titleText, adminPath }) {
   const { list } = useList();
   const listHref = `${adminPath}/${list.path}`;
   const { itemHeaderActions } = useUIHooks();
   return (
-    <HeaderInset>
+    <Container>
       <PageTitle>{titleText}</PageTitle>
+      <ListDescription text={list.adminDoc} />
       <FlexGroup align="center" justify="space-between" css={{ marginBottom: '0.9rem' }}>
         <div>
           <IconButton
@@ -42,6 +50,6 @@ export const ItemTitle = memo(function ItemTitle({ titleText, adminPath }) {
           </div>
         )}
       </FlexGroup>
-    </HeaderInset>
+    </Container>
   );
 });
