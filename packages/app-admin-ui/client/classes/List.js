76ec31d8f0ae7ba38268c0eb310b8556f589db1f
Tim Leslie <timl@thinkmill.com.au>
2020-05-06 14:32:49 +1000

    Add List.fullPath

--- packages/app-admin-ui/client/classes/List.js
+++ packages/app-admin-ui/client/classes/List.js
@@ -21,6 +21,7 @@ export default class List {
     this.path = path;
     this.plural = plural;
     this.singular = singular;
+    this.fullpath = `${adminMeta.adminPath}/${path}`;
 
     this.fields = fields.map(fieldConfig => {
       const [Controller] = readViews([views[fieldConfig.path].Controller]);
