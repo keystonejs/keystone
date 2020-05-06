b6fb47ed74bba769fcd1c5e4460814c9a3fcd018
Tomasz Tunik <tomasztunik@gmail.com>
2020-05-06 18:43:13 +0200

    fix formatting for prettier code

--- packages/app-admin-ui/client/components/AnimateHeight.js
+++ packages/app-admin-ui/client/components/AnimateHeight.js
@@ -78,9 +78,7 @@ export default class AnimateHeight extends Component {
       <div
         css={{
           height,
-          transition: isTransitioning
-            ? 'height 220ms cubic-bezier(0.2, 0, 0, 1)'
-            : 'none',
+          transition: isTransitioning ? 'height 220ms cubic-bezier(0.2, 0, 0, 1)' : 'none',
           overflow,
         }}
         onTransitionEnd={event => {
