ced7665a6aa407c18b3b0d0cf1490b50b7c7183c
Charles Dang <exodia339@gmail.com>
2020-05-13 16:03:13 +1100

    Improved at-tracking date display format

--- packages/list-plugins/lib/tracking/atTracking.js
+++ packages/list-plugins/lib/tracking/atTracking.js
@@ -8,7 +8,7 @@ const _atTracking = ({ created = true, updated = true }) => ({
 } = {}) => ({ fields = {}, hooks = {}, ...rest }) => {
   const dateTimeOptions = {
     type: DateTime,
-    format: 'MM/dd/yyyy h:mm a',
+    format: 'MMMM do, yyyy - h:mm a',
     access: {
       read: true,
       create: false,
