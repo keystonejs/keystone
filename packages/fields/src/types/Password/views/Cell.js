a1dca3bbc65adb72ef5a8ffae8b22926ee857dc2
Charles Dang <exodia339@gmail.com>
2020-05-13 15:52:14 +1100

    Formatting

--- packages/fields/src/types/Password/views/Cell.js
+++ packages/fields/src/types/Password/views/Cell.js
@@ -4,7 +4,11 @@ import { jsx } from '@emotion/core';
 import { colors } from '@arch-ui/theme';
 
 const PasswordCellView = ({ item: { password_is_set } = {} }) => {
-  return password_is_set ? 'Is set' : <span css={{ color: colors.danger, fontWeight: 'bold' }}>Not set</span>;
+  return password_is_set ? (
+    'Is set'
+  ) : (
+    <span css={{ color: colors.danger, fontWeight: 'bold' }}>Not set</span>
+  );
 };
 
 export default PasswordCellView;
