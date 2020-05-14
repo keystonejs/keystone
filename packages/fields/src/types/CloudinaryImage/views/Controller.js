fc9939c3e564696474a432b2156f9953c8d9704a
Charles Dang <exodia339@gmail.com>
2020-05-14 20:52:24 +1100

    Added originalFilename to the query fragment to match the File field

--- packages/fields/src/types/CloudinaryImage/views/Controller.js
+++ packages/fields/src/types/CloudinaryImage/views/Controller.js
@@ -3,12 +3,13 @@ import FieldController from '../../File/views/Controller';
 export default class CloudinaryImageController extends FieldController {
   getQueryFragment = () => `
     ${this.path} {
-       id
-       path
-       filename
-       mimetype
-       encoding
-       publicUrlTransformed(transformation: { width: "120" crop: "limit" })
+      id
+      path
+      filename
+      originalFilename
+      mimetype
+      encoding
+      publicUrlTransformed(transformation: { width: "120" crop: "limit" })
     }
   `;
 }
