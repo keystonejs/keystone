5d94264b485b9cca58d0b7050cf25057991264e6
Tim Leslie <tim.leslie@gmail.com>
2020-05-21 15:25:26 +1000

    Apply suggestions from code review

--- api-tests/relationships/nested-mutations/connect-singular.test.js
+++ api-tests/relationships/nested-mutations/connect-singular.test.js
@@ -27,8 +27,12 @@ function setupKeystone(adapterName) {
       });
 
       keystone.createList('GroupNoRead', {
-        fields: { name: { type: Text } },
-        access: { read: () => false },
+        fields: {
+          name: { type: Text },
+        },
+        access: {
+          read: () => false,
+        },
       });
 
       keystone.createList('EventToGroupNoRead', {
@@ -51,8 +55,12 @@ function setupKeystone(adapterName) {
       });
 
       keystone.createList('GroupNoCreate', {
-        fields: { name: { type: Text } },
-        access: { create: () => false },
+        fields: {
+          name: { type: Text },
+        },
+        access: {
+          create: () => false,
+        },
       });
 
       keystone.createList('EventToGroupNoCreate', {
