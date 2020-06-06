dbd2eed3ab7d510be2f5aab521d98deffae8ff36
Jed Watson <jed.watson@me.com>
2020-06-07 00:31:31 +1000

    More experimentation with the document editor

--- packages/fields-document/src/views/Field.js
+++ packages/fields-document/src/views/Field.js
@@ -2,22 +2,30 @@
 
 import { jsx } from '@emotion/core';
 import { useCallback, useMemo } from 'react';
-import { Slate, Editable, withReact } from 'slate-react';
+import { Slate, Editable, ReactEditor, useSlate, withReact } from 'slate-react';
 import { createEditor, Editor, Transforms, Text } from 'slate';
 import { withHistory } from 'slate-history';
 
 import { FieldContainer, FieldLabel, FieldDescription } from '@arch-ui/fields';
 
-/** vvvvvv */
-
-// const Element = ({ attributes, children, element }) => {
-//   switch (element.type) {
-//     case 'title':
-//       return <h2 {...attributes}>{children}</h2>;
-//     case 'paragraph':
-//       return <p {...attributes}>{children}</p>;
-//   }
-// };
+const Button = ({ isPressed, ...props }) => (
+  <button
+    type="button"
+    css={{
+      background: isPressed ? '#f3f3f3' : 'white',
+      borderColor: isPressed ? '#aaa' : '#ccc',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      borderRadius: 5,
+      boxShadow: isPressed ? 'inset 0px 3px 5px -4px rgba(0,0,0,0.50)' : undefined,
+      marginRight: 4,
+      ':hover': {
+        borderColor: isPressed ? '#666' : '#999',
+      },
+    }}
+    {...props}
+  />
+);
 
 const DocumentEditor = {
   // Bold
@@ -58,11 +66,7 @@ const DocumentEditor = {
 
   // Code
   isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.type === 'code',
-    });
-
-    return !!match;
+    return isBlockActive(editor, 'code');
   },
   toggleCodeBlock(editor) {
     const isActive = DocumentEditor.isCodeBlockActive(editor);
@@ -73,7 +77,21 @@ const DocumentEditor = {
     );
   },
 
-  // Event Handlers
+  // Access Boundary
+  insertAccessBoundary(editor) {
+    if (isBlockActive(editor, 'access-boundary')) return;
+    const children = [{ type: 'i', text: 'one' }, { text: 'two' }];
+    const boundary = { type: 'access-boundary', roles: [], children };
+    Transforms.insertNodes(editor, boundary);
+  },
+};
+
+const isBlockActive = (editor, format) => {
+  const [match] = Editor.nodes(editor, {
+    match: n => n.type === format,
+  });
+
+  return !!match;
 };
 
 const getKeyDownHandler = editor => event => {
@@ -82,7 +100,6 @@ const getKeyDownHandler = editor => event => {
   }
 
   switch (event.key) {
-    // When "`" is pressed, keep our existing code block logic.
     case '`': {
       event.preventDefault();
       DocumentEditor.toggleCodeBlock(editor);
@@ -105,39 +122,99 @@ const getKeyDownHandler = editor => event => {
 
 /* UI Components */
 
-const Toolbar = ({ editor }) => (
-  <div>
-    <button
-      type="button"
-      onMouseDown={event => {
-        event.preventDefault();
-        DocumentEditor.toggleBoldMark(editor);
-      }}
-    >
-      Bold
-    </button>
-    <button
-      type="button"
-      onMouseDown={event => {
-        event.preventDefault();
-        DocumentEditor.toggleItalicMark(editor);
-      }}
-    >
-      Italic
-    </button>
-    <button
-      type="button"
-      onMouseDown={event => {
-        event.preventDefault();
-        DocumentEditor.toggleCodeBlock(editor);
-      }}
-    >
-      Code Block
-    </button>
-  </div>
-);
+const Toolbar = () => {
+  const editor = useSlate();
+  return (
+    <div>
+      <Button
+        isPressed={DocumentEditor.isBoldMarkActive(editor)}
+        onMouseDown={event => {
+          event.preventDefault();
+          DocumentEditor.toggleBoldMark(editor);
+        }}
+      >
+        Bold
+      </Button>
+      <Button
+        isPressed={DocumentEditor.isItalicMarkActive(editor)}
+        onMouseDown={event => {
+          event.preventDefault();
+          DocumentEditor.toggleItalicMark(editor);
+        }}
+      >
+        Italic
+      </Button>
+      <Button
+        isPressed={DocumentEditor.isCodeBlockActive(editor)}
+        onMouseDown={event => {
+          event.preventDefault();
+          DocumentEditor.toggleCodeBlock(editor);
+        }}
+      >
+        Code
+      </Button>
+      <Button
+        onMouseDown={event => {
+          event.preventDefault();
+          DocumentEditor.insertAccessBoundary(editor);
+        }}
+      >
+        access
+      </Button>
+    </div>
+  );
+};
 
 /* Block Elements */
+
+const accessRoles = ['public', 'member', 'admin'];
+
+const AccessBoundaryElement = ({ attributes, children, element }) => {
+  const editor = useSlate();
+  const elementRoles = Array.isArray(element.roles) ? element.roles : [];
+  return (
+    <div
+      css={{
+        margin: '8px 0',
+        border: '3px dashed #eee',
+        borderRadius: 5,
+      }}
+      {...attributes}
+    >
+      <div
+        contentEditable={false}
+        style={{
+          backgroundColor: '#f9f9f9',
+          borderBottom: '1px solid #eee',
+          padding: 8,
+          userSelect: 'none',
+        }}
+      >
+        <span css={{ marginRight: 8 }}>Restrict to:</span>
+        {accessRoles.map(role => {
+          const roleIsSelected = elementRoles.includes(role);
+          return (
+            <Button
+              key={role}
+              isPressed={roleIsSelected}
+              onMouseDown={event => {
+                event.preventDefault();
+                const path = ReactEditor.findPath(editor, element);
+                const newRoles = roleIsSelected
+                  ? elementRoles.filter(i => i !== role)
+                  : [...elementRoles, role];
+                Transforms.setNodes(editor, { roles: newRoles }, { at: path });
+              }}
+            >
+              {role}
+            </Button>
+          );
+        })}
+      </div>
+      <div css={{ padding: 8 }}>{children}</div>
+    </div>
+  );
+};
 
 const CodeElement = props => {
   return (
@@ -167,16 +244,37 @@ const Leaf = props => {
   );
 };
 
+const withAccess = editor => {
+  const { insertBreak } = editor;
+  editor.insertBreak = () => {
+    const { selection } = editor;
+
+    if (selection) {
+      const [boundary] = Editor.nodes(editor, { match: n => n.type === 'access-boundary' });
+
+      if (boundary) {
+        console.log('inside access boundary');
+        return;
+      }
+    }
+
+    insertBreak();
+  };
+  return editor;
+};
+
 export default function DocumentField({ field, errors, value, onChange, isDisabled }) {
   const htmlID = `ks-input-${field.path}`;
   const accessError = errors.find(
     error => error instanceof Error && error.name === 'AccessDeniedError'
   );
 
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const editor = useMemo(() => withAccess(withHistory(withReact(createEditor()))), []);
 
   const renderElement = useCallback(props => {
     switch (props.element.type) {
+      case 'access-boundary':
+        return <AccessBoundaryElement {...props} />;
       case 'code':
         return <CodeElement {...props} />;
       default:
