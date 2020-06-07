dd8626079e3a19dc62028a3301dc1ee350b8f7e3
Jed Watson <jed.watson@me.com>
2020-06-07 16:22:56 +1000

    Implemented access security for document field access over GraphQL

--- packages/fields-document/src/index.js
+++ packages/fields-document/src/index.js
@@ -1,9 +1,11 @@
 import { Text } from '@keystonejs/fields';
 import { importView } from '@keystonejs/build-field-types';
 
+import { DocumentImplementation } from './Implementation';
+
 export const DocumentField = {
   type: 'Document',
-  implementation: Text.implementation,
+  implementation: DocumentImplementation,
   views: {
     Controller: importView('./views/Controller'),
     Field: importView('./views/Field'),
