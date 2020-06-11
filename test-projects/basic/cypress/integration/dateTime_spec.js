4c208898cdb539b54f843e49e02b08b3311c355c
Mike <mike@madebymike.com.au>
2020-06-11 10:35:26 +1000

    Remove cypress test

--- test-projects/basic/cypress/integration/dateTime_spec.js
+++ test-projects/basic/cypress/integration/dateTime_spec.js
@@ -28,32 +28,6 @@ describe('CalendarDay Component - Formatting', () => {
   });
 });
 
-describe('CalendarDay Component - Functionality', () => {
-  beforeEach(() => {
-    cy.visit(path);
-    cy.get(`#ks-list-table tbody > tr:nth-child(1) > td:nth-child(2) > a`).click({ force: true });
-  });
-
-  it('can accept natural language like today', () => {
-    cy.get(calendarDayInputSelector).type('today', { force: true });
-    cy.get('label:contains("Name")').click({ force: true });
-
-    cy.get(calendarDayInputSelector).should('have.value', '3rd March 2019');
-  });
-
-  it(`can accept natural language like tomorrow`, () => {
-    cy.get(calendarDayInputSelector).type('tomorrow', { force: true });
-    cy.get('label:contains("Name")').click({ force: true });
-    cy.get(calendarDayInputSelector).should('have.value', '4th March 2019');
-  });
-
-  it(`can accept a date`, () => {
-    cy.get(calendarDayInputSelector).type('20 september 2015', { force: true });
-    cy.get('label:contains("Name")').click({ force: true });
-    cy.get(calendarDayInputSelector).should('have.value', '20th September 2015');
-  });
-});
-
 ///// Begin Date Time Component
 
 describe('DateTime Component - Formatting', () => {
