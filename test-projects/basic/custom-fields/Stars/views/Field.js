3300182713cdc832dbc40dcb0d4adea1fba58a11
Charles Dang <exodia339@gmail.com>
2020-06-17 18:28:15 +1100

    Converted a stray test project component to functional

--- test-projects/basic/custom-fields/Stars/views/Field.js
+++ test-projects/basic/custom-fields/Stars/views/Field.js
@@ -3,23 +3,22 @@ import React from 'react';
 import { FieldContainer, FieldLabel, FieldInput } from '@arch-ui/fields';
 import Stars from './Stars';
 
-export default class StarsField extends React.Component {
-  handleChange = newValue => {
-    this.props.onChange(newValue);
+const StarsField = ({ field, value, errors, onChange }) => {
+  const handleChange = newValue => {
+    onChange(newValue);
   };
 
-  render() {
-    const { field, value, errors } = this.props;
-    const { starCount } = field.config;
-    const htmlID = `ks-input-${field.path}`;
+  const { starCount } = field.config;
+  const htmlID = `ks-input-${field.path}`;
 
-    return (
-      <FieldContainer>
-        <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
-        <FieldInput>
-          <Stars count={starCount} value={value} onChange={this.handleChange} />
-        </FieldInput>
-      </FieldContainer>
-    );
-  }
-}
+  return (
+    <FieldContainer>
+      <FieldLabel htmlFor={htmlID} field={field} errors={errors} />
+      <FieldInput>
+        <Stars count={starCount} value={value} onChange={handleChange} />
+      </FieldInput>
+    </FieldContainer>
+  );
+};
+
+export default StarsField;
