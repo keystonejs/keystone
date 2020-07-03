534866b264eb22d21952ef205529e174357feec0
Aman Singh <aman.singh@thinkmill.com.au>
2020-07-03 15:11:04 +1000

    Add update usage of Apollo (NextJs pattern)
    Ref: https://github.com/zeit/next.js/tree/2789e7e0c25c72fbf6be3ef070e530186af14430/examples/with-apollo
    
    close #3128

---
+++ .changeset/light-llamas-wonder.md
@@ -0,0 +1,5 @@
+---
+'@keystonejs/demo-project-meetup': patch
+---
+
+Updated usage of Apollo based on Next.js [example](https://github.com/zeit/next.js/tree/2789e7e0c25c72fbf6be3ef070e530186af14430/examples/with-apollo)
--- demo-projects/meetup/site/pages/_app.js
+++ demo-projects/meetup/site/pages/_app.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import gql from 'graphql-tag';
-import { ApolloProvider } from '@apollo/react-hooks';
 import { ToastProvider } from 'react-toast-notifications';
 
-import withApollo from '../lib/withApollo';
+import { withApollo } from '../lib/apollo';
 import { AuthProvider } from '../lib/authentication';
 import StylesBase from '../primitives/StylesBase';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 
-const MyApp = ({ Component, pageProps, apolloClient, user }) => {
+const MyApp = ({ Component, pageProps, user }) => {
   return (
     <ToastProvider>
-      <ApolloProvider client={apolloClient}>
-        <AuthProvider initialUserValue={user}>
-          <Head>
-            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
-            />
-          </Head>
-          <StylesBase />
-          <Component {...pageProps} />
-        </AuthProvider>
-      </ApolloProvider>
+      <AuthProvider initialUserValue={user}>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+          />
+        </Head>
+        <StylesBase />
+        <Component {...pageProps} />
+      </AuthProvider>
       <GoogleAnalytics />
     </ToastProvider>
   );
