0e64979986c04219f608287c94a04bf1984d3fbd
Aman Singh <aman.singh@thinkmill.com.au>
2020-07-10 15:12:41 +1000

    Seed initial data for `basic` cypress test project
    Remove the old initial data format (where clause), and used the newly
    implemented `createItems` API to insert the initial data while resetting
    the database.
    
    The `seed` function demonstrates the usage of `createItems` API.

--- packages/orm/lib/orm.js
+++ packages/orm/lib/orm.js
@@ -70,11 +70,8 @@ const runChunkedMutation = async ({ pageSize = 500, items, ...rest }) => {
   const chunks = chunkArray(items, pageSize);
 
   const result = await Promise.all(
-    chunks.map(async chunk => {
-      return await runQuery({ ...rest, variables: { items: chunk } });
-    })
+    chunks.map(chunk => runQuery({ ...rest, variables: { items: chunk } }))
   );
-
   return result;
 };
 
@@ -126,7 +123,6 @@ const updateItems = ({ keystone, listName, items, returnFields = `id`, ...rest }
   const query = `mutation ($items: [${updateManyInputName}]){
     ${updateManyMutationName}(data: $items) { ${returnFields} }
   }`;
-
   return runChunkedMutation({ keystone, query, items, ...rest });
 };
 
