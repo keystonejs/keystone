d1ac65e68c2497f679804c8585baa7c64b79cf00
Aman Singh <aman.singh@thinkmill.com.au>
2020-07-17 17:30:59 +1000

    Add getItems where clause tests

--- packages/orm/lib/orm.js
+++ packages/orm/lib/orm.js
@@ -33,7 +33,13 @@ const runQuery = async ({
   return data;
 };
 
-const runPaginatedQuery = async ({ pageSize = 1, variables = {}, verbose = false, ...rest }) => {
+const runPaginatedQuery = async ({
+  pageSize = 1,
+  variables = {},
+  verbose = false,
+  where = {},
+  ...rest
+}) => {
   let skip = 0;
   let latestResult;
   const allItems = [];
@@ -48,7 +54,7 @@ const runPaginatedQuery = async ({ pageSize = 1, variables = {}, verbose = false
 
     const response = await runQuery({
       ...rest,
-      variables: { ...variables, first: pageSize, skip },
+      variables: { ...variables, first: pageSize, skip, where },
     });
 
     latestResult = response[Object.keys(response || {})[0]];
@@ -107,15 +113,15 @@ const createItems = ({ keystone, listName, items, returnFields = `id`, ...rest }
   });
 };
 
-const getItem = ({ keystone, listName, returnFields, item: id, ...rest }) => {
+const getItemById = ({ keystone, listName, returnFields, itemId: id, ...rest }) => {
   const { itemQueryName } = keystone.lists[listName].gqlNames;
   const query = `query ($id: ID!) { ${itemQueryName}(where: { id: $id }) { ${returnFields} }  }`;
   return runQuery({ keystone, query, variables: { id }, ...rest });
 };
 
-const getAllItems = ({ keystone, listName, returnFields, ...rest }) => {
-  const { listQueryName } = keystone.lists[listName].gqlNames;
-  const query = `query ($first: Int!, $skip: Int!) { ${listQueryName}(first: $first, skip: $skip) { ${returnFields} }  }`;
+const getItems = ({ keystone, listName, returnFields, ...rest }) => {
+  const { listQueryName, whereInputName } = keystone.lists[listName].gqlNames;
+  const query = `query ($first: Int!, $skip: Int!, $where: ${whereInputName}) { ${listQueryName}(first: $first, skip: $skip, where: $where) { ${returnFields} }  }`;
   return runPaginatedQuery({ keystone, query, pageSize: 500, ...rest });
 };
 
@@ -162,8 +168,8 @@ const deleteItems = ({ keystone, listName, items, returnFields = `id`, ...rest }
 
 module.exports = {
   runQuery,
-  getItem,
-  getAllItems,
+  getItemById,
+  getItems,
   createItems,
   createItem,
   updateItem,
