ad5f9a22632c505f36cb3fe1ad77784525ae1420
Mike <mike@madebymike.com.au>
2020-08-03 13:39:41 +1000

    Update tracking tests

--- packages/list-plugins/lib/tracking/byTracking.js
+++ packages/list-plugins/lib/tracking/byTracking.js
@@ -25,9 +25,12 @@ const _byTracking = ({ created = true, updated = true }) => ({
   }
 
   const newResolveInput = ({ resolvedData, operation, originalInput, context }) => {
+    // if no data received from the mutation, skip the update
+    if (Object.keys(originalInput).length === 0) {
+      return resolvedData;
+    }
+
     if (
-      // if no data received from the mutation, skip the update
-      Object.keys(originalInput).length === 0 &&
       // opted-in to updatedBy tracking
       updated &&
       // this is an update
