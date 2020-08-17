7b1cb363be7e393ca52a5e07831fcef440d86554
singhArmani <singh_aman@hotmail.com.au>
2020-08-17 13:10:11 +1000

    Add filter tests for `fields-color`

--- packages/fields-color/src/test-fixtures.js
+++ packages/fields-color/src/test-fixtures.js
@@ -1,7 +1,216 @@
+import { getItems } from '@keystonejs/server-side-graphql-client';
 import { Color } from '.';
+import { Text } from '@keystonejs/fields';
 
 export const name = 'Color';
 export { Color as type };
 export const exampleValue = '"red"';
 export const exampleValue2 = '"green"';
 export const supportsUnique = true;
+
+export const getTestFields = () => {
+  return {
+    name: { type: Text },
+    hexColor: { type: Color },
+  };
+};
+
+export const initItems = () => {
+  return [
+    { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+    { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+    { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+    { name: 'd', hexColor: null },
+  ];
+};
+
+export const filterTests = withKeystone => {
+  const match = async (keystone, where, expected, sortBy = 'name_ASC') =>
+    expect(
+      await getItems({
+        keystone,
+        listKey: 'test',
+        where,
+        returnFields: 'name hexColor',
+        sortBy,
+      })
+    ).toEqual(expected);
+
+  test(
+    'No filter',
+    withKeystone(({ keystone }) =>
+      match(keystone, undefined, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Empty filter',
+    withKeystone(({ keystone }) =>
+      match(keystone, {}, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor: 'rgba(45, 228, 39, 1)' }, [
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_not',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not: 'rgba(45, 228, 39, 1)' }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_not null',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not: null }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_in (empty list)',
+    withKeystone(({ keystone }) => match(keystone, { hexColor_in: [] }, []))
+  );
+
+  test(
+    'Filter: hexColor_not_in (empty list)',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not_in: [] }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_in',
+    withKeystone(({ keystone }) =>
+      match(
+        keystone,
+        {
+          hexColor_in: ['rgba(223, 57, 57, 1)', 'rgba(45, 228, 39, 1)'],
+        },
+        [
+          { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+          { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        ]
+      )
+    )
+  );
+
+  test(
+    'Filter: hexColor_not_in',
+    withKeystone(({ keystone }) =>
+      match(
+        keystone,
+        {
+          hexColor_not_in: [null, 'rgba(223, 57, 57, 1)', 'rgba(45, 228, 39, 1)'],
+        },
+        [{ name: 'c', hexColor: 'rgba(50, 121, 206, 1)' }]
+      )
+    )
+  );
+
+  test(
+    'Filter: hexColor_in null',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_in: [null] }, [{ name: 'd', hexColor: null }])
+    )
+  );
+
+  test(
+    'Filter: hexColor_not_in null',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not_in: [null] }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_contains',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_contains: '57' }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_not_contains',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not_contains: '57' }, [
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_starts_with',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_starts_with: 'rgba(45' }, [
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+      ])
+    )
+  );
+  test(
+    'Filter: hexColor_not_starts_with',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not_starts_with: 'rgba(45' }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_ends_with',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_ends_with: '39, 1)' }, [
+        { name: 'b', hexColor: 'rgba(45, 228, 39, 1)' },
+      ])
+    )
+  );
+
+  test(
+    'Filter: hexColor_not_ends_with',
+    withKeystone(({ keystone }) =>
+      match(keystone, { hexColor_not_ends_with: '39, 1)' }, [
+        { name: 'a', hexColor: 'rgba(223, 57, 57, 1)' },
+        { name: 'c', hexColor: 'rgba(50, 121, 206, 1)' },
+        { name: 'd', hexColor: null },
+      ])
+    )
+  );
+};
