6b9ec18fc710573db265945196f46cc336e72c28
Tim Leslie <timl@thinkmill.com.au>
2020-08-18 15:58:59 +1000

    Remove the obsolete field-content code from the repo

--- packages/field-content/index.js
+++
@@ -1,3 +0,0 @@
-throw new Error(
-  '`@keystonejs/field-content` has been renamed to `@keystonejs/fields-content`. Please install `@keystonejs/fields-content` and change your imports from `@keystonejs/field-content` to `@keystonejs/fields-content`'
-);
