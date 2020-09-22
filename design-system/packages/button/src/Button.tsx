2febbf8d10a665a035a3f7f76a0de72fb67c9e3e
Jed Watson <jed.watson@me.com>
2020-09-22 18:00:46 +1000

    New interfaces: Adding as prop support to Button (#3745)
    * Adding as prop support to Button
    
    * Tweaks to Navigation UI

--- design-system/packages/button/src/Button.tsx
+++ design-system/packages/button/src/Button.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { ReactNode, useContext } from 'react';
-import { jsx } from '@keystone-ui/core';
+import { forwardRefWithAs, jsx } from '@keystone-ui/core';
 import { LoadingDots } from '@keystone-ui/loading';
 
 import { ButtonContext } from './context';
@@ -32,36 +32,43 @@ const loadingContainerStyles = {
   transform: 'translateX(-50%)',
 } as const;
 
-export const Button = ({
-  children,
-  isDisabled,
-  isLoading,
-  size,
-  tone,
-  weight,
-  onPress,
-  type = 'button',
-  ...otherProps
-}: ButtonProps) => {
-  const { useButtonStyles, useButtonTokens, defaults } = useContext(ButtonContext);
-  const tokens = useButtonTokens({
-    tone: tone || defaults.tone,
-    size: size || defaults.size,
-    weight: weight || defaults.weight,
-  });
-  const styles = useButtonStyles({
-    isDisabled,
-    tokens,
-  });
+export const Button = forwardRefWithAs<'button', ButtonProps>(
+  (
+    {
+      as: Tag = 'button',
+      children,
+      isDisabled,
+      isLoading,
+      size,
+      tone,
+      weight,
+      onPress,
+      type = 'button',
+      ...otherProps
+    },
+    ref
+  ) => {
+    const { useButtonStyles, useButtonTokens, defaults } = useContext(ButtonContext);
+    const tokens = useButtonTokens({
+      tone: tone || defaults.tone,
+      size: size || defaults.size,
+      weight: weight || defaults.weight,
+    });
+    const styles = useButtonStyles({
+      isDisabled,
+      tokens,
+    });
+    const typeProp = Tag === 'button' ? type : undefined;
 
-  return (
-    <button type={type} css={styles} onClick={onPress} {...otherProps}>
-      <span css={isLoading ? { opacity: 0 } : null}>{children}</span>
-      {isLoading && (
-        <span css={loadingContainerStyles}>
-          <LoadingDots size={size || defaults.size} label="Button loading indicator" />
-        </span>
-      )}
-    </button>
-  );
-};
+    return (
+      <Tag type={typeProp} css={styles} onClick={onPress} ref={ref} {...otherProps}>
+        <span css={isLoading ? { opacity: 0 } : null}>{children}</span>
+        {isLoading && (
+          <span css={loadingContainerStyles}>
+            <LoadingDots size={size || defaults.size} label="Button loading indicator" />
+          </span>
+        )}
+      </Tag>
+    );
+  }
+);
