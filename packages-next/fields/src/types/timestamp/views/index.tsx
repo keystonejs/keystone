f86c380ca2efcdebdebe9b81bd239e44ad24b79c
John Molomby <john@thinkmill.com.au>
2020-09-24 09:42:53 +1000

    Initial stub-like timestamp implementation for spike (#3762)
    Pretty basic, just wraps `DateTimeUtc`, but in line with [current planning for the date/time types](https://github.com/keystonejs/keystone/blob/master/packages-next/fields/TODO.md#date--time-types-reboot).

---
+++ packages-next/fields/src/types/timestamp/views/index.tsx
@@ -0,0 +1,43 @@
+/* @jsx jsx */
+import { jsx } from '@keystone-ui/core';
+import { FieldContainer, FieldLabel, TextInput } from '@keystone-ui/fields';
+
+import {
+  CellComponent,
+  FieldController,
+  FieldControllerConfig,
+  FieldProps,
+} from '@keystone-spike/types';
+import { Link } from '@keystone-spike/admin-ui/router';
+import { Fragment } from 'react';
+
+// TODO: Bring across the datetime/datetimeUtc interfaces, date picker, etc.
+
+export const Field = ({ field, value, onChange }: FieldProps<typeof controller>) => (
+  <FieldContainer>
+    <FieldLabel>{field.label}</FieldLabel>
+    <TextInput
+      onChange={event => {
+        onChange(event.target.value);
+      }}
+      value={value}
+    />
+  </FieldContainer>
+);
+
+export const Cell: CellComponent = ({ item, path, linkTo }) => {
+  let value = item[path];
+  return linkTo ? <Link {...linkTo}>{value}</Link> : <Fragment>{value}</Fragment>;
+};
+Cell.supportsLinkTo = true;
+
+export const controller = (config: FieldControllerConfig): FieldController<string, string> => {
+  return {
+    path: config.path,
+    label: config.label,
+    graphqlSelection: config.path,
+    defaultValue: '',
+    deserialize: data => data[config.path],
+    serialize: value => ({ [config.path]: value }),
+  };
+};
