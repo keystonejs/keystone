2e5b8692c5f316cc7df38f453c8e513f0ef0ab15
Jed Watson <jed.watson@me.com>
2020-09-23 18:08:15 +1000

    Styling the new List UI (#3759)
    * Styling the new List UI
    
    * Type Fix

--- packages-next/admin-ui/src/pages/ListPage.tsx
+++ packages-next/admin-ui/src/pages/ListPage.tsx
@@ -1,8 +1,9 @@
 /* @jsx jsx */
 
 import { useQuery, gql } from '../apollo';
-import { jsx } from '@keystone-ui/core';
-import { Fragment, useMemo } from 'react';
+import { Button } from '@keystone-ui/button';
+import { Box, H1, jsx, Stack, useTheme } from '@keystone-ui/core';
+import { Fragment, ReactNode, useMemo } from 'react';
 import { LinkIcon } from '@keystone-ui/icons/icons/LinkIcon';
 import { PageContainer } from '../components/PageContainer';
 import { useList } from '../KeystoneContext';
@@ -13,6 +14,90 @@ type ListPageProps = {
   listKey: string;
 };
 type Filter = { field: string; type: string; value: JSONValue };
+
+const ListPageHeader = ({ listKey }: { listKey: string }) => {
+  const list = useList(listKey);
+  return (
+    <Stack
+      across
+      marginY="large"
+      gap="medium"
+      css={{
+        display: 'flex',
+        flexDirection: 'row',
+        // justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <H1>{list.label}</H1>
+      <Button tone="positive">Create</Button>
+    </Stack>
+  );
+};
+
+const TableContainer = ({ children }: { children: ReactNode }) => {
+  const { colors, shadow } = useTheme();
+  return (
+    <Box
+      padding="large"
+      rounding="medium"
+      css={{
+        background: colors.background,
+        boxShadow: shadow.s200,
+      }}
+    >
+      <table
+        css={{ minWidth: '100%', 'tr:last-child td': { borderBottomWidth: 0 } }}
+        cellPadding="0"
+        cellSpacing="0"
+      >
+        {children}
+      </table>
+    </Box>
+  );
+};
+
+const TableHeaderRow = ({ children }: { children: ReactNode }) => {
+  return (
+    <thead>
+      <tr>{children}</tr>
+    </thead>
+  );
+};
+
+const TableHeaderCell = ({ children }: { children?: ReactNode }) => {
+  const { colors, spacing, typography } = useTheme();
+  return (
+    <th
+      css={{
+        borderBottom: `2px solid ${colors.border}`,
+        color: colors.foregroundDim,
+        fontSize: typography.fontSize.large,
+        fontWeight: typography.fontWeight.regular,
+        padding: spacing.small,
+        textAlign: 'left',
+      }}
+    >
+      {children}
+    </th>
+  );
+};
+
+const TableBodyCell = ({ children }: { children: ReactNode }) => {
+  const { colors, spacing, typography } = useTheme();
+  return (
+    <td
+      css={{
+        borderBottom: `1px solid ${colors.border}`,
+        fontSize: typography.fontSize.medium,
+        padding: spacing.small,
+        textAlign: 'left',
+      }}
+    >
+      {children}
+    </td>
+  );
+};
 
 export const ListPage = ({ listKey }: ListPageProps) => {
   const list = useList(listKey);
@@ -117,7 +202,7 @@ export const ListPage = ({ listKey }: ListPageProps) => {
 
   return (
     <PageContainer>
-      <h2>List: {list.label}</h2>
+      <ListPageHeader listKey={listKey} />
       <p>
         {data
           ? (() => {
@@ -156,34 +241,33 @@ export const ListPage = ({ listKey }: ListPageProps) => {
         </p>
       ) : null}
       {error ? (
+        // TODO: Show errors nicely and with information
         'Error...'
       ) : data ? (
-        <table>
-          <thead>
-            <tr>
-              {selectedFields.includeLabel && <th>Label</th>}
-              {shouldShowNonCellLink && <th />}
-              {selectedFields.fields.map(key => {
-                return <th key={key}>{list.fields[key].label}</th>;
-              })}
-            </tr>
-          </thead>
+        <TableContainer>
+          <TableHeaderRow>
+            {selectedFields.includeLabel && <TableHeaderCell>Label</TableHeaderCell>}
+            {shouldShowNonCellLink && <TableHeaderCell />}
+            {selectedFields.fields.map(path => {
+              return <TableHeaderCell key={path}>{list.fields[path].label}</TableHeaderCell>;
+            })}
+          </TableHeaderRow>
           <tbody>
             {data.items.map((item: any) => {
               return (
                 <tr key={item.id}>
                   {selectedFields.includeLabel && (
-                    <td>
+                    <TableBodyCell>
                       <Link
                         href={`/${list.path}/[id]`}
                         as={`/${list.path}/${encodeURIComponent(item.id)}`}
                       >
                         {item._label_}
                       </Link>
-                    </td>
+                    </TableBodyCell>
                   )}
                   {shouldShowNonCellLink && (
-                    <td>
+                    <TableBodyCell>
                       <Link
                         css={{ textDecoration: 'none' }}
                         href={`/${list.path}/[id]`}
@@ -191,15 +275,15 @@ export const ListPage = ({ listKey }: ListPageProps) => {
                       >
                         <LinkIcon aria-label="Go to item" />
                       </Link>
-                    </td>
+                    </TableBodyCell>
                   )}
-                  {selectedFields.fields.map((fieldKey, i) => {
-                    let { Cell } = list.fields[fieldKey].views;
+                  {selectedFields.fields.map((path, i) => {
+                    let { Cell } = list.fields[path].views;
                     return (
-                      <td key={fieldKey}>
+                      <TableBodyCell key={path}>
                         <Cell
                           item={item}
-                          path={fieldKey}
+                          path={path}
                           linkTo={
                             i === 0 && !selectedFields.includeLabel && Cell.supportsLinkTo
                               ? {
@@ -209,14 +293,14 @@ export const ListPage = ({ listKey }: ListPageProps) => {
                               : undefined
                           }
                         />
-                      </td>
+                      </TableBodyCell>
                     );
                   })}
                 </tr>
               );
             })}
           </tbody>
-        </table>
+        </TableContainer>
       ) : (
         'Loading...'
       )}
