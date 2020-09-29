0f832f48d70c117225d06afac016f75cab508557
Jed Watson <jed.watson@me.com>
2020-09-29 22:36:22 +1000

    Impriving "no access" error experience

--- packages-next/admin-ui/src/components/index.ts
+++ packages-next/admin-ui/src/components/index.ts
@@ -1,5 +1,6 @@
 export { CellContainer } from './CellContainer';
 export { CellLink } from './CellLink';
+export { ErrorBoundary, ErrorContainer } from './Errors';
 export { Logo } from './Logo';
 export { Navigation } from './Navigation';
 export { PageContainer } from './PageContainer';
