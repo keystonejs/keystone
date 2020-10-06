c01e65abefee6ee006b94a26007eb42a1bad958f
Mitchell Hamilton <mitchell@hamil.town>
2020-10-06 15:46:37 +1000

    Create form in new interfaces (#3883)
    * Create form in new interfaces
    
    * Fix a thing

---
+++ packages-next/admin-ui/src/components/CreateForm.tsx
@@ -0,0 +1,104 @@
+/* @jsx jsx */
+
+import { Fragment, useState } from 'react';
+import { gql, useMutation } from '../apollo';
+import { jsx, Stack } from '@keystone-ui/core';
+import isDeepEqual from 'fast-deep-equal';
+import { useList } from '../context';
+import { Button } from '@keystone-ui/button';
+import { Notice } from '@keystone-ui/notice';
+
+export function CreateForm({
+  listKey,
+  fieldModes,
+  onClose,
+  onCreate,
+}: {
+  listKey: string;
+  fieldModes: Record<string, 'edit' | 'hidden'>;
+  onClose: () => void;
+  onCreate: (id: string) => void;
+}) {
+  const list = useList(listKey);
+
+  const [createItem, { loading, error }] = useMutation(
+    gql`mutation($data: ${list.gqlNames.createInputName}!) {
+      item: ${list.gqlNames.createMutationName}(data: $data) {
+        id
+      }
+    }`
+  );
+
+  const [valuesByFieldPath, setValuesByFieldPath] = useState(() => {
+    const value: Record<string, unknown> = {};
+    Object.keys(list.fields).forEach(fieldPath => {
+      value[fieldPath] = list.fields[fieldPath].controller.defaultValue;
+    });
+    return value;
+  });
+  const fields = Object.keys(list.fields)
+    .filter(fieldKey => fieldModes[fieldKey] !== 'hidden')
+    .map(fieldKey => {
+      const field = list.fields[fieldKey];
+      const Field = list.fields[fieldKey].views.Field;
+      return (
+        <Field
+          key={fieldKey}
+          field={field.controller}
+          value={valuesByFieldPath[fieldKey]}
+          onChange={fieldValue => {
+            setValuesByFieldPath({
+              ...valuesByFieldPath,
+              [fieldKey]: fieldValue,
+            });
+          }}
+        />
+      );
+    });
+
+  return (
+    <Fragment>
+      {error && <Notice tone="negative">{error.message}</Notice>}
+      {fields}
+      {fields.length === 0 && 'There are no fields that you can read or edit'}
+      <div css={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Stack across gap="small">
+          <Button
+            isLoading={loading}
+            weight="bold"
+            tone="active"
+            onClick={() => {
+              const data: Record<string, any> = {};
+              Object.keys(list.fields).forEach(fieldPath => {
+                const { controller } = list.fields[fieldPath];
+                const serialized = controller.serialize(valuesByFieldPath[fieldPath]);
+                if (!isDeepEqual(serialized, controller.serialize(controller.defaultValue))) {
+                  Object.assign(data, serialized);
+                }
+              });
+
+              createItem({
+                variables: {
+                  data,
+                },
+              })
+                .then(({ data }) => {
+                  onCreate(data.item.id);
+                })
+                .catch(() => {});
+            }}
+          >
+            Create
+          </Button>
+        </Stack>
+        <Button
+          onClick={() => {
+            onClose();
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
+    </Fragment>
+  );
+}
---
+++ packages-next/admin-ui/src/utils/serialization.tsx
@@ -0,0 +1,47 @@
+import { ListMeta, JSONValue } from '@keystone-spike/types';
+import { GraphQLError } from 'graphql';
+import { DataGetter, DeepNullable } from './dataGetter';
+import { getRootGraphQLFieldsFromFieldController } from './getRootGraphQLFieldsFromFieldController';
+
+export type ItemData = DeepNullable<{ id: string; _label_: string; [key: string]: any }>;
+
+export type DeserializedValue = Record<
+  string,
+  | { kind: 'error'; errors: readonly [GraphQLError, ...GraphQLError[]] }
+  | { kind: 'value'; value: any }
+>;
+
+export function deserializeValue(list: ListMeta, itemGetter: DataGetter<ItemData>) {
+  const value: DeserializedValue = {};
+  Object.keys(list.fields).forEach(fieldKey => {
+    const field = list.fields[fieldKey];
+    const itemForField: Record<string, any> = {};
+    const errors = new Set<GraphQLError>();
+    for (const graphqlField of getRootGraphQLFieldsFromFieldController(field.controller)) {
+      const fieldGetter = itemGetter.get(graphqlField);
+      if (fieldGetter.errors) {
+        fieldGetter.errors.forEach(error => {
+          errors.add(error);
+        });
+      }
+      itemForField[graphqlField] = fieldGetter.data;
+    }
+    if (errors.size) {
+      value[fieldKey] = { kind: 'error', errors: [...errors] as any };
+    } else {
+      value[fieldKey] = { kind: 'value', value: field.controller.deserialize(itemForField) };
+    }
+  });
+  return value;
+}
+
+export function serializeValueToObjByFieldKey(list: ListMeta, value: DeserializedValue) {
+  const obj: Record<string, Record<string, JSONValue>> = {};
+  Object.keys(list.fields).map(fieldKey => {
+    const val = value[fieldKey];
+    if (val.kind === 'value') {
+      obj[fieldKey] = list.fields[fieldKey].controller.serialize(val.value);
+    }
+  });
+  return obj;
+}
