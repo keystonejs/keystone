1eab7ba7fd2039d74f193e471766874dad7383a1
Tim Leslie <timl@thinkmill.com.au>
2020-10-12 11:43:10 +1100

    Prepare create-keystone-app for more than two adapters

---
+++ .changeset/nine-deers-hide.md
@@ -0,0 +1,5 @@
+---
+'create-keystone-app': patch
+---
+
+Updated internals to better support more than two database adapter options.
--- packages/create-keystone-app/lib/generate-code.js
+++ packages/create-keystone-app/lib/generate-code.js
@@ -6,14 +6,15 @@ const generateCode = async () => {
   const projectName = await getProjectName();
 
   const adapterChoice = await getAdapterChoice();
-  const adapterConfig =
-    adapterChoice.name === 'MongoDB'
-      ? `{ mongoUri: '${await getAdapterConfig()}' }`
-      : `{ knexOptions: { connection: '${await getAdapterConfig()}' } }`;
-  const adapterRequire =
-    adapterChoice.name === 'MongoDB'
-      ? `const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');`
-      : `const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');`;
+  const adapterConfig = {
+    MongoDB: `{ mongoUri: '${await getAdapterConfig()}' }`,
+    PostgreSQL: `{ knexOptions: { connection: '${await getAdapterConfig()}' } }`,
+  }[adapterChoice.key];
+
+  const adapterRequire = {
+    MongoDB: `const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');`,
+    PostgreSQL: `const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');`,
+  }[adapterChoice.key];
 
   return `${adapterRequire}
 const PROJECT_NAME = '${projectName}';
