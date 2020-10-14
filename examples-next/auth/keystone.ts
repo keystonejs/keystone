df9f5f54a048890e0ed7941d86e73e51ab794281
Jed Watson <jed.watson@me.com>
2020-10-14 13:27:32 +1100

    Logging because things aren't working

--- examples-next/auth/keystone.ts
+++ examples-next/auth/keystone.ts
@@ -51,7 +51,7 @@ export default withAuth(
       // Stateless sessions will store the listKey and itemId of the signed-in user in a cookie
       statelessSessions({
         // The session secret is used to encrypt cookie data (should be an environment variable)
-        secret: 'super secret session key',
+        secret: 'super secret session key super secret session key',
       }),
       // withItemData will fetch these fields for signed-in User items to populate session.data
       { User: 'name isAdmin' }
