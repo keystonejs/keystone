d91a638352f637e95fe6ffed9268e2ed9950e2c5
Mitchell Hamilton <mitchell@hamil.town>
2020-10-30 14:39:15 +1000

    Only work around Rollup bug for router entrypoint in new interfaces (#4085)

--- packages-next/admin-ui/src/apollo.tsx
+++ packages-next/admin-ui/src/apollo.tsx
@@ -3,21 +3,4 @@
  * packages import the same instance of apollo.
  */
 
-// not using export * because Rollup emits CJS code that redefines __esModule which causes problems because __esModule generally isn't a configurable property
-export {
-  useQuery,
-  useMutation,
-  useLazyQuery,
-  gql,
-  ApolloError,
-  InMemoryCache,
-  ApolloClient,
-  ApolloProvider,
-} from '@apollo/client';
-export type {
-  ServerError,
-  ServerParseError,
-  QueryResult,
-  DocumentNode,
-  TypedDocumentNode,
-} from '@apollo/client';
+export * from '@apollo/client';
