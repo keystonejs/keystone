98dd7dcffa797eb40eb1713ba1ac2697dfef95e3
Mitchell Hamilton <mitchell@hamil.town>
2020-11-11 18:00:17 +1000

    Rename CRUD API (#4200)
    * Rename CRUD API
    
    * Rename crud-api.ts to itemAPI.ts
    
    Co-authored-by: Jed Watson <jed.watson@me.com>

--- packages-next/keystone/src/lib/createKeystone.ts
+++ packages-next/keystone/src/lib/createKeystone.ts
@@ -15,7 +15,7 @@ import { mergeSchemas } from '@graphql-tools/merge';
 import { gql } from '../schema';
 import { GraphQLSchema, GraphQLObjectType } from 'graphql';
 import { mapSchema } from '@graphql-tools/utils';
-import { crudForList } from './crud-api';
+import { itemAPIForList } from './itemAPI';
 import { adminMetaSchemaExtension } from '@keystone-next/admin-ui/templates';
 import { accessControlContext, skipAccessControlContext } from './createAccessControlContext';
 import { autoIncrement, mongoId } from '@keystone-next/fields';
@@ -175,7 +175,7 @@ export function createKeystone(config: KeystoneConfig): Keystone {
     return {
       schemaName: 'public',
       ...(skipAccessControl ? skipAccessControlContext : accessControlContext),
-      crud,
+      lists: itemAPI,
       totalResults: 0,
       keystone,
       maxTotalResults: (keystone as any).queryLimits.maxTotalResults,
@@ -183,9 +183,13 @@ export function createKeystone(config: KeystoneConfig): Keystone {
       ...sessionContext,
     };
   }
-  let crud: Record<string, ReturnType<typeof crudForList>> = {};
+  let itemAPI: Record<string, ReturnType<typeof itemAPIForList>> = {};
   for (const listKey of Object.keys(adminMeta.lists)) {
-    crud[listKey] = crudForList((keystone as any).lists[listKey], graphQLSchema, createContext);
+    itemAPI[listKey] = itemAPIForList(
+      (keystone as any).lists[listKey],
+      graphQLSchema,
+      createContext
+    );
   }
 
   const createSessionContext = sessionImplementation?.createContext;
