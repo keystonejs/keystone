1368b881b745e1079c1995380981466fd0d72e5b
Jed Watson <jed.watson@me.com>
2020-11-17 10:23:54 +1100

    Collapsing comments to use less vertical space

--- examples-next/roles/schema.ts
+++ examples-next/roles/schema.ts
@@ -32,21 +32,13 @@ export const lists = createSchema({
       },
     },
     fields: {
-      /*
-        The label of the todo item
-      */
+      /* The label of the todo item */
       label: text({ isRequired: true }),
-      /*
-        Whether the todo item is complete
-      */
+      /* Whether the todo item is complete */
       isComplete: checkbox(),
-      /*
-        Private todo items are only visible to the user they are assigned to
-      */
+      /* Private todo items are only visible to the user they are assigned to */
       isPrivate: checkbox(),
-      /*
-        The person the todo item is assigned to
-      */
+      /* The person the todo item is assigned to */
       assignedTo: relationship({ ref: 'Person.tasks' }),
     },
   }),
@@ -67,27 +59,17 @@ export const lists = createSchema({
       },
     },
     fields: {
-      /*
-        The name of the user
-      */
+      /* The name of the user */
       name: text({ isRequired: true }),
-      /*
-        The email of the user, used to sign in
-      */
+      /* The email of the user, used to sign in */
       email: text({ isRequired: true }),
-      /*
-        The password of the user
-      */
+      /* The password of the user */
       password: password({ isRequired: true }),
-      /*
-        The role assigned to the user
-      */
+      /* The role assigned to the user */
       role: relationship({
         ref: 'Role.assignedTo',
       }),
-      /*
-        Todo items assigned to the user
-      */
+      /* Todo items assigned to the user */
       tasks: relationship({
         ref: 'Todo.assignedTo',
         many: true,
@@ -109,45 +91,29 @@ export const lists = createSchema({
       - [ ] Extend the Admin UI with client-side validation based on the same set of rules
     */
     fields: {
-      /*
-        The name of the role
-      */
+      /* The name of the role */
       name: text(),
-      /*
-        Create Todos means:
-        - create todos (can only assign them to others with canManageAllTodos)
-      */
+      /* Create Todos means:
+         - create todos (can only assign them to others with canManageAllTodos) */
       canCreateTodos: checkbox(),
-      /*
-        Manage All Todos means:
-        - create new Todo items and assign them to someone else (with canCreateTodos)
-        - update and delete Todo items not assigned to the current user
-      */
+      /* Manage All Todos means:
+         - create new Todo items and assign them to someone else (with canCreateTodos)
+         - update and delete Todo items not assigned to the current user */
       canManageAllTodos: checkbox(),
-      /*
-        See Other Users means:
-        - list all users in the database (users can always see themselves)
-      */
+      /* See Other Users means:
+         - list all users in the database (users can always see themselves) */
       canSeeOtherPeople: checkbox(),
-      /*
-        Edit Other Users means:
-        - edit other users in the database (users can always edit their own item)
-      */
+      /* Edit Other Users means:
+         - edit other users in the database (users can always edit their own item) */
       canEditOtherPeople: checkbox(),
-      /*
-        Manage Users means:
-        - change passwords (users can always change their own password)
-        - assign roles to themselves and other users
-      */
+      /* Manage Users means:
+         - change passwords (users can always change their own password)
+         - assign roles to themselves and other users */
       canManagePeople: checkbox(),
-      /*
-        Manage Roles means:
-        - create, edit, and delete roles
-      */
+      /* Manage Roles means:
+         - create, edit, and delete roles */
       canManageRoles: checkbox(),
-      /*
-        This list of People assigned to this role
-      */
+      /* This list of People assigned to this role */
       assignedTo: relationship({
         ref: 'Person.role',
         many: true,
