a248398d89faea65881243f695f6f069494fad12
Wes Bos <wesbos@gmail.com>
2020-11-19 12:03:21 -0500

    clean up insert seed data

--- examples-next/ecommerce/seed-data/index.ts
+++ examples-next/ecommerce/seed-data/index.ts
@@ -1,16 +1,18 @@
 import { products } from './data';
 
 export async function insertSeedData(keystone: any) {
-  console.log('------------ INSERTING DUMMY DATA ------------');
+  console.log(`---- 🌱 Inserting Seed Data: ${products.length} Products -----`);
   const { mongoose } = keystone.adapters.MongooseAdapter;
   for (const product of products) {
+    console.log(`Adding: ${product.name}`);
     const { _id } = await mongoose
-      .model('ProductImage')
-      .create({ image: product.image, altText: product.description });
+    .model('ProductImage')
+    .create({ image: product.image, altText: product.description });
     product.image = _id;
-    await mongoose.model('Product').create(product);
+    const res = await mongoose.model('Product').create(product);
+    console.log(res);
   }
-  console.log('------------ DUMMY DATA ADDED! ------------');
-  console.log('👋🏻 Please start the process with `yarn dev`');
+  console.log(`---- ✅ Seed Data Inserted: ${products.length} Products -----`);
+  console.log(`👋🏻 Please start the process with \`yarn dev\``);
   process.exit();
 }
