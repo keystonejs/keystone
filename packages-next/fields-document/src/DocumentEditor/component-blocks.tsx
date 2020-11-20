d9a7dfefba2b23c63aa433e7657505451a6117ec
mitchellhamilton <mitchell@hamil.town>
2020-11-20 14:09:23 +1000

    Fix a thing

--- packages-next/fields-document/src/DocumentEditor/component-blocks.tsx
+++ packages-next/fields-document/src/DocumentEditor/component-blocks.tsx
@@ -346,7 +346,6 @@ export const ComponentBlocksElement = ({ attributes, children, element }: Render
             fontWeight: 600,
           }}
         >
-          <div css={{ padding: 4 }}>{componentBlock.label}</div>
           <Button
             css={{ float: 'right' }}
             onMouseDown={event => {
@@ -370,6 +369,7 @@ export const ComponentBlocksElement = ({ attributes, children, element }: Render
               <Spacer />
             </div>
           ) : null}
+          <div css={{ padding: 4 }}>{componentBlock.label}</div>
         </div>
       </NotEditable>
       {editMode && (
