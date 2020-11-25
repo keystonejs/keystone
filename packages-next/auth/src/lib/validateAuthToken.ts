bb747c9cbe4ebb56eecc70be2e90f77165e5fab6
Tim Leslie <timl@thinkmill.com.au>
2020-11-25 14:37:04 +1100

    Use validateSecret in validateAuthItem (#4381)

--- packages-next/auth/src/lib/validateAuthToken.ts
+++ packages-next/auth/src/lib/validateAuthToken.ts
@@ -1,4 +1,5 @@
 import { AuthTokenRedemptionErrorCode } from '../types';
+import { validateSecret } from './validateSecret';
 
 // The tokensValidForMins config is from userland so could be anything; make it sane
 function sanitiseValidForMinsConfig(input: any): number {
@@ -19,56 +20,43 @@ export async function validateAuthToken(
   | { success: false; code: AuthTokenRedemptionErrorCode }
   | { success: true; item: { id: any; [prop: string]: any } }
 > {
-  const fieldKeys = {
-    token: `${tokenType}Token`,
-    issuedAt: `${tokenType}IssuedAt`,
-    redeemedAt: `${tokenType}RedeemedAt`,
-  };
-  const tokenFieldInstance = list.fieldsByPath[fieldKeys.token];
-  const identity = args[identityField];
-  const canidatePlaintext = args.token;
-
-  // TODO: Allow additional filters to be suppled in config? eg. `validUserConditions: { isEnable: true, isVerified: true, ... }`
-  const items = await itemAPI.findMany({ where: { [identityField]: identity } });
-
-  // Check the for identity-related failures first
-  let specificCode: AuthTokenRedemptionErrorCode | undefined;
-  if (items.length === 0) {
-    specificCode = 'IDENTITY_NOT_FOUND';
-  } else if (items.length === 1 && !items[0][fieldKeys.token]) {
-    specificCode = 'TOKEN_NOT_SET';
-  } else if (items.length > 1) {
-    specificCode = 'MULTIPLE_IDENTITY_MATCHES';
-  }
-  if (typeof specificCode !== 'undefined') {
-    // See "Identity Protection" in the README as to why this is a thing
-    if (protectIdentities) {
-      await tokenFieldInstance.generateHash('simulated-password-to-counter-timing-attack');
-    }
-    return { success: false, code: protectIdentities ? 'FAILURE' : specificCode };
+  const result = await validateSecret(
+    list,
+    identityField,
+    `${tokenType}Token`,
+    protectIdentities,
+    args,
+    args.token,
+    itemAPI
+  );
+  if (!result.success) {
+    // Rewrite error codes
+    if (result.code === 'SECRET_NOT_SET') return { success: false, code: 'TOKEN_NOT_SET' };
+    if (result.code === 'SECRET_MISMATCH') return { success: false, code: 'TOKEN_MISMATCH' };
+    return result as { success: false; code: AuthTokenRedemptionErrorCode };
   }
 
-  // Check for non-identity failures
-  const item = items[0];
-  if (await tokenFieldInstance.compare(canidatePlaintext, item[fieldKeys.token])) {
-    // Now that we know the identity and token are valid, we can always return 'helpful' errors and stop worrying about protectIdentities
-    if (item[fieldKeys.redeemedAt]) {
-      return { success: false, code: 'TOKEN_REDEEMED' };
-    }
-    if (!item[fieldKeys.issuedAt] || typeof item[fieldKeys.issuedAt].getTime !== 'function') {
-      throw new Error(
-        `Error redeeming authToken: field ${list.listKey}.${fieldKeys.issuedAt} isn't a valid Date object.`
-      );
-    }
-    const elapsedMins = (Date.now() - item[fieldKeys.issuedAt].getTime()) / (1000 * 60);
-    const validForMins = sanitiseValidForMinsConfig(tokenValidMins);
-    if (elapsedMins > validForMins) {
-      return { success: false, code: 'TOKEN_EXPIRED' };
-    }
+  // Now that we know the identity and token are valid, we can always return 'helpful' errors and stop worrying about protectIdentities
+  const { item } = result;
+  const fieldKeys = { issuedAt: `${tokenType}IssuedAt`, redeemedAt: `${tokenType}RedeemedAt` };
 
-    // Authenticated!
-    return { success: true, item };
-  } else {
-    return { success: false, code: protectIdentities ? 'FAILURE' : 'TOKEN_MISMATCH' };
+  // Check that the token has not been redeemed already
+  if (item[fieldKeys.redeemedAt]) {
+    return { success: false, code: 'TOKEN_REDEEMED' };
   }
+
+  // Check that the token has not expired
+  if (!item[fieldKeys.issuedAt] || typeof item[fieldKeys.issuedAt].getTime !== 'function') {
+    throw new Error(
+      `Error redeeming authToken: field ${list.listKey}.${fieldKeys.issuedAt} isn't a valid Date object.`
+    );
+  }
+  const elapsedMins = (Date.now() - item[fieldKeys.issuedAt].getTime()) / (1000 * 60);
+  const validForMins = sanitiseValidForMinsConfig(tokenValidMins);
+  if (elapsedMins > validForMins) {
+    return { success: false, code: 'TOKEN_EXPIRED' };
+  }
+
+  // Authenticated!
+  return { success: true, item };
 }
