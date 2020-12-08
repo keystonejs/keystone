5c75534f6e9e0f10a6556a1f1dc87b5fdd986dd4
Tim Leslie <timl@thinkmill.com.au>
2020-12-08 16:45:38 +1100

    Simplify admin UI generation (#4491)

---
+++ .changeset/lemon-kids-tease.md
@@ -0,0 +1,5 @@
+---
+'@keystone-next/admin-ui': patch
+---
+
+Simplified admin UI generation code.
--- packages-next/admin-ui/src/templates/index.ts
+++ packages-next/admin-ui/src/templates/index.ts
@@ -15,52 +15,26 @@ export { getAdminMetaSchema } from './adminMetaSchemaExtension';
 export const writeAdminFiles = (
   session: KeystoneConfig['session'],
   system: KeystoneSystem,
-  configFile: boolean,
+  configFileExists: boolean,
   projectAdminPath: string
-): AdminFileToWrite[] => {
-  const { adminMeta } = system;
-  return [
-    {
-      mode: 'copy',
-      inputPath: Path.join(pkgDir, 'static', 'next.config.js'),
-      outputPath: 'next.config.js',
-    },
-    {
-      mode: 'copy',
-      inputPath: Path.join(pkgDir, 'static', 'tsconfig.json'),
-      outputPath: 'tsconfig.json',
-    },
-    {
-      mode: 'write',
-      outputPath: 'pages/no-access.js',
-      src: noAccessTemplate(session),
-    },
-    {
-      mode: 'write',
-      outputPath: 'pages/_app.js',
-      src: appTemplate(system, { configFile, projectAdminPath }),
-    },
-    {
-      mode: 'write',
-      src: homeTemplate(adminMeta),
-      outputPath: 'pages/index.js',
-    },
-    ...Object.values(adminMeta.lists)
-      .map(list => {
-        const { path } = list;
-        return [
-          {
-            mode: 'write',
-            src: listTemplate({ list }),
-            outputPath: `pages/${path}/index.js`,
-          },
-          {
-            mode: 'write',
-            src: itemTemplate({ list }),
-            outputPath: `pages/${path}/[id].js`,
-          },
-        ] as const;
-      })
-      .flat(),
-  ];
-};
+): AdminFileToWrite[] => [
+  ...['next.config.js', 'tsconfig.json'].map(
+    outputPath =>
+      ({ mode: 'copy', inputPath: Path.join(pkgDir, 'static', outputPath), outputPath } as const)
+  ),
+  { mode: 'write', outputPath: 'pages/no-access.js', src: noAccessTemplate(session) },
+  {
+    mode: 'write',
+    outputPath: 'pages/_app.js',
+    src: appTemplate(system, { configFileExists, projectAdminPath }),
+  },
+  { mode: 'write', src: homeTemplate(system.adminMeta.lists), outputPath: 'pages/index.js' },
+  ...Object.values(system.adminMeta.lists).map(
+    ({ path, key }) =>
+      ({ mode: 'write', src: listTemplate(key), outputPath: `pages/${path}/index.js` } as const)
+  ),
+  ...Object.values(system.adminMeta.lists).map(
+    ({ path, key }) =>
+      ({ mode: 'write', src: itemTemplate(key), outputPath: `pages/${path}/[id].js` } as const)
+  ),
+];
