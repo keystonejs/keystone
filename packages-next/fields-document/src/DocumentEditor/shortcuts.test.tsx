06c918d5b15632f5108d1a6a2ed24dcaf3f5fe4b
mitchellhamilton <mitchell@hamil.town>
2020-12-24 14:13:18 +1000

    Update tests

--- packages-next/fields-document/src/DocumentEditor/shortcuts.test.tsx
+++ packages-next/fields-document/src/DocumentEditor/shortcuts.test.tsx
@@ -2,31 +2,29 @@
 import { shortcuts } from './shortcuts';
 import { jsx, makeEditor } from './tests/utils';
 
-describe.each(Object.entries(shortcuts))('shortcut "%s" for "%s"', (shortcut, result) => {
-  test('shortcut at start of block', () => {
-    let editor = makeEditor(
+test.each(Object.entries(shortcuts))('shortcut "%s" for "%s"', (shortcut, result) => {
+  let editor = makeEditor(
+    <editor>
+      <paragraph>
+        <text>
+          {shortcut}
+          <cursor />
+        </text>
+      </paragraph>
+    </editor>
+  );
+
+  editor.insertText(' ');
+  expect(editor).toEqualEditor(
+    makeEditor(
       <editor>
         <paragraph>
           <text>
-            {shortcut}
+            {result + ' '}
             <cursor />
           </text>
         </paragraph>
       </editor>
-    );
-
-    editor.insertText(' ');
-    expect(editor).toEqualEditor(
-      makeEditor(
-        <editor>
-          <paragraph>
-            <text>
-              {result + ' '}
-              <cursor />
-            </text>
-          </paragraph>
-        </editor>
-      )
-    );
-  });
+    )
+  );
 });
