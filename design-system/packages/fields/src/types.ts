5c0edb7255849d7b90d349c5dbddfdf065e3f621
gwyneplaine <cc.lee@live.com.au>
2021-01-11 13:40:01 +1100

    remove null from DTP value type

--- design-system/packages/fields/src/types.ts
+++ design-system/packages/fields/src/types.ts
@@ -3,4 +3,4 @@ export type SizeType = 'small' | 'medium' | 'large';
 export type ShapeType = 'square' | 'round';
 
 // A primitive data type for date fields, which is an un-formatted ISO8601 date string or null for empty
-export type DateType = string | null;
+export type DateType = string;
