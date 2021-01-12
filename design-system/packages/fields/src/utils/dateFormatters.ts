7ec126e96c40a4a118765e98c712f0f459e89e94
gwyneplaine <cc.lee@live.com.au>
2021-01-12 18:04:55 +1100

    remove unused formatter methods

--- design-system/packages/fields/src/utils/dateFormatters.ts
+++ design-system/packages/fields/src/utils/dateFormatters.ts
@@ -15,23 +15,3 @@ export const formatDateType = (date: Date): DateType => {
  * @usage formatDMY(new Date('2019-09-18T19:00:52')) => "09/18/2019"
  */
 export const formatDMY = (date: Date): string => format(date, 'MM/dd/yyyy');
-
-/**
- * Format day, month, year as human readable, like "Dec 01 2010" as "1 Dec 2010"
- */
-export const formatDMYHumanised = (date: Date): string => format(date, 'd LLL yyyy');
-
-/**
- * Format day, month, year with time, like "Dec 01 2010 22:42:13" as "12/01/2010, 10:42 PM"
- */
-export const formatDMYTime = (date: Date): string => format(date, 'MM/dd/yyyy, p');
-
-/**
- * Format day, month, year with time, like "Dec 01 2010 22:42:13" as "12 Dec 2010, 10:42 PM"
- */
-export const formatDMYTimeHumanised = (date: Date): string => format(date, 'd LLL yyyy, p');
-
-/**
- * Format time, like "Dec 01 2010 22:42:13" as "10:42 PM"
- */
-export const formatTime = (date: Date): string => format(date, 'p');
