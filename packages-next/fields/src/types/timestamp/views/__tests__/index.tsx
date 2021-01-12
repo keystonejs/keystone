de1f4823f89c59f619a6ef862300a62cc59bd044
gwyneplaine <cc.lee@live.com.au>
2021-01-12 18:00:31 +1100

    remove unnecessary describe blocks

--- packages-next/fields/src/types/timestamp/views/__tests__/index.tsx
+++ packages-next/fields/src/types/timestamp/views/__tests__/index.tsx
@@ -8,34 +8,32 @@ const STUBCONFIG = {
   fieldMeta: undefined,
 };
 
-describe('./index.tsx', () => {
-  describe('controller', () => {
-    describe('validate', () => {
-      it('should return true if neither date nor time value are specified', () => {
-        const { validate } = controller(STUBCONFIG);
-        expect(validate!({ dateValue: '', timeValue: '' })).toBe(true);
-      });
-      it('should return true if both date and time values are valid', () => {
-        const { validate } = controller(STUBCONFIG);
-        const value = {
+describe('controller', () => {
+  describe('validate', () => {
+    it('should return true if neither date nor time value are specified', () => {
+      const { validate } = controller(STUBCONFIG);
+      expect(validate!({ dateValue: '', timeValue: '' })).toBe(true);
+    });
+    it('should return true if both date and time values are valid', () => {
+      const { validate } = controller(STUBCONFIG);
+      const value = {
+        dateValue: formatISO(new Date(), { representation: 'date' }),
+        timeValue: '10:00',
+      } as const;
+      expect(validate!(value)).toBe(true);
+    });
+    it('should return false if only the date value is missing', () => {
+      const { validate } = controller(STUBCONFIG);
+      expect(validate!({ dateValue: '', timeValue: '10:00' })).toBe(false);
+    });
+    it('should return false if only the time value is missing', () => {
+      const { validate } = controller(STUBCONFIG);
+      expect(
+        validate!({
           dateValue: formatISO(new Date(), { representation: 'date' }),
-          timeValue: '10:00',
-        } as const;
-        expect(validate!(value)).toBe(true);
-      });
-      it('should return false if only the date value is missing', () => {
-        const { validate } = controller(STUBCONFIG);
-        expect(validate!({ dateValue: '', timeValue: '10:00' })).toBe(false);
-      });
-      it('should return false if only the time value is missing', () => {
-        const { validate } = controller(STUBCONFIG);
-        expect(
-          validate!({
-            dateValue: formatISO(new Date(), { representation: 'date' }),
-            timeValue: '',
-          })
-        ).toBe(false);
-      });
+          timeValue: '',
+        })
+      ).toBe(false);
     });
   });
 });
