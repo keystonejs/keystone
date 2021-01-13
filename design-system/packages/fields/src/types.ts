b8714632b93baeb8797107f406761d7ea4bcd53e
gwyneplaine <cc.lee@live.com.au>
2021-01-13 11:33:20 +1100

    correct comments for DateType type

--- design-system/packages/fields/src/types.ts
+++ design-system/packages/fields/src/types.ts
@@ -2,5 +2,5 @@ export type WidthType = 'small' | 'medium' | 'large' | 'full';
 export type SizeType = 'small' | 'medium' | 'large';
 export type ShapeType = 'square' | 'round';
 
-// A primitive data type for date fields, which is an un-formatted ISO8601 date string or null for empty
+// A primitive data type for date fields, which is a formatted ISO8601 date string or "" for empty
 export type DateType = string;
