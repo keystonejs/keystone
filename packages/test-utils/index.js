6ea4ff3cf77d5d2278bf4f0415d11aa7399a0490
Tim Leslie <timl@thinkmill.com.au>
2021-01-14 16:54:16 +1100

    Convert test-utils package to TypeScript (#4660)

--- packages/test-utils/index.js
+++
@@ -1,8 +0,0 @@
-const {
-  setupServer,
-  setupFromConfig,
-  multiAdapterRunners,
-  networkedGraphqlRequest,
-} = require('./lib/test-utils');
-
-module.exports = { setupServer, setupFromConfig, multiAdapterRunners, networkedGraphqlRequest };
