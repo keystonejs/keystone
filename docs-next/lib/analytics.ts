e11a985fb5060cdd5099f46427f6b8917d7f38bb
gwyneplaine <cc.lee@live.com.au>
2021-02-03 10:57:36 +1100

    add prod flag and remove window invocation

--- docs-next/lib/analytics.ts
+++ docs-next/lib/analytics.ts
@@ -1,7 +1,15 @@
 export const GA_TRACKING_ID = 'G-KZHKNXV8CM';
 
+// declare global {
+//   interface Window {
+//     gtag: any;
+//   }
+// }
+
 export const handleRouteChange = (url: URL) => {
-  /* @ts-ignore */ window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (process.env.NODE_ENV === 'production') {
+    gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
