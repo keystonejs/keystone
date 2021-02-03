3d501e6c9d5c97d750463d6eea5b30707ae7e2d7
Jed Watson <jed.watson@me.com>
2021-02-03 14:27:22 +1100

    Adding GA to docs-next website (#4752)
    * Adding GA to docs-next website
    
    * Prettier
    
    * Prettier two
    
    * move gtag types from devDeps to deps
    
    * add prod flag and remove window invocation
    
    * add <reference for gtag.js and missing getInitialProps type in document
    
    Co-authored-by: gwyneplaine <cc.lee@live.com.au>

---
+++ docs-next/pages/_document.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+import { GA_TRACKING_ID } from '../lib/analytics';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
