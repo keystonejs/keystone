ff0d4d8b7f5ee6e7f6c1386be0fe0229843a924c
gwyneplaine <cc.lee@live.com.au>
2021-02-03 11:38:10 +1100

    add <reference for gtag.js and missing getInitialProps type in document

--- docs-next/lib/analytics.ts
+++ docs-next/lib/analytics.ts
@@ -1,14 +1,9 @@
+/// <reference types="gtag.js" />
 export const GA_TRACKING_ID = 'G-KZHKNXV8CM';
-
-// declare global {
-//   interface Window {
-//     gtag: any;
-//   }
-// }
 
 export const handleRouteChange = (url: URL) => {
   if (process.env.NODE_ENV === 'production') {
-    gtag('config', GA_TRACKING_ID, {
+    window.gtag('config', GA_TRACKING_ID, {
       page_path: url,
     });
   }
--- docs-next/pages/_document.tsx
+++ docs-next/pages/_document.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 import { GA_TRACKING_ID } from '../lib/analytics';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
