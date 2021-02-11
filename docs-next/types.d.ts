6ea1eb1d79390a12f07a42d43d7d62828f2f8d14
gwyneplaine <cc.lee@live.com.au>
2021-02-11 15:07:26 +1100

    add types.d.ts for @mdx-js/react

---
+++ docs-next/types.d.ts
@@ -0,0 +1,37 @@
+declare module '@mdx-js/react' {
+  import * as React from 'react';
+  type ComponentType =
+    | 'a'
+    | 'blockquote'
+    | 'code'
+    | 'del'
+    | 'em'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'hr'
+    | 'img'
+    | 'inlineCode'
+    | 'li'
+    | 'ol'
+    | 'p'
+    | 'pre'
+    | 'strong'
+    | 'sup'
+    | 'table'
+    | 'td'
+    | 'thematicBreak'
+    | 'tr'
+    | 'ul';
+  export type Components = {
+    [key in ComponentType]?: React.ComponentType<any>;
+  };
+  export interface MDXProviderProps {
+    children: React.ReactNode;
+    components: Components;
+  }
+  export class MDXProvider extends React.Component<MDXProviderProps> {}
+}
