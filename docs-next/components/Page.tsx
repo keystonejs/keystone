f4163a06d27fcb106a0159711010e0087eebc945
Charles <cc.lee@live.com.au>
2021-02-15 19:34:24 +1100

    Add heading anchors to @keystone-next/website (#4838)
    * add anchors and remove backticks from headings
    * add click to copy functionality to anchors
    * remove unnecessary imports
    
    Co-authored-by: Jed Watson <jed@keystonejs.com>

--- docs-next/components/Page.tsx
+++ docs-next/components/Page.tsx
@@ -1,9 +1,11 @@
-import React, { ReactNode } from 'react';
+/** @jsx jsx  */
+import { ReactNode } from 'react';
+import { jsx } from '@keystone-ui/core';
 import { Code } from '../components/Code';
+import { H1, H2, H3, H4, H5, H6 } from '../components/Heading';
 import { MDXProvider } from '@mdx-js/react';
 import cx from 'classnames';
 import Link from 'next/link';
-
 import { Navigation } from './Navigation';
 
 export const Page = ({ children, isProse }: { children: ReactNode; isProse?: boolean }) => {
@@ -67,6 +69,12 @@ export const Page = ({ children, isProse }: { children: ReactNode; isProse?: boo
 
 export const components = {
   code: Code,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
 };
 
 export const Markdown = ({ children }: { children: ReactNode }) => (
