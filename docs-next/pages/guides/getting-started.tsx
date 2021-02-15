a3d7634eb49561a9d30a0b08371d582b4444f6b7
gwyneplaine <cc.lee@live.com.au>
2021-02-15 15:40:05 +1100

    remove unecessary ToastProvider

--- docs-next/pages/guides/getting-started.tsx
+++ docs-next/pages/guides/getting-started.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { ToastProvider } from '@keystone-ui/toast';
 import { Page } from '../../components/Page';
 import { H1 } from '../../components/Heading';
 
 export default function IndexPage() {
   return (
     <Page isProse>
-      <ToastProvider>
-        <H1>Getting Started</H1>
-        <p>(coming soon)</p>
-      </ToastProvider>
+      <H1>Getting Started</H1>
+      <p>(coming soon)</p>
     </Page>
   );
 }
