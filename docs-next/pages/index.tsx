39de79d4a1738892d5f655b6ee20b33d21b29e58
Charles <cc.lee@live.com.au>
2021-02-18 16:57:33 +1100

    Signup form (#4877)
    * signup form on index
    
    * updates
    
    * cleanup
    
    * typescript
    
    Co-authored-by: Jed Watson <jed@keystonejs.com>

--- docs-next/pages/index.tsx
+++ docs-next/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import { Page } from '../components/Page';
+import { SignupForm } from '../components/SignupForm';
 
 export default function IndexPage() {
   return (
     <Page isProse>
       <h1>Welcome</h1>
       <p>These are the docs for the next version of KeystoneJS.</p>
+      <SignupForm />
       <p>We still need to write most of them – thanks for your patience as we work on it.</p>
       <p>
         If you have feedback, please reach out to us on Twitter at{' '}
