885185eb9fb36a120c7864859b7d14b545ba5ac9
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-18 12:05:40 +0300

    Set port and db.url from env in examples-next

---
+++ .changeset/stale-gorillas-prove.md
@@ -0,0 +1,6 @@
+---
+'@keystone-next/example-ecommerce': minor
+'@keystone-next/example-todo': minor
+---
+
+Set port and db.url from env file
--- examples-next/todo/keystone.ts
+++ examples-next/todo/keystone.ts
@@ -2,8 +2,9 @@ import { config } from '@keystone-next/keystone/schema';
 import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
 import { lists } from './schema';
 import { createAuth } from '@keystone-next/auth';
+import 'dotenv/config';
 
-const sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
+const sessionSecret = process.env.COOKIE_SECRET || '-- DEV COOKIE SECRET; CHANGE ME --';
 const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
 const sessionConfig = {
   maxAge: sessionMaxAge,
@@ -22,10 +23,13 @@ const { withAuth } = createAuth({
 export default withAuth(
   config({
     db: {
-      adapter: 'prisma_postgresql',
-      url: 'postgres://keystone5:k3yst0n3@localhost:5432/todo-example',
+      adapter: process.env.DATABASE_ADAPTER || 'prisma_postgresql',
+      url: process.env.DATABASE_URL || 'postgres://keystone5:k3yst0n3@localhost:5432/todo-example',
     },
     lists,
+    server: {
+      port: parseInt(process.env.PORT || '3000'),
+    },
     ui: {
       isAccessAllowed: ({ session }) => !!session,
     },
