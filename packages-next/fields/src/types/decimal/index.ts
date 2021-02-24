2e28a59aa8af92672935448e448c0807578633af
Alexey Murz Korepov <MurzNN@gmail.com>
2021-02-24 17:25:19 +0300

    Added decimal field with precision, scale

---
+++ packages-next/fields/src/types/decimal/index.ts
@@ -0,0 +1,29 @@
+import { Decimal } from '@keystonejs/fields';
+import type { FieldType, BaseGeneratedListTypes, FieldDefaultValue } from '@keystone-next/types';
+import { resolveView } from '../../resolve-view';
+import type { FieldConfig } from '../../interfaces';
+
+export type Int = number & { __int__: void };
+
+export type DecimalFieldConfig<
+  TGeneratedListTypes extends BaseGeneratedListTypes
+> = FieldConfig<TGeneratedListTypes> & {
+  isRequired?: boolean;
+  isIndexed?: boolean;
+  isUnique?: boolean;
+  precision?: Int;
+  scale?: Int;
+  defaultValue?: FieldDefaultValue<string>;
+};
+
+export const decimal = <TGeneratedListTypes extends BaseGeneratedListTypes>(
+  config: DecimalFieldConfig<TGeneratedListTypes> = {}
+): FieldType<TGeneratedListTypes> => ({
+  type: Decimal,
+  config,
+  views: resolveView('decimal/views'),
+  getAdminMeta: () => ({
+    precision: config.precision,
+    scale: config.scale,
+  }),
+});
