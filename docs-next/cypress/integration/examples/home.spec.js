f3dc476f29ae86d60326e3098664f45bcb57cb3e
gwyneplaine <cc.lee@live.com.au>
2021-02-25 15:36:23 +1100

    initial cypress setup for @keystone-next/website

---
+++ docs-next/cypress/integration/examples/home.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context('basic docs tests', () => {
+  beforeEach(() => {
+    console.log(process.env.SITE_URL);
+    cy.visit('http://localhost:8000');
+  });
+  it('finds the Welcome content', () => {
+    cy.contains('Welcome');
+  });
+});
