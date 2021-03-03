9f24091273d761529da514724ed2cb4174e9b697
Tim Leslie <timl@thinkmill.com.au>
2021-03-03 13:48:48 +1100

    No /init in publicPages if not using initFirstItem (#5005)

---
+++ .changeset/spotty-rockets-design.md
@@ -0,0 +1,5 @@
+---
+'@keystone-next/auth': patch
+---
+
+Updated `createAuth` to only include `/init` in `publicPages` if `initFirstItem` is being used.
--- packages-next/auth/src/index.ts
+++ packages-next/auth/src/index.ts
@@ -158,7 +158,10 @@ export function createAuth<GeneratedListTypes extends BaseGeneratedListTypes>({
    *
    * Must be added to the ui.publicPages config
    */
-  const publicAuthPages = ['/signin', '/init'];
+  const publicAuthPages = ['/signin'];
+  if (initFirstItem) {
+    publicAuthPages.push('/init');
+  }
 
   /**
    * extendGraphqlSchema
