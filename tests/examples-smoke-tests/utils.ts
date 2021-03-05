77d5b6baa28f7323870e36c457f4198785fa6ca2
mitchellhamilton <mitchell@hamil.town>
2021-03-05 12:43:49 +1000

    Test with other browsers

--- tests/examples-smoke-tests/utils.ts
+++ tests/examples-smoke-tests/utils.ts
@@ -110,12 +110,12 @@ export const exampleProjectTests = (
 
     describe.each([
       'chromium',
-      // 'firefox',
-      // // we don't run the tests on webkit in production
-      // // because unlike chromium and firefox
-      // // webkit doesn't treat localhost as a secure context
-      // // and we enable secure cookies in production
-      // ...(mode === 'prod' ? [] : (['webkit'] as const)),
+      'firefox',
+      // we don't run the tests on webkit in production
+      // because unlike chromium and firefox
+      // webkit doesn't treat localhost as a secure context
+      // and we enable secure cookies in production
+      ...(mode === 'prod' ? [] : (['webkit'] as const)),
     ] as const)('%s', browserName => {
       beforeAll(async () => {
         await deleteAllData(projectDir);
