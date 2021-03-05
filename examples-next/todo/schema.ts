20a0f18fa45d39573907445ddf6dbbe735dd9bef
mitchellhamilton <mitchell@hamil.town>
2021-03-05 11:34:22 +1000

    Fix todo example

--- examples-next/todo/schema.ts
+++ examples-next/todo/schema.ts
@@ -1,6 +1,7 @@
 import { createSchema, list } from '@keystone-next/keystone/schema';
 import { checkbox, password, relationship, text, timestamp } from '@keystone-next/fields';
 
+// this implementation for createdBy and updatedBy is currently wrong so they're disabled for now
 const trackingFields = {
   createdAt: timestamp({
     access: { create: false, read: true, update: false },
@@ -10,16 +11,16 @@ const trackingFields = {
       itemView: { fieldMode: 'read' },
     },
   }),
-  createdBy: relationship({
-    ref: 'User',
-    access: { create: false, read: true, update: false },
-    defaultValue: ({ context: { session } }) =>
-      session ? { connect: { id: session.itemId } } : null,
-    ui: {
-      createView: { fieldMode: 'hidden' },
-      itemView: { fieldMode: 'read' },
-    },
-  }),
+  // createdBy: relationship({
+  //   ref: 'User',
+  //   access: { create: false, read: true, update: false },
+  //   defaultValue: ({ context: { session } }) =>
+  //     session ? { connect: { id: session.itemId } } : null,
+  //   ui: {
+  //     createView: { fieldMode: 'hidden' },
+  //     itemView: { fieldMode: 'read' },
+  //   },
+  // }),
   updatedAt: timestamp({
     access: { create: false, read: true, update: false },
     hooks: {
@@ -30,24 +31,24 @@ const trackingFields = {
       itemView: { fieldMode: 'read' },
     },
   }),
-  updatedBy: relationship({
-    ref: 'User',
-    access: { create: false, read: true, update: false },
-    hooks: {
-      resolveInput: ({ context: { session } }) => (session ? session.itemId : null),
-    },
-    ui: {
-      createView: { fieldMode: 'hidden' },
-      itemView: { fieldMode: 'read' },
-    },
-  }),
+  // updatedBy: relationship({
+  //   ref: 'User',
+  //   access: { create: false, read: true, update: false },
+  //   hooks: {
+  //     resolveInput: ({ context: { session } }) => (session ? session.itemId : null),
+  //   },
+  //   ui: {
+  //     createView: { fieldMode: 'hidden' },
+  //     itemView: { fieldMode: 'read' },
+  //   },
+  // }),
 };
 
 export const lists = createSchema({
   Todo: list({
     ui: {
       listView: {
-        initialColumns: ['label', 'isComplete', 'createdAt', 'createdBy', 'updatedAt', 'updatedBy'],
+        initialColumns: ['label', 'isComplete', 'createdAt', 'updatedAt'],
       },
     },
     fields: {
@@ -61,7 +62,7 @@ export const lists = createSchema({
   User: list({
     ui: {
       listView: {
-        initialColumns: ['name', 'tasks', 'createdAt', 'createdBy', 'updatedAt', 'updatedBy'],
+        initialColumns: ['name', 'tasks', 'createdAt', 'updatedAt'],
       },
     },
     fields: {
