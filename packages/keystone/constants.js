74ca895b82e2ded62ae8d6d3bd06bbbd2482221e
Tim Leslie <timl@thinkmill.com.au>
2021-03-08 16:19:29 +1100

    Remove legacy keystone CLI

--- packages/keystone/constants.js
+++
@@ -1,9 +0,0 @@
-const PORT = process.env.PORT || 3000;
-module.exports = {
-  DEFAULT_PORT: PORT,
-  DEFAULT_APP_URL: process.env.APP_URL || `http://localhost:${PORT}`,
-  DEFAULT_ENTRY: 'index.js',
-  DEFAULT_SERVER: 'server.js',
-  DEFAULT_DIST_DIR: 'dist',
-  DEFAULT_COMMAND: 'dev',
-};
