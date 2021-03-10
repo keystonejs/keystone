62118970451702572a1115f3ae8141a26c38ca79
mitchellhamilton <mitchell@hamil.town>
2021-03-10 11:16:00 +1000

    Progress

--- packages-next/admin-ui/src/templates/api.ts
+++ packages-next/admin-ui/src/templates/api.ts
@@ -1,14 +1,14 @@
 export const apiTemplate = `
 import keystoneConfig from '../../../../keystone';
 import { initConfig, createSystem, createApolloServerMicro } from '@keystone-next/keystone';
-import { PrismaClient } from '../../../prisma/generated-client';
+import { PrismaClient } from '../../../../../prisma/generated-client';
 
 const initializedKeystoneConfig = initConfig(keystoneConfig);
 const { graphQLSchema, keystone, createContext } = createSystem(initializedKeystoneConfig, '.keystone', 'start', PrismaClient);
 const apolloServer = createApolloServerMicro({
   graphQLSchema,
   createContext,
-  sessionStrategy: _config.session ? _config.session() : undefined,
+  sessionStrategy: initializedKeystoneConfig.session ? initializedKeystoneConfig.session() : undefined,
   connectionPromise: keystone.connect(),
 });
 
