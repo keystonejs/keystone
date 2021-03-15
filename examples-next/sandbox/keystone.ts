309f1ab0dfa40ebefccd26a6286c11bc486059e1
mitchellhamilton <mitchell@hamil.town>
2021-03-15 18:01:54 +1000

    Remove session

--- examples-next/sandbox/keystone.ts
+++ examples-next/sandbox/keystone.ts
@@ -1,13 +1,5 @@
 import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
 import { lists } from './schema';
-
-const sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
-const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-const sessionConfig = {
-  maxAge: sessionMaxAge,
-  secret: sessionSecret,
-};
 
 export default config({
   db: process.env.DATABASE_URL?.startsWith('postgres')
@@ -21,5 +13,4 @@ export default config({
       },
   experimental: { prismaSqlite: true },
   lists,
-  session: withItemData(statelessSessions(sessionConfig)),
 });
