cc979cbd1a6746749449ab91cf297b8ed2a326e9
mitchellhamilton <mitchell@hamil.town>
2021-03-18 16:48:44 +1000

    Make preconstruct happy

--- packages/adapter-prisma/src/index.ts
+++ packages/adapter-prisma/src/index.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 export { PrismaAdapter, PrismaListAdapter, PrismaFieldAdapter } from './adapter-prisma';
 export { createMigration } from './migrations';
 export type { CLIOptionsForCreateMigration } from './migrations';
