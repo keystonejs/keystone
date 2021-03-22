a77e181492de7d8c988634557c9478962124c700
Jed Watson <jed.watson@me.com>
2021-03-23 10:16:15 +1100

    Adding basic keystone config

---
+++ examples-next/next-lite/keystone.ts
@@ -0,0 +1,11 @@
+import { config } from '@keystone-next/keystone/schema';
+
+import { Post } from './schema';
+
+export default config({
+  db: { adapter: 'prisma_sqlite', url: 'file:./app.db' },
+  experimental: {
+    prismaSqlite: true,
+  },
+  lists: { Post },
+});
