3c75c36947155fe1bbc0717bba01734586a3ecb0
mitchellhamilton <mitchell@hamil.town>
2021-03-22 14:20:59 +1000

    Revert "Remove commented code"
    This reverts commit db2d26ba36d2b7513e70e1199f73923eca436c74.

--- packages/adapter-prisma/src/adapter-prisma.js
+++ packages/adapter-prisma/src/adapter-prisma.js
@@ -251,6 +251,33 @@ class PrismaAdapter extends BaseKeystoneAdapter {
         path.resolve(this.schemaPath),
         true
       );
+      // if (this.provider === 'postgresql') {
+      //   // Special fast path to drop data from a postgres database.
+      //   // This is an optimization which is particularly crucial in a unit testing context.
+      //   // This code path takes milliseconds, vs ~7 seconds for a migrate reset + db push
+      //   const schemaName = uriToCredentials(this.url).schema || 'public';
+      //   for (const { tablename } of await this.prisma.$queryRaw(
+      //     `SELECT tablename FROM pg_tables WHERE schemaname='${schemaName}'`
+      //   )) {
+      //     await this.prisma.$queryRaw(`TRUNCATE TABLE \"${schemaName}\".\"${tablename}\" CASCADE;`);
+      //   }
+      //   for (const { relname } of await this.prisma.$queryRaw(
+      //     `SELECT c.relname FROM pg_class AS c JOIN pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind='S' AND n.nspname='${this.dbSchemaName}';`
+      //   )) {
+      //     await this.prisma.$queryRaw(
+      //       `ALTER SEQUENCE \"${schemaName}\".\"${relname}\" RESTART WITH 1;`
+      //     );
+      //   }
+      // } else if (this.provider === 'sqlite') {
+      //   const tables = await this.prisma.$queryRaw(
+      //     "SELECT name FROM sqlite_master WHERE type='table';"
+      //   );
+      //   for (const { name } of tables) {
+      //     await this.prisma.$queryRaw(`DELETE FROM "${name}";`);
+      //   }
+      // } else {
+      //   throw new Error('Only "postgresql" and "sqlite" providers are supported');
+      // }
     } else {
       resetDatabaseWithMigrations(this._url(), path.resolve(this.schemaPath));
     }
