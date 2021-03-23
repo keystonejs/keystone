aa76102c11bdfea02059df66f406a8b1d387c879
Tim Leslie <timl@thinkmill.com.au>
2021-03-23 13:10:14 +1100

    Remove legacy app-admin-ui package (#5186)

--- packages/app-admin-ui/client/components/ToastContainer.js
+++
@@ -1,8 +0,0 @@
-/** @jsx jsx */
-
-import { jsx } from '@emotion/core';
-import { DefaultToastContainer } from 'react-toast-notifications';
-
-const ToastContainer = props => <DefaultToastContainer {...props} css={{ zIndex: 3 }} />;
-
-export default ToastContainer;
