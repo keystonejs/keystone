fa9fd8c206983b094aa7fc3c072caf5e5bcc75e5
Ronald Aveling <luma.chroma@gmail.com>
2021-03-31 07:47:47 +1100

    add Next Vs 5 guide

--- docs-next/components/Navigation.tsx
+++ docs-next/components/Navigation.tsx
@@ -64,6 +64,7 @@ export const Navigation = () => {
         <NavItem href="/guides/testing" isPlaceholder>
           Testing
         </NavItem>
+        <NavItem href="/guides/keystone-5-vs-keystone-next">Keystone 5 vs Next</NavItem>
       </Section>
       <Section label="API">
         <NavItem href="/apis/config">Config API</NavItem>
