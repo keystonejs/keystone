1e6d12f47076816d2a2441b42471176c5a7f2f8c
Mitchell Hamilton <mitchell@hamil.town>
2021-03-31 13:21:50 +1000

    Move usages of @prisma/migrate and @prisma/sdk from adapter to @keystone-next/keystone (#5302)

---
+++ .changeset/serious-dryers-argue.md
@@ -0,0 +1,5 @@
+---
+'@keystone-next/adapter-prisma-legacy': major
+---
+
+Removed formatting of Prisma schema returned from `_generatePrismaSchema` method and made it return synchronously
--- packages/adapter-prisma/src/index.ts
+++ packages/adapter-prisma/src/index.ts
@@ -1,3 +1,1 @@
 export { PrismaAdapter, PrismaListAdapter, PrismaFieldAdapter } from './adapter-prisma';
-export { createMigration, devMigrations, runPrototypeMigrations } from './migrations';
-export type { CLIOptionsForCreateMigration } from './migrations';
