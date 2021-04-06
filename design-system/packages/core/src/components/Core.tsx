746eb8bc27e249f93f88b5ddcddc14c48ac8e179
gwyneplaine <cc.lee@live.com.au>
2021-04-06 13:27:25 +1000

    neverminderinos

--- design-system/packages/core/src/components/Core.tsx
+++ design-system/packages/core/src/components/Core.tsx
@@ -61,12 +61,6 @@ const BaseCSS = ({ includeNormalize, optimizeLegibility }: BaseCSSProps) => {
             h5: { fontSize: '0.83rem' },
             h6: { fontSize: '0.67rem' },
 
-            // h1: { fontSize: '2em' },
-            // h2: { fontSize: '1.5em' },
-            // h3: { fontSize: '1.17em' },
-            // h5: { fontSize: '0.83em' },
-            // h6: { fontSize: '0.67em' },
-
             // optimize legibility
             ...(optimizeLegibility && {
               textRendering: 'optimizeLegibility',
