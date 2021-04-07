a503b33564da20152a669755570754a0b2ef9b47
Tim Leslie <timl@thinkmill.com.au>
2021-04-08 08:42:09 +1000

    Move fields out of legacy packages (#5375)

--- packages/fields/src/types/Password/index.js
+++
@@ -1,9 +0,0 @@
-import { Password, PrismaPasswordInterface } from './Implementation';
-
-export default {
-  type: 'Password',
-  implementation: Password,
-  adapters: {
-    prisma: PrismaPasswordInterface,
-  },
-};
