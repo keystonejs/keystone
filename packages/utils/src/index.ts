adf00ec556ae0dbdf6171edaed3adab286e99968
gwyneplaine <cc.lee@live.com.au>
2021-04-08 15:02:54 +1000

    prettier fix

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -248,9 +248,11 @@ export const upcase = (str: string) => str.substr(0, 1).toUpperCase() + str.subs
  * @param {String} str The string to convert.
  * @returns The new string
  */
-export const humanize = (str: string) => str
-  .replace(/([a-z])([A-Z]+)/g, '$1 $2')
-  .split(/\s|_|\-/)
-  .filter(i => i)
-  .map(upcase)
-  .join(' ');
+export const humanize = (str: string) => {
+  return str
+    .replace(/([a-z])([A-Z]+)/g, '$1 $2')
+    .split(/\s|_|\-/)
+    .filter(i => i)
+    .map(upcase)
+    .join(' ');
+};
