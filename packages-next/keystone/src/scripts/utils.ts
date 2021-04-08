20081c2c774fc08f1c8a51cb263c204c7ce27618
mitchellhamilton <mitchell@hamil.town>
2021-04-08 12:01:25 +1000

    Start of testing the CLI

--- packages-next/keystone/src/scripts/utils.ts
+++ packages-next/keystone/src/scripts/utils.ts
@@ -7,3 +7,11 @@ export function getConfigPath(cwd: string) {
 export function getAdminPath(cwd: string) {
   return path.join(cwd, '.keystone/admin');
 }
+
+export class ExitError extends Error {
+  code: number;
+  constructor(code: number) {
+    super(`The process should exit with ${code}`);
+    this.code = code;
+  }
+}
