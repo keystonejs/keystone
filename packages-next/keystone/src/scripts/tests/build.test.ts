d9dea941d0588c32db69350ee394c47908cae208
mitchellhamilton <mitchell@hamil.town>
2021-04-13 13:34:31 +1000

    Fix things

--- packages-next/keystone/src/scripts/tests/build.test.ts
+++ packages-next/keystone/src/scripts/tests/build.test.ts
@@ -1,4 +1,5 @@
 import execa from 'execa';
+import * as fs from 'fs-extra';
 import { ExitError } from '../utils';
 import {
   basicKeystoneConfig,
@@ -11,7 +12,7 @@ import {
   testdir,
 } from './utils';
 
-// the success cases for build and start are tested in
+// the general success cases for build and start are tested in the example smoke tests
 
 test("start errors when a build hasn't happened", async () => {
   const tmp = await testdir({
@@ -58,9 +59,11 @@ test('build works with typescript without the user defining a babel config', asy
     all: true,
     cwd: tmp,
   });
+  expect(await fs.readFile(`${tmp}/node_modules/.keystone/types.js`, 'utf8')).toBe('');
   expect(result.exitCode).toBe(0);
-  expect(result.all!.replace(/\d+(|\.\d) k?B/g, 'size').replace(/chunks\/.*\.js/, 'chunks/hash.js'))
-    .toMatchInlineSnapshot(`
+  expect(
+    result.all!.replace(/\d+(|\.\d+) k?B/g, 'size').replace(/chunks\/.*\.js/g, 'chunks/hash.js')
+  ).toMatchInlineSnapshot(`
     "✨ Building Keystone
     ✨ Generating Admin UI code
     ✨ Generating Keystone config code
@@ -78,22 +81,22 @@ test('build works with typescript without the user defining a babel config', asy
     info  - Finalizing page optimization...
 
     Page                                                           Size     First Load JS
-    ┌ ○ /                                                          1.76 kB         199 kB
-    ├   /_app                                                      0 B             197 kB
-    ├ ○ /404                                                       3.44 kB         201 kB
-    ├ λ /api/__keystone_api_build                                  0 B             197 kB
-    ├ ○ /no-access                                                 1.54 kB         199 kB
-    ├ ○ /todos                                                     24.5 kB         222 kB
-    └ ○ /todos/[id]                                                5.24 kB         203 kB
-    + First Load JS shared by all                                  197 kB
-      ├ chunks/126c777f9f9b7289ece21ae42bc83989dfa1d6ca.565b0f.js  65.8 kB
-      ├ chunks/55daa7e308de45b7fdc3753e04ca073c7b6cdd4c.800dc2.js  30.6 kB
-      ├ chunks/6af0358f98ea97ed9221d1ad4fa493d9c36bec5a.2fbf1f.js  13.4 kB
-      ├ chunks/99bfd935b074ada4d52ef17fef5bb16a2d3b0efa.dad4f8.js  34.7 kB
-      ├ chunks/framework.054e3a.js                                 42 kB
-      ├ chunks/main.6ffe44.js                                      7.12 kB
-      ├ chunks/pages/_app.d610d6.js                                2.98 kB
-      └ chunks/webpack.50bee0.js                                   751 B
+    ┌ ○ /                                                          size         size
+    ├   /_app                                                      size             size
+    ├ ○ /404                                                       size         size
+    ├ λ /api/__keystone_api_build                                  size             size
+    ├ ○ /no-access                                                 size         size
+    ├ ○ /todos                                                     size         size
+    └ ○ /todos/[id]                                                size         size
+    + First Load JS shared by all                                  size
+      ├ chunks/hash.js  size
+      ├ chunks/hash.js  size
+      ├ chunks/hash.js  size
+      ├ chunks/hash.js  size
+      ├ chunks/hash.js                                 size
+      ├ chunks/hash.js                                      size
+      ├ chunks/hash.js                                size
+      └ chunks/hash.js                                   size
 
     λ  (Server)  server-side renders at runtime (uses getInitialProps or getServerSideProps)
     ○  (Static)  automatically rendered as static HTML (uses no initial props)
