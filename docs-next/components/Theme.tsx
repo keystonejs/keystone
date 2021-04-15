8b73a6aaceb11ea6c94b90614073d904a3d80146
Dominik Wilkowski <hi@dominik-wilkowski.com>
2021-04-15 20:58:48 +1000

    added spacing tokens

--- docs-next/components/Theme.tsx
+++ docs-next/components/Theme.tsx
@@ -7,7 +7,16 @@ export function Theme() {
   return (
     <Global
       styles={{
-        ':root': COLORS,
+        ':root': {
+          ...COLORS,
+          '--space-xxsmall': '0.375rem',
+          '--space-xsmall': '0.5rem',
+          '--space-small': '0.75rem',
+          '--space-medium': '1rem',
+          '--space-large': '1.5rem',
+          '--space-xlarge': '3rem',
+          '--space-xxlarge': '6rem',
+        },
       }}
     />
   );
