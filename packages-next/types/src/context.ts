31fcb9873517f483cc3af4126c35ca1e5a31710d
mitchellhamilton <mitchell@hamil.town>
2021-04-19 12:45:28 +1000

    Fix stuff

--- packages-next/types/src/context.ts
+++ packages-next/types/src/context.ts
@@ -38,14 +38,14 @@ export type KeystoneListsAPI<
     ): Promise<readonly Record<string, any>[]>;
     findOne(
       args: { readonly where: { readonly id: string } } & ResolveFields
-    ): Promise<Record<string, any> | null>;
+    ): Promise<Record<string, any>>;
     count(args: KeystoneListsTypeInfo[Key]['args']['listQuery']): Promise<number>;
     updateOne(
       args: {
         readonly id: string;
         readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'];
       } & ResolveFields
-    ): Promise<Record<string, any> | null>;
+    ): Promise<Record<string, any>>;
     updateMany(
       args: {
         readonly data: readonly {
@@ -53,19 +53,19 @@ export type KeystoneListsAPI<
           readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'];
         }[];
       } & ResolveFields
-    ): Promise<(Record<string, any> | null)[] | null>;
+    ): Promise<Record<string, any>[]>;
     createOne(
       args: { readonly data: KeystoneListsTypeInfo[Key]['inputs']['create'] } & ResolveFields
-    ): Promise<Record<string, any> | null>;
+    ): Promise<Record<string, any>>;
     createMany(
       args: {
         readonly data: readonly { readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'] }[];
       } & ResolveFields
-    ): Promise<(Record<string, any> | null)[] | null>;
+    ): Promise<Record<string, any>[]>;
     deleteOne(args: { readonly id: string } & ResolveFields): Promise<Record<string, any> | null>;
     deleteMany(
       args: { readonly ids: readonly string[] } & ResolveFields
-    ): Promise<(Record<string, any> | null)[] | null>;
+    ): Promise<Record<string, any>[]>;
   };
 };
 
@@ -90,28 +90,28 @@ export type KeystoneDbAPI<KeystoneListsTypeInfo extends Record<string, BaseGener
     ): Promise<readonly KeystoneListsTypeInfo[Key]['backing'][]>;
     findOne(args: {
       readonly where: { readonly id: string };
-    }): Promise<KeystoneListsTypeInfo[Key]['backing'] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing']>;
     count(args: KeystoneListsTypeInfo[Key]['args']['listQuery']): Promise<number>;
     updateOne(args: {
       readonly id: string;
       readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'];
-    }): Promise<KeystoneListsTypeInfo[Key]['backing'] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing']>;
     updateMany(args: {
       readonly data: readonly {
         readonly id: string;
         readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'];
       }[];
-    }): Promise<(KeystoneListsTypeInfo[Key]['backing'] | null)[] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing'][]>;
     createOne(args: {
       readonly data: KeystoneListsTypeInfo[Key]['inputs']['create'];
-    }): Promise<KeystoneListsTypeInfo[Key]['backing'] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing']>;
     createMany(args: {
       readonly data: readonly { readonly data: KeystoneListsTypeInfo[Key]['inputs']['update'] }[];
-    }): Promise<(KeystoneListsTypeInfo[Key]['backing'] | null)[] | null>;
-    deleteOne(args: { readonly id: string }): Promise<KeystoneListsTypeInfo[Key]['backing'] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing'][]>;
+    deleteOne(args: { readonly id: string }): Promise<KeystoneListsTypeInfo[Key]['backing']>;
     deleteMany(args: {
       readonly ids: readonly string[];
-    }): Promise<(KeystoneListsTypeInfo[Key]['backing'] | null)[] | null>;
+    }): Promise<KeystoneListsTypeInfo[Key]['backing'][]>;
   };
 };
 
