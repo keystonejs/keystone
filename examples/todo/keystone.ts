44c1f9494e8a5587a43f9ba793ea1833e15f8ada
Tim Leslie <timl@thinkmill.com.au>
2021-05-04 09:53:46 +1000

    Simplify the todo example (#5591)

--- examples/todo/keystone.ts
+++ examples/todo/keystone.ts
@@ -1,34 +1,10 @@
 import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
-import { createAuth } from '@keystone-next/auth';
 import { lists } from './schema';
 
-const sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
-const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-const sessionConfig = {
-  maxAge: sessionMaxAge,
-  secret: sessionSecret,
-};
-
-const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  initFirstItem: {
-    fields: ['name', 'email', 'password'],
+export default config({
+  db: {
+    provider: 'sqlite',
+    url: process.env.DATABASE_URL || 'file:./keystone-example.db',
   },
+  lists,
 });
-
-export default withAuth(
-  config({
-    db: {
-      provider: 'sqlite',
-      url: process.env.DATABASE_URL || 'file:./keystone-example.db',
-    },
-    lists,
-    ui: {
-      isAccessAllowed: ({ session }) => !!session,
-    },
-    session: withItemData(statelessSessions(sessionConfig)),
-  })
-);
