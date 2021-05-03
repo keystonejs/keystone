2d05fa16669d4281da08ae910fa00fead5715ebf
Tim Leslie <timl@thinkmill.com.au>
2021-05-03 16:07:49 +1000

    Simplify the todo example

---
+++ .changeset/six-numbers-join.md
@@ -0,0 +1,5 @@
+---
+'@keystone-next/example-todo': major
+---
+
+Simplified the Todo application to just the basics to allow other examples to build on it.
--- tests/examples-smoke-tests/todo.test.ts
+++ tests/examples-smoke-tests/todo.test.ts
@@ -1,5 +1,5 @@
 import { Browser, Page } from 'playwright';
-import { exampleProjectTests, initFirstItemTest } from './utils';
+import { exampleProjectTests } from './utils';
 
 exampleProjectTests('todo', browserType => {
   let browser: Browser = undefined as any;
@@ -9,7 +9,10 @@ exampleProjectTests('todo', browserType => {
     page = await browser.newPage();
     page.goto('http://localhost:3000');
   });
-  initFirstItemTest(() => page);
+  test('Load list', async () => {
+    await Promise.all([page.waitForNavigation(), page.click('h3:has-text("People")')]);
+    await page.waitForSelector('button:has-text("Create Person")');
+  });
   afterAll(async () => {
     await browser.close();
   });
