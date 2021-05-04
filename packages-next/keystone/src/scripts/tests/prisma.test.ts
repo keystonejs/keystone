6a26ba12f03139eff55f75411445e5f6daaf9da5
Tim Leslie <timl@thinkmill.com.au>
2021-05-05 08:59:06 +1000

    Fix test snapshot

--- packages-next/keystone/src/scripts/tests/prisma.test.ts
+++ packages-next/keystone/src/scripts/tests/prisma.test.ts
@@ -52,12 +52,12 @@ test('keystone-next prisma exits with the same code as the prisma child process 
 
       Create migrations from your Prisma schema, apply them to the database, generate artifacts (e.g. Prisma Client)
       $ prisma migrate dev
-      
+
       Pull the schema from an existing database, updating the Prisma schema
       $ prisma db pull
 
       Push the Prisma schema state to the database
-      $ prisma db push --preview-feature
+      $ prisma db push
     "
   `);
 });
