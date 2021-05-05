c369b84e05d3531abac9e123329f8e2aec1c9c08
gwyneplaine <cc.lee@live.com.au>
2021-05-05 12:31:01 +1000

    add Post list with basic document field

--- examples/blog/schema.ts
+++ examples/blog/schema.ts
@@ -1,28 +1,50 @@
 import { createSchema, list } from '@keystone-next/keystone/schema';
-import { checkbox, relationship, text, timestamp } from '@keystone-next/fields';
-import { select } from '@keystone-next/fields';
+import { select, relationship, text, timestamp, image } from '@keystone-next/fields';
+import { document } from '@keystone-next/fields-document';
 
 export const lists = createSchema({
-  Task: list({
+  Post: list({
     fields: {
-      label: text({ isRequired: true }),
-      priority: select({
+      title: text({ isRequired: true }),
+      status: select({
         dataType: 'enum',
         options: [
-          { label: 'Low', value: 'low' },
-          { label: 'Medium', value: 'medium' },
-          { label: 'High', value: 'high' },
+          { label: 'Draft', value: 'draft' },
+          { label: 'Published', value: 'published' },
         ],
+        ui: {
+          displayMode: 'segmented-control',
+        },
       }),
-      isComplete: checkbox(),
-      assignedTo: relationship({ ref: 'Person.tasks', many: false }),
+      content: document({
+        formatting: true,
+        links: true,
+      }),
+      publishDate: timestamp(),
+      // author: relationship({
+      //   ref: 'Author.posts',
+      //   ui: {
+      //     displayMode: 'cards',
+      //     cardFields: ['name', 'email'],
+      //     inlineEdit: { fields: ['name', 'email'] },
+      //     linkToItem: true,
+      //     inlineCreate: { fields: ['name', 'email'] },
+      //   },
+      // }),
       finishBy: timestamp(),
     },
   }),
-  Person: list({
-    fields: {
-      name: text({ isRequired: true }),
-      tasks: relationship({ ref: 'Task.assignedTo', many: true }),
-    },
-  }),
+  // Author: list({
+  //   ui: {
+  //     listView: {
+  //       initialColumns: ['name', 'posts', 'avatar'],
+  //     },
+  //   },
+  //   fields: {
+  //     name: text({ isRequired: true }),
+  //     email: text({ isRequired: true, isUnique: true }),
+  //     avatar: image(),
+  //     posts: relationship({ ref: 'Post.author', many: true }),
+  //   },
+  // }),
 });
