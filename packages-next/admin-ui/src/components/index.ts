0fc69a643236bf87255f56f4627428247b358ac5
Gautam Singh <5769869+gautamsi@users.noreply.github.com>
2021-05-09 03:56:01 +0530

    missing ui components export for customizing admin ui pages

--- packages-next/admin-ui/src/components/index.ts
+++ packages-next/admin-ui/src/components/index.ts
@@ -1,8 +1,10 @@
 export { CellContainer } from './CellContainer';
 export { CellLink } from './CellLink';
+export { Container } from './Container';
 export { ErrorBoundary, ErrorContainer } from './Errors';
 export { Logo } from './Logo';
 export { Navigation } from './Navigation';
 export { PageContainer } from './PageContainer';
+export { Pagination, PaginationLabel } from './Pagination';
 export { CreateItemDrawer } from './CreateItemDrawer';
 export { GraphQLErrorNotice } from './GraphQLErrorNotice';
---
+++ packages-next/admin-ui/src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from './ListPage/FieldSelection';
+export * from './ListPage/FilterAdd';
+export * from './ListPage/FilterList';
+export * from './ListPage/SortSelection';
+export * from './ListPage/useFilters';
+export * from './ListPage/useSelectedFields';
+export * from './ListPage/useSort';
