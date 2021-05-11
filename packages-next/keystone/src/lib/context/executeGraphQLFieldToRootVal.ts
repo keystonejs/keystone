29fc64f363f5b504bf9e86621cf8c556ea4dd843
mitchellhamilton <mitchell@hamil.town>
2021-05-11 16:12:00 +1000

    Fix a type error

--- packages-next/keystone/src/lib/context/executeGraphQLFieldToRootVal.ts
+++ packages-next/keystone/src/lib/context/executeGraphQLFieldToRootVal.ts
@@ -55,7 +55,9 @@ function getVariablesForGraphQLField(field: GraphQLField<any, any>) {
     type: getTypeNodeForType(arg.type),
     variable: { kind: 'Variable', name: { kind: 'Name', value: arg.name } },
     defaultValue:
-      arg.defaultValue === undefined ? undefined : astFromValue(arg.defaultValue, arg.type),
+      arg.defaultValue === undefined
+        ? undefined
+        : astFromValue(arg.defaultValue, arg.type) ?? undefined,
   }));
 
   const argumentNodes: ArgumentNode[] = field.args.map(arg => ({
