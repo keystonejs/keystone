48e54f247897c814941e4e8f9ebdf9272c959949
Tim Leslie <timl@thinkmill.com.au>
2021-05-19 12:15:58 +1000

    yarn format

--- packages-next/admin-ui/src/system/index.ts
+++ packages-next/admin-ui/src/system/index.ts
@@ -2,4 +2,4 @@ throw new Error(
   'The @keystone-next/admin-ui package has been deprecated. Please import from @keystone-next/keystone/admin-ui instead.'
 );
 
-export {}
+export {};
--- packages-next/admin-ui/src/templates/index.ts
+++ packages-next/admin-ui/src/templates/index.ts
@@ -1,5 +1,5 @@
 throw new Error(
-    'The @keystone-next/admin-ui package has been deprecated. Please import from @keystone-next/keystone/admin-ui instead.'
-  );
+  'The @keystone-next/admin-ui package has been deprecated. Please import from @keystone-next/keystone/admin-ui instead.'
+);
 
-  export {}
+export {};
