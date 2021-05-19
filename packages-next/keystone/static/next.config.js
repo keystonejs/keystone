dfd806f3030e951a1c4e18e66662acc14ec906cf
Tim Leslie <timl@thinkmill.com.au>
2021-05-19 13:32:17 +1000

    Fix lint

--- packages-next/keystone/static/next.config.js
+++ packages-next/keystone/static/next.config.js
@@ -1,4 +1,4 @@
 // this file is copied somewhere else so this lint rule is incorrect
-// eslint-disable-next-line import/no-extraneous-dependencies
 module.exports =
+  // eslint-disable-next-line import/no-extraneous-dependencies
   require('@keystone-next/keystone/___internal-do-not-use-will-break-in-patch/admin-ui/next-config').config;
