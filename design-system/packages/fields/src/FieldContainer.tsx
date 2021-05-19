20efe957330cd339e38774f26c55a0587d7062cb
gwyneplaine <cc.lee@live.com.au>
2021-05-20 09:53:44 +1000

    remove FieldContainerProps and no longer explicitly call children on FieldContainer

--- design-system/packages/fields/src/FieldContainer.tsx
+++ design-system/packages/fields/src/FieldContainer.tsx
@@ -1,18 +1,6 @@
 /* @jsx jsx */
-
-import { ReactNode } from 'react';
 import { jsx, forwardRefWithAs } from '@keystone-ui/core';
 
-type FieldContainerProps = {
-  children: ReactNode;
-};
-
-export const FieldContainer = forwardRefWithAs<'div', FieldContainerProps>(
-  ({ as: Tag = 'div', children, ...props }, ref) => {
-    return (
-      <Tag ref={ref} {...props}>
-        {children}
-      </Tag>
-    );
-  }
-);
+export const FieldContainer = forwardRefWithAs<'div', {}>(({ as: Tag = 'div', ...props }, ref) => {
+  return <Tag ref={ref} {...props} />;
+});
