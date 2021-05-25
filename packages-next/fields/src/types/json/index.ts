59421c0399368e56e46537c1c687daa27f5912d0
Charles <cc.lee@live.com.au>
2021-05-25 15:44:12 +1000

    Field/json (#5779)
    * json type init
    
    * null values
    
    * add initial tests + remove logs
    
    * add json example
    
    * fix package.json
    
    * more fixes
    
    * add docs
    
    * more fixes
    
    * update schema
    
    * add example to directory, typo fix
    
    * changeset
    
    * fix example schema
    
    * Update examples/json/README.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update examples/json/README.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update examples/json/README.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update examples/json/README.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update examples/README.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update docs/pages/apis/fields.mdx
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * Update .changeset/fuzzy-spiders-film.md
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    
    * add error handling
    
    * update exampel schema;
    
    * Update packages-next/fields/src/types/json/Implementation.ts
    
    * update grapqhl schema
    
    * Update .changeset/fuzzy-spiders-film.md
    
    Co-authored-by: Mitchell Hamilton <mitchell@hamil.town>
    
    * Update packages-next/fields/src/types/json/views/index.tsx
    
    Co-authored-by: Mitchell Hamilton <mitchell@hamil.town>
    
    * remove displayMode
    
    * JSONValue for defaultValue
    
    * Update packages-next/fields/src/types/json/views/index.tsx
    
    * prettier;
    
    * Update schema.graphql
    
    Co-authored-by: Tim Leslie <timl@thinkmill.com.au>
    Co-authored-by: Mitchell Hamilton <mitchell@hamil.town>

---
+++ packages-next/fields/src/types/json/index.ts
@@ -0,0 +1,27 @@
+import type {
+  FieldType,
+  BaseGeneratedListTypes,
+  FieldDefaultValue,
+  JSONValue,
+} from '@keystone-next/types';
+import { resolveView } from '../../resolve-view';
+import type { FieldConfig } from '../../interfaces';
+import { Json, PrismaJsonInterface } from './Implementation';
+
+export type JsonFieldConfig<TGeneratedListTypes extends BaseGeneratedListTypes> =
+  FieldConfig<TGeneratedListTypes> & {
+    defaultValue?: FieldDefaultValue<JSONValue>;
+    isRequired?: boolean;
+  };
+
+export const json = <TGeneratedListTypes extends BaseGeneratedListTypes>(
+  config: JsonFieldConfig<TGeneratedListTypes> = {}
+): FieldType<TGeneratedListTypes> => ({
+  type: {
+    type: 'Json',
+    implementation: Json,
+    adapter: PrismaJsonInterface,
+  },
+  config,
+  views: resolveView('json/views'),
+});
