e4cdae679bd787375ad7640e72f79ddc941f4111
Tim Leslie <timl@thinkmill.com.au>
2021-05-28 10:32:27 +1000

    [WIP] Add smoke tests for examples

---
+++ tests/examples-smoke-tests/json.test.ts
@@ -0,0 +1,19 @@
+import { Browser, Page } from 'playwright';
+import { exampleProjectTests } from './utils';
+
+exampleProjectTests('json', browserType => {
+  let browser: Browser = undefined as any;
+  let page: Page = undefined as any;
+  beforeAll(async () => {
+    browser = await browserType.launch();
+    page = await browser.newPage();
+    page.goto('http://localhost:3000');
+  });
+  test('Load list', async () => {
+    await Promise.all([page.waitForNavigation(), page.click('h3:has-text("People")')]);
+    await page.waitForSelector('button:has-text("Create Person")');
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+});
