195d4fb1218517d7b9a40d3bba1a087d40e6d1d6
Tim Leslie <timl@thinkmill.com.au>
2021-05-31 13:41:14 +1000

    Remove keystone argument from artifacts functions (#5832)

---
+++ .changeset/late-chicken-cheat.md
@@ -0,0 +1,6 @@
+---
+'@keystone-next/keystone': major
+'@keystone-next/test-utils-legacy': major
+---
+
+Updated the functions `getCommittedArtifacts`, `validateCommittedArtifacts`, `generateCommittedArtifacts`, and `generateNodeModulesArtifacts` exported from `artifacts.ts` to accept a `KeystoneConfig` argument rather than a `BaseKeystone` object.
--- packages-next/keystone/src/scripts/run/dev.ts
+++ packages-next/keystone/src/scripts/run/dev.ts
@@ -32,11 +32,11 @@ export const dev = async (cwd: string, shouldDropDatabase: boolean) => {
 
   const initKeystone = async () => {
     {
-      const { keystone, graphQLSchema } = createSystem(config);
+      const { graphQLSchema } = createSystem(config);
 
       console.log('✨ Generating GraphQL and Prisma schemas');
-      const prismaSchema = (await generateCommittedArtifacts(graphQLSchema, keystone, cwd)).prisma;
-      await generateNodeModulesArtifacts(graphQLSchema, keystone, config, cwd);
+      const prismaSchema = (await generateCommittedArtifacts(graphQLSchema, config, cwd)).prisma;
+      await generateNodeModulesArtifacts(graphQLSchema, config, cwd);
 
       if (config.db.useMigrations) {
         await devMigrations(
