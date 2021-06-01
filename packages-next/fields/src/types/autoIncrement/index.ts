6ad0d2711a8e6bc08fc4fbe09b2f4a7b7f351eda
mitchellhamilton <mitchell@hamil.town>
2021-06-01 10:39:05 +1000

    Fix autoIncrement

--- packages-next/fields/src/types/autoIncrement/index.ts
+++ packages-next/fields/src/types/autoIncrement/index.ts
@@ -1,5 +1,6 @@
 import {
   BaseGeneratedListTypes,
+  FieldDefaultValue,
   fieldType,
   FieldTypeFunc,
   filters,
@@ -9,59 +10,88 @@ import {
 } from '@keystone-next/types';
 import { resolveView } from '../../resolve-view';
 import type { CommonFieldConfig } from '../../interfaces';
+import { getIndexType } from '../../get-index-type';
 
 export type AutoIncrementFieldConfig<TGeneratedListTypes extends BaseGeneratedListTypes> =
-  CommonFieldConfig<TGeneratedListTypes>;
+  CommonFieldConfig<TGeneratedListTypes> & {
+    defaultValue?: FieldDefaultValue<number, TGeneratedListTypes>;
+    isRequired?: boolean;
+    isIndexed?: boolean;
+    isUnique?: boolean;
+  };
 
 export const autoIncrement =
-  <TGeneratedListTypes extends BaseGeneratedListTypes>(
-    config: AutoIncrementFieldConfig<TGeneratedListTypes> = {}
-  ): FieldTypeFunc =>
+  <TGeneratedListTypes extends BaseGeneratedListTypes>({
+    isRequired,
+    defaultValue,
+    isIndexed,
+    isUnique,
+    ...config
+  }: AutoIncrementFieldConfig<TGeneratedListTypes> = {}): FieldTypeFunc =>
   meta => {
     const type = meta.fieldKey === 'id' ? types.ID : types.Int;
+    const __legacy = {
+      isRequired,
+      defaultValue,
+      filters: {
+        fields: {
+          ...legacyFilters.fields.equalityInputFields(meta.fieldKey, type),
+          ...legacyFilters.fields.orderingInputFields(meta.fieldKey, type),
+          ...legacyFilters.fields.inInputFields(meta.fieldKey, type),
+        },
+        impls: {
+          ...equalityConditions(meta.fieldKey, x => Number(x) || -1),
+          ...legacyFilters.impls.orderingConditions(meta.fieldKey, x => Number(x) || -1),
+          ...inConditions(meta.fieldKey, x => x.map((xx: any) => Number(xx) || -1)),
+        },
+      },
+    };
+    if (meta.fieldKey === 'id') {
+      return fieldType({
+        kind: 'scalar',
+        mode: 'required',
+        scalar: 'Int',
+        default: { kind: 'autoincrement' },
+      })({
+        ...config,
+        input: {
+          where: { arg: types.arg({ type: filters[meta.provider].Int.required }) },
+          // TODO: fix the fact that TS did not catch that a resolver is needed here
+          uniqueWhere: {
+            arg: types.arg({ type }),
+            resolve(value) {
+              return Number(value);
+            },
+          },
+          orderBy: { arg: types.arg({ type: orderDirectionEnum }) },
+        },
+        output: types.field({
+          type: types.nonNull(types.ID),
+          // TODO: should @ts-gql/schema understand the coercion that graphql-js can do here?
+          resolve({ value }) {
+            return value.toString();
+          },
+        }),
+        views: resolveView('integer/views'),
+        __legacy,
+      });
+    }
     return fieldType({
       kind: 'scalar',
-      mode: 'required',
+      mode: 'optional',
       scalar: 'Int',
       default: { kind: 'autoincrement' },
+      index: getIndexType({ isIndexed, isUnique }),
     })({
       ...config,
       input: {
-        where: { arg: types.arg({ type: filters[meta.provider].Int.required }) },
-        // TODO: fix the fact that TS did not catch that a resolver is needed here
-        uniqueWhere: {
-          arg: types.arg({ type }),
-          resolve(value) {
-            return Number(value);
-          },
-        },
+        where: { arg: types.arg({ type: filters[meta.provider].Int.optional }) },
+        uniqueWhere: { arg: types.arg({ type: types.Int }) },
         orderBy: { arg: types.arg({ type: orderDirectionEnum }) },
       },
-      output:
-        meta.fieldKey === 'id'
-          ? types.field({
-              type: types.nonNull(types.ID),
-              // TODO: should @ts-gql/schema understand the coercion that graphql-js can do here?
-              resolve({ value }) {
-                return value.toString();
-              },
-            })
-          : types.field({ type: types.Int }),
+      output: types.field({ type: types.Int }),
       views: resolveView('integer/views'),
-      __legacy: {
-        filters: {
-          fields: {
-            ...legacyFilters.fields.equalityInputFields(meta.fieldKey, type),
-            ...legacyFilters.fields.orderingInputFields(meta.fieldKey, type),
-            ...legacyFilters.fields.inInputFields(meta.fieldKey, type),
-          },
-          impls: {
-            ...equalityConditions(meta.fieldKey, x => Number(x) || -1),
-            ...legacyFilters.impls.orderingConditions(meta.fieldKey, x => Number(x) || -1),
-            ...inConditions(meta.fieldKey, x => x.map((xx: any) => Number(xx) || -1)),
-          },
-        },
-      },
+      __legacy,
     });
   };
 
