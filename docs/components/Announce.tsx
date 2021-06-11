ac7430c326ddfcb201035fd9ee7784099b9d4f89
Dominik Wilkowski <hi@dominik-wilkowski.com>
2021-06-11 11:50:18 +1000

    Announcement for survey (#5892)
    * added announcement for survey
    
    * prettier

---
+++ docs/components/Announce.tsx
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx } from '@keystone-ui/core';
+import { HTMLAttributes } from 'react';
+
+export function Announce(props: HTMLAttributes<HTMLElement>) {
+  return (
+    <div
+      css={{
+        background: 'var(--black)',
+        color: 'var(--white)',
+        padding: '0.7rem',
+        textAlign: 'center',
+      }}
+      {...props}
+    />
+  );
+}
