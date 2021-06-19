d6bca695e595e4c418483f596436af67cacd6856
Jed Watson <jed.watson@me.com>
2021-06-19 20:36:47 +1000

    Typed Alert component

--- docs/components/primitives/Alert.tsx
+++ docs/components/primitives/Alert.tsx
@@ -1,6 +1,11 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
+import { HTMLAttributes } from 'react';
 
-export function Alert({ look = 'neutral', className, ...props }) {
+type AlertProps = {
+  look: 'neutral' | 'tip' | 'warn' | 'error';
+} & HTMLAttributes<HTMLElement>;
+
+export function Alert({ look = 'neutral', className, ...props }: AlertProps) {
   return <p className={`hint ${look} ${className}`} {...props} />;
 }
