844fb0f4c4b827e4c0f49d37c75146ac0c29dd3c
Ronald Aveling <luma.chroma@gmail.com>
2021-06-20 10:15:37 +1000

    update apis landing page

--- docs/pages/docs/apis/index.tsx
+++ docs/pages/docs/apis/index.tsx
@@ -1,8 +1,11 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 
+import { Button } from '../../../components/primitives/Button';
+import { Alert } from '../../../components/primitives/Alert';
 import { Type } from '../../../components/primitives/Type';
 import { Well } from '../../../components/primitives/Well';
+import { ArrowR } from '../../../components/icons/ArrowR';
 import { DocsPage } from '../../../components/Page';
 import { useMediaQuery } from '../../../lib/media';
 
@@ -11,20 +14,31 @@ export default function Docs() {
 
   return (
     <DocsPage noRightNav noProse>
-      <Type as="h1" look="heading64">
-        Keystone API
+      <Type as="h1" look="heading64" css={{ lineHeight: 1 }}>
+        API Reference
       </Type>
 
-      <Type as="p" look="body18" margin="1.25rem 0 0 0">
-        Lead text
-      </Type>
+      <Alert css={{ margin: '2rem 0' }}>
+        <span
+          css={{
+            display: 'inline-block',
+            margin: '0 1rem 0.5rem 0',
+          }}
+        >
+          Need answers to Keystone questions? Get the help you need in the
+        </span>
+        <Button
+          as="a"
+          href="https://community.keystonejs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Community Slack <ArrowR />
+        </Button>
+      </Alert>
 
       <Type as="h2" look="heading30" margin="2rem 0 1rem 0">
-        Sub heading
-      </Type>
-
-      <Type as="p" look="body18" margin="0 0 1.5rem 0">
-        Intro text
+        Configuration
       </Type>
 
       <div
@@ -34,20 +48,34 @@ export default function Docs() {
           gap: 'var(--space-xlarge)',
         })}
       >
-        <Well heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="System Configuration API" href="/docs/apis/config">
+          Keystone’s config function accepts an object representing all the configurable parts of
+          your backend system.
         </Well>
-        <Well heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="Schema API" href="/docs/apis/schema">
+          This is where you define the data model, or schema, of your Keystone system.
+        </Well>
+        <Well grad="grad4" heading="Fields API" href="/docs/apis/fields">
+          Defines the names, types, and configuration of the fields in a Keystone list.
+        </Well>
+        <Well grad="grad4" heading="Access Control" href="/docs/apis/access-control">
+          Configures who can read, create, update, and delete items in your Keystone system
+        </Well>
+        <Well grad="grad4" heading="Hooks" href="/docs/apis/hooks">
+          Let you execute code at different stages of the mutation lifecycle when performing create,
+          update, and delete operations.
+        </Well>
+        <Well grad="grad4" heading="Session" href="/docs/apis/session">
+          Lets you configure session management in your Keystone system.
+        </Well>
+        <Well grad="grad4" heading="Authentication" href="/docs/apis/auth">
+          Supports authentication against a password field, creating initial items, password resets,
+          and one-time authentication tokens.
         </Well>
       </div>
 
       <Type as="h2" look="heading30" margin="2rem 0 1rem 0">
-        Sub heading
-      </Type>
-
-      <Type as="p" look="body18" margin="0 0 1.5rem 0">
-        Intro text
+        Context
       </Type>
 
       <div
@@ -57,26 +85,20 @@ export default function Docs() {
           gap: 'var(--space-xlarge)',
         })}
       >
-        <Well grad="grad2" heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="Context API" href="/docs/apis/context">
+          The primary API entry point for all of the run-time functionally of your Keystone system.
         </Well>
-        <Well grad="grad2" heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="List Items API" href="/docs/apis/list-items">
+          A programmatic API for running CRUD operations against your GraphQL API.
         </Well>
-        <Well grad="grad2" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad2" heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="Database Items API" href="/docs/apis/db-items">
+          A programmatic API for running CRUD operations against the internal GraphQL resolvers in
+          your system.
         </Well>
       </div>
 
       <Type as="h2" look="heading30" margin="2rem 0 1rem 0">
-        Sub heading
-      </Type>
-
-      <Type as="p" look="body18" margin="0 0 1.5rem 0">
-        Intro text
+        GraphQL
       </Type>
 
       <div
@@ -86,46 +108,12 @@ export default function Docs() {
           gap: 'var(--space-xlarge)',
         })}
       >
-        <Well grad="grad3" heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="GraphQL API" href="/docs/apis/graphql">
+          Generates a CRUD (create, read, update, delete) GraphQL API based on the schema definition
+          provided in your system configuration.
         </Well>
-        <Well grad="grad3" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad3" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad3" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-      </div>
-
-      <Type as="h2" look="heading30" margin="2rem 0 1rem 0">
-        Sub heading
-      </Type>
-
-      <Type as="p" look="body18" margin="0 0 1.5rem 0">
-        Intro text
-      </Type>
-
-      <div
-        css={mq({
-          display: 'grid',
-          gridTemplateColumns: ['1fr', '1fr 1fr'],
-          gap: 'var(--space-xlarge)',
-        })}
-      >
-        <Well grad="grad4" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad4" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad4" heading="Your heading" href="/docs/apis/TODO">
-          Your text
-        </Well>
-        <Well grad="grad4" heading="Your heading" href="/docs/apis/TODO">
-          Your text
+        <Well grad="grad4" heading="Query filter API" href="/docs/apis/filters">
+          A list of the filters you can query against for each field type.
         </Well>
       </div>
     </DocsPage>
