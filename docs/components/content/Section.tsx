dd72e951c66268e5633c68df0b399b6e18d9f92d
Jed Watson <jed.watson@me.com>
2021-06-24 19:32:48 +1000

    Fixed content ordering issue for mobile viewport

--- docs/components/content/Section.tsx
+++ docs/components/content/Section.tsx
@@ -16,3 +16,35 @@ export function Section(props: HTMLAttributes<HTMLElement>) {
     />
   );
 }
+
+type SideBySideSectionProps = {
+  reverse?: boolean;
+} & HTMLAttributes<HTMLElement>;
+
+export function SideBySideSection({ reverse, children, ...props }: SideBySideSectionProps) {
+  const mq = useMediaQuery();
+
+  return (
+    <Section
+      css={mq({
+        display: 'grid',
+        gridTemplateColumns: ['1fr', null, '1fr 1fr'],
+        gap: '2rem',
+        alignItems: 'center',
+        ...(reverse
+          ? {
+              'div:nth-of-type(1)': {
+                order: [1, null, 2],
+              },
+              'div:nth-of-type(2)': {
+                order: [2, null, 1],
+              },
+            }
+          : {}),
+      })}
+      {...props}
+    >
+      {children}
+    </Section>
+  );
+}
