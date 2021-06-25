1975ea2a6347f01c796510f8df5d976d96d1c6f9
Tim Leslie <timl@thinkmill.com.au>
2021-06-25 16:09:47 +1000

    WIP

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -17,8 +17,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32X32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16X16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="mask-icon" color="#2684FF" href="/safari-pinned-tab.svg" />
           <link rel="shortcut icon" href="/favicon.ico" />
 
