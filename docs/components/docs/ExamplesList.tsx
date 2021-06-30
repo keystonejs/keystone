27c134004e4b570a462399624cfc1d310b1bb8b1
Ronald Aveling <ronald.aveling@thinkmill.com.au>
2021-06-30 17:05:29 +1000

    Add missing examples (#6033)
    * added missing examples to list
    
    * changed CTA copy for Github link

--- docs/components/docs/ExamplesList.tsx
+++ docs/components/docs/ExamplesList.tsx
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/react';
 
 import { Well } from '../primitives/Well';
 import { useMediaQuery } from '../../lib/media';
+import { InlineCode } from '../../components/primitives/Code';
 
 export function Examples() {
   const mq = useMediaQuery();
@@ -51,8 +52,56 @@ export function Examples() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        This project demonstrates how to use default values for fields. Builds upon the Task Manager
-        starter project.
+        Demonstrates how to use default values for fields. Builds upon the Task Manager starter
+        project.
+      </Well>
+      <Well
+        grad="grad3"
+        heading="Virtual fields"
+        href="https://github.com/keystonejs/keystone/tree/master/examples/virtual-field"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Implements virtual fields in a Keystone list. Builds on the Blog starter project.
+      </Well>
+      <Well
+        grad="grad3"
+        heading="Document field"
+        href="https://github.com/keystonejs/keystone/tree/master/examples/document-field"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Illustrates how to configure <InlineCode>document</InlineCode> fields in your Keystone
+        system and render their data in a frontend application. Builds on the Blog starter project.
+      </Well>
+      <Well
+        grad="grad3"
+        heading="Testing"
+        href="https://github.com/keystonejs/keystone/tree/master/examples/testing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Shows you how to write tests against the GraphQL API to your Keystone system. Builds on the
+        Authentication example project.
+      </Well>
+      <Well
+        grad="grad3"
+        heading="Authentication"
+        href="https://github.com/keystonejs/keystone/tree/master/examples/with-auth"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Adds password-based authentication to the Task Manager starter project.
+      </Well>
+      <Well
+        grad="grad3"
+        heading="JSON Field"
+        href="https://github.com/keystonejs/keystone/tree/master/examples/json"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Illustrates how to use the <InlineCode>json</InlineCode> field type. Builds on the Task
+        Manager starter project.
       </Well>
     </div>
   );
