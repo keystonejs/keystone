037ec72af68bdf04d5e412de9182ef6bbcd4a008
Alexey Murz Korepov <MurzNN@gmail.com>
2021-07-02 17:47:23 +0300

    Linting

--- packages-next/fields/src/types/text/index.ts
+++ packages-next/fields/src/types/text/index.ts
@@ -34,46 +34,49 @@ export const text =
     fixedLength,
     ...config
   }: TextFieldConfig<TGeneratedListTypes> = {}): FieldTypeFunc =>
-    meta => {
-      if ((maxLength || fixedLength) && meta.provider === 'sqlite') {
-        throw new Error('The maxLength and fixedLength properties for text field does not support sqlite');
-      }
-      return fieldType({
-        kind: 'scalar',
-        mode: 'optional',
-        scalar: 'String',
-        nativeType: meta.provider === 'sqlite'
+  meta => {
+    if ((maxLength || fixedLength) && meta.provider === 'sqlite') {
+      throw new Error(
+        'The maxLength and fixedLength properties for text field does not support sqlite'
+      );
+    }
+    return fieldType({
+      kind: 'scalar',
+      mode: 'optional',
+      scalar: 'String',
+      nativeType:
+        meta.provider === 'sqlite'
           ? undefined
           : maxLength
-            ? `VarChar(${maxLength})`
-            : fixedLength
-              ? `Char(${fixedLength})`
-              : undefined,
-        index: getIndexType({ isIndexed, isUnique }),
-      })({
-        ...config,
-        input: {
-          uniqueWhere: isUnique ? { arg: schema.arg({ type: schema.String }) } : undefined,
-          create: { arg: schema.arg({ type: schema.String }) },
-          update: { arg: schema.arg({ type: schema.String }) },
-          orderBy: { arg: schema.arg({ type: orderDirectionEnum }) },
-        },
-        output: schema.field({ type: schema.String }),
-        views: resolveView('text/views'),
-        getAdminMeta() {
-          return {
-            displayMode: config.ui?.displayMode ?? 'input',
-            maxLength: maxLength,
-            fixedLength: fixedLength,
-          };
-        },
-        __legacy: {
-          filters: {
-            fields: {
-              ...legacyFilters.fields.equalityInputFields(meta.fieldKey, schema.String),
-              ...(meta.provider === 'sqlite'
-                ? legacyFilters.fields.containsInputFields(meta.fieldKey, schema.String)
-                : {
+          ? `VarChar(${maxLength})`
+          : fixedLength
+          ? `Char(${fixedLength})`
+          : undefined,
+      index: getIndexType({ isIndexed, isUnique }),
+    })({
+      ...config,
+      input: {
+        uniqueWhere: isUnique ? { arg: schema.arg({ type: schema.String }) } : undefined,
+        create: { arg: schema.arg({ type: schema.String }) },
+        update: { arg: schema.arg({ type: schema.String }) },
+        orderBy: { arg: schema.arg({ type: orderDirectionEnum }) },
+      },
+      output: schema.field({ type: schema.String }),
+      views: resolveView('text/views'),
+      getAdminMeta() {
+        return {
+          displayMode: config.ui?.displayMode ?? 'input',
+          maxLength: maxLength,
+          fixedLength: fixedLength,
+        };
+      },
+      __legacy: {
+        filters: {
+          fields: {
+            ...legacyFilters.fields.equalityInputFields(meta.fieldKey, schema.String),
+            ...(meta.provider === 'sqlite'
+              ? legacyFilters.fields.containsInputFields(meta.fieldKey, schema.String)
+              : {
                   ...legacyFilters.fields.stringInputFields(meta.fieldKey, schema.String),
                   ...legacyFilters.fields.equalityInputFieldsInsensitive(
                     meta.fieldKey,
@@ -84,23 +87,23 @@ export const text =
                     schema.String
                   ),
                 }),
-              ...legacyFilters.fields.inInputFields(meta.fieldKey, schema.String),
-            },
-            impls: {
-              ...legacyFilters.impls.equalityConditions(meta.fieldKey),
-              ...(meta.provider === 'sqlite'
-                ? legacyFilters.impls.containsConditions(meta.fieldKey)
-                : {
+            ...legacyFilters.fields.inInputFields(meta.fieldKey, schema.String),
+          },
+          impls: {
+            ...legacyFilters.impls.equalityConditions(meta.fieldKey),
+            ...(meta.provider === 'sqlite'
+              ? legacyFilters.impls.containsConditions(meta.fieldKey)
+              : {
                   ...legacyFilters.impls.stringConditions(meta.fieldKey),
                   ...legacyFilters.impls.equalityConditionsInsensitive(meta.fieldKey),
                   ...legacyFilters.impls.stringConditionsInsensitive(meta.fieldKey),
                 }),
-              // These have no case-insensitive counter parts
-              ...legacyFilters.impls.inConditions(meta.fieldKey),
-            },
+            // These have no case-insensitive counter parts
+            ...legacyFilters.impls.inConditions(meta.fieldKey),
           },
-          defaultValue,
-          isRequired,
         },
-      });
-    };
+        defaultValue,
+        isRequired,
+      },
+    });
+  };
