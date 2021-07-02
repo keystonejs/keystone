f00444a96fcec990a1b72ad53b3670cc8356c187
Tim Leslie <timl@thinkmill.com.au>
2021-07-02 16:02:47 +1000

    Improve inline relationship docs and examples (#6038)

--- examples/document-field/src/pages/post/[slug].tsx
+++ examples/document-field/src/pages/post/[slug].tsx
@@ -4,13 +4,37 @@ import React from 'react';
 import { DocumentRenderer, DocumentRendererProps } from '@keystone-next/document-renderer';
 import { fetchGraphQL, gql } from '../../utils';
 
-// by default the DocumentRenderer will render unstyled html elements
-// we're customising how headings are rendered here but you can customise any of the renderers that the DocumentRenderer uses
+// By default the DocumentRenderer will render unstyled html elements.
+// We're customising how headings are rendered here but you can customise
+// any of the renderers that the DocumentRenderer uses.
 const renderers: DocumentRendererProps['renderers'] = {
+  // Render heading blocks
   block: {
     heading({ level, children, textAlign }) {
       const Comp = `h${level}` as const;
       return <Comp style={{ textAlign, textTransform: 'uppercase' }}>{children}</Comp>;
+    },
+  },
+  // Render inline relationships
+  inline: {
+    relationship({ relationship, data }) {
+      // If there is more than one inline relationship defined on the document
+      // field we need to handle each of them separately by checking the `relationship` argument.
+      // It is good practice to include this check even if you only have a single inline relationship.
+      if (relationship === 'mention') {
+        if (data === null || data.data === undefined) {
+          // data can be null if the content writer inserted a mention but didn't select an author to mention.
+          // data.data can be undefined if the logged in user does not have permission to read the linked item
+          // or if the linked item no longer exists.
+          return <span>[unknown author]</span>;
+        } else {
+          // If the data exists then we render the mention as a link to the author's bio.
+          // We have access to `id` an `name` fields here because we named them in the
+          // `selection` config argument.
+          return <Link href={`/author/${data.data.id}`}>{data.data.name}</Link>;
+        }
+      }
+      return null;
     },
   },
 };
@@ -54,13 +78,15 @@ export async function getStaticPaths(): Promise<GetStaticPathsResult> {
 }
 
 export async function getStaticProps({ params }: GetStaticPropsContext) {
+  // We use (hydrateRelationships: true) to ensure we have the data we need
+  // to render the inline relationships.
   const data = await fetchGraphQL(
     gql`
       query ($slug: String!) {
         Post(where: { slug: $slug }) {
           title
           content {
-            document
+            document(hydrateRelationships: true)
           }
           publishDate
           author {
