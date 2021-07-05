5989d405fd4a0016101ba6a9886e6dbde007cbe2
gwyneplaine <cc.lee@live.com.au>
2021-07-05 16:02:04 +1000

    pages.goto should be awaited

--- tests/examples-smoke-tests/basic.test.ts
+++ tests/examples-smoke-tests/basic.test.ts
@@ -8,7 +8,7 @@ exampleProjectTests('../examples-staging/basic', browserType => {
   beforeAll(async () => {
     browser = await browserType.launch();
     page = await browser.newPage();
-    page.goto('http://localhost:3000');
+    await page.goto('http://localhost:3000');
   });
   initFirstItemTest(() => page);
   test('sign out and sign in', async () => {
