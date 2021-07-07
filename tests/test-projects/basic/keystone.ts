adc64fd994ecde987f40569295b6b9e051498d12
gwyneplaine <cc.lee@live.com.au>
2021-07-07 11:01:00 +1000

    make db path explicitly test.db

--- tests/test-projects/basic/keystone.ts
+++ tests/test-projects/basic/keystone.ts
@@ -4,7 +4,7 @@ import { lists } from './schema';
 export default config({
   db: {
     provider: 'sqlite',
-    url: process.env.DATABASE_URL || 'file:./keystone-example.db',
+    url: process.env.DATABASE_URL || 'file:./test.db',
   },
   lists,
 });
