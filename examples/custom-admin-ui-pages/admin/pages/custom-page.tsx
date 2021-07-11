e41dc7e822f9e722a84778aac3a27260f1df1925
gwyneplaine <cc.lee@live.com.au>
2021-07-12 09:44:00 +1000

    more examples commits

--- examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
+++ examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, Stack } from '@keystone-ui/core';
 // Please note that while this capability is driven by NextJS's pages directory
 // We do not currently expose any of the auxillary methods that NextJS provides i.e. `getStaticProps`
 // Presently the only export from the directory that is respected is the page component itself.
 export default function () {
-  return <h1>Hello this is a custom page</h1>;
+  return (
+    <Stack as="main" gap="sm">
+      <h1
+        css={{
+          maxWidth: '80%',
+          width: '100%',
+          textAlign: 'center',
+        }}
+      >
+        Hello this is a custom page
+      </h1>
+      <p>This is a custom page added to the admin-ui</p>
+    </Stack>
+  );
 }
