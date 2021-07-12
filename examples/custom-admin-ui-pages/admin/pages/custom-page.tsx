a40eb8196d79dd329b0b8fc4ac1acc8eeb48ba39
gwyneplaine <cc.lee@live.com.au>
2021-07-12 10:34:54 +1000

    add more markup

--- examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
+++ examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
@@ -1,21 +1,35 @@
 /** @jsx jsx */
-import { jsx, Stack } from '@keystone-ui/core';
+import { jsx, Stack, Text, H1 } from '@keystone-ui/core';
 // Please note that while this capability is driven by NextJS's pages directory
 // We do not currently expose any of the auxillary methods that NextJS provides i.e. `getStaticProps`
 // Presently the only export from the directory that is respected is the page component itself.
-export default function () {
+export default function CustomPage() {
   return (
-    <Stack as="main" gap="small">
-      <h1
+    <Stack
+      as="main"
+      gap="small"
+      css={{
+        margin: '0 auto',
+        width: '60%',
+      }}
+    >
+      <H1
         css={{
-          maxWidth: '80%',
           width: '100%',
           textAlign: 'center',
         }}
       >
         Hello this is a custom page
-      </h1>
-      <p>This is a custom page added to the admin-ui</p>
+      </H1>
+      <Text
+        as="p"
+        css={{
+          textAlign: 'center',
+        }}
+      >
+        This is a custom page added to the admin-ui, leveraging layout components from{' '}
+        <em>@keystone-ui/core</em>
+      </Text>
     </Stack>
   );
 }
