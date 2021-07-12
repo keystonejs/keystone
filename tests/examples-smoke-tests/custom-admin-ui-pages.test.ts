fd37c26cfcd66cba63504be430a5086268e7f0d8
gwyneplaine <cc.lee@live.com.au>
2021-07-12 13:58:52 +1000

    add smoke tests

---
+++ tests/examples-smoke-tests/custom-admin-ui-pages.test.ts
@@ -0,0 +1,20 @@
+import { Browser, Page } from 'playwright';
+import { exampleProjectTests } from './utils';
+
+exampleProjectTests('custom-admin-ui-pages', browserType => {
+  let browser: Browser = undefined as any;
+  let page: Page = undefined as any;
+  beforeAll(async () => {
+    browser = await browserType.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+  });
+  test('Load list', async () => {
+    await page.goto('https://localhost:3000/custom-page');
+    const content = await page.textContent('body h1');
+    expect(content).toBe('Hello this is a custom page');
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+});
