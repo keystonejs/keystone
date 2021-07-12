f07686ba8b16ca5cf89d18acb5cb61b3fc8d4c67
gwyneplaine <cc.lee@live.com.au>
2021-07-12 13:58:52 +1000

    add smoke tests

---
+++ tests/examples-smoke-tests/custom-admin-ui-logo.test.ts
@@ -0,0 +1,20 @@
+import { Browser, Page } from 'playwright';
+import { exampleProjectTests } from './utils';
+
+exampleProjectTests('custom-admin-ui-components', browserType => {
+  let browser: Browser = undefined as any;
+  let page: Page = undefined as any;
+  beforeAll(async () => {
+    browser = await browserType.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+  });
+  test('Load list', async () => {
+    await page.goto('https://localhost:3000');
+    const content = await page.textContent('h3 a');
+    expect(content).toBe('LegendBoulder After');
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+});
