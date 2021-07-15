498e0f2a31614619489b8b333b564dbad89b713a
gwyneplaine <cc.lee@live.com.au>
2021-07-15 10:39:45 +1000

    Feedback from Tim

--- examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
+++ examples/custom-admin-ui-pages/admin/pages/custom-page.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx */
 import { Fragment } from 'react';
 import { jsx } from '@keystone-ui/core';
-// Please note that while this capability is driven by NextJS's pages directory
-// We do not currently expose any of the auxillary methods that NextJS provides i.e. `getStaticProps`
-// Presently the only export from the directory that is respected is the page component itself.
+// Please note that while this capability is driven by Next.js's pages directory
+// We do not currently support any of the auxillary methods that Next.js provides i.e. `getStaticProps`
+// Presently the only export from the directory that is supported is the page component itself.
 export default function CustomPage() {
   return (
     <Fragment>
