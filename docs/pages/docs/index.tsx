60231e369a19b154488e3a0a28ebfff44ba844a1
Ronald Aveling <luma.chroma@gmail.com>
2021-07-16 09:34:34 +1000

    Attempt to fix lint test fail (TS)

--- docs/pages/docs/index.tsx
+++ docs/pages/docs/index.tsx
@@ -58,9 +58,9 @@ export default function Docs() {
               height="315"
               src="https://www.youtube-nocookie.com/embed/fPWRlmedCbo?rel=0"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
--- docs/pages/updates/index.tsx
+++ docs/pages/updates/index.tsx
@@ -183,9 +183,9 @@ export default function WhatsNew(props: ComponentProps<typeof DocsPage>) {
               height="315"
               src="https://www.youtube-nocookie.com/embed/fPWRlmedCbo?rel=0"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </Box>
