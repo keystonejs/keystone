b6d3de210652e573b833a4c55f748ee50f775a17
gwyneplaine <cc.lee@live.com.au>
2021-07-19 16:28:41 +1000

    add custom route

--- examples/custom-navigation-component/admin/components/CustomNavigation.tsx
+++ examples/custom-navigation-component/admin/components/CustomNavigation.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-import { ListNavItems, NavigationContainer } from '@keystone-next/keystone/admin-ui/components';
+import {
+  ListNavItems,
+  NavigationContainer,
+  NavItem,
+} from '@keystone-next/keystone/admin-ui/components';
 import type { NavigationProps } from '@keystone-next/keystone/admin-ui/components';
 
 export function CustomNavigation({ routes, authenticatedItem }: NavigationProps) {
   return (
     <NavigationContainer authenticatedItem={authenticatedItem}>
       <ListNavItems routes={routes} />
+      <NavItem href="https://keystonejs.com/guides/custom-navigation-guide">
+        Navigation Guide
+      </NavItem>
     </NavigationContainer>
   );
 }
