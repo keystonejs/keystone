93f1e5d302701c610b6cba74e0c5c86a3ac8aacc
Jed Watson <jed@keystonejs.com>
2021-07-27 12:50:31 +1000

    Add health check endpoint to Keystone's express server (#6192)

---
+++ packages/keystone/src/lib/server/addHealthCheck.ts
@@ -0,0 +1,23 @@
+import type { KeystoneConfig } from '@keystone-next/types';
+import { Application } from 'express';
+
+import { defaults } from '../config/defaults';
+
+type AddHealthCheckArgs = { config: KeystoneConfig; server: Application };
+
+export const addHealthCheck = async ({ config, server }: AddHealthCheckArgs) => {
+  if (!config.server?.healthCheck) return;
+  const healthCheck = config.server.healthCheck === true ? {} : config.server.healthCheck;
+
+  const path = healthCheck.path || defaults.healthCheckPath;
+
+  server.use(path, (req, res) => {
+    const data = (typeof healthCheck.data === 'function'
+      ? healthCheck.data()
+      : healthCheck.data) || {
+      status: 'pass',
+      timestamp: Date.now(),
+    };
+    res.json(data);
+  });
+};
