dd9e8cb295ac8f4abdecb1d82aab224727727bdb
gwyneplaine <cc.lee@live.com.au>
2021-07-28 16:28:40 +1000

    update types in admin-meta

--- packages/types/src/admin-meta.ts
+++ packages/types/src/admin-meta.ts
@@ -1,6 +1,5 @@
 import { GraphQLError } from 'graphql';
 import type { ReactElement } from 'react';
-import type { NavigationProps } from '@keystone-next/keystone/admin-ui/components';
 import { GqlNames, JSONValue } from './utils';
 
 export type NavigationProps = {
