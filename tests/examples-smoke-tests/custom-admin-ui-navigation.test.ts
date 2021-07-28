acbfc5d3fd2fba37dff598eda0a800291f282050
gwyneplaine <cc.lee@live.com.au>
2021-07-28 15:29:00 +1000

    tests updated

--- tests/examples-smoke-tests/custom-admin-ui-navigation.test.ts
+++ tests/examples-smoke-tests/custom-admin-ui-navigation.test.ts
@@ -5,12 +5,30 @@ exampleProjectTests('custom-admin-ui-navigation', browserType => {
   let browser: Browser = undefined as any;
   let page: Page = undefined as any;
   beforeAll(async () => {
-    browser = await browserType.launch({ headless: false });
+    browser = await browserType.launch();
     page = await browser.newPage();
     await page.goto('http://localhost:3000');
   });
-  test('Contains a Navigation Guide link', async () => {
-    const customNavItem = await page.$('text="Keystone Docs"');
+  test('Has a nav link to the Dashboard', async () => {
+    const navElement = await page.waitForSelector('nav a:has-text("Dashboard")');
+    const href = await navElement?.getAttribute('href');
+    expect(href).toBe('/');
+  });
+  test('Has a nav link to the Tasks list', async () => {
+    const navElement = await page.waitForSelector('nav a:has-text("Tasks")');
+    const href = await navElement?.getAttribute('href');
+    expect(href).toBe('/tasks');
+  });
+  test('Has a nav link to the People list', async () => {
+    const navElement = await page.waitForSelector('nav a:has-text("People")');
+    const href = await navElement?.getAttribute('href');
+    expect(href).toBe('/people');
+  });
+  test('Has a nav link to the Keystone docs', async () => {
+    const navElement = await page.waitForSelector('text="Keystone Docs"');
+    const href = await navElement?.getAttribute('href');
+    console.log(href);
+    expect(href).toBe('https://keystonejs.com');
   });
   afterAll(async () => {
     await browser.close();
