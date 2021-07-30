3564b342d6dc2127ae591d7ac055af9eae90543c
Mitchell Hamilton <mitchell@hamil.town>
2021-07-30 12:59:38 +1000

    To-many relationship input updates (#6224)

--- tests/api-tests/relationships/nested-mutations/set-many.test.ts
+++ tests/api-tests/relationships/nested-mutations/set-many.test.ts
@@ -2,7 +2,7 @@ import { gen, sampleOne } from 'testcheck';
 import { text, relationship } from '@keystone-next/fields';
 import { createSchema, list } from '@keystone-next/keystone/schema';
 import { setupTestRunner } from '@keystone-next/testing';
-import { apiTestConfig } from '../../utils';
+import { apiTestConfig, expectGraphQLValidationError, expectRelationshipError } from '../../utils';
 
 const alphanumGenerator = gen.alphaNumString.notEmpty();
 
@@ -54,7 +54,7 @@ const runner = setupTestRunner({
 
 describe('no access control', () => {
   test(
-    'removes all items from list',
+    'set: [] removes all items from list',
     runner(async ({ context }) => {
       const noteContent = `foo${sampleOne(alphanumGenerator)}`;
       const noteContent2 = `foo${sampleOne(alphanumGenerator)}`;
@@ -76,7 +76,7 @@ describe('no access control', () => {
       // Update the item and link the relationship field
       const user = await context.lists.User.updateOne({
         where: { id: createUser.id },
-        data: { username: 'A thing', notes: { disconnectAll: true } },
+        data: { username: 'A thing', notes: { set: [] } },
         query: 'id notes { id content }',
       });
 
@@ -85,15 +85,91 @@ describe('no access control', () => {
   );
 
   test(
-    'silently succeeds if used during create',
+    'set and connect removes all existing items and adds the items specified in set and connect',
     runner(async ({ context }) => {
-      // Create an item that does the linking
-      const user = await context.lists.User.createOne({
-        data: { notes: { disconnectAll: true } },
-        query: 'id notes { id }',
+      const createNote = await context.lists.Note.createOne({ data: {} });
+      const createNote2 = await context.lists.Note.createOne({ data: {} });
+      const createNote3 = await context.lists.Note.createOne({ data: {} });
+
+      // Create an item to update
+      const createUser = await context.lists.User.createOne({
+        data: {
+          username: 'A thing',
+          notes: { connect: [{ id: createNote.id }] },
+        },
       });
 
-      expect(user).toMatchObject({ id: expect.any(String), notes: [] });
+      // Update the item and link the relationship field
+      const user = await context.lists.User.updateOne({
+        where: { id: createUser.id },
+        data: {
+          username: 'A thing',
+          notes: { set: [{ id: createNote2.id }], connect: [{ id: createNote3.id }] },
+        },
+        query: 'id notes { id }',
+      });
+      expect(user.id).toEqual(createUser.id);
+      expect(user.notes.map((note: any) => note.id).sort()).toEqual(
+        [createNote2.id, createNote3.id].sort()
+      );
+    })
+  );
+
+  test(
+    'set and disconnect throws an error',
+    runner(async ({ context }) => {
+      const createNote = await context.lists.Note.createOne({ data: {} });
+
+      // Create an item to update
+      const createUser = await context.lists.User.createOne({
+        data: {
+          username: 'A thing',
+          notes: { connect: [{ id: createNote.id }] },
+        },
+      });
+
+      // Update the item and link the relationship field
+      const { data, errors } = await context.graphql.raw({
+        query: `
+          mutation ($id: ID!) {
+            updateUser(
+              where: { id: $id }
+              data: { notes: { disconnect: [{ id: "c5b84f38256d3c2df59a0d9bf" }], set: [] } }
+            ) {
+              id
+            }
+          }
+        `,
+        variables: { id: createUser.id },
+      });
+      expect(data).toEqual({ updateUser: null });
+      expectRelationshipError(errors, [
+        {
+          path: ['updateUser'],
+          message:
+            'The set and disconnect fields cannot both be provided to to-many relationship inputs but both were provided at User.notes<Note>',
+        },
+      ]);
+    })
+  );
+
+  test(
+    'causes a validation error if used during create',
+    runner(async ({ graphQLRequest }) => {
+      const { body } = await graphQLRequest({
+        query: `
+          mutation {
+            createUser(data: { notes: { set: [] } }) {
+              id
+            }
+          }
+        `,
+      }).expect(400);
+      expectGraphQLValidationError(body.errors, [
+        {
+          message: `Field "set" is not defined by type "NoteRelateToManyForCreateInput".`,
+        },
+      ]);
     })
   );
 });
@@ -101,7 +177,7 @@ describe('no access control', () => {
 describe('with access control', () => {
   describe('read: false on related list', () => {
     test(
-      'has no effect when specifying disconnectAll',
+      'has no effect when specifying set: []',
       runner(async ({ context }) => {
         const noteContent = sampleOne(alphanumGenerator);
 
@@ -121,7 +197,7 @@ describe('with access control', () => {
         // Update the item and link the relationship field
         await context.lists.UserToNotesNoRead.updateOne({
           where: { id: createUser.id },
-          data: { username: 'A thing', notes: { disconnectAll: true } },
+          data: { username: 'A thing', notes: { set: [] } },
         });
 
         const data = await context.sudo().lists.UserToNotesNoRead.findOne({
