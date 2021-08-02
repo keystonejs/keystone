41bc98ed7df6a99c56aafca1f7c68462c1750ea6
gwyneplaine <cc.lee@live.com.au>
2021-08-02 16:38:55 +1000

    more navigation related tests

--- tests/admin-ui-tests/init.test.ts
+++ tests/admin-ui-tests/init.test.ts
@@ -33,6 +33,16 @@ adminUITests('./tests/test-projects/basic', browserType => {
     const content = await page.textContent('body h1');
     expect(content).toBe('404');
   });
+  // test('Should see a 404 on request of the /init route', async () => {
+  //   await page.goto('http://localhost:3000/init');
+  //   const content = await page.textContent('body h1');
+  //   expect(content).toBe('404');
+  // });
+  // test('Should see a 404 on request of the /signin route', async () => {
+  //   await page.goto('http://localhost:3000/signin');
+  //   const content = await page.textContent('body h1');
+  //   expect(content).toBe('404');
+  // });
   afterAll(async () => {
     await browser.close();
   });
