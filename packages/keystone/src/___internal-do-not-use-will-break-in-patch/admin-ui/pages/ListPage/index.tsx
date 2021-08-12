eb50a37f0e70f5891ce1fb414bea61a5f9ddbb3e
gwyneplaine <cc.lee@live.com.au>
2021-08-12 17:29:32 +1000

    update bug fix to be more verbose

--- packages/keystone/src/___internal-do-not-use-will-break-in-patch/admin-ui/pages/ListPage/index.tsx
+++ packages/keystone/src/___internal-do-not-use-will-break-in-patch/admin-ui/pages/ListPage/index.tsx
@@ -476,7 +476,9 @@ function DeleteManyButton({
     if (successfulItems?.length) {
       toasts.addToast({
         tone: 'positive',
-        title: `Deleted ${successfulItems.length} items successful`,
+        title: `Deleted ${successfulItems.length} of ${
+          data[list.gqlNames.deleteManyMutationName].length
+        } ${list.plural} successful`,
         message: successfulItems.join(', '),
       });
     }
@@ -485,7 +487,7 @@ function DeleteManyButton({
         tone: 'negative',
         title: `Failed to delete ${unsuccessfulItems.length} of ${
           data[list.gqlNames.deleteManyMutationName].length
-        } items`,
+        } ${list.plural}`,
         message: errors
           .reduce((acc, error) => {
             if (acc.indexOf(error.message) < 0) {
