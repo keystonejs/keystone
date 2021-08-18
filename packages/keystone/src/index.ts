815274f7983fe36349d4718c687314245253ac95
Aaron Powell <me@aaron-powell.com>
2021-08-18 02:32:27 +0000

    forgot to export some types

--- packages/keystone/src/index.ts
+++ packages/keystone/src/index.ts
@@ -2,3 +2,5 @@ export { createSystem } from './lib/createSystem';
 export { createExpressServer } from './lib/server/createExpressServer';
 export { initConfig } from './lib/config/initConfig';
 export { createApolloServerMicro } from './lib/server/createApolloServer';
+export { KeystoneCloudFileAdapter } from './lib/keystone-cloud/KeystoneCloudFileAdapter';
+export { KeystoneCloudImageAdapter } from './lib/keystone-cloud/KeystoneCloudImageAdapter';
