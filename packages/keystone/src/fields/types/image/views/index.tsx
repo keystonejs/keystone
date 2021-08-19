21ff13fc66ba3c99aaca812b07d50cbdff9df49e
mitchellhamilton <mitchell@hamil.town>
2021-08-19 14:40:57 +1000

    Linting

--- packages/keystone/src/fields/types/image/views/index.tsx
+++ packages/keystone/src/fields/types/image/views/index.tsx
@@ -1,13 +1,13 @@
 /* @jsx jsx */
 
 import { jsx } from '@keystone-ui/core';
+import { FieldContainer, FieldLabel } from '@keystone-ui/fields';
 import {
   CardValueComponent,
   CellComponent,
   FieldController,
   FieldControllerConfig,
 } from '../../../../types';
-import { FieldContainer, FieldLabel } from '@keystone-ui/fields';
 import { validateImage, validateRef, ImageWrapper } from './Field';
 
 export { Field } from './Field';
