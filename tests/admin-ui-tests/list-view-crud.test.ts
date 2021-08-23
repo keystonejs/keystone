20f40b3873bb55876cda7cad499b43027a0612f2
gwyneplaine <cc.lee@live.com.au>
2021-08-23 10:50:09 +1000

    add waitForNaigation to beforeEach to avoid navigation destroying evaluation context

--- tests/admin-ui-tests/list-view-crud.test.ts
+++ tests/admin-ui-tests/list-view-crud.test.ts
@@ -29,7 +29,7 @@ adminUITests('./tests/test-projects/crud-notifications', (browserType, deleteAll
   beforeAll(async () => {
     browser = await browserType.launch();
     page = await browser.newPage();
-    await page.goto('http://localhost:3000');
+    await Promise.all([page.waitForNavigation(), page.goto('http://localhost:3000')]);
   });
   beforeEach(async () => {
     // Drop the database
