9c0681ca061fc9b081b712edac37f6752b4ac4c1
Charles <cc.lee@live.com.au>
2021-09-01 13:56:30 +1000

    Integration tests admin UI (#6260)
    * navigation tests
    
    * more navigation related tests
    
    * commit out bad test
    
    * update schema.graphql
    
    * update nav tests
    
    * update schema.graphql
    
    * update tests.yml to include navigation admin-ui test
    
    * fix tests
    
    * add determinism to init.test.ts
    
    * update nav test to be a bit more deterministic
    
    * update init.test.ts
    
    * rename seedData fn and move it to utils module
    
    * remove log
    
    * update util for better errors
    
    * remove unnecessary try catch

--- tests/admin-ui-tests/init.test.ts
+++ tests/admin-ui-tests/init.test.ts
@@ -13,6 +13,16 @@ adminUITests('./tests/test-projects/basic', browserType => {
   test('Task List card should be visible', async () => {
     await page.waitForSelector('h3:has-text("Task")');
   });
+  test('Clicking on the logo should return you to the Dashboard route', async () => {
+    await page.goto('http://localhost:3000/tasks');
+    await page.waitForSelector('h3 a:has-text("Keystone 6")');
+    await Promise.all([
+      page.waitForNavigation({
+        url: 'http://localhost:3000',
+      }),
+      page.click('h3 a:has-text("Keystone 6")'),
+    ]);
+  });
   test('Should see a 404 on request of the /init route', async () => {
     await page.goto('http://localhost:3000/init');
     const content = await page.textContent('body h1');
