6aad1df4082e59f18142b0bb47bc5d6acd84726d
mitchellhamilton <mitchell@hamil.town>
2021-09-06 12:24:06 +1000

    Add a test

---
+++ tests/api-tests/extend-express-app.test.ts
@@ -0,0 +1,32 @@
+import { createSchema, list } from '@keystone-next/keystone';
+import { text } from '@keystone-next/keystone/fields';
+import { setupTestRunner } from '@keystone-next/keystone/testing';
+import supertest from 'supertest';
+import { apiTestConfig } from './utils';
+
+const runner = setupTestRunner({
+  config: apiTestConfig({
+    lists: createSchema({ User: list({ fields: { name: text() } }) }),
+    server: {
+      extendExpressApp: app => {
+        app.get('/magic', (req, res) => {
+          res.json({ magic: true });
+        });
+      },
+    },
+  }),
+});
+
+test(
+  'basic extension',
+  runner(async ({ app }) => {
+    const { text } = await supertest(app)
+      .get('/magic')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(JSON.parse(text)).toEqual({
+      magic: true,
+    });
+  })
+);
