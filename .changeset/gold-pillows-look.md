0218a421576fb3ceb38eb5f38223a9ef0af4c4d2
Tim Leslie <timl@thinkmill.com.au>
2021-09-10 09:16:31 +1000

    Remove deprecated config.db.adapter option (#6518)

---
+++ .changeset/gold-pillows-look.md
@@ -0,0 +1,5 @@
+---
+'@keystone-next/keystone': major
+---
+
+Removed the deprecated `config.db.adapter` option. Please use `config.db.provider` to indicate the database provider for your system.
--- packages/keystone/src/artifacts.ts
+++ packages/keystone/src/artifacts.ts
@@ -139,7 +139,7 @@ const makeVercelIncludeTheSQLiteDB = (
   directoryOfFileToBeWritten: string,
   config: KeystoneConfig
 ) => {
-  if (config.db.adapter === 'prisma_sqlite' || config.db.provider === 'sqlite') {
+  if (config.db.provider === 'sqlite') {
     const sqliteDbAbsolutePath = path.resolve(cwd, config.db.url.replace('file:', ''));
 
     return `import path from 'path';
