2ceb6151647b2d0001b78cbd5901564f47566b88
Tim Leslie <timl@thinkmill.com.au>
2021-09-10 11:25:56 +1000

    Remove unused BaseKeystone type (#6521)

--- packages/keystone/src/types/index.ts
+++ packages/keystone/src/types/index.ts
@@ -3,7 +3,6 @@ export * from './config';
 export * from './utils';
 export * from './session';
 export * from './admin-meta';
-export * from './base';
 export * from './context';
 export * from './next-fields';
 export * as filters from './filters';
