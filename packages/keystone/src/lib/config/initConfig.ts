975b5846d4d6ac7bb02c3def0e9e80106003cb45
Tim Leslie <timl@thinkmill.com.au>
2021-09-10 11:49:49 +1000

    Only check for a valid db.provider once

--- packages/keystone/src/lib/config/initConfig.ts
+++ packages/keystone/src/lib/config/initConfig.ts
@@ -8,5 +8,11 @@ import { applyIdFieldDefaults } from './applyIdFieldDefaults';
 */
 
 export function initConfig(config: KeystoneConfig) {
+  if (!['postgresql', 'sqlite'].includes(config.db.provider)) {
+    throw new Error(
+      'Invalid db configuration. Please specify db.provider as either "sqlite" or "postgresql"'
+    );
+  }
+
   return { ...config, lists: applyIdFieldDefaults(config) };
 }
