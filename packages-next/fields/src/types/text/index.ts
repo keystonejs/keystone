87d62c433cf552cd9087e5d1e224b6565f47cbef
Nikita Mavrychev <m.nikitoring@gmail.com>
2021-09-13 08:44:24 +0300

    fix getAdminMeta

--- packages-next/fields/src/types/text/index.ts
+++ packages-next/fields/src/types/text/index.ts
@@ -46,35 +46,35 @@ export const text =
         nativeType: meta.provider === 'sqlite'
           ? undefined
           : maxLength
-            ? `VarChar(${maxLength})`
-            : fixedLength
-              ? `Char(${fixedLength})`
-              : undefined,
-        index: getIndexType({ isIndexed, isUnique }),
-      })({
-        ...config,
-        input: {
-          uniqueWhere: isUnique ? { arg: schema.arg({ type: schema.String }) } : undefined,
-          create: { arg: schema.arg({ type: schema.String }) },
-          update: { arg: schema.arg({ type: schema.String }) },
-          orderBy: { arg: schema.arg({ type: orderDirectionEnum }) },
-        },
-        output: schema.field({ type: schema.String }),
-        views: resolveView('text/views'),
-        getAdminMeta() {
-          return {
-            displayMode: config.ui?.displayMode ?? 'input',
-            maxLength: maxLength,
-            fixedLength: fixedLength,
-          };
-        },
-        __legacy: {
-          filters: {
-            fields: {
-              ...legacyFilters.fields.equalityInputFields(meta.fieldKey, schema.String),
-              ...(meta.provider === 'sqlite'
-                ? legacyFilters.fields.containsInputFields(meta.fieldKey, schema.String)
-                : {
+          ? `VarChar(${maxLength})`
+          : fixedLength
+          ? `Char(${fixedLength})`
+          : undefined,
+      index: getIndexType({ isIndexed, isUnique }),
+    })({
+      ...config,
+      input: {
+        uniqueWhere: isUnique ? { arg: schema.arg({ type: schema.String }) } : undefined,
+        create: { arg: schema.arg({ type: schema.String }) },
+        update: { arg: schema.arg({ type: schema.String }) },
+        orderBy: { arg: schema.arg({ type: orderDirectionEnum }) },
+      },
+      output: schema.field({ type: schema.String }),
+      views: resolveView('text/views'),
+      getAdminMeta() {
+        return {
+          displayMode: config.ui?.displayMode ?? 'input',
+          maxLength: maxLength,
+          fixedLength: fixedLength,
+        } as JSONValue;
+      },
+      __legacy: {
+        filters: {
+          fields: {
+            ...legacyFilters.fields.equalityInputFields(meta.fieldKey, schema.String),
+            ...(meta.provider === 'sqlite'
+              ? legacyFilters.fields.containsInputFields(meta.fieldKey, schema.String)
+              : {
                   ...legacyFilters.fields.stringInputFields(meta.fieldKey, schema.String),
                   ...legacyFilters.fields.equalityInputFieldsInsensitive(
                     meta.fieldKey,
