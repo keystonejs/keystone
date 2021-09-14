088a32e47c308e02bb56b2cd6c3bd19ae001fe7e
Mitchell Hamilton <mitchell@hamil.town>
2021-09-14 10:44:55 +1000

    Virtual field updates (#6538)

--- packages/keystone/src/fields/types/virtual/index.ts
+++ packages/keystone/src/fields/types/virtual/index.ts
@@ -1,3 +1,4 @@
+import { getNamedType, isLeafType } from 'graphql';
 import {
   BaseGeneratedListTypes,
   graphql,
@@ -6,10 +7,11 @@ import {
   FieldTypeFunc,
   fieldType,
   ListInfo,
+  getGqlNames,
 } from '../../../types';
 import { resolveView } from '../../resolve-view';
 
-type VirtualFieldGraphQLField = graphql.Field<ItemRootValue, any, any, string>;
+type VirtualFieldGraphQLField = graphql.Field<ItemRootValue, any, graphql.OutputType, string>;
 
 export type VirtualFieldConfig<TGeneratedListTypes extends BaseGeneratedListTypes> =
   CommonFieldConfig<TGeneratedListTypes> & {
@@ -17,18 +19,55 @@ export type VirtualFieldConfig<TGeneratedListTypes extends BaseGeneratedListType
       | VirtualFieldGraphQLField
       | ((lists: Record<string, ListInfo>) => VirtualFieldGraphQLField);
     unreferencedConcreteInterfaceImplementations?: graphql.ObjectType<any>[];
-    graphQLReturnFragment?: string;
+    ui?: {
+      /**
+       * Defines what the Admin UI should fetch from this field, it's interpolated into a query like this:
+       * ```graphql
+       * query {
+       *   item(where: { id: "..." }) {
+       *     field${ui.query}
+       *   }
+       * }
+       * ```
+       *
+       * This is only needed when you your field returns a GraphQL type other than a scalar(String and etc.)
+       * or an enum or you need to provide arguments to the field.
+       */
+      query?: string;
+    };
   };
 
 export const virtual =
   <TGeneratedListTypes extends BaseGeneratedListTypes>({
-    graphQLReturnFragment = '',
     field,
     ...config
   }: VirtualFieldConfig<TGeneratedListTypes>): FieldTypeFunc =>
   meta => {
     const usableField = typeof field === 'function' ? field(meta.lists) : field;
-
+    const namedType = getNamedType(usableField.type.graphQLType);
+    const hasRequiredArgs =
+      usableField.args &&
+      Object.values(
+        usableField.args as Record<string, graphql.Arg<graphql.InputType, boolean>>
+      ).some(x => x.type.kind === 'non-null' && x.defaultValue === undefined);
+    if (
+      (!isLeafType(namedType) || hasRequiredArgs) &&
+      !config.ui?.query &&
+      (config.ui?.itemView?.fieldMode !== 'hidden' || config.ui?.listView?.fieldMode !== 'hidden')
+    ) {
+      throw new Error(
+        `The virtual field at ${meta.listKey}.${meta.fieldKey} requires a selection for the Admin UI but ui.query is unspecified and ui.listView.fieldMode and ui.itemView.fieldMode are not both set to 'hidden'.\n` +
+          `Either set ui.query with what the Admin UI should fetch or hide the field from the Admin UI by setting ui.listView.fieldMode and ui.itemView.fieldMode to 'hidden'.\n` +
+          `When setting ui.query, it is interpolated into a GraphQL query like this:\n` +
+          `query {\n` +
+          `  ${
+            getGqlNames({ listKey: meta.listKey, pluralGraphQLName: '' }).itemQueryName
+          }(where: { id: "..." }) {\n` +
+          `    ${meta.fieldKey}\${ui.query}\n` +
+          `  }\n` +
+          `}`
+      );
+    }
     return fieldType({
       kind: 'none',
     })({
@@ -40,6 +79,6 @@ export const virtual =
         },
       }),
       views: resolveView('virtual/views'),
-      getAdminMeta: () => ({ graphQLReturnFragment }),
+      getAdminMeta: () => ({ query: config.ui?.query || '' }),
     });
   };
