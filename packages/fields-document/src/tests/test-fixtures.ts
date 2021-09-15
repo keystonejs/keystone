b6c8c3bff9d3d98f743c47c015ae27e63db0271e
Mitchell Hamilton <mitchell@hamil.town>
2021-09-16 09:13:30 +1000

    Document field updates (#6564)

--- packages/fields-document/src/tests/test-fixtures.ts
+++ packages/fields-document/src/tests/test-fixtures.ts
@@ -13,6 +13,7 @@ export const updateReturnedValue = [
 ];
 
 export const supportsUnique = false;
+export const skipRequiredTest = true;
 export const fieldName = 'content';
 export const subfieldName = 'document';
 
@@ -39,7 +40,7 @@ export const initItems = () => {
       name: 'd',
       content: [{ type: 'paragraph', children: [{ text: 'text' }] }],
     },
-    { name: 'e', content: null },
+    { name: 'e', content: [{ type: 'paragraph', children: [{ text: '' }] }] },
     { name: 'f' },
     { name: 'g' },
   ];
@@ -69,9 +70,9 @@ export const storedValues = () => [
     name: 'd',
     content: { document: [{ type: 'paragraph', children: [{ text: 'text' }] }] },
   },
-  { name: 'e', content: null },
-  { name: 'f', content: null },
-  { name: 'g', content: null },
+  { name: 'e', content: { document: [{ type: 'paragraph', children: [{ text: '' }] }] } },
+  { name: 'f', content: { document: [{ type: 'paragraph', children: [{ text: '' }] }] } },
+  { name: 'g', content: { document: [{ type: 'paragraph', children: [{ text: '' }] }] } },
 ];
 
 export const supportedFilters = () => [];
