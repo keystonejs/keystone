c2c93fa846feea933c45bac6833f4c4759ce3554
Jed Watson <jed.watson@me.com>
2021-09-24 16:56:05 +1000

    Add smoke test for rest-api example

---
+++ tests/examples-smoke-tests/rest-api.test.ts
@@ -0,0 +1,29 @@
+import { Browser, Page } from 'playwright';
+import fetch from 'node-fetch';
+import { exampleProjectTests } from './utils';
+
+exampleProjectTests('rest-api', browserType => {
+  let browser: Browser = undefined as any;
+  let page: Page = undefined as any;
+  beforeAll(async () => {
+    browser = await browserType.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+  });
+  test('Load list', async () => {
+    await Promise.all([page.waitForNavigation(), page.click('h3:has-text("People")')]);
+    await page.waitForSelector('button:has-text("Create Person")');
+  });
+  test('Get Tasks', async () => {
+    const tasks = await fetch('http://localhost:3000/rest/tasks', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json());
+    expect(tasks).toEqual([]);
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+});
