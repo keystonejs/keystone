4a45690e0a365ea75e509acc94bc6bbf8d5ace03
mitchellhamilton <mitchell@hamil.town>
2021-09-28 11:10:12 +1000

    Tests

--- packages/keystone/src/fields/types/decimal/tests/test-fixtures.ts
+++ packages/keystone/src/fields/types/decimal/tests/test-fixtures.ts
@@ -6,6 +6,7 @@ export const typeFunction = decimal;
 export const exampleValue = () => '6.28';
 export const exampleValue2 = () => '6.45';
 export const supportsUnique = true;
+export const skipRequiredTest = true;
 export const fieldName = 'price';
 export const unSupportedAdapterList = ['sqlite'];
 
@@ -26,9 +27,9 @@ export const initItems = () => {
 export const storedValues = () => [
   { name: 'price1', price: '-123.45' },
   { name: 'price2', price: '0.01' },
-  { name: 'price3', price: '50.00' },
-  { name: 'price4', price: '2000.00' },
-  { name: 'price5', price: '40000.00' },
+  { name: 'price3', price: '50' },
+  { name: 'price4', price: '2000' },
+  { name: 'price5', price: '40000' },
   { name: 'price6', price: null },
   { name: 'price7', price: null },
 ];
