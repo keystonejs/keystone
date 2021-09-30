95534863cce30e1a47514a046172a76b4e939522
mitchellhamilton <mitchell@hamil.town>
2021-10-01 09:34:05 +1000

    Fix tests

--- packages/keystone/src/fields/types/timestamp/views/__tests__/index.tsx
+++ packages/keystone/src/fields/types/timestamp/views/__tests__/index.tsx
@@ -1,85 +1,118 @@
-import { formatISO, parseISO } from 'date-fns';
-import { controller } from '../index';
-const STUBCONFIG = {
+import { formatISO } from 'date-fns';
+import { FieldControllerConfig } from '../../../../../types';
+import { controller, TimestampFieldMeta } from '../index';
+const STUBCONFIG: FieldControllerConfig<TimestampFieldMeta> = {
   listKey: 'timestamp',
   path: './timestamp',
   label: 'timestmap',
   customViews: {},
-  fieldMeta: undefined,
+  fieldMeta: { defaultValue: null, isRequired: false, updatedAt: false },
 };
 
 describe('controller', () => {
   describe('validate', () => {
     it('should return true if neither date nor time value are specified', () => {
       const { validate } = controller(STUBCONFIG);
-      expect(validate!({ dateValue: '', timeValue: '' })).toBe(true);
+      expect(
+        validate!({
+          kind: 'create',
+          value: { dateValue: null, timeValue: { kind: 'parsed', value: null } },
+        })
+      ).toBe(true);
     });
     it('should return true if both date and time values are valid', () => {
       const { validate } = controller(STUBCONFIG);
       const value = {
-        dateValue: formatISO(new Date(), { representation: 'date' }),
-        timeValue: '10:00',
+        kind: 'create',
+        value: {
+          dateValue: formatISO(new Date(), { representation: 'date' }),
+          timeValue: {
+            kind: 'parsed',
+            value: '10:00:00.000',
+          },
+        },
       } as const;
       expect(validate!(value)).toBe(true);
     });
     it('should return false if only the date value is missing', () => {
       const { validate } = controller(STUBCONFIG);
-      expect(validate!({ dateValue: '', timeValue: '10:00' })).toBe(false);
+      expect(
+        validate!({
+          kind: 'create',
+          value: {
+            dateValue: null,
+            timeValue: {
+              kind: 'parsed',
+              value: '10:00:00.000',
+            },
+          },
+        })
+      ).toBe(false);
     });
     it('should return false if only the time value is missing', () => {
       const { validate } = controller(STUBCONFIG);
       expect(
         validate!({
-          dateValue: formatISO(new Date(), { representation: 'date' }),
-          timeValue: '',
+          kind: 'create',
+          value: {
+            dateValue: formatISO(new Date(), { representation: 'date' }),
+            timeValue: { kind: 'parsed', value: null },
+          },
         })
-      ).toBe(true);
+      ).toBe(false);
     });
   });
   describe('serialize', () => {
     it('should return null if neither date nor time value is specified', () => {
       const { serialize } = controller(STUBCONFIG);
-      expect(serialize({ dateValue: '', timeValue: '' })).toStrictEqual({
-        [STUBCONFIG.path]: null,
-      });
-    });
-    it('should return null if an invalid date value is specified', () => {
-      const { serialize } = controller(STUBCONFIG);
-      expect(serialize({ dateValue: 'aasdfasdf', timeValue: '10:00' })).toStrictEqual({
+      expect(
+        serialize({
+          kind: 'create',
+          value: {
+            dateValue: null,
+            timeValue: { kind: 'parsed', value: null },
+          },
+        })
+      ).toStrictEqual({
         [STUBCONFIG.path]: null,
       });
     });
     it('should return null if an invalid time value is specified', () => {
       const { serialize } = controller(STUBCONFIG);
-      expect(serialize({ dateValue: '2020-10-20', timeValue: 'hello' })).toStrictEqual({
+      expect(
+        serialize({
+          kind: 'create',
+          value: {
+            dateValue: '2020-10-20',
+            timeValue: 'hello',
+          },
+        })
+      ).toStrictEqual({
         [STUBCONFIG.path]: null,
       });
     });
     it('should return null if no dateValue is specified', () => {
       const { serialize } = controller(STUBCONFIG);
-      expect(serialize({ dateValue: '', timeValue: '10:00' })).toStrictEqual({
+      expect(
+        serialize({
+          kind: 'create',
+          value: {
+            dateValue: null,
+            timeValue: { kind: 'parsed', value: '10:00:00.000' },
+          },
+        })
+      ).toStrictEqual({
         [STUBCONFIG.path]: null,
       });
-    });
-    it('should return a valid ISO8601 string if no timeValue is specified but a valid date value is specified', () => {
-      const { serialize } = controller(STUBCONFIG);
-      expect(
-        Boolean(
-          parseISO(
-            serialize({ dateValue: '2020-10-20', timeValue: '' })[STUBCONFIG.path]
-          ).toISOString()
-        )
-      ).toBe(true);
     });
     it('should return a valid ISO8601 string if a valid time and date value are specified', () => {
       const { serialize } = controller(STUBCONFIG);
       expect(
-        Boolean(
-          parseISO(
-            serialize({ dateValue: '2020-10-20', timeValue: '10:00' })[STUBCONFIG.path]
-          ).toISOString()
-        )
-      ).toBe(true);
+        serialize({
+          kind: 'create',
+          value: { dateValue: '2020-10-20', timeValue: { kind: 'parsed', value: '10:00:00.000' } },
+        })[STUBCONFIG.path]
+      ).toEqual('2020-10-20T00:00:00.000Z');
     });
   });
 });
