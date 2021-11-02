e57b6ec1323586e4ce6cc078191510724983b3de
mitchellhamilton <mitchell@hamil.town>
2021-11-02 10:21:41 +1000

    Fix flakey test

--- tests/examples-smoke-tests/custom-admin-ui-pages.test.ts
+++ tests/examples-smoke-tests/custom-admin-ui-pages.test.ts
@@ -1,6 +1,19 @@
 import { Browser, Page } from 'playwright';
 import { exampleProjectTests, loadIndex } from './utils';
 
+async function retry(cb: () => Promise<void>, maxTimes: number) {
+  while (true) {
+    try {
+      return await cb();
+    } catch (err) {
+      if (maxTimes === 0) {
+        throw err;
+      }
+      maxTimes--;
+    }
+  }
+}
+
 exampleProjectTests('custom-admin-ui-pages', browserType => {
   let browser: Browser = undefined as any;
   let page: Page = undefined as any;
@@ -9,10 +22,11 @@ exampleProjectTests('custom-admin-ui-pages', browserType => {
     page = await browser.newPage();
     await loadIndex(page);
   });
-  test('Load list', async () => {
-    await page.goto('http://localhost:3000/custom-page');
-    await page.waitForSelector('main h1:has-text("This is a custom Admin UI page")');
-  });
+  test('Load list', () =>
+    retry(async () => {
+      await page.goto('http://localhost:3000/custom-page');
+      await page.waitForSelector('main h1:has-text("This is a custom Admin UI page")');
+    }, 5));
   afterAll(async () => {
     await browser.close();
   });
