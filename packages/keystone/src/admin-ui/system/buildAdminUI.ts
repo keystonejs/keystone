530464230b8a59e7ac553344481d1a673b448950
mitchellhamilton <mitchell@hamil.town>
2021-11-02 16:38:32 +1000

    Fix Schema must contain uniquely named types error

--- packages/keystone/src/admin-ui/system/buildAdminUI.ts
+++ packages/keystone/src/admin-ui/system/buildAdminUI.ts
@@ -1,7 +1,7 @@
 export async function buildAdminUI(projectAdminPath: string) {
-  if (process.env.NODE_ENV !== 'production') {
-    throw new Error('process.env.NODE_ENV must be set to production to build the Admin UI');
-  }
+  // Next does a broken build unless we set NODE_ENV to production
+  // @ts-ignore
+  process.env.NODE_ENV = 'production';
   // importing next/dist/build is quite expensive so we're requiring it lazily
   /** We do this to stop webpack from bundling next inside of next */
   const next = 'next/dist/build';
