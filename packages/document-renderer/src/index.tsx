ca85a5b18337e709a427fb2b6ce28c950ae33a4b
Yuval Datner <contact@datner.me>
2021-11-17 15:52:56 +0200

    Add missing layout area component

--- packages/document-renderer/src/index.tsx
+++ packages/document-renderer/src/index.tsx
@@ -42,6 +42,7 @@ interface Renderers {
     blockquote: OnlyChildrenComponent;
     code: Component<{ children: string }> | keyof JSX.IntrinsicElements;
     layout: Component<{ layout: [number, ...number[]]; children: ReactElement[] }>;
+    layoutArea: OnlyChildrenComponent;
     divider: Component<{}> | keyof JSX.IntrinsicElements;
     heading: Component<{
       level: 1 | 2 | 3 | 4 | 5 | 6;
@@ -101,6 +102,7 @@ export const defaultRenderers: Renderers = {
         </div>
       );
     },
+    layoutArea: 'div',
   },
 };
 
@@ -148,6 +150,9 @@ function DocumentNode({
     }
     case 'layout': {
       return <renderers.block.layout layout={node.layout as any} children={children} />;
+    }
+    case 'layout-area': {
+      return <renderers.block.layoutArea children={children} />;
     }
     case 'divider': {
       return <renderers.block.divider />;
