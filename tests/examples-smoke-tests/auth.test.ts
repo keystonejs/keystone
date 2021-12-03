26e4f07f462da93f5155fd6e6428d462244879b3
mitchellhamilton <mitchell@hamil.town>
2021-12-03 11:14:55 +1000

    Add a test

--- tests/examples-smoke-tests/auth.test.ts
+++ tests/examples-smoke-tests/auth.test.ts
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import { Browser, Page } from 'playwright';
 import { exampleProjectTests, initFirstItemTest, loadIndex } from './utils';
 
@@ -8,6 +9,11 @@ exampleProjectTests('../examples-staging/auth', browserType => {
     browser = await browserType.launch();
     page = await browser.newPage();
     await loadIndex(page);
+  });
+  test('going to any page redirects to /init with a Cache-Control: no-cache, max-age=0 header', async () => {
+    const res = await fetch('http://localhost:3000', { redirect: 'manual' });
+    expect(res.status).toEqual(302);
+    expect(res.headers.get('Cache-Control')).toEqual('no-cache, max-age=0');
   });
   initFirstItemTest(() => page);
   afterAll(async () => {
