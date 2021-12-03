698a11967b7d3a83c4fb93cb9680513e8e5e9622
mitchellhamilton <mitchell@hamil.town>
2021-12-03 12:22:14 +1000

    More checks

--- tests/examples-smoke-tests/auth.test.ts
+++ tests/examples-smoke-tests/auth.test.ts
@@ -13,7 +13,12 @@ exampleProjectTests('../examples-staging/auth', browserType => {
   test('going to any page redirects to /init with a Cache-Control: no-cache, max-age=0 header', async () => {
     const res = await fetch('http://localhost:3000', { redirect: 'manual' });
     expect(res.status).toEqual(302);
+    expect(res.headers.get('Location')).toEqual('http://localhost:3000/init');
     expect(res.headers.get('Cache-Control')).toEqual('no-cache, max-age=0');
+
+    const resWithAutoRedirect = await fetch('http://localhost:3000');
+    expect(resWithAutoRedirect.status).toBe(200);
+    expect(resWithAutoRedirect.url).toBe('http://localhost:3000/init');
   });
   initFirstItemTest(() => page);
   afterAll(async () => {
