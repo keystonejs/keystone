215c501c24baf38e3eb31fbcb08faef5129cb43c
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2021-12-21 14:13:47 +1100

    change to Context type passing

--- packages/keystone/src/scripts/tests/artifacts.test.ts
+++ packages/keystone/src/scripts/tests/artifacts.test.ts
@@ -112,9 +112,9 @@ describe('postinstall', () => {
     expect(await getFiles(tmp, ['node_modules/.keystone/api.{d.ts,js}'])).toMatchInlineSnapshot(`
       ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ node_modules/.keystone/api.d.ts ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
       import { KeystoneListsAPI } from '@keystone-6/core/types';
-      import { Lists } from './types';
+      import { Context } from './types';
 
-      export const query: KeystoneListsAPI<Lists>;
+      export const query: Context['query'];
       ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ node_modules/.keystone/api.js ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
       import keystoneConfig from '../../keystone';
       import { PrismaClient } from '.prisma/client';
