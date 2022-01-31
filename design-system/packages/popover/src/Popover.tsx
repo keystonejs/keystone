2f868442c3ed855752bcbfbfc346e0871ea4bb15
Mohamed Moselhy <look4d@gmail.com>
2022-01-31 18:29:57 -0500

    toggle popover open on triggerRenderer click (#7136)
    * as opposed to only opening, now it opens/closes

--- design-system/packages/popover/src/Popover.tsx
+++ design-system/packages/popover/src/Popover.tsx
@@ -164,7 +164,7 @@ export const Popover = ({ placement = 'bottom', triggerRenderer, ...props }: Pro
         triggerProps: {
           ref: trigger.ref,
           ...trigger.props,
-          onClick: () => setOpen(true),
+          onClick: () => setOpen(open => !open),
         },
       })}
       <PopoverDialog
