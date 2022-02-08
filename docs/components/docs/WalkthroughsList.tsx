d55812a11c27db3ccdb36160e23d521538f3e826
Noviny <ben@thinkmill.com.au>
2022-02-08 16:37:08 +1100

    Walkthroughs / guides (#7063)
    * "Removals" and "Renames" continued (see https://keystonejs.com/releases/2021-10-05 (#7013)) (#7112)
    
    * Update embedded tutorial docs (#7113)
    
    * "Removals" and "Renames" continued (see https://keystonejs.com/releases/2021-10-05 (#7013))
    
    * Update embedded-mode-with-sqlite-nextjs.mdx
    
    * fix typo (#7132)
    
    * lessons 1 and 2
    
    * use code blocks unless inline
    
    * s/sh/bash
    
    * small changes
    
    * typo
    
    * add lessons 3-5
    
    * add rendering text
    
    * changes
    
    * update copy for Vercel
    
    * formatting and grammar changes
    
    * copy changes and typescript as a default
    
    * add additional explanations
    
    * always use the config object
    
    * add missing newline
    
    * add exploration hint
    
    * better labels
    
    * copy tweaks
    
    * Content Edits (#7210)
    
    * This commit is a squashed version of Ronald Raveling's edits, squashed to remove added images
    
    Get lessons 4 + 5 working in local
    
    Updated Walkthroughs index
    
    WIP
    
    Bring Walkthough component back (used in Docs index)
    
    Fixed walkthrough card title
    
    Updated content in Lesson 1
    
    Update docs/pages/docs/walkthroughs/lesson-1.mdx
    
    Co-authored-by: Noviny <ben@thinkmill.com.au>
    
    Update docs/pages/docs/walkthroughs/lesson-1.mdx
    
    Co-authored-by: Noviny <ben@thinkmill.com.au>
    
    WIP
    
    syntax highlighting tweaks
    
    Lesson 2: first pass
    
    Lesson 3 WIP
    
    Edits: Lesson 3
    
    Edits: Lesson 4
    
    Added callout about "content" field
    
    Edits to lesson 5 (WIP)
    
    Lesson 5 content
    
    Updated landing pages and site navigation
    
    Added images to Lesson 3
    
    Typo
    
    updates-and-content-fixes
    
    remove images from repo
    
    * tidy up placeholder images
    
    Co-authored-by: Noviny <noviny@thinkmill.com.au>
    
    * remove accidental change
    
    * image URL fix
    
    * typo
    
    * typo
    
    * sentence case
    
    * sentence case
    
    * sentence case
    
    * typo
    
    * typo
    
    * typos
    
    * typos
    
    * fixes
    
    * last code updates for lessons
    
    * Added images
    
    Co-authored-by: Thomas Walker <bladey@me.com>
    Co-authored-by: Trenton Kennedy <trentontri@gmail.com>
    Co-authored-by: Daniel Cousens <413395+dcousens@users.noreply.github.com>
    Co-authored-by: Daniel Cousens <dcousens@users.noreply.github.com>
    Co-authored-by: Ronald Aveling <luma.chroma@gmail.com>

--- docs/components/docs/WalkthroughsList.tsx
+++ docs/components/docs/WalkthroughsList.tsx
@@ -4,31 +4,68 @@ import { jsx } from '@emotion/react';
 
 import { Well } from '../primitives/Well';
 import { useMediaQuery } from '../../lib/media';
+import { InlineCode } from '../../components/primitives/Code';
 
 export function Walkthroughs() {
   const mq = useMediaQuery();
   return (
-    <div
-      css={mq({
-        display: 'grid',
-        gridTemplateColumns: ['1fr', '1fr', '1fr', '1fr 1fr'],
-        gap: 'var(--space-xlarge)',
-      })}
-    >
-      <Well
-        heading="Getting started with Keystone 6"
-        href="/docs/walkthroughs/getting-started-with-create-keystone-app"
+    <div>
+      <div
+        css={mq({
+          display: 'grid',
+          gridTemplateColumns: ['1fr'],
+          gap: 'var(--space-xlarge)',
+          margin: '0 0 var(--space-xlarge) 0',
+        })}
       >
-        Learn how to use our CLI to get Keystone’s Admin UI and GraphQL API running in a new local
-        project folder.
-      </Well>
-      <Well
-        heading="How to embed Keystone + SQLite in a Next.js app"
-        href="/docs/walkthroughs/embedded-mode-with-sqlite-nextjs"
+        <Well
+          heading="Keystone Quick Start"
+          href="/docs/walkthroughs/getting-started-with-create-keystone-app"
+        >
+          Take a tour of Keystone in minutes with our CLI starter project
+        </Well>
+      </div>
+      <div
+        css={mq({
+          display: 'grid',
+          gridTemplateColumns: ['1fr', '1fr', '1fr', '1fr 1fr'],
+          gap: 'var(--space-xlarge)',
+        })}
       >
-        Learn how to run Keystone in the same folder as your frontend code and commit everything to
-        Git. You end up with a queryable GraphQL endpoint running live on Vercel for free.
-      </Well>
+        <Well
+          grad="grad2"
+          heading="Lesson 1: Installing Keystone "
+          href="/docs/walkthroughs/lesson-1"
+        >
+          Get Keystone up and running with your first content type
+        </Well>
+        <Well grad="grad2" heading="Lesson 2: Relating things" href="/docs/walkthroughs/lesson-2">
+          Connect two content types and learn how to configure the appearance of field inputs
+        </Well>
+        <Well
+          grad="grad2"
+          heading="Lesson 3: Publishing workflows"
+          href="/docs/walkthroughs/lesson-3"
+        >
+          Support publishing needs with Keystone's <InlineCode>select</InlineCode> and{' '}
+          <InlineCode>timestamp</InlineCode> fields
+        </Well>
+        <Well grad="grad2" heading="Lesson 4: Auth & Sessions" href="/docs/walkthroughs/lesson-4">
+          Add sessions, password protection, and a sign-in screen to your Keystone app
+        </Well>
+        <Well grad="grad2" heading="Lesson 5: Rich Text" href="/docs/walkthroughs/lesson-5">
+          Add a powerful <InlineCode>document</InlineCode> field to your app and learn how to
+          configure it to meet your needs
+        </Well>
+        <Well
+          grad="grad2"
+          heading="How to embed Keystone + SQLite in a Next.js app"
+          href="/docs/walkthroughs/embedded-mode-with-sqlite-nextjs"
+        >
+          Learn how to run Keystone in the same folder as your frontend code and commit everything
+          to Git. You end up with a queryable GraphQL endpoint running live on Vercel.
+        </Well>
+      </div>
     </div>
   );
 }
