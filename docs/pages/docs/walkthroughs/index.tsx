14c9441506ff79da38ca2b1de7e5850437c85848
Noviny <ben@thinkmill.com.au>
2022-02-08 16:37:08 +1100

    Walkthroughs / guides (#7063)
    * "Removals" and "Renames" continued (see https://keystonejs.com/releases/2021-10-05 (#7013)) (#7112)
    
    * Update embedded tutorial docs (#7113)
    
    * "Removals" and "Renames" continued (see https://keystonejs.com/releases/2021-10-05 (#7013))
    
    * Update embedded-mode-with-sqlite-nextjs.mdx
    
    * fix typo (#7132)
    
    * lessons 1 and 2
    
    * use code blocks unless inline
    
    * s/sh/bash
    
    * small changes
    
    * typo
    
    * add lessons 3-5
    
    * add rendering text
    
    * changes
    
    * update copy for Vercel
    
    * formatting and grammar changes
    
    * copy changes and typescript as a default
    
    * add additional explanations
    
    * always use the config object
    
    * add missing newline
    
    * add exploration hint
    
    * better labels
    
    * copy tweaks
    
    * Content Edits (#7210)
    
    * This commit is a squashed version of Ronald Raveling's edits, squashed to remove added images
    
    Get lessons 4 + 5 working in local
    
    Updated Walkthroughs index
    
    WIP
    
    Bring Walkthough component back (used in Docs index)
    
    Fixed walkthrough card title
    
    Updated content in Lesson 1
    
    Update docs/pages/docs/walkthroughs/lesson-1.mdx
    
    Co-authored-by: Noviny <ben@thinkmill.com.au>
    
    Update docs/pages/docs/walkthroughs/lesson-1.mdx
    
    Co-authored-by: Noviny <ben@thinkmill.com.au>
    
    WIP
    
    syntax highlighting tweaks
    
    Lesson 2: first pass
    
    Lesson 3 WIP
    
    Edits: Lesson 3
    
    Edits: Lesson 4
    
    Added callout about "content" field
    
    Edits to lesson 5 (WIP)
    
    Lesson 5 content
    
    Updated landing pages and site navigation
    
    Added images to Lesson 3
    
    Typo
    
    updates-and-content-fixes
    
    remove images from repo
    
    * tidy up placeholder images
    
    Co-authored-by: Noviny <noviny@thinkmill.com.au>
    
    * remove accidental change
    
    * image URL fix
    
    * typo
    
    * typo
    
    * sentence case
    
    * sentence case
    
    * sentence case
    
    * typo
    
    * typo
    
    * typos
    
    * typos
    
    * fixes
    
    * last code updates for lessons
    
    * Added images
    
    Co-authored-by: Thomas Walker <bladey@me.com>
    Co-authored-by: Trenton Kennedy <trentontri@gmail.com>
    Co-authored-by: Daniel Cousens <413395+dcousens@users.noreply.github.com>
    Co-authored-by: Daniel Cousens <dcousens@users.noreply.github.com>
    Co-authored-by: Ronald Aveling <luma.chroma@gmail.com>

--- docs/pages/docs/walkthroughs/index.tsx
+++ docs/pages/docs/walkthroughs/index.tsx
@@ -2,9 +2,97 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 
-import { Walkthroughs } from '../../../components/docs/WalkthroughsList';
 import { Type } from '../../../components/primitives/Type';
 import { DocsPage } from '../../../components/Page';
+import { Well } from '../../../components/primitives/Well';
+import { useMediaQuery } from '../../../lib/media';
+import { InlineCode } from '../../../components/primitives/Code';
+
+export function QuickStart() {
+  const mq = useMediaQuery();
+  return (
+    <div>
+      <div
+        css={mq({
+          display: 'grid',
+          gridTemplateColumns: ['1fr'],
+          gap: 'var(--space-xlarge)',
+        })}
+      >
+        <Well
+          heading="Keystone Quick Start"
+          href="/docs/walkthroughs/getting-started-with-create-keystone-app"
+        >
+          Take a tour of Keystone in minutes with our CLI starter project
+        </Well>
+      </div>
+    </div>
+  );
+}
+
+export function Foundations() {
+  const mq = useMediaQuery();
+  return (
+    <div>
+      <div
+        css={mq({
+          display: 'grid',
+          gridTemplateColumns: ['1fr', '1fr', '1fr', '1fr 1fr'],
+          gap: 'var(--space-xlarge)',
+          margin: 'var(--space-xlarge) 0 0 0',
+        })}
+      >
+        <Well
+          grad="grad2"
+          heading="Lesson 1: Installing Keystone "
+          href="/docs/walkthroughs/lesson-1"
+        >
+          Get Keystone up and running with your first content type
+        </Well>
+        <Well grad="grad2" heading="Lesson 2: Relating things" href="/docs/walkthroughs/lesson-2">
+          Connect two content types and learn how to configure the appearance of field inputs
+        </Well>
+        <Well
+          grad="grad2"
+          heading="Lesson 3: Publishing workflows"
+          href="/docs/walkthroughs/lesson-3"
+        >
+          Support publishing needs with Keystone's <InlineCode>select</InlineCode> and{' '}
+          <InlineCode>timestamp</InlineCode> fields
+        </Well>
+        <Well grad="grad2" heading="Lesson 4: Auth & Sessions" href="/docs/walkthroughs/lesson-4">
+          Add sessions, password protection, and a sign-in screen to your Keystone app
+        </Well>
+        <Well grad="grad2" heading="Lesson 5: Rich Text" href="/docs/walkthroughs/lesson-5">
+          Add a powerful <InlineCode>document</InlineCode> field to your app and learn how to
+          configure it to meet your needs
+        </Well>
+      </div>
+    </div>
+  );
+}
+
+export function ExtendedLearning() {
+  const mq = useMediaQuery();
+  return (
+    <div
+      css={mq({
+        display: 'grid',
+        gridTemplateColumns: ['1fr', '1fr', '1fr', '1fr 1fr'],
+        gap: 'var(--space-xlarge)',
+      })}
+    >
+      <Well
+        grad="grad4"
+        heading="How to embed Keystone + SQLite in a Next.js app"
+        href="/docs/walkthroughs/embedded-mode-with-sqlite-nextjs"
+      >
+        Learn how to run Keystone in the same folder as your frontend code and commit everything to
+        Git. You end up with a queryable GraphQL endpoint running live on Vercel.
+      </Well>
+    </div>
+  );
+}
 
 export default function Docs() {
   return (
@@ -22,10 +110,36 @@ export default function Docs() {
       </Type>
 
       <Type as="p" look="body18" margin="1.25rem 0 1.5rem 0">
-        Step-by-step instructions for getting things done with Keystone.
+        Step-by-step tutorials for building with Keystone.
       </Type>
 
-      <Walkthroughs />
+      <Type as="h2" look="heading30" margin="2rem 0 1rem 0">
+        Getting started
+      </Type>
+
+      <Type as="p" look="body18" margin=".25rem 0 1.5rem 0">
+        If you’re new to Keystone begin here. These walkthroughs introduce the system, key concepts,
+        and show you how to getup and running with schema-driven development the Keystone way.
+      </Type>
+
+      <QuickStart />
+
+      <Type as="h3" look="heading24" margin="2rem 0 1rem 0" id="learn-keystone">
+        Learn Keystone
+      </Type>
+
+      <Type as="p" look="body18" margin=".25rem 0 1.5rem 0">
+        Learn how to build a functioning blog backend with relationships, auth, and session data
+        from an empty folder, and gain insights into Keystone’s core concepts along the way.
+      </Type>
+
+      <Foundations />
+
+      <Type as="h2" look="heading30" margin="2rem 0 1rem 0" id="extended-learning">
+        Extended learning
+      </Type>
+
+      <ExtendedLearning />
     </DocsPage>
   );
 }
