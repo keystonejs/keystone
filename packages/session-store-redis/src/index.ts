330aaf68c575e0342f7797774998b53f34794d4d
mitchellhamilton <mitchell@hamil.town>
2022-03-23 15:52:13 +1000

    Remove promisify

--- packages/session-store-redis/src/index.ts
+++ packages/session-store-redis/src/index.ts
@@ -1,35 +1,27 @@
-import { promisify } from 'util';
-import type { RedisClientType } from 'redis';
+import type { RedisClientType } from '@node-redis/client';
 import type { SessionStoreFunction } from '@keystone-6/core/types';
 
 type Options = {
-  /** An initialised redis client from the `redis` npm package */
+  /** An initialised redis client from the `@node-redis/client` npm package (it is also re-exported from the `redis` npm package) */
   client: RedisClientType;
 };
+
 export const redisSessionStore = ({ client }: Options): SessionStoreFunction => {
-  let promisifiedGet = promisify(client.get).bind(client);
-  let promisifiedSetex = promisify(client.setEx).bind(client);
-  let promisifiedDel = promisify(client.del).bind(client);
-  let promisifiedQuit = promisify(client.quit).bind(client);
   return ({ maxAge }) => ({
     async get(key) {
-      let result = await promisifiedGet(key);
+      let result = await client.get(key);
       if (typeof result === 'string') {
         return JSON.parse(result);
       }
     },
-
     async set(key, value) {
-      await promisifiedSetex(key, maxAge, JSON.stringify(value));
+      await client.setEx(key, maxAge, JSON.stringify(value));
     },
     async delete(key) {
-      await promisifiedDel(
-        // @ts-ignore - the types for promisifiy are confused by the definition of del
-        key
-      );
+      await client.del(key);
     },
     async disconnect() {
-      await promisifiedQuit();
+      await client.disconnect();
     },
   });
 };
