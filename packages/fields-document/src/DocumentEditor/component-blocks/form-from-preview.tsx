39757d36a0b9995995863a1c9c22e7dc01417b18
Daniel Cousens <dcousens@users.noreply.github.com>
2022-05-25 16:21:16 +1000

    unnest the components, revert to functions

--- packages/fields-document/src/DocumentEditor/component-blocks/form-from-preview.tsx
+++ packages/fields-document/src/DocumentEditor/component-blocks/form-from-preview.tsx
@@ -6,7 +6,6 @@ import { Button } from '@keystone-ui/button';
 import { jsx, Stack } from '@keystone-ui/core';
 import { FieldContainer, FieldLabel } from '@keystone-ui/fields';
 import { memo, useMemo } from 'react';
-import { ReactElement } from 'react';
 import { DragHandle, OrderableItem, OrderableList } from '../primitives/orderable';
 import {
   ArrayField,
@@ -20,123 +19,120 @@ import {
 } from './api';
 import { assertNever } from './utils';
 
-const fieldRenderers: {
-  [Key in ComponentSchema['kind']]: (
-    props: GenericPreviewProps<Extract<ComponentSchema, { kind: Key }>, unknown> & {
-      autoFocus?: boolean;
-      forceValidation?: boolean;
-    }
-  ) => ReactElement | null;
-} = {
-  array(props) {
-    return (
-      <Stack gap="medium">
-        <OrderableList {...props}>
-          {props.elements.map(val => {
-            return <OrderableItemInForm elementKey={val.key} {...val} />;
-          })}
-        </OrderableList>
-        <Button
-          autoFocus={props.autoFocus}
-          onClick={() => {
-            props.onChange([...props.elements.map(x => ({ key: x.key })), { key: undefined }]);
-          }}
-        >
-          Add
-        </Button>
-      </Stack>
-    );
-  },
-  relationship({ schema, autoFocus, onChange, value }) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const keystone = useKeystone();
-    return (
-      <FieldContainer>
-        <FieldLabel>{schema.label}</FieldLabel>
-        <RelationshipSelect
-          autoFocus={autoFocus}
-          controlShouldRenderValue
-          isDisabled={false}
-          list={keystone.adminMeta.lists[schema.listKey]}
-          extraSelection={schema.selection || ''}
-          portalMenu
-          state={
-            schema.many
-              ? {
-                  kind: 'many',
-                  value: (value as RelationshipData[]).map(x => ({
-                    id: x.id,
-                    label: x.label || x.id,
-                    data: x.data,
-                  })),
-                  onChange: onChange,
-                }
-              : {
-                  kind: 'one',
-                  value: value
-                    ? {
-                        ...(value as RelationshipData),
-                        label: (value as RelationshipData).label || (value as RelationshipData).id,
-                      }
-                    : null,
-                  onChange: onChange,
-                }
-          }
-        />
-      </FieldContainer>
-    );
-  },
-  child: () => null,
-  form({ schema, autoFocus, forceValidation, onChange, value }) {
-    return (
-      <schema.Input
-        autoFocus={!!autoFocus}
-        value={value}
-        onChange={onChange}
-        forceValidation={!!forceValidation}
-      />
-    );
-  },
-  object({ schema, autoFocus, fields }) {
-    const firstFocusable = autoFocus ? findFocusableObjectFieldKey(schema) : undefined;
-    return (
-      <Stack gap="xlarge">
-        {Object.entries(fields).map(
-          ([key, propVal]) =>
-            isNonChildFieldPreviewProps(propVal) && (
-              <FormValueContentFromPreviewProps
-                autoFocus={key === firstFocusable}
-                key={key}
-                {...propVal}
-              />
-            )
-        )}
-      </Stack>
-    );
-  },
-  conditional({ schema, autoFocus, discriminant, onChange, value }) {
-    const schemaDiscriminant = schema.discriminant as FormField<string | boolean, unknown>;
-    return (
-      <Stack gap="xlarge">
-        {
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          useMemo(
-            () => (
-              <schemaDiscriminant.Input
-                autoFocus={!!autoFocus}
-                value={discriminant}
-                onChange={onChange}
-                forceValidation={false}
-              />
-            ),
-            [autoFocus, schemaDiscriminant, discriminant, onChange]
-          )
-        }
-        {isNonChildFieldPreviewProps(value) && <FormValueContentFromPreviewProps {...value} />}
-      </Stack>
-    );
-  },
+type DefaultFieldProps<Key> = GenericPreviewProps<Extract<ComponentSchema, { kind: Key }>, unknown> & {
+  autoFocus?: boolean;
+  forceValidation?: boolean;
 };
+
+function ArrayFieldPreview(props: DefaultFieldProps<'array'>) {
+  return (
+    <Stack gap="medium">
+      <OrderableList {...props}>
+        {props.elements.map(val => {
+          return <OrderableItemInForm elementKey={val.key} {...val} />;
+        })}
+      </OrderableList>
+      <Button
+        autoFocus={props.autoFocus}
+        onClick={() => {
+          props.onChange([...props.elements.map(x => ({ key: x.key })), { key: undefined }]);
+        }}
+      >
+        Add
+      </Button>
+    </Stack>
+  );
+}
+
+function RelationshipFieldPreview({ schema, autoFocus, onChange, value }: DefaultFieldProps<'relationship'>) {
+  const keystone = useKeystone();
+  return (
+    <FieldContainer>
+      <FieldLabel>{schema.label}</FieldLabel>
+      <RelationshipSelect
+        autoFocus={autoFocus}
+        controlShouldRenderValue
+        isDisabled={false}
+        list={keystone.adminMeta.lists[schema.listKey]}
+        extraSelection={schema.selection || ''}
+        portalMenu
+        state={
+          schema.many
+            ? {
+                kind: 'many',
+                value: (value as RelationshipData[]).map(x => ({
+                  id: x.id,
+                  label: x.label || x.id,
+                  data: x.data,
+                })),
+                onChange: onChange,
+              }
+            : {
+                kind: 'one',
+                value: value
+                  ? {
+                      ...(value as RelationshipData),
+                      label: (value as RelationshipData).label || (value as RelationshipData).id,
+                    }
+                  : null,
+                onChange: onChange,
+              }
+        }
+      />
+    </FieldContainer>
+  );
+}
+
+function FormFieldPreview({ schema, autoFocus, forceValidation, onChange, value }: DefaultFieldProps<'form'>) {
+  return (
+    <schema.Input
+      autoFocus={!!autoFocus}
+      value={value}
+      onChange={onChange}
+      forceValidation={!!forceValidation}
+    />
+  );
+}
+
+function ObjectFieldPreview({ schema, autoFocus, fields }: DefaultFieldProps<'object'>) {
+  const firstFocusable = autoFocus ? findFocusableObjectFieldKey(schema) : undefined;
+  return (
+    <Stack gap="xlarge">
+      {Object.entries(fields).map(
+        ([key, propVal]) =>
+          isNonChildFieldPreviewProps(propVal) && (
+            <FormValueContentFromPreviewProps
+              autoFocus={key === firstFocusable}
+              key={key}
+              {...propVal}
+            />
+          )
+      )}
+    </Stack>
+  );
+}
+
+function ConditionalFieldPreview({ schema, autoFocus, discriminant, onChange, value }: DefaultFieldProps<'conditional'>) {
+  const schemaDiscriminant = schema.discriminant as FormField<string | boolean, unknown>;
+  return (
+    <Stack gap="xlarge">
+      {
+        useMemo(
+          () => (
+            <schemaDiscriminant.Input
+              autoFocus={!!autoFocus}
+              value={discriminant}
+              onChange={onChange}
+              forceValidation={false}
+            />
+          ),
+          [autoFocus, schemaDiscriminant, discriminant, onChange]
+        )
+      }
+      {isNonChildFieldPreviewProps(value) && <FormValueContentFromPreviewProps {...value} />}
+    </Stack>
+  );
+}
 
 export type NonChildFieldComponentSchema =
   | FormField<any, any>
@@ -150,6 +146,15 @@ function isNonChildFieldPreviewProps(
 ): props is GenericPreviewProps<NonChildFieldComponentSchema, unknown> {
   return props.schema.kind !== 'child';
 }
+
+const fieldRenderers = {
+  array: ArrayFieldPreview,
+  relationship: RelationshipFieldPreview,
+  child: () => null,
+  form: FormFieldPreview,
+  object: ObjectFieldPreview,
+  conditional: ConditionalFieldPreview
+};
 
 export const FormValueContentFromPreviewProps = memo(function FormValueContentFromPreview(
   props: GenericPreviewProps<NonChildFieldComponentSchema, unknown> & {
