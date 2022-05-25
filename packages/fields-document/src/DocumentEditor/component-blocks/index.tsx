5cb6f9232be418dc5f7663d7827284a2dd07e5c7
Daniel Cousens <dcousens@users.noreply.github.com>
2022-05-25 15:03:54 +1000

    prefer const

--- packages/fields-document/src/DocumentEditor/component-blocks/index.tsx
+++ packages/fields-document/src/DocumentEditor/component-blocks/index.tsx
@@ -62,11 +62,13 @@ export function insertComponentBlock(
   componentBlocks: Record<string, ComponentBlock>,
   componentBlock: string
 ) {
-  let node = getInitialValue(componentBlock, componentBlocks[componentBlock]);
+  const node = getInitialValue(componentBlock, componentBlocks[componentBlock]);
   insertNodesButReplaceIfSelectionIsAtEmptyParagraphOrHeading(editor, node);
+
   const componentBlockEntry = Editor.above(editor, {
     match: node => node.type === 'component-block',
   });
+
   if (componentBlockEntry) {
     const start = Editor.start(editor, componentBlockEntry[1]);
     Transforms.setSelection(editor, { anchor: start, focus: start });
