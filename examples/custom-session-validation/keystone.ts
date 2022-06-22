ff4402c57827f68ed84b370cdbed3e75b413cef4
Josh Calder <josh.calder@thinkmill.com.au>
2022-06-22 15:38:15 +1000

    add custom session validation example

---
+++ examples/custom-session-validation/keystone.ts
@@ -0,0 +1,81 @@
+import { SessionStrategy } from '@keystone-6/core/src/types/session';
+import { config } from '@keystone-6/core';
+import { statelessSessions } from '@keystone-6/core/session';
+import { createAuth } from '@keystone-6/auth';
+import { lists } from './schema';
+
+// createAuth configures signin functionality based on the config below. Note this only implements
+// authentication, i.e signing in as an item using identity and secret fields in a list. Session
+// management and access control are controlled independently in the main keystone config.
+const { withAuth } = createAuth({
+  // This is the list that contains items people can sign in as
+  listKey: 'Person',
+  // The identity field is typically a username or email address
+  identityField: 'email',
+  // The secret field must be a password type field
+  secretField: 'password',
+
+  // initFirstItem turns on the "First User" experience, which prompts you to create a new user
+  // when there are no items in the list yet
+  initFirstItem: {
+    // These fields are collected in the "Create First User" form
+    fields: ['name', 'email', 'password'],
+  },
+});
+const sessionAge = 60 * 60 * 8; // 8 hours
+// Stateless sessions will store the listKey and itemId of the signed-in user in a cookie.
+// This session object will be made available on the context object used in hooks, access-control,
+// resolvers, etc.
+
+const withTimeData = (
+  _sessionStrategy: SessionStrategy<Record<string, any>>
+): SessionStrategy<{ startTime: string }> => {
+  const { get, start, ...sessionStrategy } = _sessionStrategy;
+  return {
+    ...sessionStrategy,
+    get: async ({ req, createContext }) => {
+      const session = await get({ req, createContext });
+      const sudoContext = createContext({ sudo: true });
+      if (!session || !session.listKey || !session.startTime) {
+        return;
+      }
+      const data = await sudoContext.query[session.listKey].findOne({
+        where: { id: session.itemId },
+        query: 'secretResetTime',
+      });
+      if (!data) return;
+      if (data.secretResetTime > session.startTime) return;
+      return { ...session, startTime: session.startTime };
+    },
+    start: async ({ res, data, createContext }) => {
+      const withTimeData = {
+        ...data,
+        startTime: new Date(),
+      };
+      const newSession = await start({ res, data: withTimeData, createContext });
+      return newSession;
+    },
+  };
+};
+
+const session = withTimeData(
+  statelessSessions({
+    // The session secret is used to encrypt cookie data (should be an environment variable)
+    maxAge: sessionAge,
+    secret: '-- EXAMPLE COOKIE SECRET; CHANGE ME --',
+  })
+);
+
+// We wrap our config using the withAuth function. This will inject all
+// the extra config required to add support for authentication in our system.
+export default withAuth(
+  config({
+    db: {
+      provider: 'sqlite',
+      url: process.env.DATABASE_URL || 'file:./keystone-example.db',
+    },
+    lists,
+    // We add our session configuration to the system here.
+    session,
+  })
+);
---
+++ examples/custom-session-validation/schema.ts
@@ -0,0 +1,56 @@
+import { list } from '@keystone-6/core';
+import { checkbox, password, relationship, text, timestamp } from '@keystone-6/core/fields';
+import { select } from '@keystone-6/core/fields';
+
+export const lists = {
+  Task: list({
+    fields: {
+      label: text({ validation: { isRequired: true } }),
+      priority: select({
+        type: 'enum',
+        options: [
+          { label: 'Low', value: 'low' },
+          { label: 'Medium', value: 'medium' },
+          { label: 'High', value: 'high' },
+        ],
+      }),
+      isComplete: checkbox(),
+      assignedTo: relationship({ ref: 'Person.tasks', many: false }),
+      finishBy: timestamp(),
+    },
+  }),
+  Person: list({
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      // Added an email and password pair to be used with authentication
+      // The email address is going to be used as the identity field, so it's
+      // important that we set isRequired and isIndexed: 'unique'.
+      email: text({ isIndexed: 'unique', validation: { isRequired: true } }),
+      // The password field stores a hash of the supplied password, and
+      // we want to ensure that all people have a password set, so we use
+      // the validation.isRequired flag.
+      password: password({
+        validation: { isRequired: true },
+        hooks: {
+          afterOperation: async ({ item, context }) => {
+            if (!item) return;
+            const sudo = context.sudo();
+            await sudo.db.Person.updateOne({
+              where: { id: item.id as string },
+              data: { secretResetTime: new Date() },
+            });
+          },
+        },
+      }),
+      secretResetTime: timestamp({
+        access: () => false,
+        ui: {
+          createView: { fieldMode: 'hidden' },
+          itemView: { fieldMode: 'hidden' },
+          listView: { fieldMode: 'hidden' },
+        },
+      }),
+      tasks: relationship({ ref: 'Task.assignedTo', many: true }),
+    },
+  }),
+};
