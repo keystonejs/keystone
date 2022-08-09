1b052080b3a03dca67a1706872d5988053f98beb
Josh Calder <josh.calder@thinkmill.com.au>
2022-08-10 08:29:51 +1000

    change ecommerce example to graphql tools

--- examples/ecommerce/mutations/index.ts
+++ examples/ecommerce/mutations/index.ts
@@ -1,20 +1,24 @@
-import { graphQLSchemaExtension } from '@keystone-6/core';
+import { mergeSchemas } from '@graphql-tools/schema';
+import type { GraphQLSchema } from 'graphql';
 import addToCart from './addToCart';
 import checkout from './checkout';
 
 // make a fake graphql tagged template literal
 const graphql = String.raw;
-export const extendGraphqlSchema = graphQLSchemaExtension({
-  typeDefs: graphql`
-    type Mutation {
-      addToCart(productId: ID): CartItem
-      checkout(token: String!): Order
-    }
-  `,
-  resolvers: {
-    Mutation: {
-      addToCart,
-      checkout,
+
+export const extendGraphqlSchema = (schema: GraphQLSchema) =>
+  mergeSchemas({
+    schemas: [schema],
+    typeDefs: graphql`
+      type Mutation {
+        addToCart(productId: ID): CartItem
+        checkout(token: String!): Order
+      }
+    `,
+    resolvers: {
+      Mutation: {
+        addToCart,
+        checkout,
+      },
     },
-  },
-});
+  });
