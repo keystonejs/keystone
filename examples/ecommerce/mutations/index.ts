e220078523ca7f631b95dea3356c718c4a293b5a
Josh Calder <josh.calder@thinkmill.com.au>
2022-08-11 14:24:06 +1000

    change ecommerce example to use graphql-ts

--- examples/ecommerce/mutations/index.ts
+++ examples/ecommerce/mutations/index.ts
@@ -1,24 +1,22 @@
-import { mergeSchemas } from '@graphql-tools/schema';
-import type { GraphQLSchema } from 'graphql';
+import { graphql } from '@keystone-6/core';
 import addToCart from './addToCart';
 import checkout from './checkout';
 
-// make a fake graphql tagged template literal
-const graphql = String.raw;
-
-export const extendGraphqlSchema = (schema: GraphQLSchema) =>
-  mergeSchemas({
-    schemas: [schema],
-    typeDefs: graphql`
-      type Mutation {
-        addToCart(productId: ID): CartItem
-        checkout(token: String!): Order
-      }
-    `,
-    resolvers: {
-      Mutation: {
-        addToCart,
-        checkout,
-      },
+export const extendGraphqlSchema = graphql.extend(base => {
+  return {
+    mutation: {
+      addToCart: graphql.field({
+        type: base.object('CartItem'),
+        args: { productId: graphql.arg({ type: graphql.nonNull(graphql.ID) }) },
+        // @ts-expect-error context is generic here until https://github.com/keystonejs/keystone/pull/7733 is merged
+        resolve: addToCart,
+      }),
+      checkout: graphql.field({
+        type: base.object('Order'),
+        args: { token: graphql.arg({ type: graphql.nonNull(graphql.String) }) },
+        // @ts-expect-error context is generic here until https://github.com/keystonejs/keystone/pull/7733 is merged
+        resolve: checkout,
+      }),
     },
-  });
+  };
+});
