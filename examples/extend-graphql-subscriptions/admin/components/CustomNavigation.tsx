404dc7b1432b43b79999c750209d3414ffe4ede9
Daniel Cousens <dcousens@users.noreply.github.com>
2022-08-15 12:28:03 +1000

    add subscriptions example

---
+++ examples/extend-graphql-subscriptions/admin/components/CustomNavigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ListNavItems, NavigationContainer, NavItem } from '@keystone-6/core/admin-ui/components';
+
+import type { NavigationProps } from '@keystone-6/core/admin-ui/components';
+
+export function CustomNavigation({ lists, authenticatedItem }: NavigationProps) {
+  return (
+    <NavigationContainer authenticatedItem={authenticatedItem}>
+      <NavItem href="/">Dashboard</NavItem>
+      <ListNavItems lists={lists} />
+      <NavItem href="/subscriptions">Subscriptions Page</NavItem>
+    </NavigationContainer>
+  );
+}
