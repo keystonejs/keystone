a5fa30f86ffd68b19e6e537f63d09ea9a321f920
Daniel Cousens <dcousens@users.noreply.github.com>
2022-08-18 14:28:10 +1000

    remove unrelated fields

--- examples/custom-field/schema.ts
+++ examples/custom-field/schema.ts
@@ -1,5 +1,4 @@
 import { list } from '@keystone-6/core';
-import { select, relationship, timestamp } from '@keystone-6/core/fields';
 
 import { text } from './1-text-field';
 import { stars } from './2-stars-field';
@@ -10,19 +9,9 @@ import { Lists } from '.keystone/types';
 export const lists: Lists = {
   Post: list({
     fields: {
-      title: text({ validation: { isRequired: true } }),
-      status: select({
-        type: 'enum',
-        options: [
-          { label: 'Draft', value: 'draft' },
-          { label: 'Published', value: 'published' },
-        ],
-      }),
       content: text(),
       rating: stars(),
       pair: pair(), // TODO: this example is a bit abstract, should be contextualised
-      publishDate: timestamp(),
-      author: relationship({ ref: 'Author.posts', many: false }),
     },
     hooks: {
       // TODO: this is  an example of how hooks interact with custom multiple-column fields,
@@ -41,11 +30,5 @@ export const lists: Lists = {
         }
       },
     },
-  }),
-  Author: list({
-    fields: {
-      name: text({ validation: { isRequired: true } }),
-      posts: relationship({ ref: 'Post.author', many: true }),
-    },
-  }),
+  })
 };
