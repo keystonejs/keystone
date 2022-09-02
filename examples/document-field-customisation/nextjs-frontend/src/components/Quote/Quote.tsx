db03e8d1321d8c102c7ab79d95a4b04dc4472058
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-09-02 17:53:30 +1000

    Example boilerplates and Document Field Customisation Example (#7813)
    * Feature and E2E boilerplate
    
    * Document field customisation and DocumentRenderer customisation
    
    * Review suggestion: Add key while iterating elements
    
    Co-authored-by: Mitchell Hamilton <mitchell@hamil.town>
    
    * Load `examples/*/keystone-server` projects in scripts/generate-artifacts-for-projects
    
    * Update examples/document-field-customisation/keystone-server/README.md
    
    * Don't seed database serially for each entry
    
    * Replace apollo client with a simple fetch
    
    * Compatibility changes after rebasing with main
    
    Co-authored-by: Mitchell Hamilton <mitchell@hamil.town>

---
+++ examples/document-field-customisation/nextjs-frontend/src/components/Quote/Quote.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import styles from './Quote.module.css';
+
+type QuoteProps = {
+  attribution: ReactNode;
+  content: ReactNode;
+};
+
+export function Quote({ attribution, content }: QuoteProps) {
+  return (
+    <div className={styles.quote}>
+      <div style={{ fontStyle: 'italic', color: '#4A5568' }}>{content}</div>
+      <div style={{ fontWeight: 'bold', color: '#47546b' }}>— {attribution}</div>
+    </div>
+  );
+}
