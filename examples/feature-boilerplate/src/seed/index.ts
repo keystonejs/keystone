1d48a28fe31e3f9fc78c96a38ae327ba70fbe991
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-09-02 15:10:33 +1000

    Don't seed database serially for each entry

--- examples/feature-boilerplate/src/seed/index.ts
+++ examples/feature-boilerplate/src/seed/index.ts
@@ -3,73 +3,38 @@ import path from 'path';
 import { Context } from '.keystone/types';
 
 const seedUsers = async (context: Context) => {
-  const { query } = context.sudo();
+  const { db } = context.sudo();
   const rawJSONData = fs.readFileSync(path.join(process.cwd(), './src/seed/users.json'), 'utf-8');
-  const seedUsers = JSON.parse(rawJSONData);
-
-  for await (const user of seedUsers) {
-    try {
-      const queriedUser = await query.User.findOne({
-        where: {
-          email: user.email,
-        },
-      });
-
-      if (!queriedUser) {
-        await query.User.createOne({
-          data: {
-            email: user.email,
-            name: user.name,
-          },
-        });
-      }
-    } catch (e) {
-      console.error('Seeding user failed: ', {
-        user,
-        message: (e as Error).message,
-      });
-    }
-  }
+  const seedUsers: any[] = JSON.parse(rawJSONData);
+  const usersAlreadyInDatabase = await db.User.findMany({
+    where: {
+      email: { in: seedUsers.map(user => user.email) },
+    },
+  });
+  const usersToCreate = seedUsers.filter(
+    seedUser => !usersAlreadyInDatabase.some(u => u.email === seedUser.email)
+  );
+  await db.User.createMany({
+    data: usersToCreate,
+  });
 };
 
 // seed posts and connect with users
 const seedPosts = async (context: Context) => {
-  const { query } = context.sudo();
+  const { db } = context.sudo();
   const rawJSONData = fs.readFileSync(path.join(process.cwd(), './src/seed/posts.json'), 'utf-8');
-  const seedPosts = JSON.parse(rawJSONData);
-
-  for await (const post of seedPosts) {
-    try {
-      const queriedPost = await query.Post.findOne({
-        where: {
-          slug: post.slug,
-        },
-      });
-
-      if (!queriedPost) {
-        await query.Post.createOne({
-          data: {
-            title: post.title,
-            slug: post.slug,
-            publishDate: post.publishDate,
-            content: post.content?.document,
-            ...(post.author?.connect?.email && {
-              author: {
-                connect: {
-                  email: post.author.connect.email,
-                },
-              },
-            }),
-          },
-        });
-      }
-    } catch (e) {
-      console.error('Seeding post failed: ', {
-        post,
-        message: (e as Error).message,
-      });
-    }
-  }
+  const seedPosts: any[] = JSON.parse(rawJSONData);
+  const postsAlreadyInDatabase = await db.Post.findMany({
+    where: {
+      slug: { in: seedPosts.map(post => post.slug) },
+    },
+  });
+  const postsToCreate = seedPosts.filter(
+    seedPost => !postsAlreadyInDatabase.some(p => p.slug === seedPost.slug)
+  );
+  await db.Post.createMany({
+    data: postsToCreate.map(p => ({ ...p, content: p?.content?.document })),
+  });
 };
 
 export const seedDatabase = async (context: Context) => {
