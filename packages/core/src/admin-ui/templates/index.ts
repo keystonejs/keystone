201c0e5c5a50042d62883f5a852a7943303b433d
mitchellhamilton <mitchell@hamil.town>
2022-09-12 15:03:52 +1000

    Remove `experimental.enableNextJsGraphqlApiEndpoint`

--- packages/core/src/admin-ui/templates/index.ts
+++ packages/core/src/admin-ui/templates/index.ts
@@ -5,7 +5,6 @@ import { appTemplate } from './app';
 import { homeTemplate } from './home';
 import { listTemplate } from './list';
 import { itemTemplate } from './item';
-import { apiTemplate } from './api';
 import { noAccessTemplate } from './no-access';
 import { createItemTemplate } from './create-item';
 
@@ -17,15 +16,6 @@ export const writeAdminFiles = (
   adminMeta: AdminMetaRootVal,
   configFileExists: boolean
 ): AdminFileToWrite[] => {
-  if (
-    config.experimental?.enableNextJsGraphqlApiEndpoint &&
-    config.graphql?.path &&
-    !config.graphql.path.startsWith('/api/')
-  ) {
-    throw new Error(
-      'config.graphql.path must start with "/api/" when using config.experimental.enableNextJsGraphqlApiEndpoint'
-    );
-  }
   return [
     ...['next.config.js', 'tsconfig.json'].map(
       outputPath =>
@@ -53,14 +43,5 @@ export const writeAdminFiles = (
       { mode: 'write', src: itemTemplate(key), outputPath: `pages/${path}/[id].js` },
       { mode: 'write', src: createItemTemplate(key), outputPath: `pages/${path}/create.js` },
     ]),
-    ...(config.experimental?.enableNextJsGraphqlApiEndpoint
-      ? [
-          {
-            mode: 'write' as const,
-            src: apiTemplate,
-            outputPath: `pages/${config.graphql?.path || '/api/graphql'}.js`,
-          },
-        ]
-      : []),
   ];
 };
