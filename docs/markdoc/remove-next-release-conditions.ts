2bef0beab482a4dd8e330218877074affbf3d126
mitchellhamilton <mitchell@hamil.town>
2022-09-26 14:09:08 +1000

    Remove unused AST transform

--- docs/markdoc/remove-next-release-conditions.ts
+++ docs/markdoc/remove-next-release-conditions.ts
@@ -1,19 +1,5 @@
-import Markdoc, { Ast, Node } from '@markdoc/markdoc';
+import Markdoc from '@markdoc/markdoc';
 import { baseMarkdocConfig } from './config';
-
-function transform(node: Node): Node[] | Node {
-  if (
-    node.type !== 'tag' ||
-    node.tag !== 'if' ||
-    !Ast.isVariable(node.attributes.primary) ||
-    node.attributes.primary.path.length !== 1 ||
-    node.attributes.primary.path[0] !== 'nextRelease'
-  ) {
-    return new Ast.Node(node.type, node.attributes, node.children.flatMap(transform), node.tag);
-  }
-  const elseTag = node.children.findIndex(child => child.type === 'tag' && child.tag === 'else');
-  return elseTag === -1 ? node.children : node.children.slice(0, elseTag);
-}
 
 const pattern =
   /{%\s+if\s+\$nextRelease\s+%}\s*([^]+?)\s*(?:{%\s+else\s+\/%}[^]*?)?{%\s+\/if\s+%}/g;
