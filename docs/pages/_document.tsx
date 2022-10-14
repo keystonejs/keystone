2af9b2166c79cdb7310874e949d334133b365c68
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-10-14 16:39:02 +1100

    FIX: Light theme flash during page load

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Script from 'next/script';
 import React from 'react';
 import createEmotionServer from '@emotion/server/create-instance';
 import { cache } from '@emotion/css';
@@ -29,7 +30,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html data-theme="light">
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -54,6 +55,19 @@ class MyDocument extends Document {
             src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
           />
           <script data-no-cookie data-respect-dnt async data-api="/_sb" src="/sb.js" />
+          {/*
+            The page is server rendered and hydrated on the browser client.
+            While server rendering we do not know what the user's preferred/saved theme is and default to light theme.
+            So we run this script in the browser before the page is rendered (not the react render but the browser render)
+            and set the theme class in html element so our styles would know which theme to paint on first paint.
+            All this to avoid a flash of default light theme when user either prefers dark theme or has previously
+            saved dark theme to their local storage. ¯\_(ツ)_/¯ React is hard sometimes.
+         */}
+          <Script
+            id="set-default-mode-script"
+            strategy="beforeInteractive"
+            src="/assets/init-theme.js"
+          />
         </Head>
         <body>
           <Main />
