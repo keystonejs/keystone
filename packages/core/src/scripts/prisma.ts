faf6658ea2f9a44533a4c9842470a87da871d6a2
Josh Calder <josh.calder@thinkmill.com.au>
2022-10-24 07:46:33 +0100

    use built config for prisma in production

--- packages/core/src/scripts/prisma.ts
+++ packages/core/src/scripts/prisma.ts
@@ -1,12 +1,18 @@
 import execa from 'execa';
 import { createSystem } from '../lib/createSystem';
 import { generateNodeModulesArtifacts, validateCommittedArtifacts } from '../artifacts';
-import { loadConfigOnce } from '../lib/config/loadConfig';
+import { loadConfigOnce, loadBuiltConfig } from '../lib/config/loadConfig';
+import { KeystoneConfig } from './../types/config/index';
+
 import { ExitError } from './utils';
 
 export async function prisma(cwd: string, args: string[]) {
-  const config = await loadConfigOnce(cwd);
-
+  let config: KeystoneConfig;
+  if (process.env.NODE_ENV === 'production') {
+    config = loadBuiltConfig(cwd);
+  } else {
+    config = await loadConfigOnce(cwd);
+  }
   const { graphQLSchema } = createSystem(config);
 
   await validateCommittedArtifacts(graphQLSchema, config, cwd);
