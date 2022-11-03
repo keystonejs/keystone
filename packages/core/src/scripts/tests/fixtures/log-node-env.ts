4b777f13505f45008676dd439b355f09e3215188
mitchellhamilton <mitchell@hamil.town>
2022-11-03 11:59:36 +1000

    Test that process.env.NODE_ENV is production when set to that in production

---
+++ packages/core/src/scripts/tests/fixtures/log-node-env.ts
@@ -0,0 +1,20 @@
+import { list, config } from '@keystone-6/core';
+import { text } from '@keystone-6/core/fields';
+import { allowAll } from '@keystone-6/core/access';
+console.log('the env is: ' + process.env.NODE_ENV);
+
+export default config({
+  db: {
+    provider: 'sqlite',
+    url: 'file:./app.db',
+  },
+  ui: { isDisabled: true },
+  lists: {
+    Todo: list({
+      access: allowAll,
+      fields: {
+        title: text(),
+      },
+    }),
+  },
+});
