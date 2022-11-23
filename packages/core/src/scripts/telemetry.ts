b337361e2d0864272b9f69e49ed69b25271ea610
Josh Calder <josh.calder@thinkmill.com.au>
2022-11-23 12:09:10 +1100

    add enable and remove setup on telemetry cli

--- packages/core/src/scripts/telemetry.ts
+++ packages/core/src/scripts/telemetry.ts
@@ -1,6 +1,5 @@
 import chalk from 'chalk';
 import Conf from 'conf';
-import { confirmPrompt } from '../lib/prompts';
 import { Configuration } from '../types/telemetry';
 
 export async function telemetry(cwd: string, option?: string) {
@@ -12,8 +11,8 @@ export async function telemetry(cwd: string, option?: string) {
       Options
         status      displays the current telemetry configuration
         reset       resets the current telemetry configuration (if any)
-        setup       resets the current telemetry configuration (if any) and setupializes the telemetry configuration
-        disable     resets the current telemetry configuration (if any) and disables all telemetry on this device
+        enable      enables telemetry for the current user
+        disable     resets the current telemetry configuration (if any) and disables all telemetry for the current user
       `;
 
   const helpText = `
@@ -24,8 +23,8 @@ export async function telemetry(cwd: string, option?: string) {
       Options
         status      displays the current telemetry configuration
         reset       resets the current telemetry configuration (if any)
-        setup       resets the current telemetry configuration (if any) and setupializes the telemetry configuration
-        disable     resets the current telemetry configuration (if any) and disables all telemetry on this device
+        enable      enables telemetry for the current user
+        disable     resets the current telemetry configuration (if any) and disables all telemetry for the current user
   
   For more details visit: https://keystonejs.com/telemetry    
         `;
@@ -81,9 +80,8 @@ KeystoneJS telemetry: ${chalk.red('Not Inilialized')}
       config.set('telemetry', false);
       console.log(disabledText);
       break;
-    case 'setup':
-      config.delete('telemetry');
-      await setupGlobalTelemetry(config, cwd);
+    case 'enable' || 'enabled':
+      await enableGlobalTelemetry(config, cwd);
       break;
     case '--help':
       console.log(helpText);
@@ -95,54 +93,29 @@ KeystoneJS telemetry: ${chalk.red('Not Inilialized')}
   return;
 }
 
-const deviceConsentText = `
-Welcome to Keystone!
-We'd love some help to see how keystone is being used.
-
-Would you like to send the following information about your developer environment? (only when you use 'keystone dev', at most once daily)
-
-- Last date you used 'keystone dev'
-- Node version
-- Operating System
-
-`;
-
-const projectConsentText = `
-We'd also love to know about your projects too.
-
-Would you like to send the following additional information about your projects? (only when you use 'keystone dev', at most once daily)
-
-- Last date you used 'keystone dev' for this project
-- The versions of any '@keystone-6', and '@opensaas' [subject to change by community contribution] packages that you are using in this project 
-- The number of lists you have
-- The name and number of field types that you are using
-
-`;
-
-async function setupGlobalTelemetry(config: Conf<Configuration>, cwd: string) {
-  const newTelemetry: Configuration['telemetry'] = {
-    device: false,
-    projects: {
-      default: false,
-    },
-  };
-  console.log(deviceConsentText);
-  const deviceContent = await confirmPrompt('Yes (y) / No (n)', true);
-  if (deviceContent) {
-    newTelemetry.device = { informedAt: new Date().toISOString() };
-  }
-  console.log(projectConsentText);
-  const projectContent = await confirmPrompt('Yes (y) / No (n)', true);
-
-  if (projectContent) {
-    newTelemetry.projects.default = { informedAt: new Date().toISOString() };
-    newTelemetry.projects = {
-      default: { informedAt: new Date().toISOString() },
-      [cwd]: { informedAt: new Date().toISOString() },
+async function enableGlobalTelemetry(config: Conf<Configuration>, cwd: string) {
+  let telemetryConfig = config.get('telemetry');
+  if (!telemetryConfig) {
+    telemetryConfig = {
+      device: {
+        informedAt: new Date().toISOString(),
+      },
+      projects: {
+        default: {
+          informedAt: new Date().toISOString(),
+        },
+      },
+    };
+  } else {
+    telemetryConfig.device = {
+      informedAt: new Date().toISOString(),
+    };
+    telemetryConfig.projects.default = {
+      informedAt: new Date().toISOString(),
     };
   }
-  config.set('telemetry', newTelemetry);
+  config.set('telemetry', telemetryConfig);
   console.log(`
-KeystoneJS telemetry: ${chalk.green('Initialized')}
+KeystoneJS telemetry: ${chalk.green('Enabled')}
   `);
 }
