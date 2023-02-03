46ac529c9beca1ffd666f197b060df99db9ea518
Daniel Cousens <dcousens@users.noreply.github.com>
2023-02-03 14:42:58 +1100

    use tsx for seeding data in the blog example

--- examples/blog/keystone.ts
+++ examples/blog/keystone.ts
@@ -1,17 +1,11 @@
 import { config } from '@keystone-6/core';
 import { lists } from './schema';
-import { insertSeedData } from './seed-data';
-import { Context, TypeInfo } from '.keystone/types';
+import { TypeInfo } from '.keystone/types';
 
 export default config<TypeInfo>({
   db: {
     provider: 'sqlite',
     url: process.env.DATABASE_URL || 'file:./keystone-example.db',
-    async onConnect(context: Context) {
-      if (process.argv.includes('--seed-data')) {
-        await insertSeedData(context);
-      }
-    },
   },
   lists,
 });
