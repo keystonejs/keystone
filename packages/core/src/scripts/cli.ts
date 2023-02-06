78b649afb68734317bf7a6a674b6eade493e0cb8
Daniel Cousens <dcousens@users.noreply.github.com>
2023-02-06 16:37:42 +1100

    tidy up cli.ts, prefer early exits, update help copy

--- packages/core/src/scripts/cli.ts
+++ packages/core/src/scripts/cli.ts
@@ -1,14 +1,12 @@
 import meow from 'meow';
-import { dev } from './run/dev';
-import { start } from './run/start';
-import { build } from './build/build';
-import { prisma } from './prisma';
-import { postinstall } from './postinstall';
-import { telemetry } from './telemetry';
 import { ExitError } from './utils';
+import { build } from './build/build';
+import { dev } from './run/dev';
 import { migrate } from './migrate';
-
-const commands = { dev, start, build, prisma, postinstall, telemetry, migrate };
+import { postinstall } from './postinstall';
+import { prisma } from './prisma';
+import { start } from './run/start';
+import { telemetry } from './telemetry';
 
 export async function cli(cwd: string, argv: string[]) {
   const { input, help, flags } = meow(
@@ -17,9 +15,9 @@ export async function cli(cwd: string, argv: string[]) {
       $ keystone [command]
     Commands
         dev           start the project in development mode (default)
-        postinstall   generate client APIs and types (optional)
-        build         build the project (must be done before using start)
-        start         start the project in production mode
+        postinstall   build the project (for development, optional)
+        build         build the project (required by \`keystone start\`)
+        start         start the project
         migrate       setup and run database migrations
         prisma        run Prisma CLI commands safely
         telemetry     sets telemetry preference (enable/disable/status)
@@ -33,28 +31,17 @@ export async function cli(cwd: string, argv: string[]) {
       argv,
     }
   );
+
   const command = input[0] || 'dev';
-  if (!isCommand(command)) {
-    console.log(`${command} is not a command that keystone accepts`);
-    console.log(help);
-    throw new ExitError(1);
-  }
+  if (command === 'dev') return dev(cwd, flags.resetDb, flags.skipDbPush);
+  if (command === 'postinstall') return postinstall(cwd, flags.fix)
+  if (command === 'build') return build(cwd);
+  if (command === 'start') return start(cwd);
+  if (command === 'migrate') return migrate(cwd, input, flags.resetDb);
+  if (command === 'prisma') return prisma(cwd, argv.slice(1));
+  if (command === 'telemetry') return telemetry(cwd, argv[1]);
 
-  if (command === 'prisma') {
-    return prisma(cwd, argv.slice(1));
-  } else if (command === 'postinstall') {
-    return postinstall(cwd, flags.fix);
-  } else if (command === 'dev') {
-    return dev(cwd, flags.resetDb, flags.skipDbPush);
-  } else if (command === 'migrate') {
-    return migrate(cwd, input, flags.resetDb);
-  } else if (command === 'telemetry') {
-    return telemetry(cwd, argv[1]);
-  } else {
-    return commands[command](cwd);
-  }
-}
-
-function isCommand(command: string): command is keyof typeof commands {
-  return command in commands;
+  console.log(`${command} is an unknown command`);
+  console.log(help);
+  throw new ExitError(1);
 }
