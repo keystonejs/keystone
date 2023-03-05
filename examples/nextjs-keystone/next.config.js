ec3440a72a36b850c4799570d00584e1897d37d0
Tim Gates <tim.gates@iress.com>
2023-03-05 12:35:11 +1100

    docs: Fix a few typos
    There are small typos in:
    - docs/pages/docs/guides/auth-and-access-control.md
    - docs/pages/docs/guides/custom-admin-ui-pages.md
    - examples/nextjs-keystone/next.config.js
    - examples/script/README.md
    - packages/core/src/admin-ui/TODO.md
    
    Fixes:
    - Should read `explicitly` rather than `explicity`.
    - Should read `escape` rather than `esacape`.
    - Should read `dynamically` rather than `dyamically`.
    - Should read `alignment` rather than `allignment`.
    - Should read `adhere` rather than `ahdere`.
    
    Signed-off-by: Tim Gates <tim.gates@iress.com>

--- examples/nextjs-keystone/next.config.js
+++ examples/nextjs-keystone/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   /*
       next@13 automatically bundles server code for server components.
       This causes a problem for the prisma binary built by Keystone.
-      We need to explicity ask Next.js to opt-out from bundling
+      We need to explicitly ask Next.js to opt-out from bundling
       dependencies that use native Node.js APIs.
 
       More here: https://beta.nextjs.org/docs/api-reference/next.config.js#servercomponentsexternalpackages
