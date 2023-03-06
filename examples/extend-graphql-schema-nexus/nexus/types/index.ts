ee829f827ecccf2e2db2c99bd5bf70c627f64360
Daniel Cousens <dcousens@users.noreply.github.com>
2023-03-06 19:59:27 +1100

    reduce nexus example down, use existing schema merged

--- examples/extend-graphql-schema-nexus/nexus/types/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Post';
-export * from './Thing';
--- examples/extend-graphql-schema-nexus/schema.ts
+++ examples/extend-graphql-schema-nexus/schema.ts
@@ -1,8 +1,12 @@
+import type { GraphQLSchema } from 'graphql';
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
 import { select, relationship, text, timestamp } from '@keystone-6/core/fields';
+import { mergeSchemas } from '@graphql-tools/schema';
+import * as nexus from 'nexus';
+import type { Context, Lists } from '.keystone/types';
 
-export const lists = {
+export const lists: Lists = {
   Post: list({
     access: allowAll,
     fields: {
@@ -28,3 +32,63 @@ export const lists = {
     },
   }),
 };
+
+export function extendGraphqlSchema(baseSchema: GraphQLSchema) {
+  const NexusPostQuery = nexus.extendType({
+    type: 'Query',
+    definition(t) {
+      t.field('nexusPosts', {
+        type: nexus.nonNull(nexus.list('Post')),
+        args: {
+          authorId: nexus.stringArg(),
+          seconds: nexus.nonNull(nexus.intArg({ default: 600 })),
+        },
+        async resolve(root, { authorId, seconds }, context: Context) {
+          const cutoff = new Date(Date.now() - seconds * 1000);
+
+          return await context.db.Post.findMany({
+            where: {
+              ...(authorId ? { author: { id: authorId } } : null),
+              publishDate: { gt: cutoff },
+            },
+          });
+        },
+      });
+    },
+  });
+
+  const NexusThing = nexus.objectType({
+    name: 'NexusThing',
+    definition(t) {
+      t.int('id');
+      t.string('title');
+    },
+  });
+
+  const NexusThingQuery = nexus.extendType({
+    type: 'Query',
+    definition(t) {
+      t.nonNull.list.field('things', {
+        type: NexusThing,
+        resolve() {
+          return [
+            { id: 1, title: 'Keystone' },
+            { id: 2, title: 'Prisma' },
+            { id: 3, title: 'Nexus' },
+          ];
+        },
+      });
+    },
+  });
+
+  return nexus.makeSchema({
+    mergeSchema: {
+      schema: baseSchema,
+    },
+    types: {
+      NexusThing,
+      NexusPostQuery,
+      NexusThingQuery,
+    },
+  });
+}
