0f99cf19b2eeba6005f18bd1a43d4aae5db667f4
Josh Calder <josh.calder@thinkmill.com.au>
2023-04-13 12:10:08 +1000

    Add example for Next 13 `app` directory and Keystone `getContext` (#8371)
    Co-authored-by: Daniel Cousens <dcousens@users.noreply.github.com>
    Co-authored-by: Daniel Cousens <413395+dcousens@users.noreply.github.com>

--- examples/nextjs-keystone-app-directory/src/keystone/schema.ts
+++ examples/nextjs-keystone-app-directory/src/keystone/schema.ts
@@ -1,6 +1,7 @@
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
 import { text, timestamp } from '@keystone-6/core/fields';
+import { document } from '@keystone-6/fields-document';
 import type { Lists } from '.keystone/types';
 
 export const lists: Lists = {
@@ -13,7 +14,15 @@ export const lists: Lists = {
 
     fields: {
       name: text({ validation: { isRequired: true } }),
-      about: text(),
+      about: document({
+        formatting: true,
+        dividers: true,
+        links: true,
+        layouts: [
+          [1, 1],
+          [1, 1, 1],
+        ],
+      }),
       createdAt: timestamp({
         defaultValue: { kind: 'now' },
       }),
