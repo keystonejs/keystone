dd1b7a75de87c2f9c47547e12a9689a4a5f6d156
Josh Calder <josh.calder@thinkmill.com.au>
2023-04-24 14:19:08 +1000

    Clean up externals

--- examples/framework-astro/astro.config.mjs
+++ examples/framework-astro/astro.config.mjs
@@ -6,24 +6,7 @@ export default defineConfig({
   // WARNING: this is only needed for our monorepo examples, dont do this
   vite: {
     ssr: {
-      external: [
-        '@apollo/cache-control-types',
-        '@aws-crypto/crc32',
-        '@aws-crypto/crc32c',
-        '@sindresorhus/slugify',
-        'bcryptjs',
-        'cuid',
-        'dataloader',
-        'dumb-passwords',
-        'entities',
-        'fast-xml-parser',
-        'filenamify',
-        'fs-extra',
-        'image-size',
-        'inflection',
-        'p-limit',
-        'pluralize',
-      ],
+      external: ['@keystone-6/core/context', '@keystone-6/core', '@keystone-6/core/fields'],
     },
   },
 });
