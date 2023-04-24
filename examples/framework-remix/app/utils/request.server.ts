77fdbaafada21b8c902a27f1bc666f7b50da6f53
Josh Calder <josh.calder@thinkmill.com.au>
2023-04-24 13:27:13 +1000

    format

--- examples/framework-remix/app/utils/request.server.ts
+++ examples/framework-remix/app/utils/request.server.ts
@@ -1,7 +1,7 @@
-import { json } from '@remix-run/node'
+import { json } from '@remix-run/node';
 
 /**
  * This helper function helps us returning the accurate HTTP status,
  * 400 Bad Request, to the client.
  */
-export const badRequest = <T>(data: T) => json<T>(data, { status: 400 })
+export const badRequest = <T>(data: T) => json<T>(data, { status: 400 });
