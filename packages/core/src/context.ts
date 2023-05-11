1dae889cdf61f1698ddec5fcb9d1f64a07c7853c
Daniel Cousens <dcousens@users.noreply.github.com>
2023-05-11 19:41:18 +1000

    flatten getContext

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -1,1 +1,12 @@
-export { getContext } from './lib/getContext';
+import { BaseKeystoneTypeInfo, KeystoneConfig, KeystoneContext } from './types';
+import { initConfig } from './lib/config';
+import { createSystem } from './lib/createSystem';
+
+export function getContext<TypeInfo extends BaseKeystoneTypeInfo>(
+  config: KeystoneConfig<TypeInfo>,
+  PrismaModule: unknown
+): KeystoneContext<TypeInfo> {
+  const system = createSystem(initConfig(config));
+  const { context } = system.getKeystone(PrismaModule as any);
+  return context;
+}
