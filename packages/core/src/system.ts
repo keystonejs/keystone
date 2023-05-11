a828a9fe4067ac511b36a19bd7818cc0f496792d
Daniel Cousens <dcousens@users.noreply.github.com>
2023-05-11 17:55:01 +1000

    restore missing createAdminUIMiddleware, add deprecation changeset
    Co-authored-by: Josh Calder <josh.calder@thinkmill.com.au>

--- packages/core/src/system.ts
+++ packages/core/src/system.ts
@@ -1,4 +1,21 @@
+import express from 'express';
+import next from 'next';
+import type { KeystoneConfig, KeystoneContext } from '../types';
+import { createAdminUIMiddlewareWithNextApp } from './lib/server/createAdminUIMiddleware';
+
 export { createSystem } from './lib/createSystem';
 export { createExpressServer } from './lib/server/createExpressServer';
-export { createAdminUIMiddleware } from './lib/server/createAdminUIMiddleware';
 export { initConfig } from './lib/config';
+
+/** @deprecated */
+export async function createAdminUIMiddleware(
+  config: KeystoneConfig,
+  context: KeystoneContext,
+  dev: boolean,
+  projectAdminPath: string
+  // TODO: return type required by pnpm
+): Promise<(req: express.Request, res: express.Response) => void> {
+  const nextApp = next({ dev, dir: projectAdminPath });
+  await nextApp.prepare();
+  return createAdminUIMiddlewareWithNextApp(config, context, nextApp);
+}
