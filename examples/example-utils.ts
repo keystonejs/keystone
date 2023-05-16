7e3b09f2ed4f81aaa49c48cd976af515f0a18567
Josh Calder <josh.calder@thinkmill.com.au>
2023-05-16 17:22:49 +1000

    fix comment

--- examples/example-utils.ts
+++ examples/example-utils.ts
@@ -2,7 +2,6 @@
 //   to build them and use them without conflict, we need to ensure .prisma/client
 //   resolves to somewhere else
 //
-//import * as Path from 'path';
 //   we use node_modules/.myprisma to differentiate from node_modules/.prisma, but
 //   still use node_modules/... to skip the painful experience that is jest/babel
 //   transforms
