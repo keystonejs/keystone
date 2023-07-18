901fec200a0360eefdd1fea9f40940b1dcff37ae
Daniel Cousens <dcousens@users.noreply.github.com>
2023-07-18 11:52:17 +1000

    add support for developers to replace the default esbuild configuration

--- packages/core/src/lib/esbuild.ts
+++ packages/core/src/lib/esbuild.ts
@@ -1,7 +1,12 @@
 // WARNING: be careful not to import `esbuild` within next
-import type { BuildOptions } from 'esbuild'
+import { type BuildOptions } from 'esbuild'
 
 export function getEsbuildConfig (cwd: string): BuildOptions {
+  try {
+    return require(require.resolve(`${cwd}/esbuild.keystone.js`))
+  } catch (e) {}
+
+  // default fallback
   return {
     entryPoints: ['./keystone'],
     absWorkingDir: cwd,
@@ -17,16 +22,12 @@ export function getEsbuildConfig (cwd: string): BuildOptions {
         setup (build) {
           build.onResolve(
             {
-              // this regex is intended to be the opposite of /^\.\.?(?:\/|$)/
-              // so it matches anything that isn't a relative import
-              // so this means that we're only going to bundle relative imports
-              // we can't use a negative lookahead/lookbehind because this regex is executed
-              // by Go's regex package which doesn't support them
-              // this regex could have less duplication with nested groups but this is probably easier to read
+              // don't bundle anything that is NOT a relative import
+              //   WARNING: we can't use a negative lookahead/lookbehind because esbuild uses Go
               filter: /(?:^[^.])|(?:^\.[^/.])|(?:^\.\.[^/])/,
             },
-            args => {
-              return { external: true, path: args.path }
+            ({ path }) => {
+              return { external: true, path }
             }
           )
         },
