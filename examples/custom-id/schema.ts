7ffba32bb9d7d677dde10e28d13ad8898cb28a7a
Daniel Cousens <dcousens@users.noreply.github.com>
2023-08-08 12:50:26 +1000

    remove redundant fields from custom-id example

--- examples/custom-id/schema.ts
+++ examples/custom-id/schema.ts
@@ -1,7 +1,6 @@
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
-import { checkbox, relationship, text, timestamp } from '@keystone-6/core/fields';
-import { select } from '@keystone-6/core/fields';
+import { relationship, text, timestamp } from '@keystone-6/core/fields';
 import { createId } from '@paralleldrive/cuid2';
 import type { Lists } from '.keystone/types';
 
@@ -17,15 +16,6 @@ export const lists: Lists = {
     },
     fields: {
       label: text({ validation: { isRequired: true } }),
-      priority: select({
-        type: 'enum',
-        options: [
-          { label: 'Low', value: 'low' },
-          { label: 'Medium', value: 'medium' },
-          { label: 'High', value: 'high' },
-        ],
-      }),
-      isComplete: checkbox(),
       assignedTo: relationship({ ref: 'Person.tasks', many: false }),
       finishBy: timestamp(),
     },
