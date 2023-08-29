98ccc48f09be96c3b674682eb1fb186a79722ff4
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-08-30 09:34:46 +1000

    prettier

--- packages/core/src/scripts/index.ts
+++ packages/core/src/scripts/index.ts
@@ -1,10 +1,9 @@
 import { cli } from './cli';
 import { ExitError } from './utils';
 
-cli(process.cwd(), process.argv.slice(2))
-  .catch(err => {
-    if (err instanceof ExitError) return process.exit(err.code);
+cli(process.cwd(), process.argv.slice(2)).catch(err => {
+  if (err instanceof ExitError) return process.exit(err.code);
 
-    console.error(err);
-    process.exit(1);
-  });
+  console.error(err);
+  process.exit(1);
+});
