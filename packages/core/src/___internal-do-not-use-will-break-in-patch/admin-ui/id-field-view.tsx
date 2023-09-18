15fa7e99d105d335fb0b3f0e7541e0d2c5506d7d
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-09-18 13:52:48 +1000

    remove unused idFieldKind

--- packages/core/src/___internal-do-not-use-will-break-in-patch/admin-ui/id-field-view.tsx
+++ packages/core/src/___internal-do-not-use-will-break-in-patch/admin-ui/id-field-view.tsx
@@ -31,13 +31,12 @@ export const CardValue: CardValueComponent = ({ item, field }) => {
 
 export const controller = (
   config: FieldControllerConfig<IdFieldConfig>
-): FieldController<void, string> & { idFieldKind: IdFieldConfig['kind'] } => {
+): FieldController<void, string> => {
   return {
     path: config.path,
     label: config.label,
     description: config.description,
     graphqlSelection: config.path,
-    idFieldKind: config.fieldMeta.kind,
     defaultValue: undefined,
     deserialize: () => {},
     serialize: () => ({}),
