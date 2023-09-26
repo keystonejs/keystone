5da601dbde532f2ebde8bcec3d35d6634e1f56e0
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-09-26 11:56:58 +1000

    prettier

--- examples/reuse/schema.ts
+++ examples/reuse/schema.ts
@@ -34,91 +34,89 @@ const readOnly = {
 // we use this function to show that completed is a boolean type
 //   which would be missing if the types were unrefined
 //   a common problem when re-using code
-function isTrue (b: boolean) {
-  return b === true
+function isTrue(b: boolean) {
+  return b === true;
 }
 
 type ListsT = TypeInfo['lists'];
-type FindListsWithField <K> = {
-  [key in keyof ListsT]: K extends ListsT[key]['fields']
-    ? ListsT[key]
-    : never
-}[keyof ListsT]
+type FindListsWithField<K> = {
+  [key in keyof ListsT]: K extends ListsT[key]['fields'] ? ListsT[key] : never;
+}[keyof ListsT];
 
 // alternatively, if you don't like type functions
 //  type CompatibleLists = Lists.Invoice.TypeInfo | Lists.Order.TypeInfo
-type CompatibleLists = FindListsWithField<'completed'>
+type CompatibleLists = FindListsWithField<'completed'>;
 //  type CompatibleLists = TypeInfo['lists'][keyof TypeInfo['lists']] // item is resolved, but not completed
 //  type CompatibleLists = BaseListTypeInfo // nothing is refined, item is Record<string, unknown>
 
-function trackingByHooks <
-  ListTypeInfo extends CompatibleLists,
-//    FieldKey extends 'createdBy' | 'updatedBy' // TODO: refined types for the return types
-> (immutable: boolean = false): FieldHooks<ListTypeInfo> {
+function trackingByHooks<
+  ListTypeInfo extends CompatibleLists
+  //    FieldKey extends 'createdBy' | 'updatedBy' // TODO: refined types for the return types
+>(immutable: boolean = false): FieldHooks<ListTypeInfo> {
   return {
-    async resolveInput ({ context, operation, resolvedData, item, fieldKey }) {
+    async resolveInput({ context, operation, resolvedData, item, fieldKey }) {
       if (operation === 'update') {
-        if (immutable) return undefined
+        if (immutable) return undefined;
 
         // show we have refined types for compatible item.* fields
-        if (isTrue(item.completed) && resolvedData.completed !== false) return undefined
+        if (isTrue(item.completed) && resolvedData.completed !== false) return undefined;
       }
 
       // TODO: refined types for the return types
       //   FIXME: CommonFieldConfig need not always be generalised
       return `${context.req?.socket.remoteAddress} (${context.req?.headers['user-agent']})` as any;
-    }
-  }
+    },
+  };
 }
 
-function trackingAtHooks <
-  ListTypeInfo extends CompatibleLists,
-//    FieldKey extends 'createdAt' | 'updatedAt' // TODO: refined types for the return types
-> (immutable: boolean = false): FieldHooks<ListTypeInfo> {
+function trackingAtHooks<
+  ListTypeInfo extends CompatibleLists
+  //    FieldKey extends 'createdAt' | 'updatedAt' // TODO: refined types for the return types
+>(immutable: boolean = false): FieldHooks<ListTypeInfo> {
   return {
     // TODO: switch to operation routing when supported for fields
-    async resolveInput ({ context, operation, resolvedData, item, fieldKey }) {
+    async resolveInput({ context, operation, resolvedData, item, fieldKey }) {
       if (operation === 'update') {
-        if (immutable) return undefined
+        if (immutable) return undefined;
 
         // show we have refined types for compatible item.* fields
-        if (isTrue(item.completed) && resolvedData.completed !== false) return undefined
+        if (isTrue(item.completed) && resolvedData.completed !== false) return undefined;
       }
 
       // TODO: refined types for the return types
       //   FIXME: CommonFieldConfig need not always be generalised
-      return new Date() as any
-    }
-  }
+      return new Date() as any;
+    },
+  };
 }
 
-function trackingFields <ListTypeInfo extends CompatibleLists> () {
+function trackingFields<ListTypeInfo extends CompatibleLists>() {
   return {
     createdBy: text<ListTypeInfo>({
       ...readOnly,
       hooks: {
         ...trackingByHooks<ListTypeInfo>(true),
-      }
+      },
     }),
     createdAt: timestamp<ListTypeInfo>({
       ...readOnly,
       hooks: {
         ...trackingAtHooks<ListTypeInfo>(true),
-      }
+      },
     }),
     updatedBy: text<ListTypeInfo>({
       ...readOnly,
       hooks: {
         ...trackingByHooks<ListTypeInfo>(),
-      }
+      },
     }),
     updatedAt: timestamp<ListTypeInfo>({
       ...readOnly,
       hooks: {
         ...trackingAtHooks<ListTypeInfo>(),
-      }
+      },
     }),
-  }
+  };
 }
 
 export const lists: Lists = {
@@ -127,7 +125,7 @@ export const lists: Lists = {
     fields: {
       title: text(),
       completed: checkbox(),
-      ...trackingFields()
+      ...trackingFields(),
     },
   }),
 
@@ -136,14 +134,14 @@ export const lists: Lists = {
     fields: {
       title: text(),
       completed: checkbox(),
-      ...trackingFields()
+      ...trackingFields(),
     },
   }),
 
   User: list({
     access: allowAll,
     fields: {
-      name: text()
+      name: text(),
     },
   }),
 };
