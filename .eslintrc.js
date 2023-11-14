fc7f167cc85a1f9f8062ff96589e830ea37ae83c
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-11-14 17:09:36 +1100

    add eslintrc (empty)

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  overrides: [{
+    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+  }],
+  rules: {
+  }
+}
