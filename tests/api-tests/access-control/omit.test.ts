7b647c199615ad4ddc5cf583b04443f2d0c5dc96
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2024-02-19 14:21:20 +1100

    prefer declarative style

--- tests/api-tests/access-control/omit.test.ts
+++ tests/api-tests/access-control/omit.test.ts
@@ -1,11 +1,51 @@
 import { setupTestRunner } from '@keystone-6/api-tests/test-runner'
 import { relationship, text } from '@keystone-6/core/fields'
-import { list, type ListSchemaConfig } from '@keystone-6/core'
+import { list } from '@keystone-6/core'
 import { allowAll } from '@keystone-6/core/access'
 import { dbProvider } from '../utils'
 
-type ListConfig = {
-  name: string
+function yesNo (x: boolean | undefined) {
+  if (x === true) return 'T'
+  if (x === false) return 'F'
+  return 'U'
+}
+
+function makeFieldEntry ({
+  isFilterable,
+  isOrderable,
+  omit
+}: {
+  isFilterable?: boolean
+  isOrderable?: boolean
+  omit?:
+    | true
+    | {
+        read?: boolean
+        create?: boolean
+        update?: boolean
+      }
+}) {
+  const suffix = [
+    `Filterable${yesNo(isFilterable)}`,
+    `Orderable${yesNo(isOrderable)}`,
+    `Omit${
+      omit === undefined ? 'U'
+    : omit === true ? 'T'
+    : [omit.read, omit.create, omit.update].map(yesNo).join('')}`
+  ].join('')
+
+  return [`Field_${suffix}`, text({
+    graphql: { omit },
+    isFilterable,
+    isOrderable,
+  })] as const
+}
+
+function makeList ({
+  isFilterable,
+  isOrderable,
+  omit
+}: {
   isFilterable?: boolean
   isOrderable?: boolean
   omit?:
@@ -16,143 +56,111 @@ type ListConfig = {
         update?: boolean
         delete?: boolean
       }
+}, fieldsMatrix: ReturnType<typeof makeFieldEntry>[]) {
+  const prefix = `List_Filterable${yesNo(isFilterable)}_Orderable${yesNo(isOrderable)}` as const
+  const name =
+    omit === undefined
+      ? `${prefix}_OmitU`
+      : omit === true
+        ? `${prefix}_OmitT`
+        : `${prefix}_Omit${[omit.query, omit.create, omit.update, omit.delete].map(yesNo).join('')}`
+
+  return {
+    name,
+    access: allowAll,
+    fields: {
+      name: text(),
+      ...Object.fromEntries(fieldsMatrix)
+    },
+    defaultIsFilterable: isFilterable,
+    defaultIsOrderable: isOrderable,
+    graphql: {
+      plural: name + 's',
+      omit,
+    },
+  } as const
 }
 
-type FieldConfig = {
-  isFilterable?: boolean
-  isOrderable?: boolean
-  omit?:
-    | true
-    | {
-        read?: boolean
-        create?: boolean
-        update?: boolean
+const listsMatrix = [...function* () {
+  const fieldsMatrix = [...function* () {
+    for (const isFilterable of [undefined, false]) {
+      for (const isOrderable of [undefined, false]) {
+        yield makeFieldEntry({ isFilterable, isOrderable, omit: undefined })
+        yield makeFieldEntry({ isFilterable, isOrderable, omit: true })
+
+        for (const read of [undefined, true]) {
+          for (const create of [undefined, true]) {
+            for (const update of [undefined, true]) {
+              yield makeFieldEntry({
+                isFilterable,
+                isOrderable,
+                omit: {
+                  read,
+                  create,
+                  update,
+                },
+              })
+            }
+          }
+        }
       }
-}
+    }
+  }()]
 
-function yesNo (x: boolean | undefined) {
-  if (x === true) return 'T'
-  if (x === false) return 'F'
-  return 'U'
-}
+  for (const isFilterable of [undefined, false]) {
+    for (const isOrderable of [undefined, false]) {
+      yield makeList({ isFilterable, isOrderable, omit: undefined }, fieldsMatrix)
+      yield makeList({ isFilterable, isOrderable, omit: true }, fieldsMatrix)
 
-function join (o: object) {
-  return Object.entries(o)
-    .map(([k, v]) => `${k}${v}`)
-    .join('_')
-}
-
-function getFieldPrefix ({ isFilterable, isOrderable, omit }: FieldConfig) {
-  return join({
-    Filterable: yesNo(isFilterable),
-    Orderable: yesNo(isOrderable),
-    Omit:
-      omit === undefined ? 'U'
-    : omit === true ? 'T'
-    : [omit.read, omit.create, omit.update].map(yesNo).join('')
-  })
-}
-
-const listMatrix: ListConfig[] = []
-for (const isFilterable of [undefined, false]) {
-  for (const isOrderable of [undefined, false]) {
-    const prefix = `List_Filterable${yesNo(isFilterable)}_Orderable${yesNo(isOrderable)}`
-    listMatrix.push({ name: `${prefix}_OmitU`, isFilterable, isOrderable, omit: undefined })
-    listMatrix.push({ name: `${prefix}_OmitT`, isFilterable, isOrderable, omit: true })
-
-    for (const query of [undefined, true]) {
-      for (const create of [undefined, true]) {
-        for (const update of [undefined, true]) {
-          for (const delete_ of [undefined, true]) {
-            const suffix = [query, create, update, delete_].map(yesNo).join('')
-            listMatrix.push({
-              name: `${prefix}_Omit${suffix}`,
-              isFilterable,
-              isOrderable,
-              omit: {
-                query,
-                create,
-                update,
-                delete: delete_,
-              }
-            })
+      for (const query of [undefined, true]) {
+        for (const create of [undefined, true]) {
+          for (const update of [undefined, true]) {
+            for (const delete_ of [undefined, true]) {
+              yield makeList({
+                isFilterable,
+                isOrderable,
+                omit: {
+                  query,
+                  create,
+                  update,
+                  delete: delete_,
+                }
+              }, fieldsMatrix)
+            }
           }
         }
       }
     }
   }
-}
+}()]
 
-const fieldMatrix: FieldConfig[] = []
-for (const isFilterable of [undefined, false]) {
-  for (const isOrderable of [undefined, false]) {
-    fieldMatrix.push({ isFilterable, isOrderable, omit: undefined })
-    fieldMatrix.push({ isFilterable, isOrderable, omit: true })
-
-    for (const read of [undefined, true]) {
-      for (const create of [undefined, true]) {
-        for (const update of [undefined, true]) {
-          fieldMatrix.push({
-            isFilterable,
-            isOrderable,
-            omit: {
-              read,
-              create,
-              update,
-            },
-          })
-        }
-      }
-    }
-  }
-}
-
-const lists: ListSchemaConfig = {}
-
-for (const listConfig of listMatrix) {
-  lists[listConfig.name] = list({
+// TODO: FIXME: skip for now, MySQL has a limit on the number of indexes
+if (dbProvider !== 'mysql') {
+  listsMatrix.push({
+    name: 'RelatedToAll',
     access: allowAll,
     fields: {
       name: text(),
       ...Object.fromEntries(function* () {
-        for (const fieldConfig of fieldMatrix) {
-          yield [`Field_${getFieldPrefix(fieldConfig)}`, text({
-            graphql: { omit: fieldConfig.omit },
-            isFilterable: fieldConfig.isFilterable,
-            isOrderable: fieldConfig.isOrderable,
-          })]
+        for (const l of listsMatrix) {
+          // WARNING: if names exceed some length, expect duplicate _AB_unique index errors
+          yield [`R${l.name}_one`, relationship({
+            ref: l.name,
+            many: false,
+          })] as const
+
+          yield [`R${l.name}_many`, relationship({
+            ref: l.name,
+            many: true,
+          })] as const
         }
-      }())
+      }()),
     },
-    defaultIsFilterable: listConfig.isFilterable,
-    defaultIsOrderable: listConfig.isOrderable,
-    graphql: {
-      plural: listConfig.name + 's',
-      omit: listConfig.omit,
-    },
-  })
-}
-
-// TODO: FIXME: skip for now, MySQL has a limit on the number of indexes
-if (dbProvider !== 'mysql') {
-  lists.RelatedToAll = list({
-    access: allowAll,
-    fields: Object.fromEntries(function* () {
-      for (const listConfig of listMatrix) {
-        // WARNING: if names exceed some length, expect duplicate _AB_unique index errors
-        yield [`R${listConfig.name}_one`, relationship({
-          ref: listConfig.name,
-          many: false,
-        })]
-
-        yield [`R${listConfig.name}_many`, relationship({
-          ref: listConfig.name,
-          many: true,
-        })]
-      }
-    }()),
+    defaultIsFilterable: undefined,
+    defaultIsOrderable: undefined,
     graphql: {
       plural: 'RelatedToAlls',
+      omit: undefined
     },
   })
 }
@@ -213,28 +221,22 @@ type IntrospectionResult = {
   }
 }
 
-function dropPostgresThings (data: any) {
-  data.__schema.types = data.__schema.types.filter((x: any) => x.name !== 'QueryMode')
-  for (const x of data.__schema.types) {
-    x.inputFields = x.inputFields?.filter((x: any) => x.name !== 'mode')
-  }
-}
-
 describe(`Omit (${dbProvider})`, () => {
-  const config = { lists }
+  const config = {
+    lists: Object.fromEntries(listsMatrix.map(({ name, ...l }) => [name, list(l)]))
+  }
 
   test('Common Schema', setupTestRunner({ config })(async ({ context }) => {
     const data = await context.graphql.run<IntrospectionResult, any>({ query: introspectionQuery })
-    dropPostgresThings(data)
 
     const schemaTypes = data.__schema.types.map(x => x.name.toLowerCase())
     const adminMetaLists = data.keystone.adminMeta.lists.map(x => x.key.toLowerCase())
     const queries = data.__schema.queryType.fields.map(x => x.name.toLowerCase())
     const mutations = data.__schema.mutationType.fields.map(x => x.name.toLowerCase())
 
-    for (const l of listMatrix) {
+    for (const l of listsMatrix) {
       const name = l.name.toLowerCase()
-      if (l.omit === true) {
+      if (l.graphql.omit === true) {
         expect(schemaTypes).not.toContain(name)
         expect(adminMetaLists).not.toContain(name)
         expect(mutations).not.toContain(`create${name}`)
@@ -245,7 +247,7 @@ describe(`Omit (${dbProvider})`, () => {
 
       expect(schemaTypes).toContain(name)
 
-      if (l.omit?.query) {
+      if (l.graphql.omit?.query) {
         expect(adminMetaLists).not.toContain(name) // TODO: see create-admin-meta.ts#L102
         expect(queries).not.toContain(name)
         expect(queries).not.toContain(name + 's')
@@ -255,19 +257,19 @@ describe(`Omit (${dbProvider})`, () => {
         expect(queries).toContain(name + 's')
       }
 
-      if (l.omit?.create) {
+      if (l.graphql.omit?.create) {
         expect(mutations).not.toContain(`create${name}`)
       } else {
         expect(mutations).toContain(`create${name}`)
       }
 
-      if (l.omit?.update) {
+      if (l.graphql.omit?.update) {
         expect(mutations).not.toContain(`update${name}`)
       } else {
         expect(mutations).toContain(`update${name}`)
       }
 
-      if (l.omit?.delete) {
+      if (l.graphql.omit?.delete) {
         expect(mutations).not.toContain(`delete${name}`)
       } else {
         expect(mutations).toContain(`delete${name}`)
@@ -277,14 +279,13 @@ describe(`Omit (${dbProvider})`, () => {
 
   test('Sudo Schema', setupTestRunner({ config })(async ({ context }) => {
     const data = await context.sudo().graphql.run<IntrospectionResult, any>({ query: introspectionQuery })
-    dropPostgresThings(data)
 
     const schemaTypes = data.__schema.types.map(x => x.name.toLowerCase())
     const amLists = data.keystone.adminMeta.lists.map(x => x.key.toLowerCase())
     const queries = data.__schema.queryType.fields.map(x => x.name.toLowerCase())
     const mutations = data.__schema.mutationType.fields.map(x => x.name.toLowerCase())
 
-    for (const l of listMatrix) {
+    for (const l of listsMatrix) {
       const name = l.name.toLowerCase()
 
       // everything is in the sudo schema, without refrain
