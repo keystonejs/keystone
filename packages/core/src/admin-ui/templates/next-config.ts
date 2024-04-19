d90dcba479b97b17cb8b2ab70bb8883740b21954
Isaac Jessup <ibjessup@gmail.com>
2024-04-19 16:14:19 +0000

    custom next.config.js support

--- packages/core/src/admin-ui/templates/next-config.ts
+++ packages/core/src/admin-ui/templates/next-config.ts
@@ -1,15 +1,32 @@
 export const nextConfigTemplate = (basePath?: string) =>
-  `const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    // We use transpilePackages for the custom admin-ui pages in the ./admin folder
-    // as they import ts files into nextjs
-    transpilePackages: ['../../admin'],
-    ${basePath ? `basePath: '${basePath}',` : ''} 
+  `const fs = require('fs');
+const path = require('path');
+const { merge } = require('lodash');
+
+// Base configuration for Next.js
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['../../admin'], // custom admin-ui pages
+  ${basePath ? `basePath: '${basePath}',` : ""} 
+};
+
+// Path to the potential custom config file
+const customConfigPath = path.join(__dirname, '..', '..', 'next.config.js');
+
+// Check if the custom config file exists and merge it
+try {
+  if (fs.existsSync(customConfigPath)) {
+    const customConfig = require(customConfigPath);
+    merge(nextConfig, customConfig);
   }
-  
-  module.exports = nextConfig`
+} catch (error) {
+  console.error('Failed to load or merge custom config:', error);
+}
+
+module.exports = nextConfig;
+`;
