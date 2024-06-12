0873c2cb7eaba2b2b2c11372e5ab4ef07d7778ee
Diego Carvalho <dcarvalho@engagedmd.com>
2024-06-12 10:31:32 -0300

    fix: build admin ui around import ESM modules

--- packages/core/src/admin-ui/templates/next-config.ts
+++ packages/core/src/admin-ui/templates/next-config.ts
@@ -1,5 +1,9 @@
 export const nextConfigTemplate = (basePath?: string) =>
   `const nextConfig = {
+    // Experimental ESM Externals
+    // https://nextjs.org/docs/messages/import-esm-externals
+    // required to fix build admin ui issues related to "react-day-picker" and "date-fn"
+    experimental: { esmExternals: 'loose' },
     typescript: {
       ignoreBuildErrors: true,
     },
