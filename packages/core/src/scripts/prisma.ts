8959f1b7938cc2705d5271d90f7bc749d6847817
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2024-06-24 13:27:22 +1000

    breakout spawnPrisma3 in scripts/prisma

--- packages/core/src/scripts/prisma.ts
+++ packages/core/src/scripts/prisma.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises'
 import { spawn } from 'node:child_process'
+
 import {
   createSystem,
   getBuiltKeystoneConfigurationPath,
@@ -9,6 +10,30 @@ import {
   validateArtifacts,
 } from '../artifacts'
 import { ExitError } from './utils'
+
+async function spawnPrisma3 (cwd: string, system: {
+  config: {
+    db: {
+      url: string
+    }
+  }
+}, commands: string[]) {
+  return new Promise<{
+    exitCode: number | null
+  }>((resolve, reject) => {
+    const p = spawn('node', [require.resolve('prisma'), ...commands], {
+      cwd,
+      env: {
+        ...process.env,
+        DATABASE_URL: system.config.db.url,
+        PRISMA_HIDE_UPDATE_MESSAGE: '1',
+      },
+      stdio: 'inherit'
+    })
+    p.on('error', err => reject(err))
+    p.on('exit', exitCode => (resolve({ exitCode })))
+  })
+}
 
 export async function prisma (cwd: string, args: string[], frozen: boolean) {
   // TODO: this cannot be changed for now, circular dependency with getSystemPaths, getEsbuildConfig
@@ -27,20 +52,5 @@ export async function prisma (cwd: string, args: string[], frozen: boolean) {
   await validateArtifacts(cwd, system)
   console.log('✨ GraphQL and Prisma schemas are up to date')
 
-  return new Promise<void>((resolve, reject) => {
-    const p = spawn('node', [require.resolve('prisma'), ...args], {
-      cwd,
-      env: {
-        ...process.env,
-        DATABASE_URL: system.config.db.url,
-        PRISMA_HIDE_UPDATE_MESSAGE: '1',
-      },
-      stdio: 'inherit',
-    })
-    p.on('error', err => reject(err))
-    p.on('exit', code => {
-      if (code) return reject(new ExitError(Number(code)))
-      resolve()
-    })
-  })
+  await spawnPrisma3(cwd, system, args)
 }
