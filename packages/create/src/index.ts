ce7a60389a4683177f3a1f29e17183a305ddd470
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2024-07-18 15:12:41 +1000

    inline src/ files to index.ts

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,39 +1,65 @@
-import fs from 'fs-extra';
-import path from 'path';
-import meow from 'meow';
-import enquirer from 'enquirer';
-import execa from 'execa';
-import ora from 'ora';
-import c from 'chalk';
-import terminalLink from 'terminal-link';
-import { checkVersion } from './checkVersion';
-import { UserError } from './utils';
-import { fileURLToPath } from 'url';
+import path from 'path'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const starterDir = path.normalize(`${__dirname}/../starter`);
+import fs from 'fs-extra'
+import meow from 'meow'
+import enquirer from 'enquirer'
+import execa from 'execa'
+import ora from 'ora'
+import c from 'chalk'
+import terminalLink from 'terminal-link'
+import getPackageJson from 'package-json';
+import { fileURLToPath } from 'url'
+import * as semver from 'semver';
+
+import currentPkgJson from '../package.json';
+
+async function checkVersion() {
+  try {
+    const { version } = await getPackageJson('create-keystone-app');
+    if (typeof version !== 'string') {
+      throw new Error(
+        'version from package metadata was expected to be a string but was not'
+      );
+    }
+    if (semver.lt(currentPkgJson.version, version)) {
+      console.error(
+        `⚠️  You're running an old version of create-keystone-app, please update to ${version}`
+      );
+    }
+  } catch (err) {
+    console.error(
+      'A problem occurred fetching the latest version of create-keystone-app'
+    );
+    console.error(err);
+  }
+}
+
+class UserError extends Error {}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const starterDir = path.normalize(`${__dirname}/../starter`)
 
 const cli = meow(
   `
 Usage
   $ create-keystone-app [directory]
 `
-);
+)
 
 type Args = {
-  directory: string;
-};
+  directory: string
+}
 
 const versionInfo = () => {
-  process.stdout.write('\n');
+  process.stdout.write('\n')
   console.log(`✨ You're about to generate a project using ${c.bold(
     'Keystone 6'
   )} packages.
-`);
-};
+`)
+}
 
 async function normalizeArgs(): Promise<Args> {
-  let directory = cli.input[0];
+  let directory = cli.input[0]
   if (!directory) {
     ({ directory } = await enquirer.prompt({
       type: 'input',
@@ -41,34 +67,34 @@ async function normalizeArgs(): Promise<Args> {
       message:
         'What directory should create-keystone-app generate your app into?',
       validate: (x) => !!x,
-    }));
-    process.stdout.write('\n');
+    }))
+    process.stdout.write('\n')
   }
   return {
     directory: path.resolve(directory),
-  };
+  }
 }
 
 async function installDeps (cwd: string) {
   const pkgManager = (process.env.npm_config_user_agent ?? 'npm').split('/').shift()
   const spinner = ora(
     `Installing dependencies with ${pkgManager}. This may take a few minutes.`
-  ).start();
+  ).start()
   try {
-    await execa(pkgManager, ['install'], { cwd });
-    spinner.succeed(`Installed dependencies with ${pkgManager}.`);
-    return pkgManager;
+    await execa(pkgManager, ['install'], { cwd })
+    spinner.succeed(`Installed dependencies with ${pkgManager}.`)
+    return pkgManager
   } catch (err) {
-    spinner.fail(`Failed to install with ${pkgManager}.`);
-    throw err;
+    spinner.fail(`Failed to install with ${pkgManager}.`)
+    throw err
   }
 }
 
 (async () => {
-  versionInfo();
-  await checkVersion();
-  const normalizedArgs = await normalizeArgs();
-  await fs.mkdir(normalizedArgs.directory);
+  versionInfo()
+  await checkVersion()
+  const normalizedArgs = await normalizeArgs()
+  await fs.mkdir(normalizedArgs.directory)
   await Promise.all([
     ...[
       '_gitignore',
@@ -86,13 +112,13 @@ async function installDeps (cwd: string) {
         path.join(normalizedArgs.directory, filename.replace(/^_/, '.'))
       )
     ),
-  ]);
-  const packageManager = await installDeps(normalizedArgs.directory);
+  ])
+  const packageManager = await installDeps(normalizedArgs.directory)
   const relativeProjectDir = path.relative(
     process.cwd(),
     normalizedArgs.directory
-  );
-  process.stdout.write('\n');
+  )
+  process.stdout.write('\n')
   console.log(`🎉  Keystone created a starter project in: ${c.bold(
     relativeProjectDir
   )}
@@ -117,12 +143,12 @@ async function installDeps (cwd: string) {
     'Star Keystone on GitHub',
     'https://github.com/keystonejs/keystone'
   )}
-`);
+`)
 })().catch((err) => {
   if (err instanceof UserError) {
-    console.error(err.message);
+    console.error(err.message)
   } else {
-    console.error(err);
+    console.error(err)
   }
-  process.exit(1);
-});
+  process.exit(1)
+})
