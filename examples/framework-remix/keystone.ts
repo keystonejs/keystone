40e85b75615e5dd6837e3265d99c3b228387b5f1
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2024-08-20 17:09:10 +1000

    Update CodeSandbox versions, and fix prisma client paths for `examples/` (continuation of #9300) (#9302)

--- examples/framework-remix/keystone.ts
+++ examples/framework-remix/keystone.ts
@@ -12,7 +12,6 @@ export default config<TypeInfo>({
     url: `file:${process.cwd()}/keystone.db`,
 
     // WARNING: this is only needed for our monorepo examples, dont do this
-    //   we use using myprisma, not .myprisma, because vite
     prismaClientPath: 'node_modules/myprisma',
   },
   server: {
