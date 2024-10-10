e6c1f9956a1484c7a9521ad8bb76d00a6396614e
marekryb <60657309+marekryb@users.noreply.github.com>
2024-10-11 00:43:24 +0900

    revert indent

--- packages/core/src/scripts/utils.ts
+++ packages/core/src/scripts/utils.ts
@@ -3,14 +3,14 @@ import fs from 'node:fs/promises'
 
 export class ExitError extends Error {
   code: number
-  constructor(code: number) {
+  constructor (code: number) {
     super(`The process exited with Error ${code}`)
     this.code = code
   }
 }
 
 // TODO: this cannot be changed for now, circular dependency with getSystemPaths, getEsbuildConfig
-export async function importBuiltKeystoneConfiguration(cwd: string) {
+export async function importBuiltKeystoneConfiguration (cwd: string) {
   try {
     const builtConfigPath = getBuiltKeystoneConfigurationPath(cwd)
     if (!(await fs.stat(builtConfigPath).catch(() => null))) {
