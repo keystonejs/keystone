ea185d3f1c829703189dd3839a811a535e4736de
Daniel Cousens <dcousens@users.noreply.github.com>
2024-12-10 16:32:03 +1100

    fix structure relationship fields

--- packages/core/src/admin-ui/utils/useAdminMeta.tsx
+++ packages/core/src/admin-ui/utils/useAdminMeta.tsx
@@ -1,45 +1,47 @@
-import { useEffect, useMemo, useState } from "react";
-import hashString from "@emotion/hash";
-import { type AdminMeta, type FieldViews } from "../../types";
-import { useLazyQuery } from "../apollo";
+import {
+  useEffect,
+  useMemo,
+  useState
+} from "react"
+import hashString from "@emotion/hash"
+import type { AdminMeta, FieldViews } from "../../types"
+import { useLazyQuery } from "../apollo"
 import {
   type StaticAdminMetaQuery,
   staticAdminMetaQuery,
-} from "../admin-meta-graphql";
+} from "../admin-meta-graphql"
 
-const expectedExports = new Set(["Field", "controller"]);
-const adminMetaLocalStorageKey = "keystone.adminMeta";
+const expectedExports = new Set(["Field", "controller"])
+const adminMetaLocalStorageKey = "keystone.adminMeta"
 
-let _mustRenderServerResult = true;
+let _mustRenderServerResult = true
 
 function useMustRenderServerResult() {
-  const [, forceUpdate] = useState(0);
+  const [, forceUpdate] = useState(0)
   useEffect(() => {
-    _mustRenderServerResult = false;
-    forceUpdate(1);
-  }, []);
+    _mustRenderServerResult = false
+    forceUpdate(1)
+  }, [])
 
-  if (typeof window === "undefined") return true;
+  if (typeof window === "undefined") return true
 
-  return _mustRenderServerResult;
+  return _mustRenderServerResult
 }
 
 export function useAdminMeta(adminMetaHash: string, fieldViews: FieldViews) {
   const adminMetaFromLocalStorage = useMemo(() => {
-    if (typeof window === "undefined") return;
+    if (typeof window === "undefined") return
 
-    const item = localStorage.getItem(adminMetaLocalStorageKey);
-    if (item === null) return;
+    const item = localStorage.getItem(adminMetaLocalStorageKey)
+    if (item === null) return
 
     try {
-      const parsed = JSON.parse(item);
-      if (parsed.hash === adminMetaHash) {
-        return parsed.meta as StaticAdminMetaQuery["keystone"]["adminMeta"];
-      }
+      const parsed = JSON.parse(item)
+      if (parsed.hash === adminMetaHash) return parsed.meta as StaticAdminMetaQuery["keystone"]["adminMeta"]
     } catch (err) {
-      return;
+      return
     }
-  }, [adminMetaHash]);
+  }, [adminMetaHash])
 
   // it seems like Apollo doesn't skip the first fetch when using skip: true so we're using useLazyQuery instead
   const [fetchStaticAdminMeta, { data, error, called }] = useLazyQuery(
@@ -47,64 +49,55 @@ export function useAdminMeta(adminMetaHash: string, fieldViews: FieldViews) {
     {
       fetchPolicy: "no-cache", // TODO: something is bugged
     }
-  );
+  )
 
   const shouldFetchAdminMeta =
-    adminMetaFromLocalStorage === undefined && !called;
+    adminMetaFromLocalStorage === undefined && !called
 
   useEffect(() => {
-    if (shouldFetchAdminMeta) {
-      fetchStaticAdminMeta();
-    }
-  }, [shouldFetchAdminMeta, fetchStaticAdminMeta]);
+    if (shouldFetchAdminMeta) fetchStaticAdminMeta()
+  }, [shouldFetchAdminMeta, fetchStaticAdminMeta])
 
   const runtimeAdminMeta = useMemo(() => {
-    if ((!data || error) && !adminMetaFromLocalStorage) return undefined;
+    if ((!data || error) && !adminMetaFromLocalStorage) return undefined
 
     const adminMeta: StaticAdminMetaQuery["keystone"]["adminMeta"] =
       adminMetaFromLocalStorage
         ? adminMetaFromLocalStorage
-        : data.keystone.adminMeta;
+        : data.keystone.adminMeta
 
     const runtimeAdminMeta: AdminMeta = {
       lists: {},
-    };
+    }
 
     for (const list of adminMeta.lists) {
       runtimeAdminMeta.lists[list.key] = {
         ...list,
         groups: [],
         fields: {},
-      };
+      }
 
       for (const field of list.fields) {
         for (const exportName of expectedExports) {
           if ((fieldViews[field.viewsIndex] as any)[exportName] === undefined) {
             throw new Error(
               `The view for the field at ${list.key}.${field.path} is missing the ${exportName} export`
-            );
+            )
           }
         }
 
-        const views = { ...fieldViews[field.viewsIndex] };
-        const customViews: Record<string, any> = {};
+        const views = { ...fieldViews[field.viewsIndex] }
+        const customViews: Record<string, any> = {}
         if (field.customViewsIndex !== null) {
-          const customViewsSource: FieldViews[number] & Record<string, any> =
-            fieldViews[field.customViewsIndex];
-          const allowedExportsOnCustomViews = new Set(
-            views.allowedExportsOnCustomViews
-          );
-          Object.keys(customViewsSource).forEach((exportName) => {
+          const customViewsSource: FieldViews[number] & Record<string, any> = fieldViews[field.customViewsIndex]
+          const allowedExportsOnCustomViews = new Set(views.allowedExportsOnCustomViews)
+          for (const exportName in customViewsSource) {
             if (allowedExportsOnCustomViews.has(exportName)) {
-              customViews[exportName] = customViewsSource[exportName];
+              customViews[exportName] = customViewsSource[exportName]
             } else if (expectedExports.has(exportName)) {
-              (views as any)[exportName] = customViewsSource[exportName];
-            } else {
-              throw new Error(
-                `Unexpected export named ${exportName} from the custom view from field at ${list.key}.${field.path}`
-              );
+              (views as any)[exportName] = customViewsSource[exportName]
             }
-          });
+          }
         }
 
         runtimeAdminMeta.lists[list.key].fields[field.path] = {
@@ -121,7 +114,7 @@ export function useAdminMeta(adminMetaHash: string, fieldViews: FieldViews) {
             path: field.path,
             customViews,
           }),
-        };
+        }
       }
 
       for (const group of list.groups) {
@@ -131,7 +124,7 @@ export function useAdminMeta(adminMetaHash: string, fieldViews: FieldViews) {
           fields: group.fields.map(
             (field) => runtimeAdminMeta.lists[list.key].fields[field.path]
           ),
-        });
+        })
       }
     }
 
@@ -142,28 +135,24 @@ export function useAdminMeta(adminMetaHash: string, fieldViews: FieldViews) {
           hash: hashString(JSON.stringify(adminMeta)),
           meta: adminMeta,
         })
-      );
+      )
     }
 
-    return runtimeAdminMeta;
-  }, [data, error, adminMetaFromLocalStorage, fieldViews]);
+    return runtimeAdminMeta
+  }, [data, error, adminMetaFromLocalStorage, fieldViews])
 
-  const mustRenderServerResult = useMustRenderServerResult();
+  const mustRenderServerResult = useMustRenderServerResult()
 
-  if (mustRenderServerResult) {
-    return { state: "loading" as const };
-  }
-  if (runtimeAdminMeta) {
-    return { state: "loaded" as const, value: runtimeAdminMeta };
-  }
+  if (mustRenderServerResult) return { state: "loading" as const }
+  if (runtimeAdminMeta) return { state: "loaded" as const, value: runtimeAdminMeta }
   if (error) {
     return {
       state: "error" as const,
       error,
       refetch: async () => {
-        await fetchStaticAdminMeta();
+        await fetchStaticAdminMeta()
       },
-    };
+    }
   }
-  return { state: "loading" as const };
+  return { state: "loading" as const }
 }
