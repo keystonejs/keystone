a588c5bde4467086147bd81392c5f42d038d961e
Emma Hamilton <git@emmas.town>
2025-02-12 16:44:07 +1000

    Fix failing to read dev-loading.html file (#9472)

--- packages/core/src/pkg-dir.ts
+++ packages/core/src/pkg-dir.ts
@@ -1,3 +1,4 @@
 import path from 'path'
+import { createRequire } from 'node:module'
 
-export const pkgDir = path.dirname(__dirname)
+export const pkgDir = path.dirname(createRequire(__dirname).resolve('@keystone-6/core/package.json'))
