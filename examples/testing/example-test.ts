691fbf59f0dd2def77ac38ea22703ec6e37e68e8
Daniel Cousens <dcousens@users.noreply.github.com>
2025-04-30 14:10:35 +1000

    tidy up

--- examples/testing/example-test.ts
+++ examples/testing/example-test.ts
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import { test, describe, beforeEach } from 'node:test'
+import { test, beforeEach } from 'node:test'
 import assert from 'node:assert/strict'
 
 import { resetDatabase } from '@keystone-6/core/testing'
