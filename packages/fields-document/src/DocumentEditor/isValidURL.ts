d3d5e9834670a80e47c778238e550ccefb6c2a89
Emma Hamilton <git@emmas.town>
2025-05-21 16:23:22 +1000

    Update isValidURL.ts

--- packages/fields-document/src/DocumentEditor/isValidURL.ts
+++ packages/fields-document/src/DocumentEditor/isValidURL.ts
@@ -1,5 +1,5 @@
 import { sanitizeUrl } from '@braintree/sanitize-url'
 
 export function isValidURL(url: string) {
-  return url === sanitizeUrl(url) || url === new URL(sanitizeUrl(url)).toString()
+  return url === sanitizeUrl(url) || new URL(url, 'https://example') === new URL(sanitizeUrl(url), 'https://example').toString()
 }
