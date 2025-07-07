8fccd2fb74573d7ea386793dc0f1cb60207febdf
Daniel Cousens <dcousens@users.noreply.github.com>
2025-07-07 13:54:25 +1000

    update logging example with pino

--- examples/logging/keystone.ts
+++ examples/logging/keystone.ts
@@ -1,6 +1,15 @@
 import { config } from '@keystone-6/core'
-import { lists } from './schema'
+import { createHash, randomBytes } from 'node:crypto'
+import pino_ from 'pino-http'
+
 import type { TypeInfo } from '.keystone/types'
+import { lists } from './schema'
+
+function sha256 (q: string) {
+  return createHash('sha256').update(q).digest('hex')
+}
+
+const pino = pino_()
 
 export default config<TypeInfo>({
   db: {
@@ -10,22 +19,43 @@ export default config<TypeInfo>({
     // WARNING: this is only needed for our monorepo examples, dont do this
     prismaClientPath: 'node_modules/myprisma',
   },
+  server: {
+    extendExpressApp(app) {
+      // TODO: use the same requestId between express and Apollo
+      app.use(pino)
+    },
+  },
   graphql: {
     apolloConfig: {
-      formatError(formattedError, error) {
+      formatError(formattedError, __error) {
         // you can customise the error returned to the client
         return formattedError
       },
       plugins: [
         {
-          async requestDidStart(requestContext) {
-            console.log(
-              'graphql operation',
-              requestContext.request.operationName ?? '(unnamed operation)'
-            )
+          async requestDidStart() {
+            // const requestId = requestContext.request.http?.headers.get('cf-ray')
+            // const requestId = requestContext.request.http?.headers.get('x-amzn-trace-id')
+            // const requestId = requestContext.request.http?.headers.get('x-request-id')
+            const requestId = randomBytes(16).toString('base64url')
+            const start = Date.now()
+
             return {
-              async didEncounterErrors(requestContext) {
-                console.error(...requestContext.errors)
+              async willSendResponse (requestContext) {
+                pino.logger.info({
+                  requestId, // TODO: share with express
+                  duration: Date.now() - start,
+                  query: {
+                    name: requestContext.request.operationName,
+                    type: requestContext.operation?.operation,
+                    hash: sha256(requestContext.request.query ?? ''),
+                    // query: requestContext.request.query, // WARNING: may be verbose
+                  },
+                  errors: requestContext.errors?.map(e => ({
+                    message: e.message,
+                    path: e.path
+                  })) || undefined
+                }, 'GraphQL')
               },
             }
           },
