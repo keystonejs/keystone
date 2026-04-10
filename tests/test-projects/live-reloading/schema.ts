49081d74b1756f74f76fe7a0f6a6c08f8e272a11
Jacob Lamont <cob@jacoblamont.com>
2026-04-10 19:02:18 -0400

    fixup! wip

--- tests/test-projects/live-reloading/schema.ts
+++ tests/test-projects/live-reloading/schema.ts
@@ -1,21 +1,31 @@
 import { graphql, list } from '@keystone-6/core'
 import { allowAll } from '@keystone-6/core/access'
-import { text } from '@keystone-6/core/fields'
+import { text, virtual } from '@keystone-6/core/fields'
+
+import type { Lists } from '.keystone/types'
 
 export const lists = {
   Something: list({
     access: allowAll,
     fields: {
-      text: text({ label: 'Initial Label For Text' }),
+      text: text({ label: 'Very Important Text' }),
+      virtual: virtual({
+        field: graphql.field({
+          type: graphql.String,
+          resolve (item) {
+            return item.text
+          },
+        }),
+      }),
     },
   }),
-}
+} satisfies Lists
 
 export const extendGraphqlSchema = graphql.extend(() => {
   return {
     query: {
       someNumber: graphql.field({
-        type: graphql.Int,
+        type: graphql.nonNull(graphql.Int),
         resolve: () => 1,
       }),
     },
