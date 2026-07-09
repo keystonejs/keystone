09710b89363a6a5bd38b471192732ddb86e7d5ee
Emma Hamilton <git@emmas.town>
2026-07-09 10:53:41 +1000

    Remove @preconstruct/next

--- examples/framework-nextjs-app-directory/next.config.mjs
+++ examples/framework-nextjs-app-directory/next.config.mjs
@@ -1,12 +1,13 @@
-// you don't need this if you're building something outside of the Keystone repo
-import withPreconstruct from '@preconstruct/next'
-
-export default withPreconstruct({
+export default {
   serverExternalPackages: ['graphql'],
+  // You don't need this if you're building something outside of the Keystone repo
+  experimental: {
+    externalDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-})
+}
