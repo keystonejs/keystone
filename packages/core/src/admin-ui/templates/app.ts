0f47e7be24a4250c921a2995fa3ab290c23d028a
Emma Hamilton <git@emmas.town>
2026-07-22 19:39:26 +1000

    Remove resolve dependency

--- packages/core/src/admin-ui/templates/app.ts
+++ packages/core/src/admin-ui/templates/app.ts
@@ -1,19 +1,50 @@
 import Path from 'node:path'
-import resolve from 'resolve'
+import fs from 'node:fs'
 
 import type { AdminMetaSource } from '../../lib/admin-meta'
 import type { KeystoneConfig } from '../../types'
 
-function doesConfigExist(path: string[]) {
-  try {
-    const configPath = Path.join(process.cwd(), ...path)
-    resolve.sync(configPath, {
-      extensions: ['.ts', '.tsx', '.js'],
-      preserveSymlinks: false,
-    })
+function isFileOrSymlinkToFile(dirent: fs.Dirent) {
+  if (dirent.isFile()) return true
+  if (dirent.isSymbolicLink() && fs.statSync(Path.join(dirent.parentPath, dirent.name)).isFile())
     return true
+  return false
+}
+
+function doesConfigExist(dir: string, name: string) {
+  try {
+    const resolvedDir = Path.join(process.cwd(), dir)
+    for (const dirent of fs.readdirSync(resolvedDir, { withFileTypes: true })) {
+      if (
+        (dirent.name === `${name}.ts` ||
+          dirent.name === `${name}.tsx` ||
+          dirent.name === `${name}.js`) &&
+        isFileOrSymlinkToFile(dirent)
+      ) {
+        return true
+      }
+      if (
+        dirent.name === name &&
+        (dirent.isDirectory() ||
+          (dirent.isSymbolicLink() &&
+            fs.statSync(Path.join(resolvedDir, dirent.name)).isDirectory()))
+      ) {
+        const innerDir = Path.join(resolvedDir, name)
+        for (const innerDirent of fs.readdirSync(innerDir, { withFileTypes: true })) {
+          if (
+            (innerDirent.name === 'index.ts' ||
+              innerDirent.name === 'index.tsx' ||
+              innerDirent.name === 'index.js') &&
+            isFileOrSymlinkToFile(innerDirent)
+          ) {
+            return true
+          }
+        }
+      }
+    }
+    return false
   } catch (err: any) {
-    if (err.code === 'MODULE_NOT_FOUND') return false
+    if (err.code === 'ENOENT') return false
     throw err
   }
 }
@@ -37,13 +68,13 @@ export function appTemplate(config: KeystoneConfig, adminMeta: AdminMetaSource) 
 ${allViews.map((views, i) => `import * as view${i} from ${views}`).join('\n')}
 
 ${
-  doesConfigExist(['.keystone', 'admin', 'config'])
+  doesConfigExist('.keystone/admin', 'config')
     ? `import * as packageAdminConfig from "../../../.keystone/admin/config"`
     : 'let packageAdminConfig = {}'
 }
 
 ${
-  doesConfigExist(['admin', 'config'])
+  doesConfigExist('admin', 'config')
     ? `import * as userAdminConfig from "../../../admin/config"`
     : 'let userAdminConfig = {}'
 }
