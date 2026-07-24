22e3c19c61d2686d59913af88980f11660269bcf
Emma Hamilton <git@emmas.town>
2026-07-24 11:42:39 +1000

    Type exports from fields-ui-empty

--- packages/fields-document/src/DocumentEditor/component-blocks/fields-ui-empty.tsx
+++ packages/fields-document/src/DocumentEditor/component-blocks/fields-ui-empty.tsx
@@ -1,14 +1,16 @@
-function throwAlways(): never {
+import type * as ui from './fields-ui'
+
+function throwAlways(..._args: any[]): never {
   throw new Error(
     'React components from @keystone-6/fields-document/component-blocks cannot be rendered on the server'
   )
 }
 
-export const TextField = throwAlways,
-  TextArea = throwAlways,
-  Checkbox = throwAlways,
-  Text = throwAlways,
-  makeIntegerFieldInput = () => throwAlways,
-  makeSelectFieldInput = () => throwAlways,
-  makeUrlFieldInput = () => throwAlways,
-  makeMultiselectFieldInput = () => throwAlways
+export const TextField = throwAlways as unknown as typeof ui.TextField,
+  TextArea = throwAlways as unknown as typeof ui.TextArea,
+  Checkbox = throwAlways as unknown as typeof ui.Checkbox,
+  Text = throwAlways as unknown as typeof ui.Text,
+  makeIntegerFieldInput = () => throwAlways as unknown as typeof ui.makeIntegerFieldInput,
+  makeSelectFieldInput = () => throwAlways as unknown as typeof ui.makeSelectFieldInput,
+  makeUrlFieldInput = () => throwAlways as unknown as typeof ui.makeUrlFieldInput,
+  makeMultiselectFieldInput = () => throwAlways as unknown as typeof ui.makeMultiselectFieldInput
